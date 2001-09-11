97d89ba016424312322275910e2479ff1b5bcc9b
krb <krb>
2001-09-11 10:19:53 +0000

    the get_wormbase_release_date function has been extended to additionally return a standard dd/mm/yy date format.  This is the 2nd argument returned by the function (which isn't the neatest solution).  So if you want both types of dates:
    ($text_date, $other_date) = &get_wormbase_release_date();
    
    where $text_date will be something like "5th September" and $other_date will
    be "05/09/01";
    
    There (currently) is no way of just getting the second date format without the
    first.  I'm being lazy and didn't want to make a new function call.  I guess I should
    maybe change it so that you need to pass an argument to the function to specify
    which format you want.
    
    Oh well that's for someone else to do.

--- scripts/Wormbase.pm
+++ scripts/Wormbase.pm
@@ -52,20 +52,23 @@ sub get_wormbase_release_date{
   my @split = split(/\s+/,$line);
 
   my $month = $split[5];
-  ($month = "January")   if ($month eq "Jan");
-  ($month = "February")  if ($month eq "Feb");
-  ($month = "March")     if ($month eq "Mar");
-  ($month = "April")     if ($month eq "Apr");
-  ($month = "May")       if ($month eq "May");
-  ($month = "June")      if ($month eq "Jun");
-  ($month = "July")      if ($month eq "Jul");
-  ($month = "August")    if ($month eq "Aug");
-  ($month = "September") if ($month eq "Sep");
-  ($month = "October")   if ($month eq "Oct");
-  ($month = "November")  if ($month eq "Nov");
-  ($month = "December")  if ($month eq "Dec");
+  my $month2;
+
+  if($month eq "Jan"){ $month = "January";   $month2 = "01";}
+  if($month eq "Feb"){ $month = "February";  $month2 = "02";}
+  if($month eq "Mar"){ $month = "March";     $month2 = "03";}
+  if($month eq "Apr"){ $month = "April";     $month2 = "04";}
+  if($month eq "May"){ $month = "May";       $month2 = "05";}
+  if($month eq "Jun"){ $month = "June";      $month2 = "06";}
+  if($month eq "Jul"){ $month = "July";      $month2 = "07";}
+  if($month eq "Aug"){ $month = "August";    $month2 = "08";}
+  if($month eq "Sep"){ $month = "September"; $month2 = "09";}
+  if($month eq "Oct"){ $month = "October";   $month2 = "10";}
+  if($month eq "Nov"){ $month = "November";  $month2 = "11";}
+  if($month eq "Dec"){ $month = "December";  $month2 = "12";}
 
   my $day = $split[6];
+
   if    ($day eq "1") {$day .= "st";}
   elsif ($day eq "2") {$day .= "nd";}
   elsif ($day eq "3") {$day .= "rd";}
@@ -75,8 +78,20 @@ sub get_wormbase_release_date{
   elsif ($day eq "31"){$day .= "st";}
   else                {$day .= "th";} 
 
+  my $day2;
+  if (length($day) == 1){$day2 = "0".$day;}
+  else{$day2 = $day;}
+
+  my $year = `date`;
+  $year = substr($year,-3,2);
+
+  # make a text style date
   my $date = $day." ".$month;
-  return($date);
+
+  # make a regular xx/xx/xx date
+  my $date2 = $day2."/".$month2."/".$year;
+
+  return($date,$date2);
 }
 
 
@@ -324,6 +339,9 @@ Looks at the date stamp on the letter.WSxx file in the wormbase ftp directory.
 Converts this date into a string such as "21st September" which it returns.
 Creation of the letter.WSxx file occurs pretty much at the end of the rebuild
 so it is really an approximate date.
+
+Additionally if the function call expects two arguments to be returned then the
+second argument will be a standard six figure date, e.g. 29/12/01
 
 =back
 
