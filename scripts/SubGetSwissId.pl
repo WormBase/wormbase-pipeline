55d1659b482b0ac75322b9e4b44f880b870a4344
ar2 <ar2>
2002-05-31 13:38:46 +0000

    Now has build specific variables and emailed error logs.  Creates SwissProtIds.ace in respective wormper dir on ws2.  Does not as yet update pepace.

--- scripts/SubGetSwissId.pl
+++ scripts/SubGetSwissId.pl
@@ -2,16 +2,23 @@
 use strict;
 use Wormbase;
 use Ace;
-
+my $maintainer = "All";
 my $wmpep_ver = &get_wormbase_version();
 $wmpep_ver = 78;#while wormpep.table broken
 
 my $wormpepdir = "/wormsrv2/WORMPEP/wormpep$wmpep_ver";
 
 #error log (email to All)
-open(LOG,">$wormpepdir/error_report");
-print LOG $^T,"\n\n";#time
+my $log = "$wormpepdir/error_report";
+open(LOG,">$log")|| die "cant open $wormpepdir/error_report";
+print LOG "# SubGetSwissId\n";
+print LOG "\n";
+print LOG "Wormpep version  :$wmpep_ver\n\n";
+print LOG "Only using 78 unitl latest build version is correct" if ($wmpep_ver == 78);
+print LOG "=============================================\n";
+print LOG "\n";
 
+my $errorLog = *LOG;
 
 my $temp_acefile = "$wormpepdir/SwissprotIDs.ace";
 #create temp ace file to output to |open and close to ensure new file (as output of data appends)
@@ -27,6 +34,9 @@ my %idextract;
 my %wormpep_acc;
 my $count;
 $count = 0;
+
+
+
 
 
 #open file with linkin data
@@ -48,8 +58,8 @@ while (<INPUT>)
 	    $accn_holder = $1;
 	    print "protein $count $protein[$count] accn is $accn_holder\n";
 	  }
-#	else{
-	#  print "protein $count $protein[$count] has no Swissprot acc\n";}
+	else{
+	  print LOG "protein $protein[$count] has no Swissprot / TrEMBL acc\n";}
 
 	#check if the pepace entry has a SWISS-PROT entry
 	my $worm_protein = $db->fetch(Protein => "WP:$protein[$count]");
@@ -57,7 +67,6 @@ while (<INPUT>)
 
 	#if the protein does not have a SwissProt ID in Pepace put it on the the list of queries to be done in next batch;
 	my $swiss = grep $_  eq "SwissProt", @ace_database;
-	print "$swiss\n";
 	unless($swiss)
 	  {
       	    $accession[$count] = $accn_holder;
@@ -65,9 +74,8 @@ while (<INPUT>)
 	    $wormpep_acc{$protein[$count]} = $accession[$count];
 	    $count++;
 	  }
-	#else{print "$worm_protein already has SwissProt\n";}
 	    
-	if($count == 2)
+	if($count == 2000)
 	  {
 	    #DEBUG########################################
 	    #print "wormpep_acc hash after building\n";
@@ -75,7 +83,7 @@ while (<INPUT>)
 	    #  print "$worm","\t",$wormpep_acc{$worm},"\n";}
 	    #############################################
 	    
-	    outputToAce(\%wormpep_acc, \@accession, \$ace_output);
+	    outputToAce(\%wormpep_acc, \@accession, \$ace_output, \$errorLog);
 
 	    #reset batch loop variables
 	    $count = 0;
@@ -85,34 +93,40 @@ while (<INPUT>)
 	    @protein = "";
 	    @accession = "";
 	    @proteinID = "";	    
-
-	    last;
 	  }
       }
   }
-print $ace_output "$count\n";
-outputToAce(\%wormpep_acc, \@accession, \$ace_output);
+#process the remainders
+outputToAce(\%wormpep_acc, \@accession, \$ace_output, \$errorLog);
 
 #update pepace
 #my @updates = $db->parse_file($temp_acefile);
 #print "@updates";
+print LOG "SubGetSwissId finished at ",`date`,"\n";
 
+#### use Wormbase.pl to mail Log ###########
+my $name = "SubGetSwissId";
+#$maintainer = "ar2\@sanger.ac.uk";
+&mail_maintainer ($name,$maintainer,$log);
+#########################################
 
-
-sub outputToAce #(\%wormpep_acc, \@accession \$ace_output)
+sub outputToAce #(\%wormpep_acc, \@accession \$ace_output, \$errorLog)
   {
-    my ($wormpep_acc, $accession, $ace_ouput) = @_;
+    my ($wormpep_acc, $accession, $ace_ouput, $errorLog) = @_;
+    #$ace_output is filehandle of file creatred in calling script
+
     #construct pfetch command string by concatenating accession no's
     my $submitString = "pfetch"." @accession";
     my %idextract;
     
-    open (FASTA,">fasta_output")||die " cant open fasta_output";# > clears before each use 
+    my $fasta_output = "../fasta_output";
+    open (FASTA,">$fasta_output")||die " cant open fasta_output";# > clears before each use 
     print FASTA `$submitString`;
     close FASTA;
     
     #build hash of accession : Swissprot/Trembl ID
-    open (FASTA,"<fasta_output")|| die "cant open fasta record";#read
-    print "build idextract hash from A file\n";
+    open (FASTA,"<$fasta_output")|| die "cant open fasta record";#read
+    print FASTA "This is a temp file created by SubGetSwissId.pl and can be removed -unless script is running!";
     
     while(<FASTA>)
       {
@@ -123,17 +137,16 @@ sub outputToAce #(\%wormpep_acc, \@accession \$ace_output)
 	    my @pfetch = split(/ |>/,$_);
 	    my $acc = $pfetch[2];
 	    $idextract{$acc} = $proteinID ;
-		  }		    
+	  }		    
       }
     close FASTA;
+
     print "\n";
     #DEBUG#############################
     #print "idextract contents\n";
     #foreach my $key (keys %idextract){
     #  print $key,"\t",$idextract{$key},"\n";}
     ##################################
-     
-    print "outputting to ace file $ace_output\n";
 
     #DEBUG########################################
     #print "wormpep_acc hash after building\n";
@@ -148,14 +161,11 @@ sub outputToAce #(\%wormpep_acc, \@accession \$ace_output)
 	my $Database = substr($full_accn,0,2);
 	if($Database eq "SW"){$Database = "SwissProt";}
 	elsif($Database eq "TR"){$Database = "Trembl";}
-	else{print LOG $peptide,"\t",$full_accn,"\tfrom unknown database\n";}
+	else{print $errorLog $peptide,"\t",$full_accn,"\tfrom unknown database\n";}
 	
 	my $databaseID = $idextract{$accn};
 	if(defined($databaseID)){
 	  print $ace_output "Protein : \"WP:$peptide\"\n";
 	  print $ace_output "Database \"$Database\" \"$accn\" \"$databaseID\"\n\n";}
-	#	else{
-	#  print LOG "$peptide : $accn has no databaseID ( $full_accn)\n";}
       }
-    print "\n";
   }
