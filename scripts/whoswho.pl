a5a9dae2b53d357f471c36e3c52def15cb1c1273
dl1 <dl1>
2002-07-02 12:16:23 +0000

    Check script to remind people of outstanding CVS edits. This will be a crontab run each week

---
+++ scripts/whoswho.pl
@@ -0,0 +1,32 @@
+#!/usr/local/bin/perl5.6.0
+
+$|=1;
+use lib "/wormsrv2/scripts/";   
+use strict;
+use Wormbase;
+
+my $maintainers = "dl1";
+my $log         = "/tmp/whoswho.log";
+my $line_count  = 0;
+
+# move to dl1 cvs checkout directory
+chdir ("/nfs/griffin2/dl1/wormbase/wormbase/scripts");
+
+open (LOG, ">$log");
+print LOG "Current checkouts from the CVS repository\n";
+print LOG "==============================================================\n\n";
+
+open (CVS, "/usr/local/bin/cvs editors |");
+while (<CVS>) {
+    print LOG;
+    $line_count++;
+}
+close CVS;
+
+print LOG "\n============================================================\n\n";
+
+unless ($line_count == 0) {
+    &mail_maintainer("CVS repository report:",$maintainers,$log);
+}
+
+exit(0);
