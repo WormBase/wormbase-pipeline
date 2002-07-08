c71a6f5ed5fec1ad36c4d365508496e4574d9ec7
krb <krb>
2002-07-08 16:24:18 +0000

    Big changes to script to streamline and optimise (and to make it work!).
    Separate log file for Erichs' checks which will also go to me.  It works and is
    much neater now.
    
    Keith

--- scripts/geneace_check.pl
+++ scripts/geneace_check.pl
@@ -2,187 +2,101 @@
 # 
 # geneace_check.pl
 #
-# by Anthony Rogers
+# by Anthony Rogers based on a script by Keith Bradnam
 #
 # Script to run consistency checks on the geneace database
 #
 # Last updated by: $Author: krb $
-# Last updated on: $Date: 2002-07-05 11:03:00 $
+# Last updated on: $Date: 2002-07-08 16:24:18 $
 
 use Ace;
 use lib "/wormsrv2/scripts/"; 
 use Wormbase;
 use strict;
 
-my $maintainer = "All";
-my $rundate    = `date +%y%m%d`; chomp $rundate;
+our $log;
+our $erichlog;
+our $errors;
+&create_log_files;
 
-my $log = "/wormsrv2/logs/geneace_check.log.$rundate";
-open(LOG,">$log")|| die "cant open $log";
-print LOG "$0 started at ",`date`,"\n";
-print LOG "=============================================\n";
-
-
-# open a local database connection
-my $db = Ace->connect(-path  => '/wormsrv1/geneace/');
-
+# open a connection to geneace and grab list of loci
+my $db = Ace->connect(-path  => '/wormsrv1/geneace/') || die "Couldn't connect to /wormsrv1/geneace\n";
 my @loci = $db->fetch(-class => 'Locus',
 		      -name  => '*');
-my $locus;
-my $logstring;
-my $checklocus = 0;
-if ($checklocus == 1)
-  {
-    foreach $locus (@loci)
-      {
-	#print "\nLOCUS: $locus\n";
-	my $warnings;
-	# check for various potential errors in the Locus object
-	$warnings = &test_locus_for_errors($locus);
-	if(defined($warnings))
-	  {
-	    print $warnings;
-	  }
-	
-	$logstring .= OtherNameCheck($db,$locus,@loci);
-      }
-  }
-      
-my $CDBstring .= FindCurrent_DB_loci(@loci);
 
-print LOG "$CDBstring";
+my $size =scalar(@loci);
 
-#print the output of the OtherNameCheck subroutine
-print LOG "\n\n-------------------------------------\n
-These are loci that have Name.Other_name tags, but the locus object being\n
-referered to does not have a Name.New_name tag\n\n";
-print LOG "$logstring\n";
-print LOG "===========================================\n";
-
-#######################################################################################
-#This part finds all sequence objs that have a Prov, Detailed or Concise descptn AND a 
-#Locus_genomic_seq.
-
-#Erich Schwarz wants this info - emsch@its.caltech.edu
-my $findLGS = 0;
-
-if ($findLGS == 1)
-{
-
-my $LSGlog;
-  my $query = <<END;
-  Find Sequence Locus_genomic_seq && (Provisional_description || Detailed_description || Concise_description)
-END
-      
-  my @oddies = $db->fetch(-query=>"$query");
-$LSGlog .= "\n\n\nThe folllowing sequences have one of the description fields\n
-(Provisional,Detailed or Concise) and also refer to a Locus_genomic_seq\n
-  ------------------------------------------------------\n\n";
-
-foreach my $seq(@oddies)
-  {
-    ($locus) = $seq->at('Visible.Locus_genomic_seq');
-    $LSGlog .= "$seq - $locus\n";
-  }
-
-$LSGlog .= "==============================================END\n";
-
-#mail Erich
-my $interested ="$maintainer"; # "emsch\@its.caltech.edu";
-my $mailname = "Annotated Sequences with loci_genomic_seq tags";
-#open (OUTLOG,  "|/bin/mailx -s \"$name    \" $maintainer ");
-open (OUTLOG,  "|/bin/mailx -s \"$mailname\" $interested");
-print OUTLOG "This mail is generated automatically.\nIf you have any queries please email ar2\@sanger.ac.uk\n\n";
-print OUTLOG "$LSGlog";
-close OUTLOG;
-
-#print to main log file
-print LOG "$LSGlog";
+# Loop through loci checkingfor various potential errors in the Locus object
+print "\nChecking loci for errors:\n";
+foreach my $locus (@loci){
+  print "$locus\n";
+  my $warnings;
+  my $erich_warnings;
+  ($warnings, $erich_warnings) = &test_locus_for_errors($locus);
+  print LOG "$warnings" if(defined($warnings));
+  #Erich Schwarz wants some of these - emsch@its.caltech.edu
+  print ERICHLOG "$erich_warnings" if(defined($erich_warnings));
 }
 
-$db->close;
-close LOG;
+# Look for loci in current_DB not in geneace
+print "\nLooking for new loci in /wormsrv2/current_DB:\n\n";
+&find_new_loci_in_current_DB($db);
 
-#$maintainer = "ar2\@sanger.ac.uk";
+print LOG "\nThere were $errors errors in $size loci.\n";
+
+
+##########################################
+# Tidy up and mail relevant log files
+##########################################
+
+$db->close;
+close(LOG);
+close(ERICHLOG);
+
+my $maintainer = "All";
 &mail_maintainer($0,$maintainer,$log);
 
+my $interested ="krb\@sanger.ac.uk, emsch\@its.caltech.edu";
+&mail_maintainer($0,"krb\@sanger.ac.uk",$erichlog);
+exit(0);
+
 ################################################
-sub FindCurrent_DB_loci
-{
-  my $returnstring;
-  my @GEN_loci = @_;
-  #make hash of loci (from wormsrv1/geneace)
-  my %GEN_loci_check;
-  
-  foreach my $loci(@GEN_loci)
-    {
-      $GEN_loci_check{$loci} = 0;
+sub find_new_loci_in_current_DB{
+  my $db = shift;
+  my $warnings;
+
+  # open a database connection to current_DB and grab all loci names (excluding polymorphisms)
+  my $new_db = Ace->connect(-path  => '/wormsrv2/current_DB') || die "Could not connect to current_DB\n";
+  my @current_DB_loci = $db->fetch(-query=>'Find Locus;!Polymorphism');
+  $new_db->close;
+
+  #cross reference in geneace
+  foreach my $loci(@current_DB_loci){
+    my $new_loci = $db->fetch(-class=>'Locus',-name=>"$loci");
+    unless(defined($new_loci)){
+      $warnings .= "ERROR 19: $new_loci in current_DB is not in /wormsrv1/geneace\n";
+      $errors++;
     }
-  
-  # open a local database connection
-  my $db = Ace->connect(-path  => '/wormsrv2/current_DB');
-
-  #check for non-polymorphism loci that are in current_DB but not geneace
-  my $query = <<END;
-  Find Locus !Polymorphism
-END
-
-  my @CDB_loci = $db->fetch(-query=>"$query");
-  $db->close;
-
-  $returnstring .= "\n\n\n----------------------------------------------
-These loci are in current_DB but not geneace -\n\n";
-  foreach my $loci(@CDB_loci)
-    {
-      unless(defined ($GEN_loci_check{$loci}))
-	{
-	  $returnstring .= "\t$loci is in current_DB but not geneace!\n"
-	}
-    }
-  $returnstring .= "==========================================\n\n\n\n";
+  }
+  print LOG "\n$warnings\n" if $warnings;;
 }
 
 
 
 ###############################################
-sub OtherNameCheck
-  {
-    my($db,$locus,@loci) = @_;
-    my ($othername) = $locus->at('Name.Other_name');
-    #print "@loci\n";
-    if(defined($othername))
-      {
-	if ("@loci" =~ m/$othername/)
-	  {
-	    #this locus has $othername as Other_name
-	    #but if $othername has this as its Name.New_name thats OK!
 
-	    my ($newloci) = $db->fetch(-class=>'Locus',-name=>"$othername");
-	    if (defined($newloci))
-	      {
-		unless($newloci->at('Name.New_name'))
-		  {
-		    return  "$locus has Name.Other_name $othername - this exists as separate Locus\n";
-		  }
-	      }
-	  }
-      }
-    return "";
-  }
-
-#################################################
 sub test_locus_for_errors{
   my $locus = shift;
   my $warnings;
-  my $errors;
+  my $erich_warnings;
 
   #test for Map AND !NEXT
-    if($locus->at('Map')){
+  if($locus->at('Map')){
     my $map = $locus->Map;
     if (!defined($map)){
       $warnings .= "ERROR 1: $locus has a 'Map' tag but that tag has no map value!\n";
-	$errors++;
-      }
+      $errors++;
+    }
   }
 
   # test for more than one gene class
@@ -193,7 +107,7 @@ sub test_locus_for_errors{
       $errors++;
     }    
   }
-
+  
   # test for no Type tag
   if(!defined($locus->at('Type'))){  
     $warnings .= "ERROR 3: $locus has no Type tag present\n";
@@ -211,22 +125,16 @@ sub test_locus_for_errors{
     $warnings .= "ERROR 5: $locus has a 'Gene_class' tag but not a 'Gene' tag\n";
     $errors++;
   }
-
-  # test for polymorphisms without a defined type
-#  if(defined($locus->at('Type.Polymorphism')) && !defined($locus->Polymorphism)){
-#    $warnings .= "ERROR 6: $locus has a 'Polymorphism' tag but no associated info\n";
-#    $errors++;
-#  }
-
+  
   # test for no Gene tag AND Genomic_sequence tag
   if(!defined($locus->at('Type.Gene')) && defined($locus->at('Molecular_information.Genomic_sequence'))){
-    $warnings .= "ERROR 7: $locus has 'Genomic_sequence' tag but no 'Gene' tag\n";
+    $warnings .= "ERROR 6: $locus has 'Genomic_sequence' tag but no 'Gene' tag\n";
     $errors++;
   }
 
   # test for Genomic_sequence tag but no value   
   if(defined($locus->at('Molecular_information.Genomic_sequence')) && !defined($locus->Genomic_sequence)){
-    $warnings .= "ERROR 8: $locus has 'Genomic_sequence' tag but no associated value\n";
+    $warnings .= "ERROR 7: $locus has 'Genomic_sequence' tag but no associated value\n";
     $errors++;
   }
 
@@ -234,47 +142,60 @@ sub test_locus_for_errors{
   # and I think my RE (which just looks for word.number.letter) might allow some errors to creep through.  
   if(defined($locus->at('Molecular_information.Genomic_sequence'))){
     my @genomic_sequences = $locus->Genomic_sequence;
+
     if(scalar(@genomic_sequences)>1){
       my @problems = $locus->at('Molecular_information.Genomic_sequence');
       foreach my $problem (@problems){
 	if ($problem !~ m/[\w\d]+\.\d+[a-z]/){
-	  $warnings .= "ERROR 9: $locus has multiple 'Genomic_sequence' tags (see $problem)\n";
+	  $warnings .= "ERROR 8: $locus has multiple 'Genomic_sequence' tags (see $problem)\n";
 	  $errors++;
 	}
       }
     }
-  }
 
+    # can also test to see if there are attached annotations to these sequences which Erich should
+    # know about, i.e. they should now be attached to the loci object instead
+    foreach my $seq (@genomic_sequences){
+      my ($newseq) = $db->fetch(-class=>'Sequence',-name=>"$seq");
+      if(defined($newseq->at('Visible.Provisional_description')) || 
+	 defined($newseq->at('Visible.Concise_description')) ||
+	 defined($newseq->at('Visible.Detailed_description'))){  
+	$erich_warnings .= "$seq has attached functional annotation which should now be attached to $locus\n";
+	$errors++;
+      }
+    }
+  }
+  
   # test for Polymorphism AND Gene tags both present
   if(defined($locus->at('Type.Gene')) && defined($locus->at('Type.Polymorphism'))){  
-    $warnings .= "ERROR 10: $locus has a 'Polymorphism' tag AND 'Gene' tag\n";
+    $warnings .= "ERROR 9: $locus has a 'Polymorphism' tag AND 'Gene' tag\n";
     $errors++;
   }
 
   # test for Enzyme tag (doesn't fit in with new Gene model)
   if(defined($locus->at('Molecular_information.Enzyme'))){  
-    $warnings .= "ERROR 11: $locus has an 'Enzyme' tag\n";
+    $warnings .= "ERROR 10: $locus has an 'Enzyme' tag\n";
     $errors++;
   }
 
   # test for Canonical_gene tag present
   if(defined($locus->at('Molecular_information.Canonical_gene'))){  
-    $warnings .= "ERROR 12: $locus has a 'Canonical_gene' tag\n";
+    $warnings .= "ERROR 11: $locus has a 'Canonical_gene' tag\n";
     $errors++;
   }
   
   # test for New_name tag and other information apart from Gene and Species
   if(defined($locus->at('Name.New_name'))){
     if(defined($locus->at('Species')) && defined($locus->at('Type.Gene'))){
-    # check for any other info in object
+      # check for any other info in object
       my @tags = $locus->tags();
       if(scalar(@tags)>3){
-	$warnings .= "ERROR 13: New_name tag + extra info. Transfer into new gene?\n";
+	$warnings .= "ERROR 12: $locus has New_name tag + extra info. Transfer into new gene?\n";
 	$errors++;
       }
     }
     else{
-      $warnings .= "ERROR 14: No species and/or Gene tag present\n";
+      $warnings .= "ERROR 13: $locus has no species and/or Gene tag present\n";
       $errors++;
     }
   }
@@ -282,7 +203,7 @@ sub test_locus_for_errors{
   # Test for Polymorphisms with no P in their title
   if(defined($locus->at('Type.Polymorphism'))){
     if($locus !~ /P/){
-      $warnings .= "ERROR 15: No 'P' in title\n";
+      $warnings .= "ERROR 14: $locus has no 'P' in title\n";
       $errors++;
     }
   }
@@ -290,15 +211,56 @@ sub test_locus_for_errors{
   # Look for Gene_class tag in non-gene objects 
   if(!defined($locus->at('Type.Gene'))){
     if(defined($locus->at('Name.Gene_class'))){
-      $warnings .= "ERROR 16: Gene_class tag in non-gene!\n";
+      $warnings .= "ERROR 15: $locus has Gene_class tag but it is not a gene!\n";
       $errors++;
     }
   }
-
+  
   # test for Other_name tag but no value   
   if(defined($locus->at('Name.Other_name')) && !defined($locus->Other_name)){
-    $warnings .= "ERROR 17: $locus has 'Other_name' tag but no associated value\n";
+    $warnings .= "ERROR 16: $locus has 'Other_name' tag but no associated value\n";
     $errors++;
   }
-  return($warnings);
+
+  # test for Other_name value which is also a Locus name in its own right
+  # N.B. This is not always a bad thing
+  if(defined($locus->at('Name.Other_name'))){
+    my @other_names = $locus->Other_name;
+    foreach my $other_name (@other_names){
+      my ($newloci) = $db->fetch(-class=>'Locus',-name=>"$other_name");
+      if(defined($newloci)){
+	unless($newloci->at('Name.New_name')){
+	  $warnings .= "ERROR 17: $locus has Other_name: $other_name which is also a separate locus object\n";
+	  $errors++;
+	}   
+      }
+    }
+  }
+  # Remind of outstanding CGC_unresolved tags
+  if(defined($locus->CGC_unresolved)){
+    my ($unresolved_details) = $locus->at('Type.Gene.CGC_unresolved');
+    $warnings .= "ERROR 18: $locus has CGC_unresolved tag: \"$unresolved_details\"\n";
+    $errors++;
+  }
+
+  return($warnings, $erich_warnings);
+
 }
+
+#####################################################################
+
+sub create_log_files{
+  my $rundate    = `date +%y%m%d`; chomp $rundate;
+  $log = "/wormsrv2/logs/geneace_check.log.$rundate";
+  $erichlog = "/wormsrv2/logs/geneace_check.erichlog.$rundate";
+  open(LOG,">$log") || die "cant open $log";
+  print LOG "$0 started at ",`date`,"\n";
+  print LOG "=============================================\n";
+
+  open(ERICHLOG,">$erichlog") || die "cant open $erichlog";
+  print ERICHLOG "$0 started at ",`date`,"\n";
+  print ERICHLOG "=============================================\n";
+  print ERICHLOG "This mail is generated automatically.\n";
+  print ERICHLOG "If you have any queries please email ar2\@sanger.ac.uk or krb\@sanger.ac.uk\n\n";
+}
+
