0d5c3432f5b5fe2e27c49206816de1d2fc11a81b
dl1 <dl1>
2002-08-22 14:42:24 +0000

    rewrites the output acefiles from BLAT analysis to a form which loads approx. 4x faster

---
+++ scripts/acecompress.pl
@@ -0,0 +1,79 @@
+#!/usr/local/bin/perl5.6.0
+#
+# acecompress.pl
+#
+# dl
+#
+# Usage : acecompress.pl [-options]
+#
+# Last edited by: $Author: dl1 $
+# Last edited on: $Date: 2002-08-22 14:42:24 $
+
+use strict;
+use Getopt::Long;
+
+my $homol;         # homol data   BLAT files
+my $feature;       # feature data Good_intron files    
+my %objects;       # hash for number of homol lines per object
+my $ace_object;  
+my %acedata;
+
+
+GetOptions (
+	    "homol"       => \$homol,
+	    "feature"     => \$feature
+	    );
+
+my $file = shift;
+
+open (FILE, "<$file");
+while (<FILE>) {
+
+    # BLAT homology data
+    if ($homol) {
+	if (/Homol_data : \"(\S+)\"/) {
+	    $ace_object = $1;
+	    $objects{$ace_object}++;
+	    next;
+	}
+	if (/DNA_homol/) {
+	    push (@{$acedata{$ace_object}{DNA_homol}},$_);
+	}
+    }
+    # Confirmed_intron feature data
+    elsif ($feature) {
+	if (/Feature_data : \"(\S+)\"/) {
+	    $ace_object = $1;
+	    $objects{$ace_object}++;
+	    next;
+	}
+	if (/Confirmed_intron/) {
+	    push (@{$acedata{$ace_object}{Confirmed_intron}},$_);
+	}
+    }
+}
+close FILE;
+
+
+# print output
+
+foreach my $obj (keys %objects) {
+    print "\n// $obj\n\n";
+
+    # homol (BLAT homology)
+    if ($homol) {
+	print "Homol_data : \"$obj\"\n";
+	foreach my $line (@{$acedata{$obj}{DNA_homol}}) {
+	    print "$line";
+	}
+    }
+    # feature (confirmed_intron)
+    elsif ($feature) {
+	print "Feature_data : \"$obj\"\n";
+	foreach my $line (@{$acedata{$obj}{Confirmed_intron}}) {
+	    print "$line";
+	}
+    }  
+}
+
+exit(0);
