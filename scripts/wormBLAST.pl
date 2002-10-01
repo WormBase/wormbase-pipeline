6dae1666d3a132203b005c7910cbe12e22b154d2
ar2 <ar2>
2002-10-01 14:42:50 +0000

    gone through live run (upto run_pipeline) happily fixing bugs on the way!

--- scripts/wormBLAST.pl
+++ scripts/wormBLAST.pl
@@ -2,7 +2,9 @@
 
 use DBI;
 use strict;
-use lib "/wormsrv2/scripts/";
+my $antdir = glob("~ar2");
+#use lib "/wormsrv2/scripts/";
+use lib "/nfs/team71/worm/ar2/wormbase_cvs/scripts/";
 use Wormbase;
 use Getopt::Long;
 
@@ -17,6 +19,7 @@ my $update_mySQL;
 my $setup_mySQL;
 my $run_pipeline;
 my $dont_SQL;
+my $dump_data;
 
 GetOptions("chromosomes" => \$chromosomes,
 	   "wormpep"     => \$wormpep,
@@ -24,11 +27,13 @@ GetOptions("chromosomes" => \$chromosomes,
 	   "updatemysql" => \$update_mySQL,
 	   "setup"       => \$setup_mySQL,
 	   "run"         => \$run_pipeline,
-	   "nosql"       => \$dont_SQL
+	   "nosql"       => \$dont_SQL,
+	   "dump"        => \$dump_data
 	  );
 
 my $wormpipe_dir = glob("~wormpipe");
 my $WPver = &get_wormbase_version;
+my $WP_old = $WPver - 1;
 
 #process Ids
 
@@ -64,18 +69,22 @@ my %wormprotprocessIds = ( wormpep => 11,
 
 if( $chromosomes ) {
   #get new chromosomes
-  `$wormpipe_dir/Pipeline/copy_files_to_acari.pl`;
+  `$wormpipe_dir/Pipeline/copy_files_to_acari.pl -c`;
 }
 
 if( $wormpep ) {
   #get new wormpep
-  `$wormpipe_dir/Pipeline/copy_files_to_acari.pl`;
+  `$wormpipe_dir/Pipeline/copy_files_to_acari.pl -w`;
 }
 
 my %currentDBs;   #ALSO used in setup_mySQL 
+
 #load in databases used in previous build
-my $extDB_file = "$wormpipe_dir/BlastDB/external_dbs";
-open (OLD_DB,"<$extDB_file") or die "cant find $extDB_file";
+my $last_build_DBs = "$wormpipe_dir/BlastDB/databases_used_WS$WP_old";
+my $database_to_use = "$wormpipe_dir/BlastDB/databases_used_WS$WPver";
+
+
+open (OLD_DB,"<$last_build_DBs") or die "cant find $last_build_DBs";
 while (<OLD_DB>) {
   chomp;
   if( /(ensembl|gadfly|yeast|slimswissprot|slimtrembl|wormpep)/ ) {
@@ -85,7 +94,6 @@ while (<OLD_DB>) {
 close OLD_DB;
 
 my @updated_DBs;
-push (@updated_DBs, "wormpep");
 #check for updated Databases
 if ( $update_databases )
   {
@@ -97,21 +105,23 @@ if ( $update_databases )
 	my $whole_file = "$1"."$'";  #match + stuff after match.
 	if( "$whole_file" ne "$currentDBs{$1}" ) {
 	  #make blastable database
+	  `setdb $whole_file`;
 	  push( @updated_DBs,$1 );
 	  #change hash entry ready to rewrite external_dbs
 	  $currentDBs{$1} = "$whole_file";
 	 	}
 	else {
-	  print "$1 database unchanged $&\n";
+	  print "$1 database unchanged $whole_file\n";
 	}
       }
     }
     close DIR;
-    open (OLD_DB,">$extDB_file") or die "cant write updated $extDB_file";
+    
+    open (NEW_DB,">$database_to_use") or die "cant write updated $database_to_use";
     foreach (keys %currentDBs){
-      print OLD_DB "$currentDBs{$_}\n";
+      print NEW_DB "$currentDBs{$_}\n";
     }
-    close OLD_DB;
+    close NEW_DB;
   }
 
 
@@ -119,7 +129,7 @@ if ( $update_databases )
 #@updated_DBs = qw(ensembl gadfly yeast slimswissprot slimtrembl);
 
 #generate distribution request based on updated databases
-my $letter = "/wormsrv2/logs/distribute_on_farm_mail";
+my $letter = "$wormpipe_dir/distribute_on_farm_mail";
 open (LETTER,">$letter");
 print LETTER "This is a script generated email from the wormpipe Blast analysis pipeline.\nAny problems should be addessed to worm\@sanger.ac.uk.\n
 =====================================================
@@ -152,10 +162,10 @@ my $maintainer = "ar2\@sanger.ac.uk";
 
 # mysql database parameters
 my $dbhost = "ecs1f";
-#my $dbuser = "wormadmin";
-my $dbuser = "wormro";
+my $dbuser = "wormadmin";
+#my $dbuser = "wormro";
 my $dbname = "worm01";
-my $dbpass = "";
+my $dbpass = "worms";
 
 my @results;
 my $query = "";
@@ -177,10 +187,10 @@ if( $update_mySQL )
     print "last_clone = $last_clone\n";
     
     #Make a concatenation of all six agp files from the last release to ~/Elegans  e.g.
-    `cat /wormsrv2/current_DB/CHROMOSOMES/*.agp > ~/Elegans/WS$WPver.agp`;
+    `cat /wormsrv2/current_DB/CHROMOSOMES/*.agp > $wormpipe_dir/Elegans/WS$WPver.agp`;
     
     #load information about any new clones
-    `~/Pipeline/agp2ensembl.pl -dbname worm01 -dbhost ecs1f -dbuser wormadmin -dbpass worms -agp ~/Elegans/WS$WPver.agp -write -v -strict`;
+    `$wormpipe_dir/Pipeline/agp2ensembl.pl -dbname worm01 -dbhost ecs1f -dbuser wormadmin -dbpass worms -agp $wormpipe_dir/Elegans/WS$WPver.agp -write -v -strict`;
     
     #check that the number of clones in the clone table equals the number of contigs and dna objects
     my ($clone_count, $contig_count, $dna_count);
@@ -206,88 +216,119 @@ clones = $clone_count\ncontigs = $contig_count\ndna = $dna_count\n";
     @results = &single_line_query( $query, $worm01 );
     my $new_last_clone = $results[0];
     print "new_last_clone = $new_last_clone\n";
-    
+    $last_clone = 3487;
+
     if( $last_clone != $new_last_clone )
       {
 	$query = "select id from contig where internal_id > $last_clone into outfile '$wormpipe_dir/Elegans/ids.txt'";
-	print &single_line_query( $query, $worm01 );
+	print &update_database( $query, $worm01 );
 	
-	`$wormpipe_dir/Pipeline/InputIdManager.pl -dbname worm01 -dbhost ecs1f -dbuser wormadmin -dbpass worms -insert -analysis SubmitContig -class contig -file ~/Elegans/ids.txt`;
+	`$wormpipe_dir/Pipeline/InputIdManager.pl -dbname worm01 -dbhost ecs1f -dbuser wormadmin -dbpass worms -insert -analysis SubmitContig -class contig -file $wormpipe_dir/Elegans/ids.txt`;
 	
       }
      $worm01->disconnect;
     `$wormpipe_dir/Pipeline/find_duplicate_clones.pl`;
-  }
 
 
+     #add new peptides to MySQL database
+     #make wormprot connection
+     $dbname = "wormprot";
+     $wormprot = DBI -> connect("DBI:mysql:$dbname:$dbhost", $dbuser, $dbpass, {RaiseError => 1})
+       || die "cannot connect to db, $DBI::errstr";
+
+     $query = "select * from protein order by proteinId desc limit 1";
+     @results = &single_line_query( $query, $wormprot );
+     my $old_topCE = $results[0];
+    # `$wormpipe_dir/Pipeline/worm_pipeline.pl -f /wormsrv2/WORMPEP/wormpep$WPver/new_entries.WS$WPver`;
+     
+     #check for updated ids
+     @results = &single_line_query( $query, $wormprot );
+     my $new_topCE = $results[0];
+     if( "$old_topCE" eq "$new_topCE" ) {
+       print "NO new peptides were added to the wormprot mysql database\n";
+     }
+     else {
+       print "new highest proteinId is $new_topCE (old was $old_topCE )\n";
+     }
+
+     $wormprot->disconnect;
+   }
+
+
+
+$dbuser = "wormadmin";
+$dbpass = "worms";
 if( $setup_mySQL )
   {    
     #make wormprot connection
     $dbname = "wormprot";
     my $wormprot =  DBI -> connect("DBI:mysql:$dbname:$dbhost", $dbuser, $dbpass, {RaiseError => 1})
       || die "cannot connect to db, $DBI::errstr";
-    
+    $dbname = "worm01";
     #make worm01 connection
     $worm01 = DBI -> connect("DBI:mysql:$dbname:$dbhost", $dbuser, $dbpass, {RaiseError => 1})
       || die "cannot connect to db, $DBI::errstr";
     
     #process new databases
-    my $extDB_file = "$wormpipe_dir/BlastDB/external_dbs";
-    open (OLD_DB,"<$extDB_file") or die "cant find $extDB_file";
+    open (OLD_DB,"<$last_build_DBs") or die "cant find $last_build_DBs";
+    my %prevDBs;
     my %currentDBs;
     my @updated_DBs;
     
-    #get database file info from external_dbs (should have been updated by script if databases changed
+    #get database file info from databases_used_WS(xx-1) (should have been updated by script if databases changed
     while (<OLD_DB>) {
+      chomp;
+      if( /(ensembl|gadfly|yeast|slimswissprot|slimtrembl|wormpep)/ ) {
+	$prevDBs{$1} = $_;
+      }
+    }  
+    open (CURR_DB,"<$database_to_use") or die "cant find $database_to_use";
+    while (<CURR_DB>) {
       chomp;
       if( /(ensembl|gadfly|yeast|slimswissprot|slimtrembl|wormpep)/ ) {
 	$currentDBs{$1} = $_;
       }
     }
-    close OLD_DB;
-
-    @updated_DBs = qw(wormpep ensembl gadfly yeast slimswissprot slimtrembl);
+    close CURR_DB;
+    
+    #compare old and new database list
+    foreach (keys %currentDBs){
+      if( "$currentDBs{$_}" ne "$prevDBs{$_}" ){
+	push( @updated_DBs, "$_");
+      }
+    }
+    
+    #update mysql with which databases need to be run against
     foreach my $database (@updated_DBs)
       {
 	my $analysis = $worm01processIDs{$database};
 	my $db_file = $currentDBs{$database};
 	print "________________________________________________________________________________\n";
-	$query = "update analysisprocess set db = '$db_file' where analysisId = $analysis";
+	$query = "update analysisprocess set db = \"$db_file\" where analysisId = $analysis";
 	print $query,"\n";
-	&single_line_query( $query, $worm01 );
-	$query = "update analysisprocess set db_file = '/data/blastdb/Worms/$db_file' where analysisId = $analysis";
+	&update_database( $query, $worm01 );
+	$query = "update analysisprocess set db_file = \"/data/blastdb/Worms/$db_file\" where analysisId = $analysis";
 	print $query,"\n\n";
-	&single_line_query( $query, $worm01 );
+	&update_database( $query, $worm01 );
 
 	$analysis = $wormprotprocessIds{$database};
-	$query = "update analysisprocess set db = '$db_file' where analysisId = $analysis";
+	$query = "update analysisprocess set db = \"$db_file\" where analysisId = $analysis";
 	print $query,"\n";
-	&single_line_query( $query, $wormprot );
-	$query = "update analysisprocess set db_file = '/data/blastdb/Worms/$db_file' where analysisId = $analysis";
+	&update_database( $query, $wormprot );
+	$query = "update analysisprocess set db_file = \"/data/blastdb/Worms/$db_file\" where analysisId = $analysis";
 	print $query,"\n";
-	&single_line_query( $query, $wormprot );
+	&update_database( $query, $wormprot );
       }
     
     $worm01->disconnect;
     $wormprot->disconnect;
 
   }
-
+my $bdir = "/nfs/farm/Worms/EnsEMBL/branch-ensembl-121/ensembl-pipeline/modules/Bio/EnsEMBL/Pipeline";
 if( $run_pipeline )
   {   
-#    #make wormprot connection
-#    $dbname = "wormprot";
-#    my $wormprot =  DBI -> connect("DBI:mysql:$dbname:$dbhost", $dbuser, $dbpass, {RaiseError => 1})
-#      || die "cannot connect to db, $DBI::errstr";
-    
-#    #make worm01 connection
-#    $worm01 = DBI -> connect("DBI:mysql:$dbname:$dbhost", $dbuser, $dbpass, {RaiseError => 1})
-#      || die "cannot connect to db, $DBI::errstr";
-
-
-
     #run worm01 stuff
-    my $bdir = "/nfs/farm/Worms/EnsEMBL/branch-ensembl-121/ensembl-pipeline/modules/Bio/EnsEMBL/Pipeline";
+   
     `cp -f $bdir/pipeConf.pl.worm01 $bdir/pipeConf.pl` and die "cant copy pipeConf worm01 file\n";
 
     `perl RuleManager3.pl -once -flushsize 3 -analysis 23`;# - wormpep
@@ -299,7 +340,7 @@ if( $run_pipeline )
       }
     
     #run wormpep stuff
-    `cp -f $bdir/pipeConf.pl.wormprot $bdir/pipeConf.pl` and die "cant copy pipeCon fwormprot file\n";
+    `cp -f $bdir/pipeConf.pl.wormprot $bdir/pipeConf.pl` and die "cant copy pipeConf wormprot file\n";
     `perl RuleManager3.pl -once -flushsize 5 -analysis $wormprotprocessIds{wormpep}`;# - wormpep
     
     `perl RuleManager3.pl -once -flushsize 5`;  #finish off anything that didn't work
@@ -310,13 +351,30 @@ if( $run_pipeline )
 	`perl RuleManager3Prot.pl -once -flushsize 5 -analysis $analysis`;
       }
     `perl RuleManager3prot.pl -once -flushsize 5`; # finish off anything that didn't work + PFams and low complexity, signalp, ncoils, transmembrane
-    
-#    $worm01->disconnect;
-#    $wormprot->disconnect;
+  }
+
+if( $dump_data )
+  {
+    #dont forget the other stuff before this
+    `$wormpipe_dir/Pipeline/dump_blastp.pl -w ~/BlastDB/wormpep$WPver.pep -s`;
+    `$wormpipe_dir/Pipeline/dump_blastx_new.pl -w ~/BlastDB/wormpep$WPver.pep -a ~/Elegans/WS$WPver.agp -g ~/Elegans/cds87.gff -c ~/Elegans/cos87.gff -m`;
+    `$wormpipe_dir/Pipeline/dump_motif.pl`;
   }
 
 exit(0);
-
+sub update_database
+  {
+    if( $dont_SQL ){
+      return;
+    }
+    else{
+      my $query = shift;
+      my $db = shift;
+      my $sth = $db->prepare( "$query" );
+      $sth->execute();
+      return;
+    }
+  }
 
 sub single_line_query
   {
@@ -327,7 +385,7 @@ sub single_line_query
     else{
       my $query = shift;
       my $db = shift;
-      my $sth = $worm01->prepare( "$query" );
+      my $sth = $db->prepare( "$query" );
       $sth->execute();
       my @results = $sth->fetchrow_array();
       $sth->finish();
