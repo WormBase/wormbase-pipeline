778d912b46fb6c76bb531615b37440fcbedc0fe6
krb <krb>
2002-12-09 16:53:55 +0000

    SCD: Many changes

--- scripts/GetPFAM_motifs.pl
+++ scripts/GetPFAM_motifs.pl
@@ -6,74 +6,60 @@
 #
 # Gets latest PFAM motifs from sanger/pub and puts info in to ace file
 #
-# Last updated by: $Author: ar2 $                      # These lines will get filled in by cvs and helps us
-# Last updated on: $Date: 2002-09-16 14:53:38 $                        # quickly see when script was last changed and by whom
+# Last updated by: $Author: krb $                      
+# Last updated on: $Date: 2002-12-09 16:53:55 $         
 
 
 use strict;                                     
 use lib "/wormsrv2/scripts/";                    
 use Wormbase;
-use Getopt::Std;
-#######################################
-# command-line options                #
-#######################################
+use Getopt::Long;
 
-use vars qw($opt_d);
-# $opt_d debug   -  all output goes to ar/allele_mapping
 
-getopts ('d');
+######################################
+# variables and command-line options # 
+######################################
 
-# Try to keep different parts of code cleanly separated using comments...
-
-##############
-# variables  #                                                                   #
-##############
-
-# Most checking scripts should produce a log file that is a) emailed to us all 
-# and b) copied to /wormsrv2/logs
-
+my ($help, $debug);
+our $log;
 my $maintainers = "All";
 my $rundate     = `date +%y%m%d`; chomp $rundate;
 my $runtime     = `date +%H:%M:%S`; chomp $runtime;
-our $log        = "/wormsrv2/logs/GetPfam_motifs.$rundate";
 
-open (LOG, ">$log") or die "cant open $log";
+GetOptions (
+            "help"      => \$help,
+            "debug=s"   => \$debug
+            );
 
-print LOG "$0\n";
-print LOG "started at ",`date`,"\n";
-print LOG "=============================================\n";
-print LOG "\n";
+# help 
+&usage("Help") if ($help);
 
-unless (defined($opt_d))
-  {
-    #Get the latest version
-    my $pfam_motifs_gz = "/wormsrv2/tmp/Pfam_motifs.gz";
-    print LOG "Attempting to wget the latest version\n";
-    print "Attempting to wget the latest version\n";
-    `wget -O $pfam_motifs_gz ftp://ftp.sanger.ac.uk/pub/databases/Pfam/Pfam-A.full.gz` and die "$0 Couldnt get Pfam-A.full.gz \n";
-    print LOG "...... got it!\nUnzipping . .";
-    print "...... got it!\nUnzipping . .";
-    `gunzip -f $pfam_motifs_gz` and die "gunzip failed\n";
-    print LOG "DONE\n";
-    print "DONE\n";
-  }
-else{
-  $maintainers = "ar2\@sanger.ac.uk";
+# Use debug mode?
+if($debug){
+  print "DEBUG = \"$debug\"\n\n";
+  ($maintainers = $debug . '\@sanger.ac.uk');
 }
+
+&create_log_files;
+
+#Get the latest version
+my $pfam_motifs_gz = "/wormsrv2/tmp/Pfam_motifs.gz";
+print LOG "Attempting to wget the latest version\n";
+print "Attempting to wget the latest version\n";
+`wget -O $pfam_motifs_gz ftp://ftp.sanger.ac.uk/pub/databases/Pfam/Pfam-A.full.gz` and die "$0 Couldnt get Pfam-A.full.gz \n";
+print LOG "...... got it!\nUnzipping . .";
+print "...... got it!\nUnzipping . .";
+`gunzip -f $pfam_motifs_gz` and die "gunzip failed\n";
+print LOG "DONE\n";
+print "DONE\n";
 
 my $pfam_motifs = "/wormsrv2/tmp/Pfam_motifs";
 print LOG "\n\nOpening file $pfam_motifs . . \n";
 print "\n\nOpening file $pfam_motifs . . \n";
 open (PFAM,"<$pfam_motifs") or die "cant open $pfam_motifs\n";
 
-my $acefile;
+my $acefile = "/wormsrv2/wormbase/misc/misc_pfam_motifs.ace";
 
-if (defined ($opt_d)) {
-    $acefile = "/wormsrv2/tmp/misc_pfam_motifs.ace";
-  }
-else {
-  $acefile = "/wormsrv2/wormbase/misc/misc_pfam_motifs.ace";
-}
 open (PFAMOUT,">$acefile") or die "cant write misc_pfam_motifs.ace\n";
 
 my $text;
@@ -121,12 +107,47 @@ print "finished at ",`date`,"\n";
 close PFAM;
 close PFAMOUT;
 close LOG;
+
 #### use Wormbase.pl to mail Log ###########
 my $name = "GetPFAM_motifs";
 &mail_maintainer ($name,$maintainers,$log);
-#########################################
 
 exit(0);
+
+
+##############################################################
+#
+# Subroutines
+#
+################################################################
+
+sub create_log_files{
+
+  my $rundate     = `date +%y%m%d`; chomp $rundate;
+
+  # touch logfile for run details
+  $0 =~ m/\/*([^\/]+)$/; system ("touch /wormsrv2/logs/history/$1.`date +%y%m%d`");
+
+  $log        = "/wormsrv2/logs/$1.$rundate.$$";
+  open (LOG, ">$log") or die "cant open $log";
+  print LOG "$0\n";
+  print LOG "started at ",`date`,"\n";
+  print LOG "=============================================\n";
+  print LOG "\n";
+
+}
+
+##########################################
+sub usage {
+  my $error = shift;
+
+  if ($error eq "Help") {
+    # Normal help menu
+    system ('perldoc',$0);
+    exit (0);
+  }
+}
+
 
 
 
