e480b04103dc3032d7bc26295ead665367da3996
wormpipe <wormpipe>
2003-02-11 12:20:20 +0000

    Changed to reflect proposed new ?Protein model Gets description line from
    ftp://genome-ftp.stanford.edu/yeast/data_download/gene_registry/registry.genenames.tab
    
    and adds this to ace file.
    
    If there is a Yeast update before the model change occurs then this will have to be rolled back!

--- scripts/BLAST_scripts/make_yeastpepfile.pl
+++ scripts/BLAST_scripts/make_yeastpepfile.pl
@@ -12,7 +12,45 @@ my $source_file = shift;
 my $pepfile = shift;
 use strict;
 
+# this bit creates a DBM database of ORF => description
+use DB_File;
 
+my $DB_dir = "/tmp";
+# DBM files 
+my $ace_info_dbm = "$DB_dir/ace_info.dbm";
+my %ACE_INFO;
+
+if( -e $ace_info_dbm ) {  #make sure starting with new dbase
+  `rm -f $ace_info_dbm` || die "cant remove $ace_info_dbm: $!\n";
+}
+my %YEAST_DESC;
+dbmopen  (%YEAST_DESC ,"$ace_info_dbm", 0777) or die "cant open $ace_info_dbm :$!\n";
+
+# get the latest info
+`wget -O $DB_dir/yeast ftp://genome-ftp.stanford.edu/yeast/data_download/gene_registry/registry.genenames.tab`;
+open (YEAST , "<$DB_dir/yeast");
+my $linecount;
+while (<YEAST>) {
+  $linecount++;
+  chomp;
+  my @info = split(/\t/,$_);
+  my $desc;
+  if("$info[2]" eq "" ) {
+    #check $info[3]
+    if("$info[3]" ne "" ) {
+      $desc = $info[3];
+    }
+  }
+  else {
+    $desc = $info[2];
+  }
+  next unless $desc;
+  if( (defined $info[5]) and (defined $desc) ){
+    $YEAST_DESC{$info[5]} = "$desc";
+  }
+}
+
+# now extract info from main FASTA file and write ace file
 open (SOURCE,"<$source_file");
 open (PEP,">$pepfile");
 open (ACE,">$acefile");
@@ -24,9 +62,12 @@ while (<SOURCE>) {
       print ACE "\nProtein : \"SGD:$ID\"\n";
       print ACE "Peptide \"SGD:$ID\"\n";
       print ACE "Species \"Saccharomyces cerevisiae\"\n";
-      print ACE "DB_remark \"SGD gene name is $2\"\n";
-      print ACE "Database \"SGD\" \"$ID\" \"$3\"\n\n";
-      print ACE "Peptide : \"SGD:$ID\"\n"; 	
+      print ACE "Gene_name  \"$2\"\n";
+      print ACE "Database \"SGD\" \"SGD_systematic_name\" \"$ID\"\n";
+      print ACE "Database \"SGD\" \"SGDID\" \"$3\"\n";
+      print ACE "Description \"$YEAST_DESC{$ID}\"\n" if $YEAST_DESC{"$ID"};
+
+      print ACE "\nPeptide : \"SGD:$ID\"\n"; 	
       
       print PEP "\>$ID\n";
     }
