3cc83797e7165c2719e173608ccc765b19c8769e
krb <krb>
2003-02-26 17:15:21 +0000

    New script which simply emails a list of checks to the next person who will be doing the build.  E.g. in this build dan should run:
    next_builder_checks.pl -user krb
    
    This sends me an email and the idea is that I only hand back to Dan when I've
    covered all of these things.
    
    *IMPORTANT*
    Each time we discover new problems we should add to this script to spot them in
    the future!
    
    I'll now update the build guide
    
    Keith

---
+++ scripts/next_builder_checks.pl
@@ -0,0 +1,154 @@
+#!/usr/local/bin/perl5.6.1 -w
+#
+# next_builder_checks.pl                           
+# 
+# by Keith Bradnam                         
+#
+# A simple script to send a check list to the person who will be performing the next
+# build to check the current build
+#
+# Last updated by: $Author: krb $     
+# Last updated on: $Date: 2003-02-26 17:15:21 $      
+
+
+$|=1;
+use strict;
+use lib "/wormsrv2/scripts/";   
+use Wormbase;
+use Getopt::Long;
+
+my $WS_current    = &get_wormbase_version;
+my $log         = "/tmp/next_builder_check.log";
+
+my ($help, $debug, $user,$maintainers);
+
+GetOptions ("help"       => \$help,
+            "debug=s"    => \$debug,
+            "user=s"     => \$user);
+
+
+# Display help if required
+&usage("Help") if ($help);
+
+# Use debug mode?
+if($debug){
+  print "DEBUG = \"$debug\"\n\n";
+  ($maintainers = $debug . '\@sanger.ac.uk');
+}
+
+# Use -user setting to send email
+if($user){
+  ($maintainers = $user . '\@sanger.ac.uk');
+}
+
+
+#######################
+#
+# Main part of script
+#
+#######################
+
+
+
+
+open (LOG, ">$log");
+print LOG "\nGreetings $user.  You are meant to be building the next release of WormBase, so\n";
+print LOG "you should take some time to check the current release.\n";
+print LOG "====================================================================================\n\n";
+
+print LOG "THINGS TO CHECK:\n\n";
+
+print LOG "1) The following 12 clones are representative of the whole genome in that\n";
+print LOG "they include one Sanger and one St. Louis clone for each chromosome.  Check\n";
+print LOG "each clone to ensure that it contains BLAT data (EST and mRNA), BLAST data,\n";
+print LOG "waba data, gene models, UTRs etc.\n\n";
+print LOG "i)    C25A1\n";
+print LOG "ii)   F56A3\n";
+print LOG "iii)  C04H5\n";
+print LOG "iv)   B0432\n";
+print LOG "v)    C07A9\n";
+print LOG "vi)   F30H5\n";
+print LOG "vii)  C10C6\n";
+print LOG "viii) B0545\n";
+print LOG "ix)   C12D8\n";
+print LOG "x)    K04F1\n";
+print LOG "xi)   C02C6\n";
+print LOG "xii)  AH9\n\n";
+
+print LOG "2) If the genome sequence has changed, you should inspect the clones containing\n";
+print LOG "those changes to see if there are any strange errors (e.g. duplicate sets of data\n";
+print LOG "which are slightly out of sync.)\n\n";
+
+print LOG "3) Check /wormsrv2/autoace/CHROMOSOMES/composition.all - are there any non-ATCGN\n";
+print LOG "characters\n\n";
+
+print LOG "\nThat's all...for now!  If you are satisfied the build is ok, please inform the person\n";
+print LOG "building the database. Please continue to add to this list as appropriate\n";
+
+print LOG "\n========================================================================================\n\n";
+close(LOG);
+
+&mail_maintainer("Please check the ongoing build of WS${WS_current}",$maintainers,$log);
+
+
+exit(0);
+
+
+##############################################################
+#
+# Subroutines
+#
+##############################################################
+
+
+sub usage {
+  my $error = shift;
+
+  if ($error eq "Help") {
+    # Normal help menu
+    system ('perldoc',$0);
+    exit (0);
+  }
+}
+
+##################################################################
+
+
+
+
+__END__
+
+=pod
+
+=head1 NAME - next_builder_checks.pl
+
+=head1 USAGE
+
+=over 4
+
+=item next_builder_checks.pl --user <user>
+
+=back
+
+This script simply sends a list of check items to the next person doing the build.
+They should 'sign off' on each build and hand back to the main person when they
+are happy all is ok.
+
+=item MANDATORY arguments: -user <valid unix username>
+
+Needed to send email
+
+=back
+
+=over 4
+
+=item OPTIONAL arguments: -help, -debug <user>
+
+
+ 
+=head1 AUTHOR - Keith Bradnam
+
+Email krb@sanger.ac.uk
+
+
+=cut
