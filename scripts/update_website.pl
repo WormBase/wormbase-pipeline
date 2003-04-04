abb73c4179855f85f4dc07cfff4fe9b3b4800a62
krb <krb>
2003-04-04 15:25:34 +0000

    R.I.P. replaced by update_web_gene_names.pl

--- scripts/update_website.pl
+++ scripts/update_website.pl
@@ -1,29 +1,33 @@
-#!/usr/local/bin/perl5.6.0
+#!/usr/local/bin/perl5.6.1 -w
 #
 # update_website.pl
 # 
 # by Keith Bradnam aged 12 and half
 #
-# Usage : update_website.pl
-#
 # A script to finish the last part of the weekly build by updating all of the
 # relevant WormBase and Wormpep web pages.
+#
+# Last updated by: $Author: krb $     
+# Last updated on: $Date: 2003-04-04 15:25:34 $      
+
+
+
 
 #################################################################################
 # load modules etc.                                                             #
 #################################################################################
 
-$|=1;
-use IO::Handle;
-use Getopt::Std;
-use Cwd;
 use strict;
 use lib "/wormsrv2/scripts/";
 use Wormbase;
+use IO::Handle;
+use Getopt::Std;
+use Cwd;
 use Symbol 'gensym';
 use Ace;
 use lib "/nfs/WWW/perl";
 use SangerWeb;
+use Carp;
 
 ##############################
 # Script variables           #
@@ -57,7 +61,7 @@ our $log             = "/wormsrv2/logs/update_website.$rundate";
 
 &create_web_directories;
 
-&create_top_level_web_pages; # e.g. release_header.shtml, finished_seqs.shtml, unfinished_seqs.shtml etc.
+&create_top_level_web_pages; # e.g. release_header.shtml, finished_seqs.shtml, etc.
 
 &create_wormpep_page; # the wormpep page accessible from the main wormbase page
 
@@ -71,9 +75,6 @@ our $log             = "/wormsrv2/logs/update_website.$rundate";
 
 &update_wormpep_pages; # update the main wormpep web pages
 
-&create_loci_designations; # previously done by the list_loci_designations script
-
-
 
 
 #########################
@@ -82,12 +83,8 @@ our $log             = "/wormsrv2/logs/update_website.$rundate";
 
 # update 'current' symlink
 print LOG "\nChanging 'current symbolic link to point to new release\n";
-system("rm -f $www/current") && die "Couldn't remove 'current' symlink\n";
-system("ln -s $www/$WS_name/ $www/current") && die "Couldn't create new symlink\n";
-
-# copy release notes file across
-#system("cp /nfs/disk69/ftp/pub/wormbase/current_release/letter.$WS_name $www/current/release_notes.txt") && die "Couldn't copy across release notes file\n";
-
+system("rm -f $www/current") && croak "Couldn't remove 'current' symlink\n";
+system("ln -s $www/$WS_name/ $www/current") && croak "Couldn't create new symlink\n";
 
 print LOG "\n\nC'est finis\n\n";
 
@@ -109,7 +106,7 @@ exit(0);
 
 sub create_log_file{
     
-  open (LOG,">$log") || die "Cannot open logfile $!\n";
+  open (LOG,">$log") || croak "Cannot open logfile $!\n";
   LOG->autoflush();
   
   print LOG "# update_website.pl\n\n";     
@@ -165,7 +162,7 @@ sub copy_overlapcheck_files{
     foreach my $chrom (@chrom) {
       print LOG "Copying file CHROMOSOME_$chrom.$file to $www/$WS_name/Checks\n";
       system ("cp -f /wormsrv2/autoace/CHECKS/CHROMOSOME_$chrom.$file $www/$WS_name/Checks/")
-	&& die "Could not copy /wormsrv2/autoace/CHECKS/CHROMOSOME_$chrom.$file $!\n";
+	&& croak "Could not copy /wormsrv2/autoace/CHECKS/CHROMOSOME_$chrom.$file $!\n";
       
       print LOG "Calculating line numbers for CHROMOSOME_$chrom.$file\n";
       my $line = `wc -l /wormsrv2/autoace/CHECKS/CHROMOSOME_$chrom.$file`;
@@ -183,8 +180,8 @@ sub copy_overlapcheck_files{
     my $newfh  = gensym();
     my $count = 0;
     
-    open ($fh, "$www/$WS_previous_name/Checks/$file.html") || die "Cannot open old html file $!\n";
-    open ($newfh, ">$www/$WS_name/Checks/$file.html") || die "Cannot open new html file $!\n";
+    open ($fh, "$www/$WS_previous_name/Checks/$file.html") || croak "Cannot open old html file $!\n";
+    open ($newfh, ">$www/$WS_name/Checks/$file.html") || croak "Cannot open new html file $!\n";
     
     print LOG "Generating $www/$WS_name/Checks/$file.html\n";
     
@@ -203,14 +200,14 @@ sub copy_overlapcheck_files{
 	print $newfh $_;
       }
     }    
-    close($fh)    || die "Cannot close old html file $!\n";
-    close($newfh) || die "Cannot close new html file $!\n";
+    close($fh)    || croak "Cannot close old html file $!\n";
+    close($newfh) || croak "Cannot close new html file $!\n";
   }
 }
 
 
 ###########################################################################################################
-# Creates: release_header.shtml, finished_seqs.shtml, unfinished_seqs.shtml, DNA_table.shtml,
+# Creates: release_header.shtml, finished_seqs.shtml, DNA_table.shtml,
 # dbcomp.shtml, and wormpep.shtml
 ###########################################################################################################
 
@@ -222,7 +219,7 @@ sub create_top_level_web_pages{
   
 
   print LOG "Creating $www/$WS_name/release_header.shtml\n";
-  open (RELEASE_HEADER, ">$www/$WS_name/release_header.shtml") || die "Couldn't create release_header.shtml\n";
+  open (RELEASE_HEADER, ">$www/$WS_name/release_header.shtml") || croak "Couldn't create release_header.shtml\n";
   print RELEASE_HEADER  "<P>\n";
   print RELEASE_HEADER  "<FONT SIZE=+2 COLOR=purple><B>$WS_name release</B></FONT><BR>\n";
   print RELEASE_HEADER  "<FONT SIZE=-2 COLOR=red><B>Last updated $release_date.</B></FONT>\n";
@@ -234,10 +231,10 @@ sub create_top_level_web_pages{
   # create DNA.table.shtml
   print LOG "Creating $www/$WS_name/DNA.table.shtml\n";
   my ($DNA_tot, $DNA_d, $DNA_dp, $DNA_n, $DNA_np, $DNA_g, $DNA_gp, $DNA_a, $DNA_ap, $DNA_t, $DNA_tp, $DNA_c, $DNA_cp);
-  open (DNA_table, ">$www/$WS_name/DNA_table.shtml") || die "Couldn't create DNA_table.shtml\n\n";
+  open (DNA_table, ">$www/$WS_name/DNA_table.shtml") || croak "Couldn't create DNA_table.shtml\n\n";
   print DNA_table "<P>\n";
   
-  open (COMPOSITION, "</wormsrv2/autoace/CHROMOSOMES/composition.all") || die "Failed to open composition.all\n";
+  open (COMPOSITION, "</wormsrv2/autoace/CHROMOSOMES/composition.all") || croak "Failed to open composition.all\n";
   while (<COMPOSITION>) {
     $DNA_tot = $1 if (/6 sequences >= 0, (\d+) total/);
     ($DNA_d,$DNA_dp) = ($1,$2) if (/  - (\d+)\s+(\d+) %/);
@@ -250,7 +247,7 @@ sub create_top_level_web_pages{
   close (COMPOSITION);
     
   print DNA_table "<SPACER TYPE=\"horizontal\" SIZE=\"50\">\n";
-  print DNA_table "<TABLE WIDTH=\"40%\" CELLSPACING=\"0\" CELLWIDTH=\"0\" BORDER=\"1\"><TR BGCOLOR=\"yellow\"><TH>&nbsp</TH> <TH>No.</TH> <TH>%</TH></TR>\n";
+  print DNA_table "<TABLE WIDTH=\"40%\" CELLSPACING=\"0\" CELLWIDTH=\"0\" BORDER=\"1\"><TR BGCOLOR=\"yellow\"><TH>&nbsp</TH> <TH>No. bases</TH> <TH>%</TH></TR>\n";
   print DNA_table "<TR> <TD ALIGN=\"CENTER\">Total</TD> <TD ALIGN=\"RIGHT\">$DNA_tot</TD> <TD ALIGN=\"RIGHT\">100</TD>     </TR>\n";
   print DNA_table "<TR> <TD ALIGN=\"CENTER\">-</TD>     <TD ALIGN=\"RIGHT\">$DNA_d</TD>   <TD ALIGN=\"RIGHT\">$DNA_dp</TD> </TR>\n";
   print DNA_table "<TR> <TD ALIGN=\"CENTER\">G</TD>     <TD ALIGN=\"RIGHT\">$DNA_g</TD>   <TD ALIGN=\"RIGHT\">$DNA_gp</TD> </TR>\n";
@@ -265,10 +262,10 @@ sub create_top_level_web_pages{
 
   # create finished_seqs.shtml
   print LOG "Creating $www/$WS_name/finished_seqs.shtml\n";
-  my $db = Ace->connect(-path=>$dbpath) || do { print "Connection failure: ",Ace->error; die();};
+  my $db = Ace->connect(-path=>$dbpath) || do { print "Connection failure: ",Ace->error; croak();};
   my $count = $db->fetch(-query=> 'find genome_sequence where Finished');
 
-  open (FINISHED_SEQS, ">$www/$WS_name/finished_seqs.shtml") || die "Couldn't create finished_seqs.shtml\n";
+  open (FINISHED_SEQS, ">$www/$WS_name/finished_seqs.shtml") || croak "Couldn't create finished_seqs.shtml\n";
   print FINISHED_SEQS  "<P>\n";
   print FINISHED_SEQS "$count projects\n";
   print FINISHED_SEQS  "</P>\n";
@@ -276,55 +273,19 @@ sub create_top_level_web_pages{
 
 
 
-  # create unfinished_seqs.shtml
-  print LOG "Creating $www/$WS_name/unfinished_seqs.shtml\n";
-  my %who = ('HX', 'Hinxton', 'RW', 'St Louis');
-
-  $count = $db->fetch(-query=> 'find genome_sequence where Remark="Unfinished fragment"');
-
-  open (UNFINISHED_SEQS, ">$www/$WS_name/unfinished_seqs.shtml") || die "Couldn't creat unfinished_seqs.shtml\n";
-  print UNFINISHED_SEQS  "<P>\n";
-  print UNFINISHED_SEQS "$count projects\n";
-  print UNFINISHED_SEQS  "</P>\n";
-  print UNFINISHED_SEQS "<SPACER TYPE=\"horizontal\" SIZE=\"50\">\n";
-  print UNFINISHED_SEQS "<TABLE WIDTH=\"40%\" CELLSPACING=\"0\" CELLWIDTH=\"0\" BORDER=\"1\"><TR BGCOLOR=\"yellow\" ><TH>Project</TH> <TH>Source</TH> <TH>EMBL_Acc</TH> <TH>Length (bp)</TH></TR>\n";
-  my $i = $db->fetch_many(-query=> 'find genome_sequence where Remark="Unfinished fragment"');  
-  while (my $obj = $i->next) {
-    my $project = $obj;
-        
-    # Database information
-    my $dbaccno = $obj->at('DB_info.Database[3]');
-    if (!$dbaccno) {
-      print "## Warning: No accession no\n";
-      $dbaccno = "n/a";
-    }
-        
-    # DNA length
-    my $DNA_len = $obj->DNA(2);
-    
-    # Laboratory
-    my $lab = $obj->From_Laboratory(1);
-    
-    print UNFINISHED_SEQS "<TR> <TD ALIGN=\"CENTER\">$project</TD> <TD ALIGN=\"CENTER\">$who{$lab}</TD> <TD ALIGN=\"RIGHT\">$dbaccno</TD> <TD ALIGN=\"RIGHT\">$DNA_len</TD> </TR>\n";
-        
-  }
-  print UNFINISHED_SEQS "</TABLE>\n";
-  close (UNFINISHED_SEQS);
-
-
 
 
   # create dbcomp.shtml
   print LOG "Creating $www/$WS_name/dbcomp.shtml\n";
 
-  system ("cp /wormsrv2/autoace/COMPARE/current.out $www/$WS_name/WS.dbcomp_output") && die "Couldn't copy current.out\n";
+  system ("cp /wormsrv2/autoace/COMPARE/current.out $www/$WS_name/WS.dbcomp_output") && croak "Couldn't copy current.out\n";
 
-  open (DB_comp, ">$www/$WS_name/dbcomp.shtml") || die "Couldn't create dbcomp.shtml\n\n";
+  open (DB_comp, ">$www/$WS_name/dbcomp.shtml") || croak "Couldn't create dbcomp.shtml\n\n";
   print DB_comp "<P>\n";
   print DB_comp "<SPACER TYPE=\"horizontal\" SIZE=\"50\">\n";
   print DB_comp "<TABLE WIDTH=\"40%\" CELLSPACING=\"0\" CELLWIDTH=\"0\" BORDER=\"1\"><TR BGCOLOR=\"yellow\" ><TH>Class</TH> <TH>$WS_previous_name</TH> <TH>$WS_name</TH> <TH>Diff</TH></TR>\n";
 
-  open (COMP, "</wormsrv2/autoace/COMPARE/current.dbcomp") || die "Failed to open composition.all\n\n";
+  open (COMP, "</wormsrv2/autoace/COMPARE/current.dbcomp") || croak "Failed to open composition.all\n\n";
   while (<COMP>) {
     next if (/\+/);
     next if (/ACEDB/);
@@ -358,7 +319,7 @@ sub create_wormpep_page{
 
   print LOG "Creating $www/$WS_name/wormpep.shtml\n";
 
-  open (WORMPEP, ">$www/$WS_name/wormpep.shtml") || die "Failed to create wormpep.shtml\n\n";
+  open (WORMPEP, ">$www/$WS_name/wormpep.shtml") || croak "Failed to create wormpep.shtml\n\n";
   print WORMPEP "<P>\n";
   print WORMPEP "<SPACER TYPE=\"horizontal\" SIZE=\"50\">\n";
   print WORMPEP "<TABLE WIDTH=\"40%\" CELLSPACING=\"0\" CELLWIDTH=\"0\" BORDER=\"1\"><TR BGCOLOR=\"yellow\" ><TH>Release</TH> <TH>Sequences</TH> <TH>Letters</TH> <TH>Alt. Splices</TH></TR>\n";
@@ -367,7 +328,7 @@ sub create_wormpep_page{
   print LOG "Opening log file '/wormsrv2/WORMPEP/wormpep$WS_current/wormpep_current.log'\n";
 
   my ($wp_seq,$wp_let);
-  open (WP_1, "</wormsrv2/WORMPEP/wormpep$WS_current/wormpep_current.log") || die "Failed to open wormpep.log\n";
+  open (WP_1, "</wormsrv2/WORMPEP/wormpep$WS_current/wormpep_current.log") || croak "Failed to open wormpep.log\n";
   while (<WP_1>) {
     if (/==\> (\S+) sequences totalling (\S+) letters/) {
       ($wp_seq,$wp_let) = ($1,$2);
@@ -380,7 +341,7 @@ sub create_wormpep_page{
   my $wp_alt;
   my @possible_logs = `ls -t /wormsrv2/logs/make_wormpep_full.${WS_current}*`; # added new logfile name 
   my $wormpeplog  = "$possible_logs[0]";
-  open (WP_2, "<$wormpeplog") || die "Failed to open wormpep.log\n";
+  open (WP_2, "<$wormpeplog") || croak "Failed to open wormpep.log\n";
   while (<WP_2>) {
     if (/\((\d+)\) alternate splice_forms/) {
       ($wp_alt) = ($1);
@@ -393,7 +354,7 @@ sub create_wormpep_page{
   
 
   my (@changed, @lost, @new, @reappeared);
-  open (WP_3, "</wormsrv2/WORMPEP/wormpep${WS_current}/wormpep.diff${WS_current}") || die "Failed to open wormpep.history\n";
+  open (WP_3, "</wormsrv2/WORMPEP/wormpep${WS_current}/wormpep.diff${WS_current}") || croak "Failed to open wormpep.history\n";
   while (<WP_3>) {
     (push (@changed,$_)) if (/changed:/);
     (push (@lost,$_)) if (/lost:/);
@@ -466,7 +427,7 @@ sub copy_EST_files{
   
   foreach my $file (keys(%files)){
     print LOG "creating $file.shtml in $www/$WS_name/Checks\n"; 
-    open(EST_HTML,">$www/$WS_name/Checks/$file.shtml") || die "Couldn't create $file.shtml\n";
+    open(EST_HTML,">$www/$WS_name/Checks/$file.shtml") || croak "Couldn't create $file.shtml\n";
     
     print EST_HTML &SangerWeb::header();
     
@@ -475,7 +436,7 @@ sub copy_EST_files{
     print EST_HTML "</FONT></H2></TD></TR></TABLE>\n";
 
     print EST_HTML "<P><PRE>";
-    open (EST_TXT, "</wormsrv2/autoace/CHECKS/$file.txt") || die "Couldn't open EST_total.txt\n";
+    open (EST_TXT, "</wormsrv2/autoace/CHECKS/$file.txt") || croak "Couldn't open EST_total.txt\n";
 
     my $line = `wc -l /wormsrv2/autoace/CHECKS/$file.txt`;
     # take line count and add to array
@@ -497,8 +458,8 @@ sub copy_EST_files{
 
   print LOG "updating EST_analysis.shtml from $www/$WS_previous/Checks to $www/$WS_name/Checks, adding new info\n"; 
 
-  open(EST_OLD, "<$www/$WS_previous_name/Checks/EST_analysis.html") || die "Couldn't read old version of EST_analysis.html\n";
-  open(EST_NEW, ">$www/$WS_name/Checks/EST_analysis.html") || die "Couldn't create new version of EST_analysis.html\n";
+  open(EST_OLD, "<$www/$WS_previous_name/Checks/EST_analysis.html") || croak "Couldn't read old version of EST_analysis.html\n";
+  open(EST_NEW, ">$www/$WS_name/Checks/EST_analysis.html") || croak "Couldn't create new version of EST_analysis.html\n";
 
   my $count=0;
   while (<EST_OLD>) {
@@ -532,12 +493,12 @@ sub copy_GFF_files{
   foreach my $chrom (@chrom) {
     foreach my $file (@gff_files){
       system("sort -u $gff/CHROMOSOME_$chrom.$file.gff | gff_sort > $www/$WS_name/GFF/CHROMOSOME_$chrom.$file.gff")
-	&& die "Couldn't copy CHROMOSOME_$chrom.$file.gff\n";
+	&& croak "Couldn't copy CHROMOSOME_$chrom.$file.gff\n";
     }
   }
 
   print LOG "Copying across GFF files from /wormsrv2/autoace/CHECKS/\n";
-  system ("cp /wormsrv2/autoace/CHECKS/*.gff $www/$WS_name/GFF/") && die "Could not copy GFF files from autoace/CHECKS $!\n";
+  system ("cp /wormsrv2/autoace/CHECKS/*.gff $www/$WS_name/GFF/") && croak "Could not copy GFF files from autoace/CHECKS $!\n";
 }
 
 
@@ -566,8 +527,8 @@ sub create_GFF_intron_files{
    }
 
    # open old and new files
-   open(OLDFILE, "<$www/$WS_previous_name/Checks/GFF_introns_confirmed_CDS_$lab.html") || die "Couldn't read old $lab GFF intron file\n";
-   open(NEWFILE, ">$www/$WS_name/Checks/GFF_introns_confirmed_CDS_$lab.html") || die "Couldn't create new $lab GFF intron file\n";
+   open(OLDFILE, "<$www/$WS_previous_name/Checks/GFF_introns_confirmed_CDS_$lab.html") || croak "Couldn't read old $lab GFF intron file\n";
+   open(NEWFILE, ">$www/$WS_name/Checks/GFF_introns_confirmed_CDS_$lab.html") || croak "Couldn't create new $lab GFF intron file\n";
 
    my $count = 0;
    # loop through old file replacing old info with new info from @line_counts 
@@ -606,7 +567,7 @@ sub update_wormpep_pages{
   print LOG "Updating wormpep pages at $www_root/wormpep\n";
   
   # write a new paragraph for the index.shtml page        
-  open(LOGFILE,"</wormsrv2/WORMPEP/wormpep${WS_current}/wormpep_current.log") || die "Couldn't open wormpep log file\n";
+  open(LOGFILE,"</wormsrv2/WORMPEP/wormpep${WS_current}/wormpep_current.log") || croak "Couldn't open wormpep log file\n";
   my $text = <LOGFILE>;
   close(LOGFILE);
 
@@ -620,35 +581,35 @@ sub update_wormpep_pages{
   $alt_spliced =~ s/\s+//g;
 
   # create release_paragraph.shtml
-  system("rm -f $www_root/wormpep/release_paragraph.shtml") && die "Couldn't remove old release_paragraph.shtml\n";
-  open (PARAGRAPH, ">$www_root/wormpep/release_paragraph.shtml") || die "Can't create the file: $www_root/wormpep/release_paragraph.shtml\n\n";
+  system("rm -f $www_root/wormpep/release_paragraph.shtml") && croak "Couldn't remove old release_paragraph.shtml\n";
+  open (PARAGRAPH, ">$www_root/wormpep/release_paragraph.shtml") || croak "Can't create the file: $www_root/wormpep/release_paragraph.shtml\n\n";
   print PARAGRAPH "The current Wormpep database, wormpep${WS_current} (released $release_date), contains $letters residues in $count protein sequences (of which $alt_spliced have splice variants) - wormpep${WS_current} is based on the <A href=\"ftp://ftp.sanger.ac.uk/pub/wormbase/WS$WS_current\">current WS$WS_current release</A> of the <I>C. elegans</I> AceDB database.\n";
   close (PARAGRAPH);
 
   # update the 'current_release.shtml' file
-  system("rm -f $www_root/wormpep/current_release.shtml") && die "Couldn't remove current_release.shtml\n";
-  open(RELEASE,">$www_root/wormpep/current_release.shtml") || die "Coudn't write to current_release.shtml\n";
+  system("rm -f $www_root/wormpep/current_release.shtml") && croak "Couldn't remove current_release.shtml\n";
+  open(RELEASE,">$www_root/wormpep/current_release.shtml") || croak "Coudn't write to current_release.shtml\n";
   print RELEASE "The current release is wormpep${WS_current}\n";
   close(RELEASE);
 
   # update the history of wormpep releases, i.e. wormpep_release.txt
 
-  open (TABLE, "<$www_root/wormpep/wormpep_release.txt") || die "Can't read from the file: $www_root/wormpep/wormpep_release.txt\n\n";
-  open (TABLE2, ">$www_root/wormpep/tmp_table.txt") || die "Can't create the file: $www_root/wormpep/tmp_table.txt\n\n";
+  open (TABLE, "<$www_root/wormpep/wormpep_release.txt") || croak "Can't read from the file: $www_root/wormpep/wormpep_release.txt\n\n";
+  open (TABLE2, ">$www_root/wormpep/tmp_table.txt") || croak "Can't create the file: $www_root/wormpep/tmp_table.txt\n\n";
   print TABLE2 "${WS_current}\t$release_date2\t$count";
   while(<TABLE>){
     print TABLE2 $_;
   }
   close(TABLE); 
   close(TABLE2);
-  system("mv $www_root/wormpep/tmp_table.txt $www_root/wormpep/wormpep_release.txt") && die "Couldn't rename file\n";
+  system("mv $www_root/wormpep/tmp_table.txt $www_root/wormpep/wormpep_release.txt") && croak "Couldn't rename file\n";
 
 
   # write a new table for the wormpep_download.shtml page    
   print LOG "Updating wormpep_download.shtml page\n";
   my $rows = $WS_current + 5;
 
-  open (LIST, ">$www_root/wormpep/releases.shtml") || die "Can't open the file: $www_root/wormpep/releases.shtml\n\n";
+  open (LIST, ">$www_root/wormpep/releases.shtml") || croak "Can't open the file: $www_root/wormpep/releases.shtml\n\n";
   print LIST "<CENTER><TABLE CELLPADDING=\"0\" CELLSPACING=\"0\" BORDER=\"0\">\n";
   print LIST "<TR VALIGN=\"top\">\n";
   print LIST "  <TD rowspan=\"$rows\" class=\"grey1\"><img src=\"/icons/blank.gif\" width=\"1\"   height=\"1\"></td>\n";
@@ -709,12 +670,3 @@ sub update_wormpep_pages{
 }
 
 
-###########################################################################################################
-# Makes the pages of loci designations for each letter of the alphabet
-###########################################################################################################
-
-sub create_loci_designations{
-  print LOG "\nRunning list_loci_desidnations\n";
-  system ("/wormsrv2/scripts/list_loci_designations") && die "Cannot execute list_loci_desinations $!\n";
-
-} 
