72f4fd7090d53086788eafdc33de9b1e340da72f
ar2 <ar2>
2003-05-20 09:53:44 +0000

    Added options to specify database and gff directories. Runs over entire genome
    Detects wrongly attached cDNAs and writes out an ace file for each centre to remove the connections
    
    Reports cases where an intron ends on the start codon, so a spliced UTR cannot be built (only for 5'fwd)

--- scripts/UTR_confirmed_introns.pl
+++ scripts/UTR_confirmed_introns.pl
@@ -6,175 +6,190 @@ use Ace;
 use Getopt::Long;
 use Data::Dumper;
 
-my ($debug,$update,$track);
+my ($debug,$update,$track,$datadump, $database, $gff_dir, $specified_genes);
 
 GetOptions ( "debug" => \$debug,
 	     "track" => \$track,
-	     "update" => \$update
+	     "datadump" => \$datadump,
+	     "update" => \$update,
+	     "database:s" => \$database,
+	     "gff_dir:s"  => \$gff_dir,
+	     "specify"    => \$specified_genes
 	   );
 
+my @chromosomes = qw(CHROMOSOME_I CHROMOSOME_II CHROMOSOME_III CHROMOSOME_IV CHROMOSOME_V CHROMOSOME_X);
 
-#my $database = glob("~wormpub/DATABASES/TEST_DBs/ANTEST");
-my $database = glob("~wormpub/DATABASES/WS98_allele_fix");
-my $gff = glob("~wormpub/DATABASES/WS98_allele_fix/IV.gff");
-#my $gff = glob("~wormpub/DATABASES/WS98_allele_fix/confirmed_UTRS_X.gff");
-#my $gff = glob("~wormpub/DATABASES/WS98_allele_fix/Y73B3A.gff");
-#my $gff = glob("~ar2/UTR/C43G2.gff_filter");
+$database = glob("~wormpub/DATABASES/WS98_allele_fix") unless $database;
+$gff_dir = glob("~wormpub/DATABASES/WS98_allele_fix") unless $gff_dir;
+my $acefile = "$database/utr.ace";
 
-open (GFF,"<$gff") or die "gff\n";
-# C43G2   curated         Sequence     10841   13282   .       +       .       Sequence "C43G2.4"
-# C43G2   BLAT_EST_BEST   similarity   8877    9029    100     +       .       Target "Sequence:yk1125e01.5" 37 189
-# C43G2   *UNKNOWN*       intron       3582    3934    .       -       .       Confirmed_by_EST ; Confirmed_in_UTR
+open (ACE,">$acefile") or die "cant write $acefile\n";
 
-my @genes_to_dump;
-my $g;
-while ($g = shift) { push(@genes_to_dump,$g) };
+# this bit is to separate wrongly attached cDNAs to centre specific acefile output
+my $STL_bad_mcDNAs = "$database/STL_bad_matching_cDNA.ace";
+my $CAM_bad_mcDNAs = "$database/CAM_bad_matching_cDNA.ace";
+open (STLBAD,">$STL_bad_mcDNAs") or die "cant open $STL_bad_mcDNAs : $!\n";
+open (CAMBAD,">$CAM_bad_mcDNAs") or die "cant open $CAM_bad_mcDNAs : $!\n";
 
-my %genes_span;
-my %cDNA;
-my @conf_intr;
-my $superlink = "CHROMOSOME_IV";
-my $acefile = glob("~wormpub/DATABASES/WS98_allele_fix/utr.ace");
+my %BAD_FH = ( 'HX' => *CAMBAD,
+	       'RW' => *STLBAD
+	     );
 
-# parse GFF file to get Sequence, exon and cDNA info
-if ($update ) {
-  print "updating data . . \n" if $track;
-  while (<GFF>) {
-    my @data = split(/\t/,$_);
-    $superlink = $data[0];
-    #print " . . \t@data\n" ;
-    if ( (defined $data[1]) and ($data[1] ne ".")) {
-      if ($data[1] eq "curated") {
-	if( defined $data[8] ) {
-	  $data[8] =~ /Sequence \"(.*)\"/;
-	  $data[8] = $1;
-	}
-	if( $data[2] eq "Sequence" ) {
-	  $genes_span{$data[8]} = [($data[3], $data[4], $data[6])];
-	}
-      }
-      elsif( ($data[1] eq "BLAT_EST_BEST" ) or ($data[1] eq "BLAT_mRNA_BEST" ) ){
-	$data[8] =~ /Target \"Sequence:(.*)\"/ ;
-	$data[8] = $1;
-	$cDNA{$data[8]}[2] = $data[6]; # strand
-	if( !(defined $cDNA{$data[8]}[0]) or ($cDNA{$data[8]}[0] > $data[3]) ) {
-	  $cDNA{$data[8]}[0] = $data[3];
-	}
-	if( !(defined $cDNA{$data[8]}[1]) or ($cDNA{$data[8]}[0] < $data[4]) ) {
-	  $cDNA{$data[8]}[1] =  $data[4];
-	}
-      }
-    }
-    elsif( ($data[2] eq "intron") and ( $data[8] =~ /Confirmed_in_UTR/ ) ) {
-      push( @conf_intr, [($data[3], $data[4]) ] );
-    }
-  }
-
-  print "writing data  . . \n" unless $track;
-  open (FH,">$database/cdna.dat") or die;
-  print FH Data::Dumper->Dump([\%cDNA]);
-  close FH;
-
-  open (FH,">$database/genes.dat") or die;
-  print FH Data::Dumper->Dump([\%genes_span]);
-  close FH;
-
-  open (FH,">$database/conf_int.dat") or die;
-  print FH Data::Dumper->Dump([\@conf_intr]);
-  close FH;
-}
-else {
-  print "loading data  . . \n" unless $track;
-  &reFetchData("cdna",\%cDNA);
-  &reFetchData("genes",\%genes_span);
-  &reFetchArrayData("conf_int",\@conf_intr);
-}
-
-# sort the array of cIntrons
-print "sorting confirmed introns\t" if $track;
-my @sorted_cis = sort { $a->[0] <=> $b->[0] } @conf_intr;
-print "DONE\n" if $track;
-# get matching_cDNA s for genes
+my %gene2centre;
+&reFetchData("gene2centre",\%gene2centre);
 
 print "\nAce->connect $database\t" if $track;
 my $db = Ace->connect(-path => "$database") or die Ace->error,"\n";
 print "DONE\n" if $track;
-# process each cDNA
-my $gene_count;
 
-my %FindExtremes =  ( '+' => \&extremes_fwd,
-		      '-' => \&extremes_rev
-		    );
+# sample GFF
+# C43G2   curated         Sequence     10841   13282   .       +       .       Sequence "C43G2.4"
+# C43G2   BLAT_EST_BEST   similarity   8877    9029    100     +       .       Target "Sequence:yk1125e01.5" 37 189
+# C43G2   *UNKNOWN*       intron       3582    3934    .       -       .       Confirmed_by_EST ; Confirmed_in_UTR
 
-my %Check_Introns = ( '+' => \&check_cis_fwd,
-		      '-' => \&check_cis_fwd
-		    );
-
-print "\n\nStarting Genes loop\n" if $track;
-
-open (ACE,">$acefile") or die "cant write $acefile\n";
-
-# if no specific gene to dump do all of them
-unless ( defined $genes_to_dump[0] ) {
-  foreach (keys %genes_span ) {
-    push (@genes_to_dump,$_);
-  }
+# get genes to output from command line or entire list
+my @genes_to_dump;
+if ($specified_genes ) {
+  my $g;
+  while ($g = shift) { push(@genes_to_dump,$g) };
 }
 
-foreach my $gene (@genes_to_dump ) {
-  print "doing gene $gene\n" if $debug;
-  my $utr5start;
-  my $utr3end;
-  my (%utr5, %utr3);
+my %genes_span;
+my %cDNA;
+my @conf_intr;
+my $superlink;
 
-  my $seq = $db->fetch(-name    => "$gene",
-		       -class   => 'Sequence'
-		      );
-  if( $seq ) {
-    my @matching_cDNA = $seq->Matching_cDNA;
-    my @extremes = ($genes_span{$gene}[0],$genes_span{$gene}[1] );
+foreach ( @chromosomes ) {
 
-    # determine extremes of transcript based on mcDNAs
-    foreach my $mcDNA (@matching_cDNA) {
-      if ( $cDNA{$mcDNA} ) {
-	$FindExtremes{"$genes_span{$gene}[2]"}->(\@extremes, \$mcDNA);
+  my $gff = "$gff_dir/$_.gff";
+  $superlink = $_;
+  %genes_span = ();
+  %cDNA = ();
+  @conf_intr = ();
+  @genes_to_dump = () unless $specified_genes;
+
+  # parse GFF file to get Sequence, exon and cDNA info
+  if ($update ) {
+    open (GFF,"<$gff") or die "gff\n";
+    print "updating data . . \n" if $track;
+    while (<GFF>) {
+      next if (/\#\#/);
+      my @data = split(/\t/,$_);
+      #print " . . \t@data\n" ;
+      if ( (defined $data[1]) and ($data[1] ne ".")) {
+	if ($data[1] eq "curated") {
+	  if( defined $data[8] ) {
+	    $data[8] =~ /Sequence \"(.*)\"/;
+	    $data[8] = $1;
+	  }
+	  if( $data[2] eq "Sequence" ) {
+	    $genes_span{$data[8]} = [($data[3], $data[4], $data[6])];
+	  }
+	}
+	elsif( ($data[1] eq "BLAT_EST_BEST" ) or ($data[1] eq "BLAT_mRNA_BEST" ) ){
+	  $data[8] =~ /Target \"Sequence:(.*)\"/ ;
+	  $data[8] = $1;
+	  $cDNA{$data[8]}[2] = $data[6]; # strand
+	  if( !(defined $cDNA{$data[8]}[0]) or ($cDNA{$data[8]}[0] > $data[3]) ) {
+	    $cDNA{$data[8]}[0] = $data[3];
+	  }
+	  if( !(defined $cDNA{$data[8]}[1]) or ($cDNA{$data[8]}[0] < $data[4]) ) {
+	    $cDNA{$data[8]}[1] =  $data[4];
+	  }
+	}
       }
-      elsif($debug) { print $gene," -> ",$mcDNA->name," not in GFF file\n"; }
-    }
-    if( ($extremes[1] - $extremes[0]) > 20000 ) {
-      print "$gene is v.long are these correct ",@matching_cDNA,"\n";
-      next;
-    }
-    # find overlapping introns
-    foreach my $ci (@sorted_cis) {
-      $Check_Introns{"$genes_span{$gene}[2]"}->(\$genes_span{$gene}[2],
-						\$gene ,
-						\@extremes ,
-						\$ci ,
-						\%utr5 ,
-						\%utr3
-					       );
+      elsif( ($data[2] eq "intron") and ( $data[8] =~ /Confirmed_in_UTR/ ) ) {
+	push( @conf_intr, [($data[3], $data[4]) ] );
+      }
     }
 
-    &fwd5UTR_introns(\$gene, \@extremes, \%utr5) if (%utr5 and( $genes_span{$gene}[2] eq "+")) ;
-    &fwd3UTR_introns(\$gene, \@extremes, \%utr3) if (%utr3 and( $genes_span{$gene}[2] eq "+")) ;
-    &rev5UTR_introns(\$gene, \@extremes, \%utr5) if (%utr5 and( $genes_span{$gene}[2] eq "-")) ;
-    &rev3UTR_introns(\$gene, \@extremes, \%utr3) if (%utr3 and( $genes_span{$gene}[2] eq "-")) ;
+    close GFF;
 
-    if ($debug) {
-      print "$gene ",$extremes[0]," - ",$extremes[1],"\n";
-      foreach (keys %utr5) {
-	print "$_\t$utr5{$_}\n";
+    if( $datadump ) {
+      print "writing data  . . \n" unless $track;
+      open (FH,">$database/cdna.dat") or die;
+      print FH Data::Dumper->Dump([\%cDNA]);
+      close FH;
+
+      open (FH,">$database/genes.dat") or die;
+      print FH Data::Dumper->Dump([\%genes_span]);
+      close FH;
+
+      open (FH,">$database/conf_int.dat") or die;
+      print FH Data::Dumper->Dump([\@conf_intr]);
+      close FH;
+    }
+  }
+  else {
+    print "loading data  . . \n" unless $track;
+    &reFetchData("cdna",\%cDNA);
+    &reFetchData("genes",\%genes_span);
+    &reFetchArrayData("conf_int",\@conf_intr);
+  }
+
+
+  # sort the array of cIntrons
+  print "sorting confirmed introns\t" if $track;
+  my @sorted_cis = sort { $a->[0] <=> $b->[0] } @conf_intr;
+  print "DONE\n" if $track;
+  # get matching_cDNA s for genes
+
+  # process each cDNA
+  my $gene_count;
+
+  print "\n\nStarting Genes loop\n" if $track;
+
+  # if no specific gene to dump do all of them
+  unless ( defined $specified_genes ) {
+    foreach (keys %genes_span ) {
+      push (@genes_to_dump,$_);
+    }
+  }
+
+  foreach my $gene (@genes_to_dump ) {
+    print "doing gene $gene\n" if $debug;
+    my $utr5start;
+    my $utr3end;
+    my (%utr5, %utr3);
+
+    my $seq = $db->fetch(-name    => "$gene",
+			 -class   => 'Sequence'
+			);
+    if( $seq ) {
+      my @matching_cDNA = $seq->Matching_cDNA;
+      my @extremes = ($genes_span{$gene}[0],$genes_span{$gene}[1] );
+
+      # determine extremes of transcript based on mcDNAs
+      foreach my $mcDNA (@matching_cDNA) {
+	if ( $cDNA{$mcDNA} ) {
+	  &FindExtremes(\@extremes, \$mcDNA, \$gene);
+	}
+	elsif($debug) { print $gene," -> ",$mcDNA->name," not in GFF file\n"; }
       }
-      foreach (keys %utr3) {
-	print "$_\t$utr3{$_}\n";
+
+      # find overlapping introns
+      foreach my $ci (@sorted_cis) {
+	last if (&Check_Introns(\$genes_span{$gene}[2],
+		       \$gene ,
+		       \@extremes ,
+		       \$ci ,
+		       \%utr5 ,
+		       \%utr3
+		      ) eq "stop" );
       }
+      # write UTR output (if there is one)
+      &fwd5UTR_introns(\$gene, \@extremes, \%utr5) if ( ($extremes[0] != $genes_span{$gene}[0]) and ( $genes_span{$gene}[2] eq "+")) ;
+      &fwd3UTR_introns(\$gene, \@extremes, \%utr3) if ( ($extremes[1] != $genes_span{$gene}[1]) and ( $genes_span{$gene}[2] eq "+")) ;
+      &rev5UTR_introns(\$gene, \@extremes, \%utr5) if ( ($extremes[1] != $genes_span{$gene}[1]) and ( $genes_span{$gene}[2] eq "-")) ;
+      &rev3UTR_introns(\$gene, \@extremes, \%utr3) if ( ($extremes[0] != $genes_span{$gene}[0]) and ( $genes_span{$gene}[2] eq "-")) ;
     }
   }
 }
 print "\n\nEND\n" if $track;
 
 close ACE;
+close STLBAD;
+close CAMBAD;
 $db->close;
 exit(0);
 
@@ -187,11 +202,18 @@ sub fwd5UTR_introns
     print ACE "\nSequence : $superlink\n";
     print ACE "UTR \"UTR_5:$$gene\" ",$$extremes[0]," ",$genes_span{$$gene}[0]-1," \n";
 
+    # link UTR to CDS
+    print ACE "\nSequence : $$gene\nMatching_UTR \"UTR_5:$$gene\"\n";
+
     print ACE "\nUTR :\"UTR_5:$$gene\"\n";
     print ACE "Method UTR\n";
     print ACE "Source_exons 1 ";
 
     foreach (sort keys %$utr5) {
+      if($$utr5{$_} +1 == $genes_span{$$gene}[0] ) {
+	print "$$gene - UTR intron without exon\n";
+	next;
+      }
       print ACE $_ - $$extremes[0] ,"\nSource_exons ",$$utr5{$_} +1 - $$extremes[0] + 1," ";
     }
     print ACE $genes_span{$$gene}[0] - $$extremes[0] ,"\n";
@@ -229,6 +251,9 @@ sub fwd3UTR_introns
     print ACE "\nSequence : $superlink\n";
     print ACE "UTR \"UTR_3:$$gene\" ",$genes_span{$$gene}[1]+1," ",$$extremes[1]," \n";
 
+    # link UTR to CDS
+    print ACE "\nSequence : $$gene\nMatching_UTR \"UTR_3:$$gene\"\n";
+
     print ACE "\nUTR :\"UTR_3:$$gene\"\n";
     print ACE "Method UTR\n";
     print ACE "Source_exons 1 ";
@@ -248,6 +273,9 @@ sub rev3UTR_introns
     print ACE "\nSequence : $superlink\n";
     print ACE "UTR \"UTR_3:$$gene\" ",$genes_span{$$gene}[0]-1," ",$$extremes[0]," \n";
 
+    # link UTR to CDS
+    print ACE "\nSequence : $$gene\nMatching_UTR \"UTR_3:$$gene\"\n";
+
     print ACE "\nUTR :\"UTR_3:$$gene\"\n";
     print ACE "Method UTR\n";
     print ACE "Source_exons 1 ";
@@ -257,7 +285,7 @@ sub rev3UTR_introns
     }
     print ACE $genes_span{$$gene}[0] - $$extremes[0] ,"\n";
   }
-sub check_cis_fwd
+sub Check_Introns
   {
     my $strand = shift;
     my $gene = shift;
@@ -274,29 +302,30 @@ sub check_cis_fwd
       $$utr3{$$ci->[0]} = $$ci->[1] if $$strand eq "+";
       $$utr5{$$ci->[0]} = $$ci->[1] if $$strand eq "-";
     }
+    # confirmed intron start after the extreme of gene - no point checking any further
+    elsif( $$ci->[0] > $$extremes[1] ) {
+      return "stop";
+    }
   }
 
-sub extremes_fwd
+sub FindExtremes
  {
    my $extremes = shift;
    my $mcDNA = shift;
-   if( $cDNA{$$mcDNA}[0] < $$extremes[0] ) {
-     $$extremes[0] = $cDNA{$$mcDNA}[0];
-   }	
-   if( $cDNA{$$mcDNA}[1] > $$extremes[1] ) {
-     $$extremes[1] = $cDNA{$$mcDNA}[1];
-   }
- }
+   my $gene = shift;
 
-sub extremes_rev
- {
-   my $extremes = shift;
-   my $mcDNA = shift;
-   if( $cDNA{$$mcDNA}[1] > $$extremes[1] ) {
-     $$extremes[1] = $cDNA{$$mcDNA}[1];
-   }	
-   if( $cDNA{$$mcDNA}[0] < $$extremes[0] ) {
-     $$extremes[0] = $cDNA{$$mcDNA}[0];
+   if( ( ($$extremes[0] - $cDNA{$$mcDNA}[0]) > 20000 ) || 
+       ( ($cDNA{$$mcDNA}[1] - $$extremes[1]) > 20000) )  {
+     # this determines which file to print to via %BAD_FH based on the gene.
+     print {$BAD_FH{ $gene2centre{$$gene} } } "\nSequence : \"$$gene\"\n-D Matching_cDNA \"$$mcDNA\"\n";
+   }
+   else {
+     if( $cDNA{$$mcDNA}[0] < $$extremes[0] ) {
+       $$extremes[0] = $cDNA{$$mcDNA}[0];
+     }	
+     if( $cDNA{$$mcDNA}[1] > $$extremes[1] ) {
+       $$extremes[1] = $cDNA{$$mcDNA}[1];
+     }
    }
  }
 
