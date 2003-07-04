6281b0c9ff453f8bafcdf7d9ba30ba0c2c76d0ab
ck1 <ck1>
2003-07-04 11:00:09 +0000

    simple script to automate the manual part of dump and concat BLAST data in DUMP BLAST RESULT

---
+++ scripts/further_BLAST_dump.pl
@@ -0,0 +1,22 @@
+#!/usr/local/bin/perl5.8.0 -w
+
+# further_BLAST_dump.pl
+#
+#
+# Author: Chao-Kung CHen
+#
+# Last updated by: $Author: ck1 $                      
+# Last updated on: $Date: 2003-07-04 11:00:09 $        
+
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/blastp_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/blastx_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/wormprot_motif_info.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/swissproteins.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/tremblproteins.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/ipi_hits.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+
+chdir "/wormsrv2/wormbase/ensembl_dumps";
+
+system("cat ipi_hits.ace fly.ace yeast.ace swissproteins.ace tremblproteins.ace brigpep.ace > ensembl_protein_info.ace");
+
+exit(0);
