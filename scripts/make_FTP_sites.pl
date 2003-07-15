ce98e25a59c8aafcf320fa6baf676ea8e15da46a
ar2 <ar2>
2003-07-15 13:16:55 +0000

    copies best_blastp_hits file to public FTP site

--- scripts/make_FTP_sites.pl
+++ scripts/make_FTP_sites.pl
@@ -7,8 +7,8 @@
 # 
 # Originally written by Dan Lawson
 #
-# Last updated by: $Author: ck1 $
-# Last updated on: $Date: 2003-07-03 14:13:02 $
+# Last updated by: $Author: ar2 $
+# Last updated on: $Date: 2003-07-15 13:16:55 $
 #
 # see pod documentation (i.e. 'perldoc make_FTP_sites.pl') for more information.
 #
@@ -390,6 +390,8 @@ sub copy_homol_data{
   system("cp $blast_dir/blastx_ensembl.ace          $private_ftp/${release}_blastx_data.ace");
   system("cp $blast_dir/wormprot_motif_info.ace     $private_ftp/${release}_protein_motif_data.ace");
   system("cp $blast_dir/worm_brigprot_motif_info.ace    $private_ftp/${release}brig_protein_motif_data.ace");
+
+  system("cp $blast_dir/best_blastp_hits    $targetdir/$release/best_blastp_hits");
 
   system("/bin/gzip $private_ftp/*ace");
 
