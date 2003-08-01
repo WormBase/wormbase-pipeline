b8801949a0de873d710943bdeb8119f4f5565f16
krb <krb>
2003-08-01 15:56:45 +0000

    Finally got around to committing this unfinished script, which will convert Sequence ace file dumps into CDS objects

--- scripts/sequence2cds.pl
+++ scripts/sequence2cds.pl
@@ -1,4 +1,13 @@
-#!/usr/local/bin/perl5.6.1 -w
+#!/usr/local/bin/perl5.8.0 -w
+#
+# sequence2cds.pl
+# 
+# by Keith Bradnam                         
+#
+# A script to take ?Sequence objects and make ?CDS (and ?Pseudogene) objects
+#
+# Last updated by: $Author: krb $     
+# Last updated on: $Date: 2003-08-01 15:56:45 $     
 
 use strict;
 use Carp;
@@ -7,6 +16,7 @@ use Carp;
 # variables etc. #
 ##################
 
+# Needs ace file dump of sequence class as input
 #my $dir      = "/wormsrv2/wormbase/camace/";
 #my $file    = glob("$dir/camace_Sequence.ace");
 
@@ -18,8 +28,13 @@ my $ts = "-O \"(\\d{4}\-\\d{2}\-\\d{2}_\\d{2}:\\d{2}:\\d{2}\.?\\d?_\\S+|original
 
 open(IN,"<$ARGV[0]") || carp "Can't open input file\n";
 while(<IN>){
-  s/Structure\s+$ts\s+Subsequence\s+$ts/CDS_child/g;
-  s/Visible\s+$ts\s+Matching_Genomic/Matching_CDS/g;
+
+  # Misc tidy up of bits of models
+  s/From\s+$ts\s+Source_Exons/Source_exons/g;
+  s/From_Laboratory/From_laboratory/g;
+
+
+  # convert things which have CDS tag to ?CDS objects
   if (m/Properties\s+$ts\s+Coding\s+$ts\s+CDS/){
     # Convert to new CDS class
     s/^Sequence :/CDS :/;
@@ -29,16 +44,30 @@ while(<IN>){
 
     # Get rid of this line (now removed in camace)
     s/Properties\s+$ts\s+Status\s+$ts\s+Annotated\s+$ts\s+\d{4}-\d{2}-\d{2}\s+$ts\s//g;
-
-    # Get rid of Interpolated_gmap tag (will be succeeded by Chao-Kung's script
-    s/Properties\s+$ts\s+Status\s+$ts\s+Annotated\s+$ts\s+\d{4}-\d{2}-\d{2}\s+$ts\s//g;
-    s/Interpolated_gMap/Interpolated_map_position/g;
   }
-  # Misc tidy up of bits of models
-  s/From\s+$ts\s+Source_Exons/Source_exons/g;
-  s/From_Laboratory/From_laboratory/g;
 
-  print;
+  # Convert Sequences with Pseudogene tag into ?Pseudogene objects
+  elsif(m/Properties\s+$ts\s+Pseudogene/){
+    # Convert to new Pseudogene class
+    s/^Sequence :/Pseudogene :/;
+
+    # No need for Pseudogene tag now, but these are all Sequence objects so can
+    # set 'Type' tag to be 'Coding_pseudogene'
+    s/Properties\s+$ts\s+Pseudogene\s+$ts/Coding_pseudogene/;
+
+    # Need to add tags for SMap
+    s/Structure\s+$ts\s+From\s+$ts\s+Source/Sequence/;
+
+    
+    print;
+  }
+
+  # Make changes in Parent sequence objects that might link to CDS objects
+  else{
+    s/Structure\s+$ts\s+Subsequence\s+$ts/CDS_child/g;
+    s/Visible\s+$ts\s+Matching_Genomic/Matching_CDS/g;
+    
+  }
 
 }
 close(IN);
