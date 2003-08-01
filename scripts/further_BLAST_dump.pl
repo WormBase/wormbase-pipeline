a8510c3b150cb6af8df9416359e1237276143010
ar2 <ar2>
2003-08-01 10:11:51 +0000

    copy over brigprot_blastp_ensembl.ace to ensembl_dumps

--- scripts/further_BLAST_dump.pl
+++ scripts/further_BLAST_dump.pl
@@ -5,8 +5,8 @@
 #
 # Author: Chao-Kung CHen
 #
-# Last updated by: $Author: krb $                      
-# Last updated on: $Date: 2003-07-25 09:43:41 $        
+# Last updated by: $Author: ar2 $                      
+# Last updated on: $Date: 2003-08-01 10:11:51 $        
 
 system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/blastp_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
 system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/blastx_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
@@ -15,6 +15,7 @@ system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/swissproteins.ace wormsrv
 system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/tremblproteins.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
 system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/ipi_hits.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
 system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/best_blastp_hits wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/brigprot_blastp_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
 
 chdir "/wormsrv2/wormbase/ensembl_dumps";
 
