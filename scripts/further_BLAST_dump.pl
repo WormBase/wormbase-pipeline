18329fc83b998b25d4522ab5c4a6f066e9175f6d
krb <krb>
2003-09-30 15:09:43 +0000

    Upgraded Chao-Kung's script to test all system calls as it copies files across.  Also, this script now makes a 'BACKUP' directory in /wormsrv2/wormbase/ensembl_dumps/.  Existing files first get moved there before files are then copied across from acari.  Optionally we could blow this directory away afterwards if it has all been successful I guess

--- scripts/further_BLAST_dump.pl
+++ scripts/further_BLAST_dump.pl
@@ -1,26 +1,54 @@
 #!/usr/local/bin/perl5.8.0 -w
-
+#
 # further_BLAST_dump.pl
 #
+# script to copy files resulting from blast pipeline on acari to wormsrv2
 #
 # Author: Chao-Kung CHen
 #
-# Last updated by: $Author: ck1 $                      
-# Last updated on: $Date: 2003-09-18 13:01:19 $        
+# Last updated by: $Author: krb $                      
+# Last updated on: $Date: 2003-09-30 15:09:43 $        
 
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/blastp_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/blastx_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/wormprot_motif_info.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/worm_brigprot_motif_info.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/swissproteins.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/tremblproteins.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/ipi_hits.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/best_blastp_hits wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/best_blastp_hits_brigprot wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
-system("/usr/apps/bin/scp /acari/work2a/wormpipe/dumps/brigprot_blastp_ensembl.ace wormsrv2:/wormsrv2/wormbase/ensembl_dumps/");
+use strict;
+use lib "/wormsrv2/scripts/";
+use Wormbase;
 
-chdir "/wormsrv2/wormbase/ensembl_dumps";
+my $acari_dir    = "/acari/work2a/wormpipe/dumps";
+my $wormsrv2_dir = "/wormsrv2/wormbase/ensembl_dumps";
+my @files        = ("blastp_ensembl.ace","blastx_ensembl.ace","wormprot_motif_info.ace","worm_brigprot_motif_info.ace",
+		    "swissproteins.ace","tremblproteins.ace","ipi_hits.ace","best_blastp_hits","best_blastp_hits_brigprot",
+		    "brigprot_blastp_ensembl.ace");
 
-system("cat ipi_hits.ace fly.ace yeast.ace swissproteins.ace tremblproteins.ace brigpep.ace > ensembl_protein_info.ace");
+
+# make backup directory on wormsrv2
+chdir "$wormsrv2_dir";
+&run_command("mkdir $wormsrv2_dir/BACKUP") unless (-e "${wormsrv2_dir}/BACKUP");
+
+
+# backup each file first, then copy across new file
+foreach my $file (@files){
+  &run_command("mv -f $file ${wormsrv2_dir}/BACKUP/$file");
+  &run_command("/usr/apps/bin/scp ${acari_dir}/${file} wormsrv2:${wormsrv2_dir}/${file}");
+}
+
+
+# Also move big ensembl_protein file to BACKUP dir before making new one.
+&run_command("mv -f $wormsrv2_dir/ensembl_protein_info.ace $wormsrv2_dir/BACKUP/ensembl_protein_info.ace.backup");
+&run_command("cat ipi_hits.ace gadfly.ace yeast.ace swissproteins.ace tremblproteins.ace brigpep.ace > ensembl_protein_info.ace");
 
 exit(0);
+
+
+##################################################################################################################
+
+
+sub run_command{
+  my $command = shift;
+  my $status = system($command);
+  if($status != 0){
+    print "ERROR: $command failed\n";
+  }
+
+  # for optional further testing by calling subroutine
+  return($status);
+}
