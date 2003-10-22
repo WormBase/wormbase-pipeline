a0fc64b8527529235be5f4d7445d5520b62cec55
pad <pad>
2003-10-22 11:44:41 +0000

    Added a system call to backup the worm_brigprot MySQL data to the same location as the worm01 and wormprot data.

--- scripts/backup_mysql.pl
+++ scripts/backup_mysql.pl
@@ -2,7 +2,7 @@
 #
 # Author: Chao-Kung Chen
 # 2001-11-28
-# What it does: (1) Backup MySQL BlastP and BlastX data to ~wormpub/MYSQL_DUMPS
+# What it does: (1) Backup MySQL BlastP, BlastX and worm_brigprot to ~wormpub/MYSQL_DUMPS
 #               (2) Delete old blast data   
 
 use strict;
@@ -17,6 +17,7 @@ print "Backup started at $start\n\n";
 
 my $worm01="worm01_$date";
 my $wormprot="wormprot_$date";
+my $worm_brigprot="worm_brigprot_$date";
 
 # password is not included for security reasons
 
@@ -25,6 +26,9 @@ system("mysqldump -h ecs1f -u wormadmin  --opt -p worm01 > /nfs/disk100/wormpub/
 
 print "Backup Blast_P homols...\n\n";
 system("mysqldump -h ecs1f -u wormadmin --opt -p wormprot > /nfs/disk100/wormpub/MYSQL_DUMPS/$wormprot");
+
+print "Backup worm_brigprot homols...\n\n";
+system("mysqldump -h ecs1f -u wormadmin --opt -p worm_brigprot > /nfs/disk100/wormpub/MYSQL_DUMPS/$worm_brigprot");
 
 my @files =`ls /nfs/disk100/wormpub/MYSQL_DUMPS`;
 foreach (@files){
