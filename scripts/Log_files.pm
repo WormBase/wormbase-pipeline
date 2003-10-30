fffd6ce1fa5f45db57b3fc7238d216e69bfdbec6
krb <krb>
2003-10-30 10:20:31 +0000

    Fixed bug (spammable???) that was writing log files to /wormsrv2/autoace/logs rather than /wormsrv2/logs
    Added a line of code to also generate files in /wormsrv2/logs/history which it wasn't doing
    (all build scripts should write empty here as well to create a record of each time we run a script)
    
    Reset 'use lib' to just use /wormsrv2/scripts
    
    Keith

--- scripts/Log_files.pm
+++ scripts/Log_files.pm
@@ -17,8 +17,7 @@
 package Log_files;
 
 
-use lib -e "/wormsrv2/scripts"  ? "/wormsrv2/scripts"
-  : glob("~ar2/wormbase/scripts");
+use lib "/wormsrv2/scripts";
 use Wormbase;
 use Carp;
 
@@ -67,9 +66,12 @@ sub make_build_log
     my $ver = &Wormbase::get_wormbase_version;
     $ver = $debug unless $ver;  #if wormsrv2 not accessible then Wormbase module wont get version
     my $filename;
-    $0 =~ /([^\/]*$)/ ? $filename = $0 :  $filename = $1 ; # get filename (sometimes $0 includes full path if not run from its dir )
+    $0 =~ m/([^\/]*$)/ ? $filename = $0 :  $filename = $1 ; # get filename (sometimes $0 includes full path if not run from its dir )
 
-    my $path = "/wormsrv2/autoace/logs";
+    # Create history logfile for script activity analysis
+    $0 =~ m/\/*([^\/]+)$/; system ("touch /wormsrv2/logs/history/$1.`date +%y%m%d`");     
+
+    my $path = "/wormsrv2/logs";
     if( defined $debug ) {
       $path = "/tmp/logs" ;
       system("mkdir $path") unless (-e $path);
@@ -78,7 +80,7 @@ sub make_build_log
     my $log_file = "$path/$filename".".$ver.".$$;
     my $log;
     open($log,">$log_file") or croak "cant open file $log_file : $!";
-    print $log "WS$ver Build script : $filename \n";
+    print $log "WS$ver Build script : $filename \n\n";
     print $log "Started at ",&Wormbase::runtime,"\n";
     print $log "-----------------------------------\n\n";
 
