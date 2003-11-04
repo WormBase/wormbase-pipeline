967d3cd63e8da0a1c63c8dd1af63d3ea9c1a34ef
krb <krb>
2003-11-04 17:05:46 +0000

    Small improvements to script

--- scripts/sequence2cds.pl
+++ scripts/sequence2cds.pl
@@ -4,10 +4,10 @@
 # 
 # by Keith Bradnam                         
 #
-# A script to take ?Sequence objects and make ?CDS (and ?Pseudogene) objects
+# A script to take ?Sequence objects and make ?CDS objects
 #
 # Last updated by: $Author: krb $     
-# Last updated on: $Date: 2003-08-11 14:12:49 $     
+# Last updated on: $Date: 2003-11-04 17:05:46 $     
 
 use strict;
 use Carp;
@@ -18,135 +18,72 @@ use Getopt::Long;
 # variables etc. #
 ##################
 
-my ($camace,$stlace, $db, $dbdir);
+my $input; # where input file is located
+my $outdir;   # path to output directory
 
 # pattern to match timestamps
 my $ts = "-O \"(\\d{4}\-\\d{2}\-\\d{2}_\\d{2}:\\d{2}:\\d{2}\.?\\d?_\\S+|original)\"";
 
-# store pseudogene names in hash
-my %pseudogenes;
 
-GetOptions ("camace"      => \$camace,
-            "stlace"      => \$stlace);
+GetOptions ("input=s"   => \$input,
+	    "outdir=s"  => \$outdir);
 
 
-if($camace){
-  $db = "camace";
-  $dbdir = "/wormsrv2/wormbase/camace/";
+if(!$outdir){
+  $outdir = "/nfs/disk100/wormpub/DATABASES/TEST_DBs/cds_ace/acefiles";
 }
 
-if($stlace){
-  $db = "stlace";
-  $dbdir = "/wormsrv2/wormbase/stlace/";
-}
+# Open output/input streams
+open(IN,"<$input") || carp "Can't open input file\n";
+open(SEQ,">$outdir/camace_new_Sequence.ace") || croak "Couldn't open output file\n";
+open(CDS,">$outdir/camace_CDS.ace") || croak "Couldn't open output file\n";
 
 
 # reset input line separator
 $/ = "";
 
 
-
-# Two output files, one for pseudogenes, one for everything else.
-open(OUT,">${dbdir}krb_${db}_Sequence.ace") || croak "Couldn't open output file\n";
-open(PSEUDOGENES,">${dbdir}${db}_Pseudogene.ace") || croak "Couldn't open pseudogenes file\n";
-
-
-open(IN,"<${dbdir}${db}_Sequence.ace") || carp "Can't open input file\n";
-
 while(<IN>){
 
   # Misc tidy up of bits of models
   s/From\s+$ts\s+Source_Exons/Source_exons/g;
   s/From_Laboratory/From_laboratory/g;
+  s/From_Database/From_database/g;
+  s/From_Author/From_author/g;
 
 
   # convert things which have CDS tag to ?CDS objects
-#  if (m/Properties\s+$ts\s+Coding\s+$ts\s+CDS/){
+  if (m/Properties\s+$ts\s+Coding\s+$ts\s+CDS/){
     # Convert to new CDS class
-#    s/^Sequence :/CDS :/;
-
-    # Need to add tags for SMap
-#    s/Structure\s+$ts\s+From\s+$ts\s+Source/Sequence/;
-
-    # Get rid of this line (now removed in camace)
-#    s/Properties\s+$ts\s+Status\s+$ts\s+Annotated\s+$ts\s+\d{4}-\d{2}-\d{2}\s+$ts\s//g;
-#  }
-
-  # Convert Sequences with Pseudogene tag into ?Pseudogene objects
-  if(m/Properties\s+$ts\s+Pseudogene/){
- 
-    # Convert to new Pseudogene class
-    s/^Sequence :/Pseudogene :/;
-
-    # Track pseudogene names for changing parent ?Sequence objects later
-    m/^Pseudogene : (\".*?\")/;
-    my $key = $1;
-
-    #Find Parent sequence object (might be superlink)    
-    m/From\s+$ts\s+Source\s+$ts\s+(\".*?\")\s+$ts/;    
-    my $parent = $3;
-
-    $pseudogenes{$key} = $parent;
-
-    # No need for Pseudogene tag now, but these are all Sequence objects so can
-    # set 'Type' tag to be 'Coding_pseudogene'
-    s/Properties\s+$ts\s+Pseudogene\s+$ts/Coding_pseudogene/;
+    s/^Sequence :/CDS :/;
 
     # Need to add tags for SMap
     s/Structure\s+$ts\s+From\s+$ts\s+Source/Sequence/;
 
+    # Get rid of this line (now removed in camace)
+    s/Properties\s+$ts\s+Status\s+$ts\s+Annotated\s+$ts\s+\d{4}-\d{2}-\d{2}\s+$ts\s//g;
     
-    print PSEUDOGENES;
+    print CDS;
   }
 
 
   # Make changes in Parent sequence objects that might link to CDS objects
   else{
-#    s/Structure\s+$ts\s+Subsequence\s+$ts/CDS_child/g;
-#    s/Visible\s+$ts\s+Matching_Genomic/Matching_CDS/g;
-    print OUT;
+    s/Structure\s+$ts\s+Subsequence\s+$ts/CDS/g;
+    s/Visible\s+$ts\s+Matching_Genomic/Matching_CDS/g;
+    print SEQ;
   }
 
 }
 
 close(IN);
-close(OUT);
-close(PSEUDOGENES);
+close(SEQ);
+close(CDS);
 
-
-# Now need to re-read the Sequence output file to replace 'Subsequence' with
-# 'Pseudogene' for those things which will be pseudogenes
-
-# read from first dumped file
-open(IN,"<${dbdir}krb_${db}_Sequence.ace") || croak "Can't open input file\n";
-
-# Need new output file
-open(OUT,">${dbdir}krb2_${db}_Sequence.ace") || croak "Couldn't open output file\n";
-
-# Second output file to generate fix file for camace/stlace
-open(OUT2,">${dbdir}${db}_fix_file_for_pseudogenes.ace") || croak "Couldn't open output file\n";
 
 # reset input line separator
 $/ = "\n";
 
-
-while(<IN>){
-
-  if(m/^Structure\s+.*\s+Subsequence\s+$ts\s+(\".*\")\s+$ts\s+\d+/){
-    my $subsequence = $2;
-    s/Subsequence/Pseudogene/ if ($pseudogenes{$subsequence});           
-    print OUT2 "\nSequence : $pseudogenes{$subsequence}\n$_" if ($pseudogenes{$subsequence});
-  }
-
-  print OUT;
-}
-
-
-close(IN);
-close(OUT);
-close(OUT2);
-
-system("mv ${dbdir}krb2_${db}_Sequence.ace ${dbdir}${db}_Sequence.ace") && croak "Couldn't overwrite file\n";
 
 exit(0);
 
