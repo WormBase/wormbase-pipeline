18de0c7d3fc1e8021eb90767e1775fa6baedc4e9
dl1 <dl1>
2003-11-05 10:59:20 +0000

    added -all option

--- scripts/transcriptmasker.pl
+++ scripts/transcriptmasker.pl
@@ -8,7 +8,7 @@
 # 031023 dl1
 
 # Last edited by: $Author: dl1 $
-# Last edited on: $Date: 2003-10-31 16:29:45 $
+# Last edited on: $Date: 2003-11-05 10:59:20 $
 
 #################################################################################
 # Initialise variables                                                          #
@@ -35,8 +35,10 @@ my $help;               # Help/Usage page
 my $mrna;               # mRNA data
 my $est;                # EST data
 my $ost;                # OST data
+my $all;                # all of the above
 
 GetOptions (
+	    "all"            => \$all,
 	    "mrna"           => \$mrna,
 	    "est"            => \$est,
 	    "ost"            => \$ost,
@@ -66,7 +68,6 @@ our %datafiles = (
 		  );
 
 # transcript accessions to names from a hash
-# 
 # !! need to deal with making this .dat file etc 
 #
 
@@ -90,100 +91,113 @@ my $seqlength;                                        #
 
 
 # which data file to parse
-my $data;
-$data = $datafiles{mrna} if ($mrna);
-$data = $datafiles{est}  if ($est);
-$data = $datafiles{ost}  if ($ost);
 
-# connect to database
+&masksequence($datafiles{mrna}) if ($mrna || $all);
+&masksequence($datafiles{est})  if ($est || $all);
+&masksequence($datafiles{ost})  if ($ost || $all);
 
-print  "Opening database ..\n" if ($debug);
-my $db = Ace->connect(-path=>$dbdir,
-                      -program =>$tace) || do { print "Connection failure: ",Ace->error; die();};
+close LOG;
+exit(0);
 
-# set input record seperator
 
-$/ = ">";
+###############################################################
 
-# assign output file
 
-open (OUTPUT, ">${data}.masked") || die "ERROR: Can't open output file: '${data}.masked'";
-
-# input file loop structure
-
-open (INPUT, "<$data")     || die "ERROR: Can't open input file: '$data'";
-while (<INPUT>) {
-    chomp;
-    next if ($_ eq "");                 # catch empty lines
+sub masksequence {
+    my $data = shift;
     
-    if (/^(\S+)\s+\S+.+\n/) {           # deal with accessions {$acc} and WormBase internal names {$id}
-	$acc = $1;
-	$id  = $EST_name{$acc};
-    }
-    $seq = "$'";                        # assign the rest of the string to $seq
-    $seq =~ s/[^gatcn]//g;              # remove non-gatcn characters (i.e. newlines)
-    $seqmasked = $seq;                  # copy sequence to masked file and
-    $seqlength = length ($seq);         # calculate the length of the sequence
+  
+    # connect to database
 
-    print "// -> Parsing $acc [$id]\n" if ($verbose);
-    
-    # ERROR if we haven't added this accession to the database yet.
-    # Use the accession for the mean time. 
-    
-    if ($id eq "") {
-	print "// ERROR: No accession-id connection for this sequence\n" if ($verbose || $debug);
-	$EST_name{$acc} = $acc;
-	$id = $acc;
-    }
-    
-    # fetch the sequence object from the database. push the feature_data objects to memory
-    
-    my $obj = $db->fetch(Sequence=>$id);
-    if (!defined ($obj)) {
-	print "ERROR: Could not fetch sequence $id \n" if ($debug);
-	next;
-    }
-    
-    @features = $obj->Feature_data(1);
-    if ( scalar (@features) == 0) {
-	print "ERROR: No Features to parse \n" if ($debug);
+    print  "Opening database for masking $data ..\n" if ($debug);
+    my $db = Ace->connect(-path=>$dbdir,
+                          -program =>$tace) || do { print "Connection failure: ",Ace->error; die();};
+
+    # set input record seperator
+
+    $/ = ">";
+
+    # assign output file
+
+    if ($debug) {
+	open (OUTPUT, ">${data}.testmasked") || die "ERROR: Can't open output file: '${data}.testmasked'";
     }
     else {
-	foreach $feature (@features) {
-	    print "// parse $feature\n" if ($verbose);
-	    
-	    $type  = $obj->Feature_data->Feature(1);         # Feature type (e.g. SL1,SL2,polyA)
-	    $start = $obj->Feature_data->Feature(2);         # start coord
-	    $stop  = $obj->Feature_data->Feature(3);         # stop coord
-	    
-	    $cut_to     = $start - 1;                        # manipulations for clipping 
-	    $cut_from   = $stop;
-	    $cut_length = $stop - $start + 1;
-	    
-	    if ($cut_to < 0 ) {$cut_to = 0;}                 # fudge to ensure non-negative clipping coords
-	    
-	    print "$acc [$id]: '$type' $start -> $stop [$cut_to : $cut_from ($cut_length)]\n" if ($debug);
-	    print "// # $acc [$id] $type:" . (substr($seq,$cut_to,$cut_length)) . " [$start - $stop]\n\n";
-       		$newseq = (substr($seqmasked,0,$cut_to))  . ('n' x $cut_length)  . (substr($seqmasked,$cut_from));
-	    $seqmasked = $newseq;
-	    
-	}
+	open (OUTPUT, ">${data}.masked") || die "ERROR: Can't open output file: '${data}.masked'";
     }
+
+    # input file loop structure
+
+    open (INPUT, "<$data")     || die "ERROR: Can't open input file: '$data'";
+    while (<INPUT>) {
+	chomp;
+	next if ($_ eq "");                 # catch empty lines
+	
+	if (/^(\S+)\s+\S+.+\n/) {           # deal with accessions {$acc} and WormBase internal names {$id}
+	    $acc = $1;
+	    $id  = $EST_name{$acc};
+	}
+	$seq = "$'";                        # assign the rest of the string to $seq
+	$seq =~ s/[^gatcn]//g;              # remove non-gatcn characters (i.e. newlines)
+	$seqmasked = $seq;                  # copy sequence to masked file and
+	$seqlength = length ($seq);         # calculate the length of the sequence
+	
+	print "// -> Parsing $acc [$id]\n" if ($verbose);
+	
+	# ERROR if we haven't added this accession to the database yet.
+	# Use the accession for the mean time. 
+	
+	if ($id eq "") {
+	    print "// ERROR: No accession-id connection for this sequence\n" if ($verbose || $debug);
+	    $EST_name{$acc} = $acc;
+	    $id = $acc;
+	}
     
-    print OUTPUT ">$acc $id\n$seqmasked\n";
+	# fetch the sequence object from the database. push the feature_data objects to memory
+	
+	my $obj = $db->fetch(Sequence=>$id);
+	if (!defined ($obj)) {
+	    print "ERROR: Could not fetch sequence $id \n" if ($debug);
+	    next;
+	}
+	
+	@features = $obj->Feature_data(1);
+	if ( scalar (@features) == 0) {
+	    print "ERROR: No Features to parse \n" if ($debug);
+	}
+	else {
+	    foreach $feature (@features) {
+		print "// parse $feature\n" if ($verbose);
+		
+		$type  = $obj->Feature_data->Feature(1);         # Feature type (e.g. SL1,SL2,polyA)
+		$start = $obj->Feature_data->Feature(2);         # start coord
+		$stop  = $obj->Feature_data->Feature(3);         # stop coord
+		
+		$cut_to     = $start - 1;                        # manipulations for clipping 
+		$cut_from   = $stop;
+		$cut_length = $stop - $start + 1;
+		
+		if ($cut_to < 0 ) {$cut_to = 0;}                 # fudge to ensure non-negative clipping coords
+		
+		print "$acc [$id]: '$type' $start -> $stop [$cut_to : $cut_from ($cut_length)]\n" if ($debug);
+		print "// # $acc [$id] $type:" . (substr($seq,$cut_to,$cut_length)) . " [$start - $stop]\n\n" if ($verbose);
+		    $newseq = (substr($seqmasked,0,$cut_to))  . ('n' x $cut_length)  . (substr($seqmasked,$cut_from));
+		$seqmasked = $newseq;
+	    } 
+	}
+	
+	print OUTPUT ">$acc $id\n$seqmasked\n";
+	
+	# close object
+	$obj->DESTROY();
+	
+    }
+    close INPUT;
+    $/ = "\n";
+
     
-    # close object
-    $obj->DESTROY();
-    
+    close OUTPUT;
 }
-close INPUT;
-$/ = "\n";
-
-
-close OUTPUT;
-close LOG;
-
-exit(0);
 
 
 ###############################################################
@@ -193,7 +207,7 @@ sub recreate_hashes {
     
     open (FH, "<$hash") or die "Can't open file $hash : $!";
     undef $/;
-    $data = <FH>;
+    my $data = <FH>;
     eval $data;
     die if $@;
     $/ = "\n";
@@ -269,6 +283,8 @@ transcriptmasker.pl mandatory arguments:
 transcriptmasker.pl OPTIONAL arguments:
 
 =over 4
+
+=item -all, process EST, mRNA & OST datafiles 
 
 =item -est, process EST datafile 
 
