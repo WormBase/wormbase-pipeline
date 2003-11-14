6fb00737b307018acf95c5fe6d1fadf8390a5415
wormpub <wormpub>
2003-11-14 15:53:48 +0000

    added the brigprot dump and options to select individual database to backup

--- scripts/BLAST_scripts/backup_mysql.pl
+++ scripts/BLAST_scripts/backup_mysql.pl
@@ -8,39 +8,49 @@
 use strict;
 use Getopt::Long;
 
-my $dbpass;
+my ($dbpass, $dump_worm01, $dump_wormprot, $dump_brigprot);
 
-GetOptions ( 'dbpass=s' => \$dbpass );
+GetOptions ( 
+	    'dbpass=s' => \$dbpass,
+	    'worm01'   => \$dump_worm01,
+	    'wormprot' => \$dump_wormprot,
+	    'worm_brigprot' => \$dump_brigprot,
+	   );
 
 die "I need the password, please - try something like - \nbackup_mysql.pl -pass  xxxxxxxx\n" unless $dbpass;
+
+unless ( defined $dump_worm01 or defined $dump_wormprot or defined $dump_brigprot ) {
+  $dump_worm01 = 1; 
+  $dump_wormprot = 1;
+  $dump_brigprot = 1;
+}
 
 my $date = `date +%y%m%d`;
 chomp $date;
 
 my $start=`date +%H:%M:%S`; chomp $start;
+print "Backup started at $start\n\n";
 
 my $worm01="worm01_$date";
 my $wormprot="wormprot_$date";
+my $worm_brigprot="worm_brigprot_$date";
 
 # password is not included for security reasons
 
-print "Backup Blast_X homols...\n\n";
-system("mysqldump -h ecs1f -u wormadmin --opt -p$dbpass worm01 > /nfs/disk100/wormpub/MYSQL_DUMPS/$worm01");
-
-print "Backup Blast_P homols...\n\n";
-system("mysqldump -h ecs1f -u wormadmin --opt -p$dbpass wormprot > /nfs/disk100/wormpub/MYSQL_DUMPS/$wormprot");
-
-my @files =`ls /nfs/disk100/wormpub/MYSQL_DUMPS`;
-foreach (@files){
-  chomp;
-  if ($_ ne "worm.+_$date"){ 
-   system ("rm -rf $_");
-  }
-}
+&dump_it("worm01",$worm01) if $dump_worm01;
+&dump_it("wormprot",$wormprot) if $dump_wormprot;
+&dump_it("worm_brigprot",$worm_brigprot) if $dump_brigprot;
 
 my $end=`date +%H:%M:%S`; chomp $end;
 
-print "\nBackup started at $start\n";
 print "Backup finished at $end\n";
 
 
+sub dump_it
+  {
+    my $database = shift;
+    my $dump_file = shift;
+
+    print "Backup $database to $dump_file...\n\n";
+    system("mysqldump -h ecs1f -u wormadmin --opt -p$dbpass $database > /nfs/disk100/wormpub/MYSQL_DUMPS/$dump_file");
+}
