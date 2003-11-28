7c5503c13d67110b861d92ac7e9b8891c4334e9f
krb <krb>
2003-11-28 11:31:38 +0000

    This script now has a set list of user names which it uses to loop through for running camcheck.  Adding one extra line to do this meant, that I could then remove about 6 lines of code.

--- scripts/check_split_camaces.pl
+++ scripts/check_split_camaces.pl
@@ -1,34 +1,29 @@
-#!/usr/local/bin/perl5.6.1 -w
+#!/usr/local/bin/perl5.8.0 -w
 #
 # check_split_camaces.pl
 #
 # Cronjob integrity check controls for split camace databases.
 #
 # Last updated by: $Author: krb $
-# Last updated on: $Date: 2003-10-29 11:16:37 $
+# Last updated on: $Date: 2003-11-28 11:31:38 $
 
 use strict;
 use lib "/wormsrv2/scripts/";
 use Wormbase;
-my $rundate = `date +%y%m%d`; chomp $rundate;
+my $rundate = &rundate;
 my $runtime = &runtime;
 
 my $wormpub_dir = glob("~wormpub");
 my $log = "/wormsrv2/logs/check_split_camaces.$rundate.$$";
-open (LOG,">$log") or &mail_maintainer("LOG failed in check_split_camaces.pl","ar2\@sanger.ac.uk");
+open (LOG,">$log") or &mail_maintainer("LOG failed in check_split_camaces.pl","wormbase\@sanger.ac.uk");
 
-opendir (DIR,"$wormpub_dir") or die "cant read $wormpub_dir\n";
-my @ll =  readdir (DIR);
-close DIR;
+my @users = ("ar2", "dl1", "pad");
 
-my @databases;
-
-foreach my $database (@ll) {
-  if( $database =~ /camace_(\w+)/ ) {
-    if(-e "$wormpub_dir/$database/database/ACEDB.wrm" ) {
-      system("/wormsrv2/scripts/camcheck.pl -s $wormpub_dir/$database -l -e $1") unless ("$1" eq "orig");
-    }
+foreach my $name (@users) {
+  if(-e "$wormpub_dir/camace_${name}/database/ACEDB.wrm" ) {
+      system("/wormsrv2/scripts/camcheck.pl -s $wormpub_dir/camace_${name} -l -e $name");
   }
+
 }
 
 close LOG;
