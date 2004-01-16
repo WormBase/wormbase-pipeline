f177c940464850f61d5766bd16325c9f43b4f1a5
ar2 <ar2>
2004-01-16 12:00:44 +0000

    Updated,properly to new database names

--- scripts/BLAST_scripts/backup_mysql.pl
+++ scripts/BLAST_scripts/backup_mysql.pl
@@ -19,7 +19,7 @@ GetOptions (
 
 die "I need the password, please - try something like - \nbackup_mysql.pl -dbpass  xxxxxxxx\n" unless $dbpass;
 
-unless ( defined $dump_worm01 or defined $dump_wormprot or defined $dump_brigprot ) {
+unless ( defined $dump_worm_dna or defined $dump_wormpep or defined $dump_brigpep ) {
   $dump_worm_dna = 1; 
   $dump_worm_pep = 1;
   $dump_brigpep = 1;
