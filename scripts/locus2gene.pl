515ef6e977b5d2a5197368bb7fd2409beab34aee
krb <krb>
2004-02-27 17:42:43 +0000

    New subroutines to process Locus names in ?Multi_pt_data, ?2_point_data, and ?Pos_neg_data
    It all works, but just need to finalise the processing of Locus names buried in the recursive
    Multi_counts hash.

--- scripts/locus2gene.pl
+++ scripts/locus2gene.pl
@@ -7,7 +7,7 @@
 # A script to convert ?Locus objects into the new ?Gene object
 #
 # Last updated by: $Author: krb $     
-# Last updated on: $Date: 2004-02-10 13:32:09 $   
+# Last updated on: $Date: 2004-02-27 17:42:43 $   
 
 use strict;
 use lib -e "/wormsrv2/scripts" ? "/wormsrv2/scripts" : $ENV{'CVS_DIR'};
@@ -21,13 +21,21 @@ use Getopt::Long;
 ################################################################
 
 my $file; # where input file is located
-my $post; # option for post-processing information in target database
+my $post_process1; # option to fix locus names created by Hides_under tags
+my $post_process2; # option for fixing one way links to Locus from 2_point_data
+my $post_process3; # option for fixing one way links to Locus class from multi_pt_data
+my $post_process4; # option for fixing one way links to Locus class from Pos_neg_data
+my $post_process5; # option for fixing one way links to Locus class from Results hash of multi_pt_data
 
-GetOptions ("file=s"       => \$file,
-	    "post_process" => \$post);
+GetOptions ("file=s" => \$file,
+	    "fix1"   => \$post_process1,
+	    "fix2"   => \$post_process2,
+	    "fix3"   => \$post_process3,
+	    "fix4"   => \$post_process4,
+	    "fix5"   => \$post_process5);
 
 
-if($post){
+if($post_process1){
   # This is just to deal with Gene objects created by Hide_under/Representative_for tags.  These are
   # objects with CGC style names, so just need to look up what the correct gene ID is in the Gene_name
   # class and make a simple fix file
@@ -61,9 +69,260 @@ if($post){
 }
 
 
+############################################################################
+
+if($post_process2){
+  # This is just to deal with Gene objects created by one-way links to Locus from the 2_point_data clas.  
+  # These need to be changed to use the Gene_1 and Gene_2 tags rather than Locus_1 and Locus_2 and also to use 
+  # correct Gene ID
+
+  # After loading the fix file, you still then need to go into geneace and remove any Gene objects
+  # that have CGC-style names.  Believe me, if you have loaded the fix file you can then safely
+  # remove any Gene object which isn't called WBGene*
+
+  open(OUT, ">fix2.ace") || die "Can't create output file\n";
+  my $db_path = "/nfs/disk100/wormpub/DATABASES/TEST_DBs/geneace_gene_model";
+  my $db = Ace->connect(-path  => $db_path) || do { print "Connection failure: ",Ace->error; die();};
+
+  # This query gets all 2_point_data objects which link to Locus objects using old CGC style names,
+  my $query = "Find 2_point_data * WHERE (Locus_1 OR Locus_2)";
+  push(my @twopoints, $db->find($query));
+  foreach my $twopoint (@twopoints){
+    print "$twopoint\n";
+    my $locus_1 = $twopoint->Locus_1;
+    my $locus_2 = $twopoint->Locus_2;
+
+    my $extra_1 = $twopoint->Locus_1(2);
+    my $extra_2 = $twopoint->Locus_2(2);
+
+    # write ace file according to whether one or two loci were present in the 2_point object
+    if ($locus_1){
+      next if (($locus_1 eq "act-123") || ($locus_1 =~ m/\w+P\d+/)); # exeception for Polymorphisms and Gene_clusters
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus_1");
+      my $gene_id = $obj->Public_name_for;      
+      print OUT "2_point_data : \"$twopoint\"\n-D Locus_1 $locus_1\n\n";
+      if($extra_1){
+	print OUT "2_point_data : \"$twopoint\"\nGene_1 $gene_id $extra_1\n\n";
+      }
+      else{
+	print OUT "2_point_data : \"$twopoint\"\nGene_1 $gene_id\n\n";
+      }
+    }
+    if ($locus_2){
+      next if (($locus_2 eq "act-123") || ($locus_2 =~ m/\w+P\d+/)); # exeception for Polymorphisms and Gene_clusters
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus_2");
+      my $gene_id = $obj->Public_name_for;      
+      if($extra_2){
+	print OUT "2_point_data : \"$twopoint\"\nGene_2 $gene_id $extra_2\n\n";
+      }
+      else{
+	print OUT "2_point_data : \"$twopoint\"\nGene_2 $gene_id\n\n";
+      }
+      print OUT "2_point_data : \"$twopoint\"\n-D Locus_2 $locus_2\n\n";
+      print OUT "2_point_data : \"$twopoint\"\nGene_2 $gene_id\n\n";
+    }
+
+  }
+
+  $db->close;  
+  close(OUT);
+}
+
+
+############################################################################
+
+if($post_process3){
+  # This is just to deal with Gene objects created by one-way links to Locus from the Multi_pt_data class.  
+  # These need to be changed to use the Gene_A and Gene_B tags rather than Locus_A and Locus_B and also to use 
+  # correct Gene ID.  Also there is a separate link to Locus objects using the Locus tag
+
+  # After loading the fix file, you still then need to go into geneace and remove any Gene objects
+  # that have CGC-style names.  Believe me, if you have loaded the fix file you can then safely
+  # remove any Gene object which isn't called WBGene*
+
+  open(OUT, ">fix3.ace") || die "Can't create output file\n";
+  my $db_path = "/nfs/disk100/wormpub/DATABASES/TEST_DBs/geneace_gene_model";
+  my $db = Ace->connect(-path  => $db_path) || do { print "Connection failure: ",Ace->error; die();};
+
+  # This query gets all Multi_pt_data objects which link to Locus objects using old CGC style names,
+  my $query = "Find Multi_pt_data * WHERE (Locus OR Locus_1 OR Locus_2)";
+  push(my @multipoints, $db->find($query));
+  foreach my $multipoint (@multipoints){
+    print "$multipoint\n";
+    my $locus_A = $multipoint->Locus_A;
+    my $locus_B = $multipoint->Locus_B;
+    my $locus   = $multipoint->Locus;
+
+    my $extra_1 = $multipoint->Locus_A(2);
+    my $extra_2 = $multipoint->Locus_B(2);
+    my $extra_3 = $multipoint->Locus(2);
+
+    # write ace file according to whether Locus, Locus_A, and Locus_B tags  were present in the 
+    # Multi_pt_data object
+
+    if ($locus_A){
+      next if (($locus_A eq "act-123") || ($locus_A =~ m/\w+P\d+/)); # exeception for Polymorphisms and Gene clusters
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus_A");
+      my $gene_id = $obj->Public_name_for;      
+
+      # remove old data
+      print OUT "Multi_pt_data : \"$multipoint\"\n-D Locus_A $locus_A\n\n";
+
+      # add new data
+      if($extra_1){
+	print OUT "Multi_pt_data : \"$multipoint\"\nGene_A $gene_id $extra_1\n\n";
+      }
+      else{
+	print OUT "Multi_pt_data : \"$multipoint\"\nGene_A $gene_id\n\n";
+      }
+    }
+
+    if ($locus_B){
+      next if (($locus_B eq "act-123") || ($locus_B =~ m/\w+P\d+/)); # exeception for Polymorphisms and Gene clusters
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus_B");
+      my $gene_id = $obj->Public_name_for;      
+
+      # delete old data
+      print OUT "Multi_pt_data : \"$multipoint\"\n-D Locus_B $locus_B\n\n";
+
+      # add new data
+      if($extra_2){
+	print OUT "Multi_pt_data : \"$multipoint\"\nGene_B $gene_id $extra_2\n\n";
+      }
+      else{
+	print OUT "Multi_pt_data : \"$multipoint\"\nGene_B $gene_id\n\n";
+      }
+    }
+
+    if ($locus){
+      next if (($locus eq "act-123") || ($locus =~ m/\w+P\d+/) || ($locus eq "RW#L17")); # exeception as not a Gene
+      next if ($locus eq "TCPAR1"); # exeception as not a Gene
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus");
+      my $gene_id = $obj->Public_name_for;      
+      # delete old link
+      print OUT "Multi_pt_data : \"$multipoint\"\n-D Locus $locus\n\n";
+
+      # add new data
+      if($extra_3){
+	print OUT "Multi_pt_data : \"$multipoint\"\nGene $gene_id $extra_3\n\n";
+      }
+      else{
+	print OUT "Multi_pt_data : \"$multipoint\"\nGene $gene_id\n\n";
+      }
+    }
+
+
+  }
+
+  $db->close;  
+  close(OUT);
+}
+
+
+############################################################################
+
+if($post_process4){
+  # This is just to deal with Gene objects created by one-way links to Locus from the Pos_neg_data class.  
+  # These need to be changed to use the Gene_1 and Gene_2 tags rather than Locus_1 and Locus_2 and also to use 
+  # correct Gene ID
+
+  # After loading the fix file, you still then need to go into geneace and remove any Gene objects
+  # that have CGC-style names.  Believe me, if you have loaded the fix file you can then safely
+  # remove any Gene object which isn't called WBGene*
+
+  open(OUT, ">fix4.ace") || die "Can't create output file\n";
+  my $db_path = "/nfs/disk100/wormpub/DATABASES/TEST_DBs/geneace_gene_model";
+  my $db = Ace->connect(-path  => $db_path) || do { print "Connection failure: ",Ace->error; die();};
+
+  # This query gets all 2_point_data objects which link to Locus objects using old CGC style names,
+  my $query = "Find Pos_neg_data * WHERE (Locus_1 OR Locus_2)";
+  push(my @pos_negs, $db->find($query));
+  foreach my $pos_neg (@pos_negs){
+    print "$pos_neg\n";
+    my $locus_1 = $pos_neg->Locus_1;
+    my $locus_2 = $pos_neg->Locus_2;
+
+    my $extra_1 = $pos_neg->Locus_1(2);
+    my $extra_2 = $pos_neg->Locus_2(2);
+
+    # write ace file according to whether one or two loci were present in the 2_point object
+    if ($locus_1){
+      next if (($locus_1 eq "act-123") || ($locus_1 =~ m/\w+P\d+/)); # exeception for Polymorphisms and Gene_clusters
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus_1");
+      my $gene_id = $obj->Public_name_for;      
+      print OUT "Pos_neg_data : \"$pos_neg\"\n-D Locus_1 $locus_1\n\n";
+      if($extra_1){
+	print OUT "Pos_neg_data : \"$pos_neg\"\nGene_1 $gene_id $extra_1\n\n";
+      }
+      else{
+	print OUT "Pos_neg_data : \"$pos_neg\"\nGene_1 $gene_id\n\n";
+      }
+    }
+    if ($locus_2){
+      next if (($locus_2 eq "act-123") || ($locus_2 =~ m/\w+P\d+/)); # exeception for Polymorphisms and Gene_clusters
+
+      my ($obj) = $db->fetch(-class=>'Gene_name',-name=>"$locus_2");
+      my $gene_id = $obj->Public_name_for;      
+      if($extra_2){
+	print OUT "Pos_neg_data : \"$pos_neg\"\nGene_2 $gene_id $extra_2\n\n";
+      }
+      else{
+	print OUT "Pos_neg_data : \"$pos_neg\"\nGene_2 $gene_id\n\n";
+      }
+      print OUT "Pos_neg_data : \"$pos_neg\"\n-D Locus_2 $locus_2\n\n";
+      print OUT "Pos_neg_data : \"$pos_neg\"\nGene_2 $gene_id\n\n";
+    }
+
+  }
+
+  $db->close;  
+  close(OUT);
+}
+
+
+############################################################################
+
+if($post_process5){
+  # This is just to deal with Gene objects created by one-way links to Locus from the Multi_pt_data class.  
+  # This is to look up Locus names that might be in the Multi_counts hash under the Results tag
+
+  # After loading the fix file, you still then need to go into geneace and remove any Gene objects
+  # that have CGC-style names.  Believe me, if you have loaded the fix file you can then safely
+  # remove any Gene object which isn't called WBGene*
+
+  open(OUT, ">fix5.ace") || die "Can't create output file\n";
+  my $db_path = "/nfs/disk100/wormpub/DATABASES/TEST_DBs/geneace_gene_model";
+  my $db = Ace->connect(-path  => $db_path) || do { print "Connection failure: ",Ace->error; die();};
+
+  # This query gets all Multi_pt_data objects which link to Locus objects using old CGC style names,
+  my $query = "Find Multi_pt_data * WHERE Results";
+  push(my @multipoints, $db->find($query));
+  foreach my $multipoint (@multipoints){
+    print "$multipoint\n";
+    # grab row data if it exists
+    (my @row1 = $multipoint->at("Results.Combined")->row) if ($multipoint->at("Results.Combined"));
+    (my @row2 = $multipoint->at("Results.A_non_B")->row) if ($multipoint->at("Results.A_non_B"));
+    (my @row3 = $multipoint->at("Results.B_non_A")->row) if ($multipoint->at("Results.B_non_A"));
+    print "@row1\n" if (@row1);
+    print "@row2\n" if (@row2);
+    print "@row3\n" if (@row3);
+  }
+
+  $db->close;  
+  close(OUT);
+}
+
+
+
 
 ################################################################
-# misc. variables
+# main conversion of input file
 ################################################################
 
 if($file){
