917c6f58c652ab6e270438d7f12f8c02e5ef237a
pad <pad>
2004-04-05 15:57:36 +0000

    added 2 system calls, 1 to remove models.wrm from the split databases and a 2nd to copy /wormsrv2/autoace/wspec/models.wrm back to synchronize all copies

--- scripts/merge_split_camaces.pl
+++ scripts/merge_split_camaces.pl
@@ -5,7 +5,7 @@
 # A script to make multiple copies of camace for curation, and merge them back again
 #
 # Last edited by: $Author: pad $
-# Last edited on: $Date: 2004-03-18 10:42:08 $
+# Last edited on: $Date: 2004-04-05 15:57:36 $
 
 use strict;
 use lib "/wormsrv2/scripts/";
@@ -37,7 +37,7 @@ GetOptions (
 	    "update"     => \$update,
 	    "help"       => \$help,
 	    "debug"      => \$debug
-	    );
+	   );
 
 # Help pod if needed
 &usage("Help") if ($help);
@@ -52,11 +52,11 @@ my @databases; #array to store what splits are to be merged.
 my $path_new = ();
 my $path_ref = ();
 
-  # load @databases array with user database names.
-  push(@databases,"orig");
-  push(@databases,"ar2") if ($ant || $all);
-  push(@databases,"dl1") if ($dan || $all);
-  push(@databases,"pad") if ($paul || $all);
+# load @databases array with user database names.
+push(@databases,"orig");
+push(@databases,"ar2") if ($ant || $all);
+push(@databases,"dl1") if ($dan || $all);
+push(@databases,"pad") if ($paul || $all);
 
 my @classes = ('Transposon', 'Transcript', 'Sequence', 'CDS', 'Feature', 'Pseudogene');
 
@@ -98,7 +98,7 @@ if ($merge) {
 
       system ("acediff $path_ref $path_new > $directory/${class}_diff_${database}.ace") && die "Failed to run acediff for ${path_new}\n";
       system ("reformat_acediff $directory/${class}_diff_${database}.ace   > $directory/update_${class}_${database}.ace") && die "Failed to run reformat ace file for $directory/${class}_diff_${database}.ace\n";
-     }
+    }
   }
 
   print "Phase 1 finished and all files can be found in $directory\n";
@@ -152,71 +152,74 @@ sub dump_camace {
 
 #(1a)data retrieval#
 sub dumpace {
-    my $class    = shift;
-    my $filepath = shift;
-
-    my $command = "nosave\nquery find $class\nshow -a -f $filepath\nquit\n";
-
-    # dump out from ACEDB
-    print "\nFilename: $filepath\n";
-    open (TACE,"| $tace") or die "Failed to open database connection\n";
-    print TACE $command;
-    close TACE;
+  my $class    = shift;
+  my $filepath = shift;
+  
+  my $command = "nosave\nquery find $class\nshow -a -f $filepath\nquit\n";
+  
+  # dump out from ACEDB
+  print "\nFilename: $filepath\n";
+  open (TACE,"| $tace") or die "Failed to open database connection\n";
+  print TACE $command;
+  close TACE;
 }
 
 #(2)data upload#
 sub loadace {
-    my $filepath = shift;
-    my $tsuser   = shift;
-
-    my $command = "pparse $filepath\nsave\nquit\n";
-
-    # dump out from ACEDB
-    print "\nFilename: $filepath\n";
-    open (TACE,"| $tace -tsuser $tsuser") or die "Failed to open database connection\n";
-    print TACE $command;
-    close TACE;
+  my $filepath = shift;
+  my $tsuser   = shift;
+  my $command = "pparse $filepath\nsave\nquit\n";
+  
+  # dump out from ACEDB
+  print "\nFilename: $filepath\n";
+  open (TACE,"| $tace -tsuser $tsuser") or die "Failed to open database connection\n";
+  print TACE $command;
+  close TACE;
 }
 
 #(2a)upload data to wormsrv1/camace#
 sub update_camace {
-    # upload processed diff files into /wormsrv1/camace
-    print "Upload diff files to /wormsrv1/camace";
-    $ENV{'ACEDB'} = $current;
-
-    foreach my $database (@databases) {
-      foreach my $class (@classes) {
-	&loadace("$directory/update_${class}_${database}.ace","${database}");
-      }
+  # upload processed diff files into /wormsrv1/camace
+  print "Upload diff files to /wormsrv1/camace";
+  $ENV{'ACEDB'} = $current;
+  
+  foreach my $database (@databases) {
+    foreach my $class (@classes) {
+      &loadace("$directory/update_${class}_${database}.ace","${database}");
     }
-    # uplaod new mRNAs into camace
-    print "Upload new mRNAs in /wormsrv1/camace\n";
-    &loadace("/nfs/disk100/wormpub/analysis/ESTs/elegans_mRNAs.ace",'NDB_data') or die "Failed to load new mRNA data";
+  }
+  # uplaod new mRNAs into camace
+  print "Upload new mRNAs in /wormsrv1/camace\n";
+  &loadace("/nfs/disk100/wormpub/analysis/ESTs/elegans_mRNAs.ace",'NDB_data') or die "Failed to load new mRNA data";
+  
+  # upload BLAT results to database
+  print "Update BLAT results in /wormsrv1/camace\n";
+  system ("load_blat2db.pl -all -dbdir $current") && die "Failed to run load_blat2db.pl\n";
 
-    # upload BLAT results to database
-    print "Update BLAT results in /wormsrv1/camace\n";
-    system ("load_blat2db.pl -all -dbdir $current") && die "Failed to run load_blat2db.pl\n";
-
-    # synchronize the locus - sequence connections
-    print  "Update locus2sequence connections in /wormsrv1/camace\n";
-    system ("locus2seq.pl -camace -update") && die "Failed to run locus2seq.pl\n";
+  # synchronize the locus - sequence connections
+  print  "Update locus2sequence connections in /wormsrv1/camace\n";
+  system ("locus2seq.pl -camace -update") && die "Failed to run locus2seq.pl\n";
 }
 
 #(3)Data dispersion#
 sub split_databases {
+  #remove all split models.wrm and update since we have started using splits without wormsrv2 linked.
+  foreach my $database (@databases) {
+    system("rm /nfs/disk100/wormpub/camace_${database}/wspec/models.wrm") && die "failed to remove camace_${database} models.wrm\n";
+    system("cp /wormsrv2/autoace/wspec/models.wrm ~wormpub/camace_${database}/wspec/ .") && die "failed to update camace_${database} models.wrm\n";
+  }
 
   # it has been decided that it is better to remove the database directory to make transfer db more stable #
-
   # initialise/copy to camace_orig (always do this)
   system("rm -rf /nfs/disk100/wormpub/camace_orig/database") && die "Failed to remove camace_orig/database\n";
   system ("TransferDB.pl -start /wormsrv1/camace -end $camace_orig -database -wspec -name camace_orig_WS$WS_version");
-
+  
   # initialise/copy to camace_ar2 (-ant or -all)
   if ($ant || $all) {
     system("rm -rf /nfs/disk100/wormpub/camace_ar2/database") && die "Failed to remove camace_ar2/database\n";
     system ("TransferDB.pl -start $camace_orig -end $camace_ar2 -database -wspec -name camace_ar2_WS$WS_version");
   }
-    # initialise/copy to camace_dl1 (-dan or -all)
+  # initialise/copy to camace_dl1 (-dan or -all)
   if ($dan || $all) {
     system("rm -rf /nfs/disk100/wormpub/camace_dl1/database") && die "Failed to remove camace_dl1/database\n";
     system ("TransferDB.pl -start $camace_orig -end $camace_dl1 -database -wspec -name camace_dl1_WS$WS_version");
@@ -230,13 +233,13 @@ sub split_databases {
 }
 
 sub usage {
-    my $error = shift;
-
-    if ($error eq "Help") {
-        # Normal help menu
-        system ('perldoc',$0);
-        exit (0);
-    }
+  my $error = shift;
+  
+  if ($error eq "Help") {
+    # Normal help menu
+    system ('perldoc',$0);
+    exit (0);
+  }
 }
 
 __END__
