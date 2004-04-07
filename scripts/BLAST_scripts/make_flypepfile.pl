034c232e74517d61b1ec9d85aa18f46208738465
ar2 <ar2>
2004-04-07 10:25:02 +0000

    update for FlyBase header change and DB_remark -> Description

--- scripts/BLAST_scripts/make_flypepfile.pl
+++ scripts/BLAST_scripts/make_flypepfile.pl
@@ -22,6 +22,9 @@ GetOptions (
 #>CG3645-PA translation from_gene[CG3645 CG3645 FBgn0031238] seq_release:3  mol_weight=58068  cds_boundaries:(2L:252,666..254,379[-]) aa_len
 #gth:505 transcript_info:[CG3645-RA  seq_release:3] gene_info:[gene symbol:CG3645 FBgn0031238 gene_boundaries:(2L:252,589..271,732[-]) ]
 
+#>CG2671-PA type=protein; loc=2L:complement(11218..11344,11410..11518,11779..12221,12286..12928,13520..13625,13683..14874,14933..15711,17053..17136); ID=l(2)gl-PA; name=l(2)gl-PA; db_xref='CG2671,FlyBase:FBgn0002121'; /gene=l(2)gl; len=1161
+
+
 my $blastdir    = "/nfs/acari/wormpipe/BlastDB";
 my $acedir      = "/nfs/acari/wormpipe/ace_files";
 my $source_file = "$blastdir/gadfly${version}.pep";
@@ -36,35 +39,23 @@ open (SOURCE,"<$source_file") or die "cant open $source_file\n";
 
 my $record_count = 0;
 my $problem_count =0;
-my ($gadID, $otherGadID, $FBname, $FBgn);
+my ($gadID, $FBname, $FBgn);
 my $count;
 while (<SOURCE>){
   chomp;
   if( /^>/ ){
     undef $FBgn;
     undef $gadID;
-    undef $otherGadID;
     undef $FBname;
     
     $count++;
     $record_count++;
-    
-    if( /^>(\S+).*from_gene\[(\S+)\s+(\S+)\s+(\S+)\]/ ) {
-      #$1 = Gadfly id  $2 = $3 = Flybase gene name   $4 = Flybase id
-      ($gadID, $otherGadID, $FBname, $FBgn)  = ($1, $2, $3, $4);
-      
+
+    if( /^>(\S+)\s.*name=(.*);\s.*FlyBase:(FBgn\d+)/ ) {
+      ($gadID, $FBname, $FBgn)  = ($1, $2, $3);
     }
-    else{
-      # some are just >CG32015-PD translation from_gene[CG32015 CG32015 ]
-      if( $_ =~ /from_gene\[(\S+) (\S+)/ ) {
-	$gadID = $1;
-	$FBname = $1;
-      }
-    }
-    
-    if( !($FBgn) ||          # no FBgn
-	!( $FBgn =~ m/FBgn/ )# invalid FBgn
-	) {
+
+    if( !($FBgn) ){
       $FBgn = $1 if ($_ =~ /(FBgn\d+)/);  #last ditch attemp to grap if from the header
     }
 
@@ -72,10 +63,6 @@ while (<SOURCE>){
     # we need to use the 1st field of the "from_gene" fields.
 
     if( $gadID ){
-      if ("$gadID" =~ /^pp-/) {
-	$gadID = $otherGadID;
-      }
-
 
       #print ace file
       print ACE "\n\nProtein : \"GADFLY:$gadID\"\n";
@@ -86,7 +73,7 @@ while (<SOURCE>){
       print ACE "Gene_name \"$FBname\"\n" if $FBname;
       print ACE "Database \"Flybase\" FlyBase_gn \"$FBgn\"\n" if ($FBgn);
       print ACE "Database \"Gadfly\" Gadfly_ID \"$gadID\"\n" if $gadID;
-      print ACE "DB_remark \"Flybase gene name is $FBname\"\n" if $FBname;
+      print ACE "Description \"Flybase gene name is $FBname\"\n" if $FBname;
       print ACE "\nPeptide : \"GADFLY:$gadID\"\n";
 
       #write database file
