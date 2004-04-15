7f26aeee60e30929d0fd66a1d9f1f2d023cd1425
ar2 <ar2>
2004-04-15 08:55:14 +0000

    replace uniquify_pepset.pl with uniquify_fasta.pl.  Use to remove redundant entries from fasta file

--- scripts/misc/uniquify_fasta.pl
+++ scripts/misc/uniquify_fasta.pl
@@ -14,3 +14,18 @@ while (<>) {
 foreach (sort keys %peps) {
   print "\>$_\n$peps{$_}";
 }
+
+
+=pod
+
+=head1 NAME
+
+ uniquify_fasta.pl
+
+ Very simple script to make a version of a fasta file unique ie no duplicate entries.
+
+ Use like this
+
+ WS123.pep | perl uniquify_fasta.pl > WS123.pep.unique
+
+=cut
