22785ec583e478428b866b43186ab939ddf32568
ck1 <ck1>
2004-06-02 12:27:17 +0000

    -removed update_person.pl and incorporated it into load_related_data_from_Build_to_geneace.pl
    -fixed problems where ?person/?person_name data get deleted when doing update after the build is finished
     as the script still looks for data in autoace, which have alreay been moved to curr_db : this is now fixed
     (the script will point to curr_db for update if autoace data is not available).
     Ie, uploading geneace with related data from autoace can now be done either during the build or after the build.

--- scripts/GENEACE/load_related_data_from_Build_to_geneace.pl
+++ scripts/GENEACE/load_related_data_from_Build_to_geneace.pl
@@ -4,13 +4,13 @@
 
 # by Chao-Kung Chen
 
-# Last updated on: $Date: 2004-05-28 12:56:05 $
+# Last updated on: $Date: 2004-06-02 12:27:17 $
 # Last updated by: $Author: ck1 $
 
 
 # load Geneace related data from Build back to Geneace
 
-# RUN this script anytime during the build when get_interpolated_map and update_inferred multi-pt data are done
+# RUN this script anytime during the build or after the build when get_interpolated_map and update_inferred multi-pt data are done
 
 
 use strict;
@@ -22,43 +22,103 @@ use Ace;
 # ----- globals -----
 ######################
 
+my $user = `whoami`; chomp $user;
+if ($user ne "wormpub"){print "\nYour need to be wormpub to upload data to geneace\n"; exit 0 };
+
 my $tace = glob("~wormpub/ACEDB/bin_ALPHA/tace");          # tace executable path
-#my $geneace_dir = "/wormsrv1/geneace";
-my $geneace_dir = "/nfs/disk100/wormpub/DATABASES/TEST_DBs/CK1TEST";
-my $autoace = get_wormbase_version(); # only the digits
-my $log = "/wormsrv2/logs/load_WS".$autoace."_build_data_to_geneace";
+my $build_ver = get_wormbase_version(); # only the digits
+
+my $geneace_dir = "/wormsrv1/geneace";
+my $autoace = "/wormsrv2/autoace";
+my $curr_db = "/nfs/disk100/wormpub/DATABASES/current_DB";
+
+my $log = "/wormsrv2/logs/load_WS".$build_ver."_build_data_to_geneace";
 
 open(LOG, ">$log") || die $!;
-print LOG "\n\nLoading WS$autoace Geneace related data back to $geneace_dir\n";
+print LOG "\n\nLoading WS$build_ver Geneace related data back to $geneace_dir\n";
 print LOG "---------------------------------------------------------------------------------------------\n";
 
 
-###################################################################
-# ----- load Geneace related data from Build back to Geneace -----
-###################################################################
+##############################
+# ----- preparing data -----
+##############################
 
 # (1) interpolated map data
-my $map = "/wormsrv2/autoace/MAPPINGS/INTERPOLATED_MAP/interpolated_map_to_geneace_WS$autoace.ace";
+my $map = "/wormsrv2/autoace/MAPPINGS/INTERPOLATED_MAP/interpolated_map_to_geneace_WS$build_ver.ace";
 
 # (2) corrected reverse physicals
-my $rev_pyhs = glob("/wormsrv2/autoace/MAPPINGS/INTERPOLATED_MAP/rev_physical_update_WS$autoace");
+my $rev_pyhs = glob("/wormsrv2/autoace/MAPPINGS/INTERPOLATED_MAP/rev_physical_update_WS$build_ver");
 
 # inferred multi-pt data
 
 # (3) new multipt obj created for pseudo markers
-my $multi = glob("/wormsrv1/geneace/JAH_DATA/MULTI_PT_INFERRED/inferred_multi_pt_obj_WS$autoace");
+my $multi = glob("/wormsrv1/geneace/JAH_DATA/MULTI_PT_INFERRED/inferred_multi_pt_obj_WS$build_ver");
 
 # (4) existing multipt obj with updated flanking marker loci
-my $multi_update = glob("/wormsrv1/geneace/JAH_DATA/MULTI_PT_INFERRED/updated_multi_pt_flanking_loci_WS$autoace");
+my $multi_update = glob("/wormsrv1/geneace/JAH_DATA/MULTI_PT_INFERRED/updated_multi_pt_flanking_loci_WS$build_ver");
 
 
-# (5) updated person from Caltech
-system("perl /nfs/team71/worm/ck1/WORMBASE_CVS/scripts/GENEACE/update_person.pl");
-#system("perl /wormsrv2/scripts/update_person.pl");
+# (5) updated geneace with person/person_name data from Caltech
+
+######################################################################
+# ----- retrieve  person/person_name datafrin autoace Or curr_db -----
+######################################################################
+
+my $person = "/tmp/new_Person.ace";
+my $person_name = "/tmp/new_Person_name.ace";
+
+my $dump_Person_Person_name_from_caltech=<<END;
+find Person *
+show -a -f $person
+find Person_name *
+show -a -f  $person_name
+quit
+END
+
+# Two possibilities of doing ?Person/?Person_name update from Caltech:
+# (1) duing the build:
+# (2) after the build: retrieved from curr_db as Person/Person_name data will not be in autoace anymore
+
+# doing update during the build
+`echo "$dump_Person_Person_name_from_caltech" | $tace $autoace`;
+`chmod 777 $person $person_name`;
+
+# doing update after the build
+if (! -e ($person && $person_name)){
+  print LOG "Autoace Person and Perosn_name data are now in curr_db .. retrieving them from current_DB (WS$build_ver)...\n\n";
+  `echo "$dump_Person_Person_name_from_caltech" | $tace $curr_db`;
+  print LOG "Got Person and Perosn_name data from current_DB (WS$build_ver)...";
+}
+
+#######################################################################################
+# ----- remove person/person_name data from Geneace : 
+#       the value of "CGC_representative_for" is kept as geneace keeps the record -----
+#######################################################################################
+
+my $remove_Person_Person_name_from_ga=<<END;
+find Person *
+edit -D PostgreSQL_id
+edit -D Name
+edit -D Laboratory
+edit -D Address
+edit -D Comment
+edit -D Tracking
+edit -D Lineage
+edit -D Publication
+
+find Person_name *
+edit -D Name
+save
+quit
+END
+
+`echo "$remove_Person_Person_name_from_ga" | $tace $geneace_dir`;
+
 
 # (6) allele mapping data from build
-my $allele_delete = "/wormsrv2/autoace/MAPPINGS/map_alleles_geneace_update_delete.WS$autoace.ace";
-my $allele_update = "/wormsrv2/autoace/MAPPINGS/map_alleles_geneace_update.WS$autoace.ace";
+
+my $allele_delete = "/wormsrv2/autoace/MAPPINGS/map_alleles_geneace_update_delete.WS$build_ver.ace";
+my $allele_update = "/wormsrv2/autoace/MAPPINGS/map_alleles_geneace_update.WS$build_ver.ace";
 
 
 ################################################
@@ -66,23 +126,23 @@ my $allele_update = "/wormsrv2/autoace/MAPPINGS/map_alleles_geneace_update.WS$au
 ################################################
 
 my $msg = <<MSG;
-\nNOTE: The following 8 files should have been loaded.\n
+\n\nNOTE: The following 8 files should have been loaded.\n
 (1) $map\n
 (2) $rev_pyhs\n
 (3) $multi\n
 (4) $multi_update\n
 (5) $allele_delete\n
 (6) $allele_update\n
-(7) /tmp/new_Person.ace\n
-(8) /tmp/new_Person_name.ace\n
+(7) $person\n
+(8) $person_name\n
 Depending on whether pseudo genetic markers are availabe to the build or if there are reverse physicals, files like (2) or (3) maybe missing. But the rest should always be available\n
 MSG
 
 print LOG $msg;
 
-######################
-# ----- Loading -----
-######################
+###################################################################
+# ----- load Geneace related data from Build back to Geneace -----
+###################################################################
 
 my  $command=<<END;
 Find Gene * where Interpolated_map_position
@@ -93,26 +153,33 @@ pparse $multi
 pparse $multi_update
 pparse $allele_delete
 pparse $allele_update
+pparse $person
+pparse $person_name
 
 save
 quit
 END
 
 my $rundate = &rundate;
-print LOG "\nLoading updated Person info from Caltech\n\n";
-my @person_log = `cat /wormsrv2/logs/update_Person.$rundate`;
-print LOG @person_log;
 
 # upload when corresponding file exists
-open (Load_GA,"| $tace -tsuser \"interpolated_map\" $geneace_dir >> $log") || die "Failed to upload to Geneace";
+open (Load_GA,"| $tace -tsuser \"autoace_update\" $geneace_dir >> $log") || die "Failed to upload to Geneace";
 print Load_GA $command;
 close Load_GA;
+
+#######################
+# ----- clean up -----
+#######################
+
+# remove temp. person, person_name data
+`rm -f $person $person_name`;
 
 
 ###########################
 # ----- email notice -----
 ###########################
 
-mail_maintainer("Loading WS$autoace Geneace related data to Geneace - test only", "ALL", $log);
+mail_maintainer("Loading WS$build_ver Geneace related data to Geneace", "ck1\@sanger.ac.uk, krb\@sanger.ac.uk", $log);
+
 
 __END__
