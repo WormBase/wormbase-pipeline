6e1a9943dd3ad82a460866d94ca011c0b372e0c6
ar2 <ar2>
2004-10-19 12:17:06 +0000

    reinstated all of the commented out code  :)

--- scripts/further_BLAST_dump.pl
+++ scripts/further_BLAST_dump.pl
@@ -7,7 +7,7 @@
 # Author: Chao-Kung CHen
 #
 # Last updated by: $Author: ar2 $
-# Last updated on: $Date: 2004-10-19 12:12:42 $
+# Last updated on: $Date: 2004-10-19 12:17:06 $
 
 use strict;
 use lib "/wormsrv2/scripts/";
@@ -41,34 +41,30 @@ my @files        = (
 		    "worm_brigpep_best_blastp_hits",
 		    "TRF.ace",
 		    "repeat_homologies.ace",
-		    #"worm_pep_blastp.ace",
-		    #"worm_brigpep_blastp.ace",
-		    #"worm_dna_blastx.ace",
+		    "worm_pep_blastp.ace",
+		    "worm_brigpep_blastp.ace",
+		    "worm_dna_blastx.ace",
 		    "worm_pep_motif_info.ace",
 		    "worm_brigpep_motif_info.ace",
 		    "swissproteins.ace",
 		    "tremblproteins.ace",
 		   );
 
+foreach my $file (@files){
+  if( ( $file eq "TRF.ace" or $file eq "repeat_homologies.ace") and !(-e $file) ){
+    next;
+  }
+  if ( -e $file ) {
+    $log->write_to("gzipping and backing up $file\n");
+    &run_command("gzip -f ${target_dir}/$file > ${target_dir}/BACKUP/$file.gz");
+  }
+  else {
+    $log->write_to($file." does not exist\n");
+  }
 
-
-
-# backup each file first, then copy across new file
-#foreach my $file (@files){
-#  if( ( $file eq "TRF.ace" or $file eq "repeat_homologies.ace") and !(-e $file) ){
-#    next;
-#  }
-#  if ( -e $file ) {
-#    $log->write_to("gzipping and backing up $file\n");
-#    &run_command("gzip -f ${target_dir}/$file > ${target_dir}/BACKUP/$file.gz");
-#  }
-#  else {
-#    $log->write_to($file." does not exist\n");
-#  }
-
-#  $log->write_to("scping new version of $file\n");
-#  &run_command("scp acari:${source_dir}/${file} ${target_dir}/${file}");
-#}
+  $log->write_to("scping new version of $file\n");
+  &run_command("scp acari:${source_dir}/${file} ${target_dir}/${file}");
+}
 
 
 # Also move big ensembl_protein file to BACKUP dir before making new one. 
