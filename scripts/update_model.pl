a0f2dfe2914af050fe3501cf240d7d40ea74cae0
ar2 <ar2>
2005-04-22 16:11:53 +0000

    added log and instruction to update models in primary databases

--- scripts/update_model.pl
+++ scripts/update_model.pl
@@ -5,23 +5,30 @@
 #
 # Script to update models.wrm in various directories for the build
 #
-# Last updated by: $Author: krb $
-# Last updated on: $Date: 2004-03-02 10:48:19 $
+# Last updated by: $Author: ar2 $
+# Last updated on: $Date: 2005-04-22 16:11:53 $
 
 
 use strict;
 use lib -e "/wormsrv2/scripts" ? "/wormsrv2/scripts" : $ENV{'CVS_DIR'};
 use Getopt::Long;
+use Log_files;
 
 # ----- specify status of model
 my $new;
 GetOptions ("n|new" => \$new);
 
+my $log = Log_files->make_build_log();
+
+$log->write_to("Updating the models to start new build\n");
+$log->write_to("\n* YOU SHOULD UPDATE PRIMARY DATABASE MODELS TOO *\n\n");
 # ----- update models.wrm in various of build dir. under /wormsrv2 only when there are changes
 
 if ($new){
+  $log->write_to("Updating in . . \n");
   my @db = qw ("autoace" "geneace" "citace" "cshace" "camace" "stlace" "brigace");
   foreach(@db){
+    $log->write_to("\t$_\n");
     system("cd /wormsrv2/$_/wspec/; cvs update");
   }
 }
@@ -29,3 +36,7 @@ if ($new){
 system("cd /wormsrv2/scripts/; cvs update");
 
 print "\n\nmodel.wrm files upated for the build..\n\n";
+
+$log->mail;
+
+exit(0);
