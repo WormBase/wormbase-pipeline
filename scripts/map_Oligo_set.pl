6520119565eaefdc975c4e858ce66a6e7bcc31ee
mh6 <mh6>
2005-11-14 11:58:01 +0000

    major rewrite to clean it up and make it more readable.

--- scripts/map_Oligo_set.pl
+++ scripts/map_Oligo_set.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl5.8.0 -w
+#!/usr/bin/env perl
 #
 # map_Oligo_set
 #
@@ -10,8 +10,9 @@
 #                parsing problems
 # 010717 :  kj : modified to (hopefully) run faster and produce the final output files in one go
 #
+# 051114 :  mh6 : rewrote it for better readability
+#
 #############################################################################################
-
 
 #############
 # variables #
@@ -23,6 +24,9 @@ use Wormbase;
 use IO::Handle;
 use Getopt::Long;
 use Cwd;
+use warnings;
+use Data::Dumper;
+use Class::Struct;
 
 ##########################
 # Script variables (run) #
@@ -38,259 +42,169 @@ my %finaloutput = ();
 # command-line options #
 ########################
 
-my $help;       # Help perdoc
+my $help;       # Help perldoc
 my $test;       # Test mode
 my $debug;      # Debug mode, output only goes to one user
 my $verbose;    # verbose mode, more command line outout
+my $acename;
 
-GetOptions ("debug=s"   => \$debug,
-	    "verbose"   => \$verbose,
-	    "test"      => \$test,
-            "help"      => \$help
-	    );
+GetOptions(
+    'debug=s'   => \$debug,
+    'verbose'   => \$verbose,
+    'test'      => \$test,
+    'help'      => \$help,
+    'acefile=s' => \$acename
+);
 
 # Display help if required
 &usage("Help") if ($help);
 
 # Use debug mode?
-if($debug){
-  print "DEBUG = \"$debug\"\n\n";
-  ($maintainers = $debug . '\@sanger.ac.uk');
+if ($debug) {
+    print "DEBUG = \"$debug\"\n\n";
+    ( $maintainers = $debug . '\@sanger.ac.uk' );
 }
-
 
 #############
 # Paths etc #
 #############
 
-my $tace        = &tace;                                     # tace executable path
-my $basedir     = $test ? glob("~wormpub/TEST_BUILD") : "/wormsrv2";
-my $dbdir       = "$basedir/autoace";                        # Database path
-my $gffdir      = "$dbdir/GFF_SPLITS/GFF_SPLITS";            # GFF splits directory
+my $tace = &tace;    # tace executable path
 
-my @chromosomes = $test ? qw(III) : qw( I II III IV V X );   # chromosomes to parse 
-my $db_version  = &get_wormbase_version_name;                # WS version number
-our %genetype   = ();                                        # gene type hash
+#my $basedir = $test ? glob("~wormpub/TEST_BUILD") : "/wormsrv2";
+my $basedir    = '/wormsrv2';
+my $dbdir      = "$basedir/autoace";                   # Database path
+my $db_version = &get_wormbase_version_name;           # WS version number
+my $gffdir     = "$dbdir/GFF_SPLITS/GFF_SPLITS/";
+my $acefile = $acename ? $acename : "$dbdir/acefiles/Oligo_mappings.ace";
+
+my @chromosomes = $test
+  ? qw( I )
+  : qw( I II III IV V X );    # chromosomes to parse (TEST_BUILD should be III)
+my %genetype;                 # gene type hash
+
+################
+# Structs      #
+################
+use Class::Struct;
+struct( Exon => [ start => '$', stop => '$', type => '$', id => '$' ] );
+struct( Gene => [ start => '$', stop => '$', exons => '@' ] );
 
 ################
 # Open logfile #
 ################
 my $log = Log_files->make_build_log($debug);
 
-
 #####################################################
 # get exons and Oligo_set info out of the gff files #
-#####################################################        
-   
+#####################################################
+
 foreach my $chromosome (@chromosomes) {
-  $log->write_to("Processing chromosome $chromosome\n");
-  print "\nProcessing chromosome $chromosome\n" if ($verbose);
-  my %exoncount  = ();
-  my %Oligocount = ();
-  my %genes      = ();
-  my %Oligo      = ();
-  my %exon       = ();
-  my @f          = ();
-  
-  # Get Oligo set info from split GFF file
-  open (GFF, "<$gffdir/CHROMOSOME_${chromosome}.Oligo_set.gff") || die "Failed to open Oligo_set gff file : $!\n\n";
-  while (<GFF>) {
-    chomp;	     
-    s/\#.*//;
-    next unless /\S/;
-    @f = split /\t/;
-    
-    my ($name) = ($f[8] =~ /Oligo_set \"(.*)\"$/);
-    unless ($name) {
-      $log->write_to("ERROR: Cant get name from $f[8]\n");
-      next;
+    $log->write_to("Processing chromosome $chromosome\n");
+    print "\nProcessing chromosome $chromosome\n" if ($verbose);
+    my %genes      = ();
+    my %Oligo      = ();
+
+    # Get Oligo set info from split GFF file
+    open( GFF, "<$gffdir/CHROMOSOME_${chromosome}.Oligo_set.gff" )
+      || die
+      "Failed to open $gffdir/CHROMOSOME_${chromosome}.Oligo_set.gff : $!\n\n";
+    while (<GFF>) {
+        chomp;
+        s/\#.*//;
+        next unless /\S/;
+         my @f = split /\t/;
+
+        my ($name) = ( $f[8] =~ /Oligo_set \"(.*)\"$/ );
+        unless ($name) {
+            $log->write_to("ERROR: Cant get name from $f[8]\n");
+            next;
+        }
+        $Oligo{$name} = [ $f[3], $f[4] ];
+        print "Oligo_set : '$name'\n" if ($verbose);
     }
-    $Oligocount{$name}++;
-    my $Oligoname = $name.".".$Oligocount{$name};
-    $Oligo{$Oligoname} = [$f[3],$f[4]];
-    print "Oligo_set : '$name'\n" if ($verbose);
-  }
-  close(GFF);
+    close(GFF);
 
-  # Get exon info from split UTR GFF files
-  open (GFF_in, "<$gffdir/CHROMOSOME_${chromosome}.UTR.gff") || die "Failed to open UTR gff file\n\n";
-  while (<GFF_in>) {
-    chomp;	     
-    s/\#.*//;
-    next unless /UTR/;
-    @f = split /\t/;
+    # Get exon info from split UTR GFF files
+    get_from_gff("$gffdir/CHROMOSOME_${chromosome}.UTR.gff",'Transcript','UTR',\%genes);
+    # Get exon info from split exon GFF files
+    get_from_gff("$gffdir/CHROMOSOME_${chromosome}.exon.gff",'CDS',qw{\S},\%genes);
+    # Get exon info from split pseudogene exon GFF files
+    get_from_gff("$gffdir/CHROMOSOME_${chromosome}.exon_pseudogene.gff",'Pseudogene',qw{\S},\%genes);
+    # Get exon info from split transcript exon GFF file
+    get_from_gff("$gffdir/CHROMOSOME_${chromosome}.exon_noncoding.gff",'Transcript',qw{\S},\%genes);
 
-    my ($name) = ($f[8] =~ /\"(\S+)\"/);
-    $exoncount{$name}++;
-    my $exonname = $name.".".$exoncount{$name};
-    $exon{$exonname} = [$f[3],$f[4]];
-  }
-  close(GFF_in);
+    print "Finished GFF loop\n" if ($verbose);
 
-  # Get exon info from split exon GFF files
-  open (GFF_in, "<$gffdir/CHROMOSOME_${chromosome}.exon.gff") || die "Failed to open exon gff file\n\n";
-  while (<GFF_in>) {
-    chomp;	     
-    s/\#.*//;
-    next unless /\S/;
-    @f = split /\t/;
+    ###################
+    # make indexlists #
+    ###################
 
-    my ($name) = ($f[8] =~ /\"(\S+)\"/);
-    $exoncount{$name}++;
-    my $exonname = $name.".".$exoncount{$name};
-    $exon{$exonname} = [$f[3],$f[4]];
-    $genetype{$name} = "CDS"        if ($f[1] eq "curated");    
-    print "Gene : '$name' [$genetype{$name}] exon $exoncount{$name}\n" if ($verbose);
-  }
-  close(GFF_in);
+    print " sorting genes\n" if ($verbose);
 
+    my @sorted_genes = sort {
+        $genes{$a}->start <=> $genes{$b}->start
+          || $genes{$a}->stop <=> $genes{$b}->stop
+    } keys %genes;
 
-  # Get exon info from split pseudogene exon GFF files
-  open (GFF_in, "<$gffdir/CHROMOSOME_${chromosome}.exon_pseudogene.gff") || die "Failed to open exon_pseudogene gff file\n\n";
-  while (<GFF_in>) {
-    chomp;	     
-    s/\#.*//;
-    next unless /\S/;
-    @f = split /\t/;
+    ##########
+    # map it #
+    ##########
 
-    my ($name) = ($f[8] =~ /\"(\S+)\"/);
-    $exoncount{$name}++;
-    my $exonname = $name.".".$exoncount{$name};
-    $exon{$exonname} = [$f[3],$f[4]];
-    $genetype{$name} = "Pseudogene" if ($f[1] eq "Pseudogene");
-    print "Gene : '$name' [$genetype{$name}] exon $exoncount{$name}\n" if ($verbose);    
-  }
-  close(GFF_in);
+    print "Find overlaps for Oligo_set\n" if ($verbose);
+    #$|=1;
+    foreach my $testOligo ( keys %Oligo ) {
+        my $Oligostart = $Oligo{$testOligo}->[0];
+        my $Oligostop  = $Oligo{$testOligo}->[1];
+	print STDERR '.' if $verbose;
+        foreach my $gene (@sorted_genes) {
 
-
-  # Get exon info from split transcript exon GFF file
-  open (GFF, "<$gffdir/CHROMOSOME_${chromosome}.exon_noncoding.gff") || die "Failed to open exon_noncoding gff file\n\n";
-  while (<GFF>) {
-    chomp;	     
-    s/\#.*//;
-    next unless /\S/;
-    @f = split /\t/;
-
-    my ($name) = ($f[8] =~ /\"(\S+)\"/);
-    $exoncount{$name}++;
-    my $exonname = $name.".".$exoncount{$name};
-    $exon{$exonname} = [$f[3],$f[4]];
-    $genetype{$name} = "Transcript" if ($f[1] eq "Non_coding_transcript");
-    print "Gene : '$name' [$genetype{$name}] exon $exoncount{$name}\n" if ($verbose);
-  }
-  close(GFF);
-  
-
-  print "Finished GFF loop\n" if ($verbose);
-  
-  #########################   
-  # make exons into genes #
-  #########################
-  
-  print "Turn exons into genes\n" if ($verbose);
-  
-  foreach my $name (sort keys %exoncount) {
-    my $v = $exoncount{$name};
-    my $w = $name.".".$v;
-    $genes{$name} = [$exon{$name.".1"}->[0],$exon{$w}->[1]];
-  }
-  
-  ###################
-  # make indexlists #
-  ###################
-  
-  print "Index exons, genes, and oligos\n" if ($verbose);
-  
-  my @exonlist  = sort { $exon{$a}->[0]  <=> $exon{$b}->[0]  } keys %exon;
-  my @genelist  = sort { $genes{$a}->[0] <=> $genes{$b}->[0] } keys %genes;
-  my @Oligolist = sort { $Oligo{$a}->[0] <=> $Oligo{$b}->[0] || $a cmp $b } keys %Oligo;
-  
-  ##########
-  # map it #
-  ##########
-  
-  print "Find overlaps for Oligo_set\n" if ($verbose);
-  
-  my $lastfail = 0;
-  
-  for (my $x = 0; $x < @Oligolist; $x++) {
-    my $testOligo   = $Oligolist[$x];
-    my $Oligostart  = $Oligo{$testOligo}->[0];
-    my $Oligostop   = $Oligo{$testOligo}->[1];
-    
-    for (my $y = 0; $y < @genelist; $y++) {
-      my $testgene = $genelist[$y];
-      my $genestart= $genes{$testgene}->[0];
-      my $genestop = $genes{$testgene}->[1];
-      
-      if ($Oligostart > $genestop) {
-	$lastfail = $y;
-	next; 
-      }
-      
-      elsif ($Oligostop < $genestart) {
-	last; 
-      }
-      
-      else {
-	for (my $z = 1; $z <= $exoncount{$testgene}; $z++) {
-	  my $exon_start = $exon{"$testgene.$z"}->[0];
-	  my $exon_stop  = $exon{"$testgene.$z"}->[1];
-	  
-	  if ( not (($Oligostart > $exon_stop) || ($Oligostop < $exon_start))) {
-	    my ($Oligo) = ($testOligo =~ /(\S+)\.\d+$/);
-	    push @{$output{$Oligo}}, $testgene;
-	  }
-	}
-      }                
+            if ( $Oligostart > $genes{$gene}->stop ) {
+                next;
+            }
+            elsif ( $Oligostop < $genes{$gene}->start ) {
+                last;
+            }
+            else {
+		my %added_exons;
+                foreach my $exon ( @{$genes{$gene}->exons} ) {
+                    if ($Oligostart > $exon->stop
+                        || $Oligostop < $exon->start ) {
+			next if $added_exons{$exon->type.$exon->id};
+			print STDERR '+' if $verbose;
+                        push @{$output{$testOligo} }, $exon;
+			$added_exons{$exon->type.$exon->id}=1;
+                        }
+                }
+            }
+        }
     }
-  }
-}
-
-
-###################
-# sort the output #
-###################
- 
-foreach my $mess (sort keys %output) {
-  @{$output{$mess}} = sort @{$output{$mess}};
-  my $count = 0; 
-  push @{$finaloutput{$mess}}, $output{$mess}->[0];
-  for (my $m = 1; $m < (scalar @{$output{$mess}}); $m++) {
-    if ($output{$mess}->[$count] ne $output{$mess}->[$m]) {
-      push @{$finaloutput{$mess}}, $output{$mess}->[$m];
-      $count = $m;    
-    }    
-  }
 }
 
 ########################
 # produce output files #
 ########################
+open( OUTACE, ">$acefile" ) || die "cannot open file $acefile\n";
 
-open (OUTACE, ">$dbdir/acefiles/Oligo_mappings.ace");
+foreach my $oligoname ( keys %output ) {
+    print "mapped $oligoname\tto ", join ' ',(map { $_->id } @{ $output{$oligoname} }), "\n"
+      if ($verbose);
 
-foreach my $mapped (sort keys %finaloutput) {
-    
-  print "$mapped\t@{$finaloutput{$mapped}}\n" if ($verbose);
-  
-  for (my $n = 0; $n < (scalar @{$finaloutput{$mapped}}); $n++) {
-    my $gene = $finaloutput{$mapped}->[$n];
-    
-    if ($genetype{$gene} eq "CDS") {
-      print OUTACE "CDS : \"$gene\"\n";
-      print OUTACE "Corresponding_oligo_set \"$mapped\"\n\n";
+    foreach my $exon ( @{ $output{$oligoname} } ) {
+        if ( $exon->type eq "CDS" ) {
+            print OUTACE "CDS : \"",$exon->id,"\"\n";
+            print OUTACE "Corresponding_oligo_set \"$oligoname\"\n\n";
+        }
+        elsif ( $exon->type eq "Pseudogene" ) {
+            print OUTACE "Pseudogene : \"",$exon->id,"\"\n";
+            print OUTACE "Corresponding_oligo_set \"$oligoname\"\n\n";
+        }
+        elsif ( $exon->type eq "Transcript" ) {
+            print OUTACE "Transcript : \"",$exon->id,"\"\n";
+            print OUTACE "Corresponding_oligo_set \"$oligoname\"\n\n";
+        }
     }
-    elsif ($genetype{$gene} eq "Pseudogene") {
-      print OUTACE "Pseudogene : \"$gene\"\n";
-      print OUTACE "Corresponding_oligo_set \"$mapped\"\n\n";
-    }
-    elsif ($genetype{$gene} eq "Transcript") {
-      print OUTACE "Transcript : \"$gene\"\n";
-      print OUTACE "Corresponding_oligo_set \"$mapped\"\n\n";
-    }
-  }
-} 
+}
 close(OUTACE);
 
 ##############################
@@ -298,19 +212,20 @@ close(OUTACE);
 ##############################
 
 unless ($test) {
-  
-  my $command = "pparse $dbdir/acefiles/Oligo_mappings.ace\nsave\nquit\n";
-  
-  open (TACE,"| $tace -tsuser map_Oligo_products $dbdir") || die "Couldn't open tace connection to $dbdir\n";
-  print TACE $command;
-  close (TACE);
+
+    my $command = "pparse $acefile\nsave\nquit\n";
+
+    open( TACE, "| $tace -tsuser map_Oligo_products $dbdir" )
+      || die "Couldn't open tace connection to $dbdir\n";
+    print TACE $command;
+    close(TACE);
 }
 
 ###############
 # hasta luego #
 ###############
 
-$log->mail("$maintainers","BUILD REPORT: $0");
+$log->mail( "$maintainers", "BUILD REPORT: $0" );
 
 exit(0);
 
@@ -319,13 +234,49 @@ exit(0);
 ##############################################################
 
 sub usage {
-  my $error = shift;
+    my $error = shift;
 
-  if ($error eq "Help") {
-    # Normal help menu
-    system ('perldoc',$0);
-    exit (0);
-  }
+    if ( $error eq "Help" ) {
+
+        # Normal help menu
+        system( 'perldoc', $0 );
+        exit(0);
+    }
+}
+
+sub get_from_gff{
+	my ($file,$type,$unless,$genes)=@_;
+	open( GFF_in, "<$file" )
+      || die "Failed to open gff file\n\n";
+    while (<GFF_in>) {
+        chomp;
+        s/\#.*//;
+        next unless /$unless/;
+        my @f = split /\t/;
+
+        my ($name) = ( $f[8] =~ /\"(\S+)\"/ );
+        my $x = new Exon;
+        $x->id($name);
+        $x->start( $f[3] );
+        $x->stop( $f[4] );
+        $x->type($type);
+        if ( $$genes{ $x->id } ) {
+            push @{ $$genes{ $x->id }->exons }, $x;
+            $$genes{ $x->id }->start( $x->start )
+              if $x->start < $$genes{ $x->id }->start;
+            $$genes{ $x->id }->stop( $x->stop )
+              if $x->stop < $$genes{ $x->id }->stop;
+        }
+        else {
+            $$genes{ $x->id } = new Gene;
+            $$genes{ $x->id }->start( $x->start );
+            $$genes{ $x->id }->stop( $x->stop );
+        }
+        print "Gene : '$name' [$type]\n"
+          if $verbose;
+    }
+    close(GFF_in);
+
 }
 __END__
 
@@ -362,6 +313,8 @@ map_Oligo_products optional arguments:
 =item -verbose, toggles extra output
 
 =item -test, Test mode, generate the acefile but do not upload 
+
+=item -acefile, custom name for the acefile
 
 =item -help, Help pages
 
