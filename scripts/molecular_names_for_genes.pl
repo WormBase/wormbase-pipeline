518a84e92788f8513c77618f70cb2d4fd46206fb
mh6 <mh6>
2005-12-19 17:41:34 +0000

    should work now, if there is a working acedb

--- scripts/molecular_names_for_genes.pl
+++ scripts/molecular_names_for_genes.pl
@@ -6,15 +6,15 @@
 #
 # quick script to populate Molecular_name tag in ?Gene model during build
 #
-# Last updated by: $Author: ar2 $
-# Last updated on: $Date: 2005-12-16 11:18:55 $
+# Last updated by: $Author: mh6 $
+# Last updated on: $Date: 2005-12-19 17:41:34 $
 
 #################################################################################
 # Initialise variables                                                          #
 #################################################################################
 
 use strict;
-use lib -e "/wormsrv2/scripts" ? "/wormsrv2/scripts" : $ENV{'CVS_DIR'};
+use lib $ENV{'CVS_DIR'};
 use Wormbase;
 use Data::Dumper;
 use Getopt::Long;
@@ -22,44 +22,46 @@ use Getopt::Long;
 ##############################
 # command-line options       #
 ##############################
-
 my $test;                # test mode, uses ~wormpub/TEST_BUILD
 my $debug;               # debug mode, email log file only goes to debugger
-my $log = Log_files->make_build_log();
 my $maintainers = "All"; # log file recipients
-my $tace = &tace;
+my $store;		# to specify any storable configuration files
 
-
-GetOptions("debug=s"       => \$debug,
-	   "test"          => \$test);
+GetOptions('debug=s'       => \$debug,
+	   'test'          => \$test,
+	   'store=s'	=> \$store
+   	);
+	
+############################
+# recreate configuration   #
+############################
+my $wb;
+if ($store) { $wb = Storable::retrieve($store) or croak("cant restore wormbase from $store\n") }
+else { $wb = Wormbase->new( -debug => $debug,-test=>$test) }
+$debug = $wb->debug if $wb->debug;    # Debug mode, output only goes to one
 
 # Use debug mode?
 if($debug){
   print "DEBUG = \"$debug\"\n\n";
-  ($maintainers = $debug . '\@sanger.ac.uk');
+  $maintainers = $debug.'\@sanger.ac.uk';
 }
-
+my $tace = $wb->tace;
+my $log = Log_files->make_build_log($wb);
 
 # Set up top level base directory which is different if in test mode
 # Make all other directories relative to this
-my $basedir   = "/wormsrv2";
-$basedir      = glob("~wormpub")."/TEST_BUILD" if ($test); 
-
-
-
+my $basedir   = $wb->basedir;
 
 ##########################################################
 #
 # Main part of script
 #
 ###########################################################
-
-
 # simple counted for number of names assigned
 my $counter = 0; 
 
 # output file
-open (OUT, ">/wormsrv2/autoace/acefiles/molecular_names_for_genes.ace") or die "Can't write ace file";
+open (OUT, ">$basedir/autoace/acefiles/molecular_names_for_genes.ace") or die "Can't write ace file";
 
 # open tace pipe, and connect to AceDB using TableMaker
 my $command="Table-maker -p $basedir/autoace/wquery/molecular_names_for_genes.def\nquit\n";
@@ -106,37 +108,25 @@ while (<TACE>) {
     $protein =~ s/^WP\://;
     print OUT "Molecular_name \"$protein\"\n\n";
     $counter++;
-
   }
-
-
 }
 close TACE; 
 close OUT;
 
 $log->write_to("Found $counter molecular names for genes\n");
 
-
 # load file to autoace using autoace_minder.pl -load
-$log->write_to("Loading /wormsrv2/autoace/molecular_names_for_genes.ace to autoace\n");
-
-$command = "autoace_minder.pl -load /wormsrv2/autoace/acefiles/molecular_names_for_genes.ace -tsuser molecular_names"; 
+$log->write_to("Loading $basedir/autoace/molecular_names_for_genes.ace to autoace\n");
+$command = "autoace_builder.pl -load $basedir/autoace/acefiles/molecular_names_for_genes.ace -tsuser molecular_names -database $basedir/autoace";
 
 my $status = system($command);
 if(($status >>8) != 0){
   $log->write_to("ERROR: Loading failed \$\? = $status\n");
 }
 
-
-
 # tidy up and exit
 $log->mail("$maintainers");
-
 exit(0);
-
-
-
-
 
 __END__
 
