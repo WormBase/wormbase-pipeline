174c5cbd92acb426ecfb98cb6c8ad579bff3038d
gw3 <gw3>
2005-12-20 12:19:25 +0000

    tidied up and added -verbose

--- scripts/landmark_genes2gff.pl
+++ scripts/landmark_genes2gff.pl
@@ -6,60 +6,62 @@
 #
 # script for creating extra GFF lines to indicate those genes that are landmark genes
 #
-# Last edited by: $Author: mh6 $
-# Last edited on: $Date: 2005-12-19 16:10:40 $
+# Last edited by: $Author: gw3 $
+# Last edited on: $Date: 2005-12-20 12:19:25 $
 use strict;
 use lib $ENV{'CVS_DIR'};
 use Wormbase;
 use Getopt::Long;
+use Carp;
+use Log_files;
+use Storable;
 
 my $database;     # choose another database (defaults to autoace)
 my $debug;        # debug mode, output only emailed to one person
 my $help;         # help mode, show perldoc
 my $test;         # use test mode in ~wormpub
 my %landmarks;    # hash containing gene IDs as keys and public_name field as value
-my $release     = &get_wormbase_version_name;    # WS release version
-my $maintainers = "All";                         # for email recipients
 my $store;                                       # to specify storable file
+my $verbose;
 
 GetOptions(
-    "help"       => \$help,
-    "debug=s"    => \$debug,
-    "test"       => \$test,
-    "database=s" => \$database,
-    'store=s'    => \$store
-);
+	   "help"       => \$help,
+	   "debug=s"    => \$debug,
+	   "test"       => \$test,
+	   "verbose"    => \$verbose,
+	   "database=s" => \$database,
+	   'store=s'    => \$store
+	   );
 
 ############################
 # recreate configuration   #
 ############################
-my $wb;
-if ($store) { $wb = Storable::retrieve($store) or croak("cant restore wormbase from $store\n") }
-else { $wb = Wormbase->new( -debug => $debug, -test => $test, ) }
+my $wormbase;
+if ($store) { $wormbase = Storable::retrieve($store) or croak("cant restore wormbase from $store\n") }
+else { $wormbase = Wormbase->new( -debug => $debug, -test => $test, ) }
 
-###########################################
-# Variables Part II (depending on $wb)    #
-###########################################
-$test  = $wb->test  if $wb->test;     # Test mode
-$debug = $wb->debug if $wb->debug;    # Debug mode, output only goes to one user
+# Display help if required
+&usage("Help") if ($help);
 
-# Use debug mode?
-if ($debug) {
-    print "DEBUG = \"$debug\"\n\n";
-    ( $maintainers = $debug . '\@sanger.ac.uk' );
+# in test mode?
+if ($test) {
+  print "In test mode\n" if ($verbose);
+
 }
 
-# need help?
-if ($help) { print `perldoc $0`; exit; }
+# establish log file.
+my $log = Log_files->make_build_log($wormbase);
+
+
+###########################################
+# Variables Part II (depending on $wormbase)    #
+###########################################
 
 # database/file paths and locations
-my $basedir = $wb->basedir;
+my $basedir = $wormbase->basedir;
 
 # set default database if -database not used
-$database = $wb->autoace if !$database;
-
-# create log file, open output file handles
-my $log = Log_files->make_build_log($wb);
+$database = $wormbase->autoace if !$database;
 
 ###############################
 #
@@ -108,8 +110,31 @@ foreach (@chromosomes) {
 }
 
 # tidy up and exit
-$log->mail( "$maintainers", "BUILD REPORT: landmark_genes2gff.pl" );
+$log->mail();
 exit(0);
+
+
+##############################################################
+#
+# Subroutines
+#
+##############################################################
+
+
+
+##########################################
+
+sub usage {
+  my $error = shift;
+
+  if ($error eq "Help") {
+    # Normal help menu
+    system ('perldoc',$0);
+    exit (0);
+  }
+}
+
+##########################################
 
 ##############################
 # get list of landmark genes
