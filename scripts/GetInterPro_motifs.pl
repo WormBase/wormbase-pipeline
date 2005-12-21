d0da96eb37655f966f698eea70624bfddf0f2efc
ar2 <ar2>
2005-12-21 15:57:07 +0000

    updated, tested and working

--- scripts/GetInterPro_motifs.pl
+++ scripts/GetInterPro_motifs.pl
@@ -6,67 +6,64 @@
 #
 # Gets latest Interpro:GO mappings from XXXX and puts info in to ace file
 #
-<<<<<<< GetInterPro_motifs.pl
 # Last updated by: $Author: ar2 $                      
-# Last updated on: $Date: 2005-12-16 11:18:54 $         
-=======
-# Last updated by: $Author: ar2 $                      
-# Last updated on: $Date: 2005-12-16 11:18:54 $         
->>>>>>> 1.7.4.1
+# Last updated on: $Date: 2005-12-21 15:57:07 $         
 
-use strict;                                     
-use lib "/wormsrv2/scripts/";                    
+use strict;
+use lib $ENV{'CVS_DIR'};
 use Wormbase;
 use Getopt::Long;
+use Log_files;
+use Storable;
 
 my $help;
 my $load;   # option for loading resulting acefile into autoace
+my ( $debug, $test, $store );
 
 GetOptions ("help"      => \$help,
-            "load"      => \$load);
+            "load"      => \$load,
+	    "test"      => \$test,
+	    "debug:s"   => \$debug,
+	    "store:s"   => \$store
+);
 
 
 # Display help if required
 &usage("Help") if ($help);
 
+my $wormbase;
+if ( $store ) {
+  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+} else {
+  $wormbase = Wormbase->new( -debug   => $debug,
+                             -test    => $test,
+			     );
+}
 
-##############
-# variables  #                                                                   
-##############
+my $rundate     = $wormbase->rundate;
+my $runtime     = $wormbase->runtime;
 
-# Most checking scripts should produce a log file that is a) emailed to us all 
-# and b) copied to /wormsrv2/logs
-
-my $maintainers = "All";
-my $rundate     = &rundate;
-my $runtime     = &runtime;
-our $log        = "/wormsrv2/logs/GetInterPro_motifs.$rundate";
-
-open (LOG, ">$log") or die "cant open $log";
-
-print LOG "$0\n";
-print LOG &runtime, ": script started\n\n";
+my $log = Log_files->make_build_log($wormbase);
 
 #Get the latest version
 my $motifs = "/tmp/interpro_motifs.html";
-print LOG &runtime, ": Running 'wget' to get interpro file from EBI\n";
-`/usr/local/bin/wget -O $motifs ftp://ftp.ebi.ac.uk/pub/databases/interpro/entry.list` and die "$0 Couldnt get listing.html\n";
+$log->write_to(": Running 'wget' to get interpro file from EBI\n");
+`/usr/local/bin/wget -O $motifs ftp://ftp.ebi.ac.uk/pub/databases/interpro/entry.list` and $log->log_and_die("$0 Couldnt get listing.html\n");
 
-
-open (I2G,"<$motifs") or die "cant open $motifs\n";
-open (IPDESC,">/wormsrv2/autoace/acefiles/interpro_motifs.ace") or die "cant write /wormsrv2/autoace/acefiles/interpro_motifs.ace\n";
+my $ip_ace_file = $wormbase->acefiles."/interpro_motifs.ace";
+open (I2G,"<$motifs") or $log->log_and_die("cant open $motifs\n");
+open (IPDESC,">$ip_ace_file") or $log->log_and_die("cant write $ip_ace_file\n");
 my %interpro_des;   #IPR000018 => "P2Y4 purinoceptor"
 my $text;
 my $ip;
 
 # IPR000177 Apple domain
-
-print LOG &runtime, ": Writing acefile to /wormsrv2/autoace/acefiles/interpro_motifs.ace\n";
+$log->write_to(": Writing acefile to $ip_ace_file");
 while (<I2G>){
   chomp;
   my @info = split;
   $ip = shift @info;
-  next if( (!defined $ip) or ("$info[0]" eq "entries") ); # header lines
+  next if( (!defined $ip) or (/entries/) ); # header lines
   #Motif : "INTERPRO:IPR000006"
   #Title    "Class I metallothionein"
   #Database         "INTERPRO" "INTERPRO_ID" "IPR000006"
@@ -77,26 +74,16 @@ while (<I2G>){
 }
 
 
-print LOG &runtime, ": script finished\n";
 close IPDESC;
 close I2G;
-close LOG;
 
 # load file if -load was specified
-if($load){
-  print LOG &runtime, ": Loading acefile to autoace\n";
-  my $command = "autoace_minder.pl -load /wormsrv2/autoace/acefiles/interpro_motifs.ace -tsuser interpro_motifs";
- 
-  my $status = system($command);
-  if(($status >>8) != 0){
-    print LOG "ERROR: Loading interpro_motifs.ace file failed \$\? = $status\n";
-  }
-}
+$wormbase->load_to_database( $wormbase->autoace,
+			     "$ip_ace_file",
+			     'interpo_motifs') if($load);
 
 # mail Log file
-&mail_maintainer ("GetInterPro_motifs.pl",$maintainers,$log);
-
-
+$log->mail;
 exit(0);
 
 ###############################
@@ -149,14 +136,6 @@ Database "INTERPRO" "INTERPRO_ID" "IPR000018"
 =item -load
 
 if specified will load resulting acefile to autoace
-
-=back
-
-=head1 REQUIREMENTS
-
-=over 4
-
-=item This script must run on a machine which can see the /wormsrv2 disk.
 
 =back
 
