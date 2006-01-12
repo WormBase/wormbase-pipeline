63fe7fada8078121366d46409b2f4db3f3105523
ar2 <ar2>
2006-01-12 12:18:45 +0000

    changed some paths and now uses split GFF files

--- scripts/WBGene_span.pl
+++ scripts/WBGene_span.pl
@@ -6,8 +6,8 @@
 #
 # Creates SMapped Gene spans for Gene objects
 #
-# Last edited by: $Author: pad $
-# Last edited on: $Date: 2005-12-20 14:37:00 $
+# Last edited by: $Author: ar2 $
+# Last edited on: $Date: 2006-01-12 12:18:45 $
 
 use strict;
 use lib $ENV{'CVS_DIR'};
@@ -15,12 +15,14 @@ use Wormbase;
 use Coords_converter;
 use Getopt::Long;
 use Log_files;
+use Storable;
 
-my ($database, $test, $gff, $no_ace, $debug, $gff_file, $chromosome, $prepare, );
+my ($database, $test, $store, $gff, $no_ace, $debug, $gff_file, $chromosome, $prepare, );
 
 GetOptions (
 	    'database=s'   => \$database,
 	    'test'         => \$test,
+	    "store:s"      => \$store,
 	    'gff'          => \$gff,
 	    'no_ace'       => \$no_ace,
 	    'debug=s'      => \$debug,
@@ -29,36 +31,37 @@ GetOptions (
 	    'prepare'      => \$prepare
 	   );
 
-  my $log = Log_files->make_build_log($debug);
+my $wormbase;
+if ( $store ) {
+  $wormbase = retrieve( $store ) or croak("cant restore wormbase from $store\n");
+} else {
+  $wormbase = Wormbase->new( -debug   => $debug,
+			     -test    => $test,
+			   );
+}
+my $log = Log_files->make_build_log($wormbase);
 
-$database = ($test ? glob("~wormpub/TEST_BUILD/autoace") : glob("~wormpub/autoace") ) unless $database;
+$database = $wormbase->autoace unless $database;
 $log->write_to("Generating WBGene spans from database $database\n");
-
-my @chromosomes = qw( I II III IV V X MtDNA);
 
 # get the required GFF files in place
 if ( $prepare ) {
   # gff dump Coding_transcript
-  &run_command("perl $ENV{'CVS_DIR'}/GFF_method_dump.pl -database $database -method Coding_transcript -dump_dir $database/CHROMOSOMES");
+  &run_command("perl $ENV{'CVS_DIR'}/GFF_method_dump.pl -database $database -method Coding_transcript -dump_dir ".$wormbase->gff_splits);
   
   # cat them to the main gff files
-  foreach my $chrom ( @chromosomes ) {
-    &run_command("cat $database/CHROMOSOMES/CHROMOSOME_${chrom}.Coding_transcript.gff >> $database/CHROMOSOMES/CHROMOSOME_${chrom}.gff");
-  }
-}
-else {
-
-  die "$gff_file doesnt exist" if ($gff_file and !(-e $gff_file) );
-
+  #foreach my $chrom ( @chromosomes ) {
+  # &run_command("cat $database/CHROMOSOMES/CHROMOSOME_${chrom}.Coding_transcript.gff >> $database/CHROMOSOMES/CHROMOSOME_${chrom}.gff");
+} else {
   my %CDS_data;
   my $cds;
   my %gene2seq;
-
+  
   my $def_file = "$database/wquery/WBGene2seq.def";
   $log->log_and_die("Tablemaker definition file WBGene2seq.def is required. Not present in $database/wquery\n") unless (-e "$def_file");
 
-  my $tace = &tace;
-  my $coords = Coords_converter->invoke($database);
+  my $tace = $wormbase->tace;
+  my $coords = Coords_converter->invoke($database,undef, $wormbase);
 
   my $command="Table-maker -p $def_file\nquit\n";
 
@@ -75,37 +78,39 @@ else {
 
   close FH;
 
-
+  
   my @chromosomes = qw( I II III IV V X MtDNA);
-  @chromosomes = qw(III) if $test;
+  @chromosomes = qw(III) if $wormbase->test;
   @chromosomes = ("$chromosome") if $chromosome;
   foreach my $chrom ( @chromosomes ) {
     my %gene_coords;
-    $gff_file = "$database/CHROMOSOMES/CHROMOSOME_${chrom}.gff" unless $gff_file;
-    open (GFF,"<$gff_file") or die "cant open GFF $gff_file\n";
-    while ( <GFF> ) {
-      my @data = split;
-      if ( ($data[1] eq "Coding_transcript") or
-	   ($data[1] eq "Non_coding_transcript") or
-	   ($data[1] eq "Pseudogene") or
-	   ($data[1] eq "curated") or
-	   ($data[1] eq "tRNAscan-SE-1.23") or
-	   ($data[1] eq "tRNA") or
-	   ($data[1] eq "snRNA") or
-	   ($data[1] eq "miRNA") or
-	   ($data[1] eq "rRNA") or
-	   ($data[1] eq "scRNA") or
-	   ($data[1] eq "snoRNA") or
-	   ($data[1] eq "tRNA") or
-	   ($data[1] eq "stRNA") or
-	   ($data[1] eq "snRNA")
-	 ) {
-	next if ( $data[2] eq "exon" or $data[2] eq "coding_exon" or $data[2] eq "intron" );
-	my ($gene) = $data[9] =~ /\"(\w+\.\w+)/;
-	push(@{$gene_coords{$gene}},[($data[3], $data[4], $data[6]) ]);
+    foreach my $method (qw(Coding_transcript Non_coding_transcrip tRNAscan-SE-1.23 miRNA) ) {
+      $gff_file = $wormbase->gff_splits."/CHROMOSOME_${chrom}_$method.gff" unless $gff_file;
+      open (GFF,"<$gff_file") or die "cant open GFF $gff_file\n";
+      while ( <GFF> ) {
+	my @data = split;
+	if ( ($data[1] eq "Coding_transcript") or
+	     ($data[1] eq "Non_coding_transcript") or
+	     ($data[1] eq "Pseudogene") or
+	     ($data[1] eq "curated") or
+	     ($data[1] eq "tRNAscan-SE-1.23") or
+	     ($data[1] eq "tRNA") or
+	     ($data[1] eq "snRNA") or
+	     ($data[1] eq "miRNA") or
+	     ($data[1] eq "rRNA") or
+	     ($data[1] eq "scRNA") or
+	     ($data[1] eq "snoRNA") or
+	     ($data[1] eq "tRNA") or
+	     ($data[1] eq "stRNA") or
+	     ($data[1] eq "snRNA")
+	   ) {
+	  next if ( $data[2] eq "exon" or $data[2] eq "coding_exon" or $data[2] eq "intron" );
+	  my ($gene) = $data[9] =~ /\"(\w+\.\w+)/;
+	  push(@{$gene_coords{$gene}},[($data[3], $data[4], $data[6]) ]);
+	}
       }
+      close GFF;
     }
-    close GFF;
     my %gene_span;
   WBG:foreach my $WBgene ( keys %gene2seq ) {
       foreach my $CDS ( @{$gene2seq{$WBgene}} ) {
@@ -126,41 +131,38 @@ else {
 	  }
 	}
       }
-      if ( $gff ) {
-	open (OUTGFF,">$database/CHROMOSOMES/CHROMOSOME_${chrom}.WBgene.gff") or do{ $log->write_to("cant open output\n"); die "cant open output\n"; }
-      }
-      my $acefile = "$database/acefiles/WBgene_spans_${chrom}.ace";
-      unless ( $no_ace) { 
-	open (ACE,">$acefile") or do{ $log->write_to("cant open output $acefile:\t$!\n"); die "cant open output $acefile:\t$!\n";}
-      }
-      foreach my $gene ( keys %gene_span ) {
-
-	if ( $gff ) {
-	  print OUTGFF "CHROMOSOME_${chrom}\tgene\tgene\t";
-	  print OUTGFF $gene_span{$gene}->{'min'},"\t",$gene_span{$gene}->{'max'},"\t";
-	  print OUTGFF ".\t",$gene_span{$gene}->{'strand'},"\t.\tGene \"$gene\"\n";
-	}
-
-	# write S-map details back to database
-	unless ($no_ace) {
-	  my @coords;
-	  @coords = $gene_span{$gene}->{'strand'} eq "+" ? $coords->LocateSpan($chrom,$gene_span{$gene}->{'min'},$gene_span{$gene}->{'max'}) : $coords->LocateSpan($chrom,$gene_span{$gene}->{'max'},$gene_span{$gene}->{'min'});
-	  print ACE "\nSequence : \"$coords[0]\"\n";
-	  print ACE "Gene_child $gene $coords[1] $coords[2]\n";
-	} 
-      }
-      close ACE;
-      close OUTGFF;
-      &load_to_database("$database","$acefile","WBGene_span") unless ( $no_ace ); # I know this would be better done all together but . . 
-      undef $gff_file;
-
-      last if $test;
     }
+    if ( $gff ) {
+      open (OUTGFF,$wormbase->gff_splits."/CHROMOSOME_${chrom}_WBgene.gff") or do{ $log->write_to("cant open output\n"); die "cant open output\n"; }
+    }
+    my $acefile = $wormbase->acefiles."/WBgene_spans_${chrom}.ace";
+    unless ( $no_ace) { 
+      open (ACE,">$acefile") or do{ $log->write_to("cant open output $acefile:\t$!\n"); die "cant open output $acefile:\t$!\n";}
+    }
+    foreach my $gene ( keys %gene_span ) {
+      if ( $gff ) {
+	print OUTGFF "CHROMOSOME_${chrom}\tgene\tgene\t";
+	  print OUTGFF $gene_span{$gene}->{'min'},"\t",$gene_span{$gene}->{'max'},"\t";
+	print OUTGFF ".\t",$gene_span{$gene}->{'strand'},"\t.\tGene \"$gene\"\n";
+      }
+
+      # write S-map details back to database
+      unless ($no_ace) {
+	my @coords;
+	@coords = $gene_span{$gene}->{'strand'} eq "+" ? $coords->LocateSpan($chrom,$gene_span{$gene}->{'min'},$gene_span{$gene}->{'max'}) : $coords->LocateSpan($chrom,$gene_span{$gene}->{'max'},$gene_span{$gene}->{'min'});
+	print ACE "\nSequence : \"$coords[0]\"\n";
+	print ACE "Gene_child $gene $coords[1] $coords[2]\n";
+      } 
+    }
+    close ACE;
+    close OUTGFF;
+    $wormbase->load_to_database("$database","$acefile","WBGene_span") unless ( $no_ace ); # I know this would be better done all together but . . 
+    undef $gff_file;
   }
 }
 $log->mail;
 
-exit(0); 
+exit(0);
 
 
 =pod
