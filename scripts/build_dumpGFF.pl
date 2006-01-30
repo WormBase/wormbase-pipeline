ef5f2a2fcb2acac6e2636b0f83254a9570a4359e
ar2 <ar2>
2006-01-30 15:35:25 +0000

    At certain stages of the build there are sets of data that need to be dumped to GFF files. This script contains a list of methods that are dumped throughout the build orgained in to stages such as 'init' so that they can be dumped at the appropriate times.

---
+++ scripts/build_dumpGFF.pl
@@ -0,0 +1,95 @@
+#!/usr/local/bin/perl -w
+# Last edited by: $Author: ar2 $
+# Last edited on: $Date: 2006-01-30 15:35:25 $
+
+use strict;
+use lib  $ENV{'CVS_DIR'};
+use Wormbase;
+use Storable;
+use Getopt::Long;
+use Log_files;
+
+our ($help, $debug, $test, $stage);
+my $store;
+
+GetOptions ("help"         => \$help,
+            "debug=s"      => \$debug,
+	    "test"         => \$test,
+	    "store:s"      => \$store,
+	    "stage:s"      => \$stage
+	   );
+
+my $wormbase;
+if( $store ) {
+  $wormbase = retrieve( $store ) or croak("cant restore wormbase from $store\n");
+}
+else {
+  $wormbase = Wormbase->new( -debug   => $debug,
+			     -test    => $test,
+			   );
+}
+
+my $log = Log_files->make_build_log($wormbase);
+$log->log_and_die("stage not specified\n") unless defined $stage;
+
+my $methods;
+my @methods;
+READARRAY: while (<DATA>) {
+  chomp;
+  my ($type,$method) = split;
+  push(@methods,"$method") if ( $type eq $stage) ;
+}
+$methods = join(',',@methods);
+
+$log->write_to("Dumping methods $methods from ".$wormbase->autoace."\n");
+
+$wormbase->run_script("dump_gff_batch.pl -database ".$wormbase->autoace." -methods $methods -dump_dir ".$wormbase->autoace."/GFF_SPLITS",$log);
+
+$log->mail();
+exit(0);
+
+__DATA__
+init Genomic_canonical
+init Link
+init Pseudogene
+init Transposon
+init Transposon_CDS
+init curated
+init history
+init miRNA
+init tRNAscan-SE-1.23
+init Non_coding_transcript
+init SAGE_transcript
+init snRNA
+init miRNA
+init rRNA
+init scRNA
+init snoRNA
+init tRNA
+init stRNA
+init snRNA
+blat BLAT_EMBL_BEST
+blat BLAT_EMBL_OTHER
+blat BLAT_EST_BEST
+blat BLAT_EST_OTHER
+blat BLAT_NEMATODE
+blat BLAT_OST_BEST
+blat BLAT_OST_OTHER
+blat BLAT_TC1_BEST
+blat BLAT_TC1_OTHER
+blat BLAT_mRNA_BEST
+blat BLAT_mRNA_OTHER
+blat BLAT_ncRNA_BEST
+blat BLAT_ncRNA_OTHER
+blat SL1
+blat SL2
+blat polyA_signal_sequence
+blat polyA_site
+homol waba_coding
+homol waba_strong
+homol waba_weak
+homol tandem
+homol RepeatMasker
+homol wublastx
+homol inverted
+__END__
