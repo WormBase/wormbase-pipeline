77b4ce3abfc927799ded6d0a03ca3e6f3186c75f
ar2 <ar2>
2006-02-13 11:41:56 +0000

    cat ace files on wormpipe and copy single file for loading

--- scripts/further_BLAST_dump.pl
+++ scripts/further_BLAST_dump.pl
@@ -2,41 +2,45 @@
 #
 # further_BLAST_dump.pl
 #
-# script to copy files resulting from blast pipeline on acari to wormsrv2
+# script to copy files resulting from blast pipeline on acari to build directories
 #
 # Author: Chao-Kung CHen
 #
 # Last updated by: $Author: ar2 $
-# Last updated on: $Date: 2005-12-16 11:18:55 $
+# Last updated on: $Date: 2006-02-13 11:41:56 $
 
 use strict;
-use lib "/wormsrv2/scripts/";
+use lib $ENV{'CVS_DIR'};
 use Wormbase;
 use Log_files;
 use Getopt::Long;
 use File::Path;
+use Storable;
 
-my $debug;
-my $test;
+my ($help, $debug, $test, $verbose, $store, $wormbase);
 
-GetOptions(
-	   "debug=s"     => \$debug,
-	   "test"        => \$test,
+GetOptions ("help"       => \$help,
+            "debug=s"    => \$debug,
+	    "test"       => \$test,
+	    "verbose"    => \$verbose,
+	    "store"      => \$store,
 	  );
 
-my $log = Log_files->make_build_log($debug);
+if ( $store ) {
+  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+} else {
+  $wormbase = Wormbase->new( -debug   => $debug,
+                             -test    => $test,
+			     );
+}
 
-my $source_dir    = "/acari/work2a/wormpipe/dumps";
-my $target_dir = "/wormsrv2/wormbase/ensembl_dumps";
-$target_dir = glob("~wormpub/TEST_BUILD/wormbase/ensembl_dumps") if $test;
+my $log = Log_files->make_build_log($wormbase);
 
-mkpath($target_dir,0) unless ( -e $target_dir);
-mkpath("$target_dir/BACKUP",0) unless (-e "${target_dir}/BACKUP");
+my $source_dir    = "acari:/acari/work2a/wormpipe/dumps";
+my $target_dir = $wormbase->acefiles;
 
-chdir "$target_dir";
 
 my @files        = (
-		    "ipi_hits.ace",
 		    "worm_pep_best_blastp_hits",
 		    "worm_brigpep_best_blastp_hits",
 		    "TRF.ace",
@@ -48,49 +52,30 @@ my @files        = (
 		    "worm_brigpep_motif_info.ace",
 		    "worm_pep_interpro_motif_info.ace",
 		    "worm_brigpep_interpro_motif_info.ace",
-		    "swissproteins.ace",
-		    "tremblproteins.ace",
 		   );
 
 foreach my $file (@files){
-  if( ( $file eq "TRF.ace" or $file eq "repeat_homologies.ace") and !(-e $file) ){
-    next;
-  }
   if ( -e $file ) {
-    $log->write_to("gzipping and backing up $file\n");
-    &run_command("gzip -f ${target_dir}/$file > ${target_dir}/BACKUP/$file.gz");
+    $log->write_to("scping new version of $file\n");
+    &run_command("scp ${source_dir}/${file} ${target_dir}/${file}");
   }
   else {
     $log->write_to($file." does not exist\n");
   }
-
-  $log->write_to("scping new version of $file\n");
-  &run_command("scp acari:${source_dir}/${file} ${target_dir}/${file}");
 }
 
+#will do this as part of blast pipeline and only copy single file
+#$log->write_to("creating new ensembl_protein_info.ace\n");
+#&run_command("cat ipi_hits.ace flybase.ace yeast.ace swissproteins.ace tremblproteins.ace brigpep.ace > ensembl_protein_info.ace");
 
-# Also move big ensembl_protein file to BACKUP dir before making new one. 
-if (-e "$target_dir/ensembl_protein_info.ace") {
-  $log->write_to("backing up protein info\n");
-  &run_command("gzip -f $target_dir/ensembl_protein_info.ace > $target_dir/BACKUP/ensembl_protein_info.ace.gz");
-}
-$log->write_to("creating new ensembl_protein_info.ace\n");
-&run_command("cat ipi_hits.ace flybase.ace yeast.ace swissproteins.ace tremblproteins.ace brigpep.ace > ensembl_protein_info.ace");
+my $farm_ace = glob("~wormpipe/acefiles") ;  # this is the only place where a path is specified outside of Wormbase.pm as cant access wormpipe and wormpub acefiles at same time
+
+$wormbase->run_command("cat $farm_ace/flybase.ace $farm_ace/yeast.ace $farm_ace/ipi_human.ace $farm_ace/swissproteins.ace $farm_ace/tremblproteins.ace >! $farm_ace/ensembl_protein_info.ace");
+&run_command("scp $farm_ace/ensembl_protein_info.ace  ${target_dir}/ensembl_protein_info.ace");
+
+#will do this as part of blast pipeline and only copy single file
+#$log->write_to("creating new ensembl_protein_info.ace\n");
+#&run_command("cat ipi_hits.ace flybase.ace yeast.ace swissproteins.ace tremblproteins.ace brigpep.ace > ensembl_protein_info.ace");
 
 $log->mail;
 exit(0);
-
-
-##################################################################################################################
-
-
-sub run_command{
-  my $command = shift;
-  my $status = system($command);
-  if($status != 0){
-    $log->write_to("ERROR: $command failed\n");
-  }
-
-  # for optional further testing by calling subroutine
-  return($status);
-}
