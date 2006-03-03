c39ddd91a88986a49dd6c7ec1e84bcf4ed76d689
gw3 <gw3>
2006-03-03 17:06:20 +0000

    added routine to write human-readable display of changes and indications of when a location has been changed

--- scripts/Modules/Remap_Sequence_Change.pm
+++ scripts/Modules/Remap_Sequence_Change.pm
@@ -13,7 +13,7 @@
 #      COMPANY:
 #     $Version:  $
 #      CREATED: 2006-02-27
-#        $Date: 2006-02-27 16:34:16 $
+#        $Date: 2006-03-03 17:06:20 $
 #===============================================================================
 package Remap_Sequence_Change;
 
@@ -91,12 +91,15 @@ sub read_mapping_data {
 #                  release WS140 to the coordinates of release WS150
 #            @mapping_data - data as returned by read_mapping_data
 # Returns:   $new_start, $new_end, $new_sense - the updated location coordinates
+#            $indel - true if indels affect this location
+#            $change - true if any sort of changes affect this location
 #
 
 sub remap_ace {
   my ($chromosome, $start, $end, $release1, $release2, @mapping_data) = @_;
                       
   my $sense = "+";
+  my ($indel, $change);
 
   if ($start > $end) {
     $sense = "-";
@@ -105,7 +108,7 @@ sub remap_ace {
     $end = $tmp;
   }
 
-  ($start, $end, $sense) = remap_gff($chromosome, $start, $end, $sense, $release1, $release2, @mapping_data);
+  ($start, $end, $sense, $indel, $change) = remap_gff($chromosome, $start, $end, $sense, $release1, $release2, @mapping_data);
 
   if ($sense eq '-') {
     my $tmp = $start;
@@ -113,7 +116,7 @@ sub remap_ace {
     $end = $tmp;
   }
                                                    
-  return ($start, $end);
+  return ($start, $end, $indel, $change);
 
 }
 
@@ -131,10 +134,15 @@ sub remap_ace {
 #                  release WS140 to the coordinates of release WS150
 #            @mapping_data - data as returned by read_mapping_data
 # Returns:   $new_start, $new_end, $new_sense - the updated location coordinates
+#            $indel - true if indels affect this location
+#            $change - true if any sort of changes affect this location
 #
 
 sub remap_gff {
   my ($chromosome, $start, $end, $sense, $release1, $release2, @mapping_data) = @_;
+
+  my $indel = 0;		# true if indels affect this location
+  my $change = 0;		# true if non-indel base changes affect this location
                                                                                                                                                             
   if ($chromosome =~ /CHROMOSOME_(\S+)/) {$chromosome = $1;}
                                                                                                                                                             
@@ -166,11 +174,25 @@ sub remap_gff {
               $start = $end;
               $end = $tmp;
             }
-          }
+
+          # does the edge of the flipped region overlap our location?
+          } elsif ($start >= $mismatch_start1 && $start < $mismatch_end1 ||		
+		   $end >= $mismatch_start1 && $end < $mismatch_end1) {
+            # don't change the location, but note that we have changes
+	    $change = 1;                                        
+	  }
                                                                                                                                                             
         } else {
+	  
+	  # if there is a change inside our location, note it
+	  if ($start < $mismatch_end1 && $end >= $mismatch_start1) {
+	    $change = 1;
+	  }
+
+	  # note the length of our location so we can see any indels occurring
+	  my $location_length = $end - $start;
+
           # if the start or end are beyond the start of the change region, apply any shift
-                                                                                                                                                            
           if ($start >= $mismatch_end1) { # if past the end of the change region, shift it
             $start += $len2 - $len1;
           } elsif ($start >= $mismatch_start1 && $start - $mismatch_start1 > $len2 ) { # if was in the change region and now out, set it to the end
@@ -182,8 +204,11 @@ sub remap_gff {
           } elsif ($end >= $mismatch_start1 && $end - $mismatch_start1 > $len2) { # if was in the change region and now out, set it to the end
             $end = $mismatch_start1 + $len2;
           }
-                                                                                                                                                            
-                                                                                                                                                            
+
+	  # see if we have any indels in our location
+	  if ($location_length != $end - $start) {
+	    $indel = 1;
+	  }
         }
                                                                                                                                                             
       }
@@ -193,12 +218,67 @@ sub remap_gff {
   }
                                                                                                                                                             
                                                                                                                                                             
-  return ($start, $end, $sense);
+  return ($start, $end, $sense, $indel, $change);
 }
 
 
+##########################################################
+# 
+# Name:      write_changes
+# Usage:     $changes = write_changes($release, @mapping_data);
+# Function:  returns the chromosomal changes to put in the release notes
+# Args:      $release - the release to display changes for
+#            @mapping_data - data as returned by read_mapping_data
+# Returns:   text of the changes in a readable form
+#
 
+sub write_changes {
+  my ($release, @mapping_data) = @_;
+ 
+  my $text;
+  my $title = "Chromosomal Changes:\n--------------------\n";
+  my $no_changes = "There are no changes to the chromosome sequences in this release.\n";
+  my $any_changes = 0;
+  my @chromosomes = qw(I II III IV V X);
+           
+  foreach my $chromosome (@chromosomes) {
+                                                                                                                                                            
+    if (exists $mapping_data[$release]{$chromosome}) {
 
+      $any_changes = 1;
+      $text .= "\nChromosome: $chromosome\n";
+
+      foreach  my $fields (@{$mapping_data[$release]{$chromosome}}) {
+# The mismatch_start value is the start of the mismatch, it is the first position which doesn't match.
+# The mismatch_end value is the base past the end of the mismatch region, the first base which matches again
+# ($mismatch_start1, $mismatch_end1, $len1, $mismatch_start2, $mismatch_end2, $len2, $flipped)
+        my ($mismatch_start1, $mismatch_end1, $len1, $mismatch_start2, $mismatch_end2, $len2, $flipped) = @$fields;
+
+# N.B. mismatch values are in the normal perl coordinate system starting at position 0.
+# Convert them to the human-readable coordinates system starting at position 1.
+	$mismatch_start1++;
+#	$mismatch_end1++; # don't increment this so it becomes the end of the changed region
+	$mismatch_start2++;
+#	$mismatch_end2++; # don't increment this so it becomes the end of the changed region
+             
+	$text .= "$mismatch_start1 $mismatch_end1 $len1   ->   $mismatch_start2 $mismatch_end2 $len2";
+                                                                                                                                               
+        if ($flipped) {
+	  $text .= " REVERSED";
+	}
+	
+	$text .= "\n";
+
+      }
+    }
+  }
+
+  if (!$any_changes) {
+    $text = $no_changes;
+  }
+
+  return $title . $text;
+}
 
 
 1;
@@ -228,21 +308,21 @@ Module for remapping chromosomal sequence locations across releases.
 
 =item @mapping_data = &read_mapping_data($release1, $release2);
 
-Reads in the mapping information from the files craeted during the
+Reads in the mapping information from the files created during the
 build of each release.
 
 =back
 
 =over 4
 
-=item ($new_start, $new_end) = remap_ace($chromosome, $start, $end, @mapping_data);
+=item ($new_start, $new_end, $indel, $change) = remap_ace($chromosome, $start, $end, @mapping_data);
 
 Maps a start and end location pair of ACE file chromosomal
 coordinates, to the new coordinates.
 
 =back
 
-=item ($new_start, $new_end, $new_sense) = remap_ace($chromosome, $start, $end, $sense, @mapping_data);
+=item ($new_start, $new_end, $new_sense, $indel, $change) = remap_ace($chromosome, $start, $end, $sense, @mapping_data);
 
 Maps a start and end location pair of GFF file chromosomal
 coordinates, with their sense ("+" or "-") to the new coordinates.
