c5c0dca77cce7621b08fc834f5a2e3ea83152ee8
mh6 <mh6>
2006-03-14 09:21:26 +0000

    put the line back in where it calls make_UTR_GFF.pl

--- scripts/autoace_builder.pl
+++ scripts/autoace_builder.pl
@@ -7,7 +7,7 @@
 # Usage : autoace_builder.pl [-options]
 #
 # Last edited by: $Author: mh6 $
-# Last edited on: $Date: 2006-03-13 18:30:41 $
+# Last edited on: $Date: 2006-03-14 09:21:26 $
 
 my $script_dir = $ENV{'CVS_DIR'};
 use lib $ENV{'CVS_DIR'};
@@ -212,6 +212,7 @@ sub map_features {
 sub make_UTR {
   my ($log)=@_;
   foreach (qw( I II III IV V X ) ) {
+	  $wormbase->run_script("make_UTR_GFF.pl -chromosome $_",$log)
   }
 }
 
