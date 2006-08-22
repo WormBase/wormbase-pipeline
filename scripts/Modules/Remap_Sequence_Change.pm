afe36d559b1422ae51c96a179d08e71c75aac292
gw3 <gw3>
2006-08-22 09:38:00 +0000

    added routine unmap_gff to unmake changes between versions

--- scripts/Modules/Remap_Sequence_Change.pm
+++ scripts/Modules/Remap_Sequence_Change.pm
@@ -13,7 +13,7 @@
 #      COMPANY:
 #     $Version:  $
 #      CREATED: 2006-02-27
-#        $Date: 2006-05-16 15:46:09 $
+#        $Date: 2006-08-22 09:38:00 $
 #===============================================================================
 package Remap_Sequence_Change;
 
@@ -206,6 +206,106 @@ sub remap_gff {
             $end += $len2 - $len1;
           } elsif ($end >= $mismatch_start1 && $end - $mismatch_start1 > $len2) { # if was in the change region and now out, set it to the end
             $end = $mismatch_start1 + $len2;
+          }
+
+	  # see if we have any indels in our location
+	  if ($location_length != $end - $start) {
+	    $indel = 1;
+	  }
+        }
+                                                                                                                                                            
+      }
+    } else {
+      #print "no change: doesn't exist: $release $chromosome\n";
+    }
+  }
+                                                                                                                                                            
+                                                                                                                                                            
+  return ($start, $end, $sense, $indel, $change);
+}
+##########################################################
+# 
+# Name:      unmap_gff
+# Usage:     ($new_start, $new_end, $new_sense, $indel, $change) = remap_gff($chromosome, $start, $end, $sense, $release2, $release1, @mapping_data);
+# Function:  does the unmapping (remapping backwards to past versions) of a pair of location values for a GFF file
+# Args:      $chromosome, the chromosome number, e.g. 'III'
+#            $start, the start value of the chromosomal location coordinate
+#            $end, the end value of the chromosomal location coordinate (always >= $start)
+#            $sense, the sense "+" or "-" of the coordinate
+#            $release2, $release1, the last and first wormbase release
+#                  numbers to use e.g. 150, 140 to convert data made using wormbase
+#                  release WS150 to the coordinates of release WS140
+#            @mapping_data - data as returned by read_mapping_data
+# Returns:   $new_start, $new_end, $new_sense - the updated chromosomal location coordinates
+#            $indel - true if indels affect this location
+#            $change - true if any sort of changes affect this location
+
+
+sub unmap_gff {
+  my ($chromosome, $start, $end, $sense, $release2, $release1, @mapping_data) = @_;
+
+  my $indel = 0;		# true if indels affect this location
+  my $change = 0;		# true if non-indel base changes affect this location
+                                                                                                                                                            
+  if ($chromosome =~ /CHROMOSOME_(\S+)/) {$chromosome = $1;}
+                                                                                                                                                            
+  foreach my $release ($release2 .. ($release1+1)) {
+                                                                                                                                                            
+    if (exists $mapping_data[$release]{$chromosome}) {
+      foreach  my $fields (@{$mapping_data[$release]{$chromosome}}) {
+        #print "$release $chromosome fields= @$fields \n";
+
+# The mismatch_start value is the start of the mismatch, it is the first position which doesn't match.
+# The mismatch_end value is the base past the end of the mismatch region, the first base which matches again
+# ($mismatch_start1, $mismatch_end1, $len1, $mismatch_start2, $mismatch_end2, $len2, $flipped)
+        my ($mismatch_start1, $mismatch_end1, $len1, $mismatch_start2, $mismatch_end2, $len2, $flipped) = @$fields;
+
+# N.B. mismatch values are in the normal perl coordinate system starting at position 0.
+# Convert them to the GFF coordinates system starting at position 1.
+	$mismatch_start2++;
+	$mismatch_end2++;
+                                                                                                                                                            
+        if ($flipped) {    # is the feature inside a flipped region?
+                                                                                                                                                            
+          if ($start >= $mismatch_start2 && $end < $mismatch_end2) {
+                                                                                                                                                            
+            if ($sense eq '+') {$sense = '-';} else {$sense = '+';} # flip the sense
+            $start = $mismatch_start2 + $mismatch_end2 - $start; # flip the start and end positions
+            $end = $mismatch_start2 + $mismatch_end2 - $end;
+            if ($start > $end) {
+              my $tmp = $start;
+              $start = $end;
+              $end = $tmp;
+            }
+
+          # does the edge of the flipped region overlap our location?
+          } elsif ($start >= $mismatch_start2 && $start < $mismatch_end2 ||		
+		   $end >= $mismatch_start2 && $end < $mismatch_end2) {
+            # don't change the location, but note that we have changes
+	    $change = 1;                                        
+	  }
+                                                                                                                                                            
+        } else {
+	  
+	  # if there is a change inside our location, note it
+	  if ($start < $mismatch_end2 && $end >= $mismatch_start2) {
+	    $change = 1;
+	  }
+
+	  # note the length of our location so we can see any indels occurring
+	  my $location_length = $end - $start;
+
+          # if the start or end are beyond the start of the change region, apply any shift
+          if ($start >= $mismatch_end2) { # if past the end of the change region, shift it
+            $start += $len1 - $len2;
+          } elsif ($start >= $mismatch_start2 && $start - $mismatch_start2 > $len1 ) { # if was in the change region and now out, set it to the end
+            $start = $mismatch_start2 + $len1;
+          }
+                                                                                                                                                            
+          if ($end >= $mismatch_end2) { # if past the end of the change region, shift it
+            $end += $len1 - $len2;
+          } elsif ($end >= $mismatch_start2 && $end - $mismatch_start2 > $len1) { # if was in the change region and now out, set it to the end
+            $end = $mismatch_start2 + $len1;
           }
 
 	  # see if we have any indels in our location
