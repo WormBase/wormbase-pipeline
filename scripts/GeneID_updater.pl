d3bab5f8405f155d33bfd42387a7ac255bbb448c
pad <pad>
2006-09-07 15:36:11 +0000

    Improved logging and documentation. Removed duplicated code and made simpler, added code to refresh SV clone data.

--- scripts/GeneID_updater.pl
+++ scripts/GeneID_updater.pl
@@ -4,24 +4,26 @@
 # 
 # by Paul Davis
 #
-# Script to refresh the CDS->WBGene connections in a chosen database from a chosen reference database.
-# Script also refreshes Protein_IDs in the chosen database from the latest build.
+# Script to refresh various information including WBGene ID's, protein_ids, clone SV's in a chosen database from a chosen reference database.
 #
-# Last updated by: $Author: ar2 $
-# Last updated on: $Date: 2006-04-12 14:25:50 $
+# Last updated by: $Author: pad $
+# Last updated on: $Date: 2006-09-07 15:36:11 $
 
 use strict;
+my $scriptdir =  $ENV{'CVS_DIR'};
 use lib $ENV{'CVS_DIR'};
 use Wormbase;
 use Getopt::Long;
 use Carp;
 use Storable;
+use IO::Handle;
+use Socket;
 
 ######################################
 # variables and command-line options #
 ######################################
 
-my ($help, $debug, $geneID, $targetDB, $sourceDB, $fileout, $update, $public, $sourceDB2, $proteinID, $version, $test, $store, $wormbase);
+my ($help, $debug, $geneID, $targetDB, $sourceDB, $update, $public, $sourceDB2, $proteinID, $version, $test, $store, $wormbase, $sv, $output_dir, $verbose, $all);
 
 GetOptions (
 	    'help'         => \$help,       #help documentation.
@@ -31,12 +33,15 @@ GetOptions (
 	    'sourceDB=s'   => \$sourceDB,   #source for gene id info
     	    'sourceDB2=s'  => \$sourceDB2,  #source for protein id's
 	    'targetDB=s'   => \$targetDB,   #target db
-	    'fileout=s'    => \$fileout,    #specify an output file
+	    'output_dir=s' => \$output_dir, #Specify your own output directory
 	    'proteinID'    => \$proteinID,  #update protein id's option
 	    'update'       => \$update,     #load ace files automatically into target db.
 	    'public'       => \$public,     #retrieve public name info ....future
 	    'version=s'    => \$version,    #version number for properly directing out files in future
 	    'store:s'      => \$store,      #storable object
+	    'sv'           => \$sv,         #check sequence versions against embl
+	    'verbose'      => \$verbose,    # Verbose output into log messages
+	    'all'          => \$all,        #Do ALL (GeneID, ProteinID, Sequence version,) syncronisations.
 	    );
 
 
@@ -49,14 +54,17 @@ else {
 			     );
 }
 
-#load know anomolies to be ignored
 my %exceptions;
-&load_exceptions;
-
 my $output_file;
 my $output_file2;
+my $output_file3;
 my %models2geneID;
-my $version_no = ($version + 1);
+
+if (!$version) {
+  $version = "666";
+}
+
+my $next_build = ($version + 1);
 
 # tace executable path
 my $tace = $wormbase->tace;
@@ -67,58 +75,62 @@ my $tace = $wormbase->tace;
 # establish log file.
 my $log = Log_files->make_build_log($wormbase);
 
+my $wormpub = "/nfs/disk100/wormpub";
+
+if (!$output_dir) {
+$output_dir = "$wormpub/camace_orig/WS${version}-WS${next_build}";
+print "Using $output_dir\n\n" if ($verbose);
+}
+
 ##########################
 # MAIN BODY OF SCRIPT
 ##########################
 
+#load know anomolies to be ignored
+&load_exceptions;
+
 # Specify Paths and files
-my $def_dir = "/nfs/disk100/wormpub/DATABASES/current_DB/wquery";
+my $def_dir = "$wormpub/DATABASES/current_DB/wquery";
 my $tablemaker_query =  "${def_dir}/SCRIPT:GeneID_updater.pl.def";
 my $tablemaker_query2 = "${def_dir}/SCRIPT:HXcds2protID.def";
-my $canonical = "/nfs/disk100/wormpub/DATABASES/camace";
+my $canonical = "$wormpub/DATABASES/camace";
 
 # Select and check source databases.
 if ($sourceDB) {
   $sourceDB = $sourceDB;
 }
 elsif (!$sourceDB) {
-  $sourceDB = "/nfs/disk100/wormpub/DATABASES/geneace";
+  $sourceDB = "$wormpub/DATABASES/geneace";
 }
 if ($targetDB) {
   $targetDB = $targetDB;
 }
 elsif (!$targetDB) {
-#$targetDB = "/nfs/disk100/wormpub/camace_pad";
+#$targetDB = "$wormpub/camace_pad";
 $targetDB = "$canonical";
 }
-if ($proteinID) {
-  $sourceDB2 ="/nfs/disk100/wormpub/DATABASES/current_DB";
+if ($proteinID || $all) {
+  $sourceDB2 ="$wormpub/DATABASES/current_DB";
 }
 
-#Check and set output file
-if ($fileout) {
-  $output_file = $fileout;
-}
-elsif (!$fileout) {
-  $output_file = "/nfs/disk100/wormpub/camace_orig/acefiles/updated_geneIDs_WS${version_no}.ace";
-}
 
 ########################################################################
 # make database connection for extracting WBGene<=>Sequence_name data  #
 # Method   : tace/tablemaker                                           #
-# SourceDB : /nfs/disk100/wormpub/DATABASES/geneace                    # 
+# SourceDB : $wormpub/DATABASES/geneace                    # 
 ########################################################################
 
-if ($geneID) {
+if ($geneID || $all) {
+$output_file = "$output_dir/updated_geneIDs_WS${next_build}\.ace";
 $log->write_to("\n==============================================================================================
 geneID option selected, updating WBGeneID connections in $targetDB
 ----------------------------------------------------------------------------------------------\n");
 $log->write_to("\nSOURCE Database for Gene IDs: $sourceDB.\n");
 $log->write_to("TARGET Database: $targetDB\n");
-$log->write_to("OUTPUT FILE:$output_file.\n\n") if ($geneID);
+$log->write_to("OUTPUT FILE:$output_file.\n\n");
 $log->write_to("Opening database connection.....\n/1/ Gathering data from $sourceDB; Object Class and GeneID\n");
     
-# connect to AceDB using TableMaker, but use /nfs/disk100/wormpub/DATABASES/current_DB/wquery for Table-maker definition
+# connect to AceDB using TableMaker, but use $wormpub/DATABASES/current_DB/wquery for Table-maker definition
 my $command = "Table-maker -p $tablemaker_query\nquit\n";
 my $gene;
 my $name;
@@ -147,11 +159,11 @@ close TACE;
 ####################################################################
 # make database connections for looping through elegans subclasses #
 # Method   : AcePerl                                               # 
-# TargetDB : /nfs/disk100/wormpub/DATABASES/camace                 # 
+# TargetDB : $wormpub/DATABASES/camace                 # 
 ####################################################################
 
 $log->write_to("/2/ Gathering data from $targetDB; Object Class and Name\n\n");
-open (OUT, ">$output_file") or die "Can't write output .acefile: $output_file\n";
+open (OUT, ">$output_file") or die "Can't write output to $output_file\n";
 
 # Retrieve model names and assign to a hash#
 my @classes = ('All_genes');
@@ -176,6 +188,7 @@ foreach my $class (@classes) {
 	$name = $obj->name;
 	my $history;
         # histories AH6.1:wp999
+	next if( $exceptions{$name} )  ;
 	if ($name =~ /(\S+.+)\:(\S+.+)/) {
 	    $lookupname = $1;
 	    $history    = 1;
@@ -183,12 +196,10 @@ foreach my $class (@classes) {
 	else {
 	    $lookupname = $name;
 	}
-
         # remove isoform letters from models with a cosmid.no name
 	$lookupname =~ s/[a-z]$// unless ($lookupname =~ /\S+\.\D/);
 
         # lookup GeneID keyed on lookupname
-	next if( $exceptions{$lookupname} )  ;
 	$geneID = $models2geneID{$lookupname};
 		
         # Print out ace file full model name and modified class name.
@@ -218,8 +229,8 @@ close OUT;
 ########################
 #  Refresh ProteinIDs  #
 ########################
-if ($proteinID) {
-  $output_file2 = "/nfs/disk100/wormpub/camace_orig/acefiles/updated_proteinIDs_WS${version_no}.ace";
+if ($proteinID || $all) {
+  $output_file2 = "$output_dir/updated_proteinIDs_WS${next_build}.ace";
   $log->write_to("\n\n==============================================================================================\nproteinID option selected, updating WP:Protein_ID connections in $targetDB\n----------------------------------------------------------------------------------------------\n\n");
   $log->write_to("SOURCE Database for Protein IDs: $sourceDB2.\n");
   $log->write_to("OUTPUT FILE: $output_file2\n\n"); 
@@ -256,7 +267,79 @@ print OUT2 "protein_id	\"MTCE\"  \"CAA38162.1\"\n";
   close OUT2;
 }
 
-##Logging##
+########################################
+#  Check Sequence versions of cosmids  #
+########################################
+
+if ($sv || $all) {
+my $continue;
+my $EM_acc;
+my $EM_seqver;
+my ($EM_rel,$EM_ver,$EM_sub);
+my ($clone,$acc,$ver);
+
+
+$log->write_to("\n==============================================================================================
+SV option selected, updating Clone Sequence Versions connections in $targetDB
+----------------------------------------------------------------------------------------------\n");
+$log->write_to("\nComparing SV\'s in $targetDB against GETZ query\n\n");
+$output_file3 = "$output_dir/sequence_version_update_WS${next_build}.ace";
+$log->write_to("OUTPUT FILE: $output_file3\n\n");
+my $command = "nosave\nTable-maker -f $targetDB/wquery/sequence_versions.def\nquit\n";
+
+#Genereate files and connections.
+open (OUT3,  ">$output_file3") or die "Cannot open output file $output_file3\n";
+print OUT3 "//Sequence version update ace file generated by check_sequence_versions\n";
+open (LIST, "echo '$command' | $tace $targetDB | ") or die "cant open $targetDB\n";
+
+#Process data retrieved from database.
+while (<LIST>) {
+  if (/acedb>/) {$continue = 1;next;}
+  next until ($continue);
+  chomp;
+  s/\"//g;
+  $log->write_to("WormBase Line = $_\n")if ($debug);
+  ($clone,$acc,$ver) = (/^(\S+)\s+\S+\s+(\S+)\.(\d+)/);
+  print "Clone: $clone\nAccession: $acc\nSV: $ver\n" if ($verbose);
+  $log->write_to("WB  Sequence version $ver\n") if ($debug);
+
+#Retrieve data from getz embl release.
+  open (GETZ, "/usr/local/pubseq/bin/getz -f \"id\" \'([EMBLNEW-acc:$acc]) | (([EMBL-acc:$acc])!(EMBL<EMBLNEW))\' | ");
+  while (<GETZ>) {
+    chomp;
+    $log->write_to("EMBL Line = $_\n") if ($debug);
+    
+    if (/^ID\s+(\S+);\s+\S+\s+(\d+);/) {
+      ($EM_acc,$EM_seqver) = ($1,$2);
+      $log->write_to("EM  Sequence version $EM_seqver\n") if ($debug);
+    }
+    if ($EM_seqver == $ver){
+      print "-----------------------\nResult: Cool\n-----------------------\n" if ($verbose);
+    }
+    else {
+      $log->write_to("ERROR: WB=$ver EMBL=$EM_seqver\n");
+      print OUT3 "\nSequence : \"$clone\"\n";
+      print OUT3 "-D Database EMBL NDB_SV\n";
+      print OUT3 "\nSequence : \"$clone\"\n";
+      print OUT3 "Database EMBL NDB_SV $acc.$EM_seqver\n";
+    }
+
+    if (/^DT\s+(\S+)\s+\(Rel. (\d+)\, Last updated\, Version (\d+)\)/) {
+      ($EM_rel,$EM_ver,$EM_sub) = ($2,$3,$1);
+      $log->write_to("Latest version : Rel. $EM_rel Ver. $EM_ver [$EM_sub] \n");
+    }
+  }
+  $log->write_to("\n");
+}
+
+close OUT3;
+close LIST;
+close GETZ;
+}
+
+#######################################################
+## Logging                                           ##
+#######################################################
 $log->write_to("Upload file(s) completed.......\n");
 
 if (!defined ($update)) {
@@ -264,28 +347,29 @@ if (!defined ($update)) {
   $log->write_to("ACTION\n");
   $log->write_to("==============================================================================================\n");
   $log->write_to("\n\t**You will have to manually load:\n");
-  $log->write_to("\t$output_file\n") if $geneID;
-  $log->write_to("\t/nfs/disk100/wormpub/camace_orig/acefiles/geneID_patch.ace\n") if $geneID;
-  $log->write_to("\t$output_file2") if $proteinID;
+  $log->write_to("\t$output_file\n") if ($geneID || $all);
+  $log->write_to("\t$wormpub/camace_orig/acefiles/geneID_patch.ace\n") if ($geneID || $all);
+  $log->write_to("\t$output_file2\n") if ($proteinID || $all);
+  $log->write_to("\t$output_file3") if ($sv || $all);
   $log->write_to("\n\n\tinto $canonical\n\n");
   $log->write_to("\tCOMMANDS:\n\ttace $canonical -tsuser merge_split\n");
-  $log->write_to("\tpparse $output_file\n") if $geneID;;
-  $log->write_to("\tpparse /nfs/disk100/wormpub/camace_orig/acefiles/geneID_patch.ace\n") if $geneID;;
-  $log->write_to("\tpparse $output_file2\n") if $proteinID;
+  $log->write_to("\tpparse $output_file\n") if ($geneID || $all);
+  $log->write_to("\tpparse $wormpub/camace_orig/acefiles/geneID_patch.ace\n") if ($geneID || $all);
+  $log->write_to("\tpparse $output_file2\n") if ($proteinID || $all);
+  $log->write_to("\tpparse $output_file3\n") if ($sv || $all);
   $log->write_to("\tsave\n\tquit\n\n");
 }
 
-###############################################################
-# Delete targetDB ID info & load new ones into $targetdb      #
-###############################################################
+###################
+# Upload new data #
+###################
 
 if ($update) {
   $log->write_to("==============================================================================================");
   $log->write_to("ACTION");
   $log->write_to("==============================================================================================");
   $log->write_to("\nLoading files........\n");
-  &load_geneIDs     if $geneID;
-  &load_protein_IDs if $proteinID;
+  &load_data;
 }
 
 $log->mail();
@@ -294,34 +378,30 @@ exit(0);
 
 
 ##############################################################
-# Subroutines
+# Subroutines                                                #
 ##############################################################
 
-sub load_geneIDs {
-  my $command = "query find curated_CDS\nedit -D Gene\nclear\n";
-  $command   .= "query find elegans_pseudogenes\nedit -D Gene\nclear\n";
-  $command   .= "query find elegans_RNA_genes\nedit -D Gene\nclear\n";
-  $command   .= "pparse $output_file\n";
-  $command   .= "pparse /nfs/disk100/wormpub/camace_orig/acefiles/geneID_patch.ace\n";
-  $command   .= "save\nquit\n";
-  
-  open (DB, "| $tace $targetDB -tsuser merge_split_update_geneID |") || die "Couldn't open $targetDB\n";
+sub load_data {
+  my $command = "query find curated_CDS\nedit -D Gene\nclear\n" if ($geneID || $all);
+  $command   .= "query find elegans_pseudogenes\nedit -D Gene\nclear\n" if ($geneID || $all);
+  $command   .= "query find elegans_RNA_genes\nedit -D Gene\nclear\n" if ($geneID || $all);
+  $command   .= "pparse $output_file\n" if ($geneID || $all);
+  $command   .= "pparse $wormpub/camace_orig/acefiles/geneID_patch.ace\n" if ($geneID || $all);
+  $command   .= "query find curated_CDS\nedit -D Protein_ID\nclear\n" if ($proteinID || $all);
+  $command   .= "pparse $output_file2\n" if ($proteinID || $all);
+  $command   .= "pparse $output_file3\n" if ($sv || $all);
+  open (DB, "| $tace $targetDB -tsuser merge_split_$next_build |") || die "Couldn't open $targetDB\n";
   print DB $command;
   close DB;
-  $log->write_to("\nLoaded Files:\n$output_file\n/nfs/disk100/wormpub/camace_orig/acefiles/geneID_patch.ace\n");
-  $log->write_to("Updated Gene data in $targetDB\n");
+
+  $log->write_to("\nLoaded Files:\n$output_file\n$wormpub/camace_orig/acefiles/geneID_patch.ace\n") if ($geneID || $all);
+  $log->write_to("Updated Gene data in $targetDB\n") if ($geneID || $all);
+  $log->write_to("\nLoaded File:\n$output_file2\n") if ($proteinID || $all);
+  $log->write_to("Updated protein_ID data in $targetDB\n") if ($proteinID || $all);
+  $log->write_to("\nLoaded File:\n$output_file3\n") if ($sv || $all);
+  $log->write_to("Updated Sequence_versions in $targetDB\n") if ($sv || $all);
 }
 
-sub load_protein_IDs {
-    my $command = "query find curated_CDS\nedit -D Protein_ID\nclear\n";
-    $command   .= "pparse $output_file2\n";
-    $command   .= "save\nquit\n";
-    open (DB, "| $tace $targetDB -tsuser merge_split_update_proteinID |") || die "Couldn't open $targetDB\n";
-    print DB $command;
-    close DB;
-    $log->write_to("\nLoaded File:\n$output_file2\n");
-    $log->write_to("Updated protein_ID data in $targetDB\n");
-  }
 
 sub usage 
   {
@@ -333,14 +413,17 @@ sub usage
   }
 
 sub load_exceptions {
-
-	%exceptions = ( 
+	%exceptions = (
+		'C54G4.7' => 1,
+		'F28A8.9' => 1,
+		'Y105E8A.30' => 1,
+		'ZK228.9' => 1,
 		'C54G4.7:yk713c3.mRNA:wp126' => 1,
 		'C54G4.7:yk728f5.mRNA:wp126' => 1,
 		'F28A8.9:wp149' => 1,
 		'Y105E8A.30a:wp128' => 1,
 		'Y105E8A.30b:wp128' => 1,
-		'ZK228.9:wp144' => 1
+		'ZK228.9:wp144' => 1,
 	);
 }
 
@@ -358,7 +441,7 @@ __END__
 
 =back
 
-This script removes all prediction->WBGene id connections from a chosen target database and re-synchronises these connections with a chosen reference database.  Defaults are in place for routine updating of /nfs/disk100/wormpub/DATABASES/camace with data from /nfs/disk100/wormpub/DATABASES/geneace.  The script can also populate the Public_name tags in the newly syncronised WBGene objects to aid curators when chosing which CDS should be renamed following a gene split event.  The script has also been modified to retrieve protein ids from the previous build and update these in a chosen target database ready for emnl dumping in the next build.
+This script removes all prediction->WBGene id connections from a chosen target database and re-synchronises these connections with a chosen reference database.  Defaults are in place for routine updating of $wormpub/DATABASES/camace with data from $wormpub/DATABASES/geneace. The script has also been modified to retrieve protein ids from the previous build and update these in a chosen target database ready for embl dumping in the next build. A second modification also allows cosmid sequence versions to be checked and the resulting .ace file to be loaded into the target batabase for resolve issues.
 
 
 =head2 GeneID_updater.pl MANDATORY arguments:
