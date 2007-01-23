074f4cd98fdfa1aa635ec2d70de8566480cb8eba
ar2 <ar2>
2007-01-23 14:17:14 +0000

    reformat brigpep so that CBP ids are used

--- scripts/BLAST_scripts/copy_files_to_acari.pl
+++ scripts/BLAST_scripts/copy_files_to_acari.pl
@@ -66,10 +66,25 @@ sub copy_worm_proteins {
    my $wp_old  = "$pep_set" . ($WS_version - 1) . ".pep";
     
    $wormbase->run_command("cp ".$wormbase->$pep_set."/$pep_set$WS_version $dir/$pep_set${WS_version}.pep",$log);
-    
+
+	if($pep_set eq "brigpep") {
+		open (BRIG,"<$dir/$pep_set${WS_version}.pep") or $log->log_and_die("cant open $dir/$pep_set${WS_version}.pep");
+		open (NEW,">$dir/$pep_set${WS_version}.pep_refmt") or $log->log_and_die("cant open $dir/$pep_set${WS_version}.pep_refmt :\n$!\n\n");
+		while (<BRIG>) {
+			if(/>CBG\d+\s+(CBP\d+)/) {
+				print NEW ">$1\n";
+			}
+			else { print NEW;}
+		}
+
+		$wormbase->run_command("mv -f $dir/$pep_set${WS_version}.pep_refmt $dir/$pep_set${WS_version}.pep",$log)
+	}
+	
    $log->write_to("Removed old version of $pep_set . . ");
    $wormbase->run_command("rm -f $dir/${wp_old}*",$log) ;
 
    $log->write_to("$pep_set copied and ready for action\n\n-------------------------------------------------------\n");
    print "finished\n";
 }
+
+ ;
