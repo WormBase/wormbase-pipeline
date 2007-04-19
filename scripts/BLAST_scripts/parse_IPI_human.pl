c4ab529afc64bdfcb04b9da4602139b731cb923b
ar2 <ar2>
2007-04-19 10:41:40 +0000

    update to new format so that gene name is extracted properly. Improved the way that the description is generated

--- scripts/BLAST_scripts/parse_IPI_human.pl
+++ scripts/BLAST_scripts/parse_IPI_human.pl
@@ -1,7 +1,7 @@
 #!/usr/local/ensembl/bin/perl -w                  
 #
-# Last updated by: $Author: wormpipe $     
-# Last updated on: $Date: 2006-11-28 13:35:35 $      
+# Last updated by: $Author: ar2 $     
+# Last updated on: $Date: 2007-04-19 10:41:40 $      
 
 use strict;
 use Getopt::Long;
@@ -41,14 +41,9 @@ open (OUT, ">$output_dir/ipi_human.ace") or die "cant write $output_dir\n";
 open (FASTA, ">$fasta") or die "cant write $fasta\n";
 
 
-
-#>SWISS-PROT:O43933|TREMBL:Q96S69;Q96S70|ENSEMBL:ENSP00000248633 Tax_Id=9606 Peroxisome biogenes
-#is factor 1
-#MWGSDRLAGAGGGGAAVTVAFTNARDCFLHLPRRLVAQLHLLQNQAIEVVWSHQPAFLSW
-#VEGRHFSDQGENVAEINRQVGQKLGLSNGGQVFLKPCSHVVSCQQVEVEPLSADDWEILE
-#LHAVSLEQHLLDQIRIVFPKAIFPVWVDQQTYIFIQIVALIPAASYGRLETDTKLLIQPK
-#TRRAKENTFSKADAEYKKLHSYGRDQKGMMKELQTKQLQSNTVGITESNENESEIPVDSS
-#SVASLWTMIGSIFSFQSEKKQETSWGLTEINAFKNMQSKVVPLDNIFRVCKSQPPSIYNA
+#>IPI:IPI00000006.1|SWISS-PROT:P01112|ENSEMBL:ENSP00000309845|REFSEQ:NP_005334|H-INV:HIT000080764|
+#VEGA:OTTHUMP00000041426;OTTHUMP00000064451;OTTHUMP00000064452;OTTHUMP00000162769;OTTHUMP00000166055
+#Tax_Id=9606 Gene_Symbol=HRAS GTPase HRas precursor
 
 # new UniProt style 
 # UniProt/Swiss-Prot:O95180-2|UniProt/TrEMBL:Q9NYY7;Q9NYY6
@@ -62,9 +57,17 @@ tie my %DESC, 'GDBM_File',"/tmp/desc.dbm",&GDBM_WRCREAT, 0777 or die "cannot ope
 tie my %PEPTIDE,'GDBM_File', "/tmp/peptide.dbm",&GDBM_WRCREAT, 0777 or die "cannot open /tmp/peptide.dbm\n";
 tie my %DATABASES,'GDBM_File', "/tmp/databases.dbm",&GDBM_WRCREAT, 0777 or die "cannot open /tmp/databases.dbm\n";
 
+my %database_field = ( 	'VEGA' 		=> 'OtterID',
+						'ENSEMBL' 	=> 'ENSEMBL_PEP_ID',
+						'SWISS-PROT'=> 'SwissProt_AC',
+						'TREMBL' 	=> 'TrEMBL_ID',
+						'H-INV' 	=> 'H-INV_ID'
+					);
+
 my $prim_DB_id;
 my $prim_DB;
 my $dont_read_pep;
+my $prim_DB_field;
 
 while (<DATA>) {
 
@@ -96,6 +99,7 @@ while (<DATA>) {
       $databases{$db} = $acc;
     }
     # select primary database
+
     if( $databases{'ENSEMBL'} ) {
       $prim_DB = "ENSEMBL";
       $prim_DB_id = $databases{'ENSEMBL'};
@@ -104,10 +108,19 @@ while (<DATA>) {
       $prim_DB = "SW";
       $prim_DB_id = $databases{'Swiss-Prot'};
     }
+    elsif( $databases{'VEGA'} ){
+      $prim_DB = "VG";
+      $prim_DB_id = $databases{'VEGA'};
+    }
     elsif( $databases{'TrEMBL'} ){
       $prim_DB = "TR";
       $prim_DB_id = $databases{'TrEMBL'};
     }
+    elsif( $databases{'H-INV'} ){
+      $prim_DB = "HI";
+      $prim_DB_id = $databases{'H-INV'};
+    } 
+    
     else {
       $dont_read_pep = 1;
       next;
@@ -123,19 +136,15 @@ while (<DATA>) {
     print OUT "\nProtein : \"$prim_DB:$prim_DB_id\"\n";
     print OUT "Peptide \"$prim_DB:$prim_DB_id\"\n";
     foreach (keys %databases) {
-      print OUT "Database $_ $databases{$_} $databases{$_}\n";
+      print OUT "Database $_ $database_field{$_} $databases{$_}\n";
       $DATABASES{$prim_DB_id} .= "$_:$databases{$_} ";
     }
-    my $i = 2;
-    if( $data[$i] ) {
-      print OUT "Title \"";
-      while( $data[$i] ){ 
-	print OUT "$data[$i] ";
-	$DESC{$prim_DB_id} .= "$data[$i] ";
-	$i++;
-      }
-      print OUT "\"\n";
+    if( $data[2] =~ /Gene_Symbol=(\w+)/) {
+    	print OUT "Gene_name \"$1\"\n";
     }
+    splice @data,0,3;
+    print OUT "Title \"".join(" ",@data)."\"\n";#"
+	$DESC{$prim_DB_id} .= join(" ",@data);
     print OUT "\nPeptide : \"$prim_DB:$prim_DB_id\"\n";
     print FASTA ">$prim_DB_id\n";
       
