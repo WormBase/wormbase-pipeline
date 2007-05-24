1ee6c67b3252a8cc2beee37ef7c5d0bd7007fbe0
gw3 <gw3>
2007-05-24 14:56:30 +0000

    changed path for data file to lustre

--- scripts/fix_interpolation.pl
+++ scripts/fix_interpolation.pl
@@ -7,7 +7,7 @@
 # This fixes the gene interpolation paoitions.
 #
 # Last updated by: $Author: gw3 $     
-# Last updated on: $Date: 2007-04-12 09:16:59 $      
+# Last updated on: $Date: 2007-05-24 14:56:30 $      
 
 use strict;                                      
 use lib $ENV{'CVS_DIR'};
@@ -71,7 +71,7 @@ my $tace            = $wormbase->tace;        # TACE PATH
 ##########################
 
 my @genes;
-my $input = "/nfs/disk100/wormpub/BUILD/autoace/logs/rev_phys.log";
+my $input = "/lustre/cbi4/work1/wormpub/BUILD/autoace/logs/rev_phys.log";
 #my $input = "rev_phys.log";
 open(IN, "< $input") || die "Can't open $input\n";
 while (my $line = <IN>) {
