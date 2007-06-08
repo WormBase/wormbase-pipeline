4e80653261309528b3b830dfead4a5cd9a0075b4
gw3 <gw3>
2007-06-08 14:00:04 +0000

    moved from lustre filesystem back to NFS /nfs/disk100/

--- scripts/Modules/Remap_Sequence_Change.pm
+++ scripts/Modules/Remap_Sequence_Change.pm
@@ -13,7 +13,7 @@
 #      COMPANY:
 #     $Version:  $
 #      CREATED: 2006-02-27
-#        $Date: 2007-05-24 09:05:03 $
+#        $Date: 2007-06-08 14:06:02 $
 #===============================================================================
 package Remap_Sequence_Change;
 
@@ -54,7 +54,8 @@ sub read_mapping_data {
                                                                                                                                                             
   foreach my $release (($release1+1) .. $release2) {
     my %chroms;
-    my $infile = "/lustre/cbi4/work1/wormpub/CHROMOSOME_DIFFERENCES/sequence_differences.WS$release";
+#    my $infile = "/lustre/cbi4/work1/wormpub/CHROMOSOME_DIFFERENCES/sequence_differences.WS$release";
+    my $infile = "/nfs/disk100/wormpub/CHROMOSOME_DIFFERENCES/sequence_differences.WS$release";
     open (IN, "< $infile") || die "Can't open $infile\n";
     my $chrom;
     while (my $line = <IN>) {
--- scripts/inspect-old-releases.pl
+++ scripts/inspect-old-releases.pl
@@ -17,7 +17,7 @@
 # foreach? end
 #
 # Last updated by: $Author: gw3 $     
-# Last updated on: $Date: 2007-05-24 09:08:57 $      
+# Last updated on: $Date: 2007-06-08 14:02:42 $      
 
 use strict;                                      
 use lib $ENV{'CVS_DIR'};
@@ -72,7 +72,8 @@ my $log = Log_files->make_build_log($wormbase);
 # MAIN BODY OF SCRIPT
 ##########################
 
-my $outfile = "/lustre/cbi4/work1/wormpub/CHROMOSOME_DIFFERENCES/sequence_differences.WS$version";
+#my $outfile = "/lustre/cbi4/work1/wormpub/CHROMOSOME_DIFFERENCES/sequence_differences.WS$version";
+my $outfile = "/nfs/disk100/wormpub/CHROMOSOME_DIFFERENCES/sequence_differences.WS$version";
 
 open (OUT, "> $outfile") || die "Can't open $outfile";
 
