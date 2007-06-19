8e9c786685672016ac9d450dcd0e8cfb4b65cab6
gw3 <gw3>
2007-06-19 16:22:27 +0000

    check that the nembase/washu/nematode EST files exist

--- scripts/BLAT_controller.pl
+++ scripts/BLAT_controller.pl
@@ -1,7 +1,7 @@
 #!/usr/local/bin/perl5.8.0 -w
 #
 # Last edited by: $Author: gw3 $
-# Last edited on: $Date: 2007-06-19 13:22:38 $
+# Last edited on: $Date: 2007-06-19 16:22:27 $
 
 
 use lib $ENV{'CVS_DIR'};
@@ -114,7 +114,8 @@ if( $mask ) {
 	#copy the nematode ESTs from BUILD_DATA
 	foreach (@nematodes) {
 	  mkpath ($wormbase->basedir."/cDNA/$_") unless  -e ($wormbase->basedir."/cDNA/$_");
-	  copy($wormbase->build_data."/cDNA/$_/EST", $wormbase->basedir."/cDNA/$_/EST.masked") if (-e $wormbase->build_data."/cDNA/$_/EST");
+	  if (! -e $wormbase->build_data."/cDNA/$_/EST") {$wormbase->log_and_die("Can't find file " . $wormbase->build_data."/cDNA/$_/EST\n");};
+	  copy($wormbase->build_data."/cDNA/$_/EST", $wormbase->basedir."/cDNA/$_/EST.masked") 
 	}
 }
 
