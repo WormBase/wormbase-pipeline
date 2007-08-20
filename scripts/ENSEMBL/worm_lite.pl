db6ab3d70b65e0208f93b0df4abd74cbdcbeb0ce
mh6 <mh6>
2007-08-20 09:26:37 +0000

    slightly better version

--- scripts/ENSEMBL/worm_lite.pl
+++ scripts/ENSEMBL/worm_lite.pl
@@ -28,7 +28,7 @@ use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use WormBase;
 use DBI qw(:sql_types);
 
-my ( $debug, $species, $setup, $dna, $genes );
+my ( $debug, $species, $setup, $dna, $genes, $test );
 
 GetOptions(
     'species=s'  => \$species,
@@ -36,48 +36,57 @@ GetOptions(
     'load_dna'   => \$dna,
     'load_genes' => \$genes,
     'debug'      => \$debug,
-);
+    'test'       => \$test,
+) || die("bad commandline parameter\n");
 
 my $config = ( YAML::LoadFile("ensembl_lite.conf") )->{$species};
-my $cvsDIR = '/nfs/acari/wormpipe/ensembl/';
+my $cvsDIR = $test
+  ? ( YAML::LoadFile("ensembl_lite.conf") )->{test}->{cvsdir}
+  : ( YAML::LoadFile("ensembl_lite.conf") )->{generics}->{cvsdir};
 
-our $gff_types="curated coding_exon";
+our $gff_types = "curated coding_exon";
 
 &setupdb( $config->{database}, $config->{version} ) if $setup;
 &load_dna($config)   if $dna;
 &load_genes($config) if $genes;
 
 ##################################
-# create database from schema     
-#    
+# create database from schema
+#
 # hardcoded paths:
 #      /nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/DataConversion/wormbase/attrib_type.sql
-#	   /nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/load_taxonomy.pl 
+#      /nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/load_taxonomy.pl
 #
-# taxondb: ecs2 -taxondbport 3365 -taxondbname ncbi_taxonomy
+# taxondb: ia64f -taxondbport 3365 -taxondbname ncbi_taxonomy
+#  if the taxondb goes down bully Abel
 
 sub setupdb {
     my ( $db, $version ) = @_;
     my $status = 0;
-    print ">>creating new database $db->{dbname} on $db->{host}\n" ;
+    print ">>creating new database $db->{dbname} on $db->{host}\n";
     my $mysql = "mysql -h $db->{host} -P $db->{port} -u $db->{user} --password=$db->{password}";
     system("$mysql -e \"DROP DATABASE IF EXISTS $db->{dbname};\"") && die;
-    system("$mysql -e \"create database $db->{dbname};\"") && die;
+    system("$mysql -e \"create database $db->{dbname};\"")         && die;
     print "loading table.sql from ensembl\n";
     system( "$mysql $db->{dbname} < " . $cvsDIR . "ensembl/sql/table.sql" ) && die;
     print "loading table.sql from ensembl-pipeline\n";
-    system( "$mysql $db->{dbname} < " . $cvsDIR . "ensembl-pipeline/sql/table.sql" ) && die;
-    system("$mysql -e 'INSERT INTO coord_system VALUES (1,\"scaffold\",\"$version\",1,\"default_version,top_level\");' $db->{dbname}") && die;
+    system( "$mysql $db->{dbname} < " . $cvsDIR . "ensembl-pipeline/sql/table.sql" )                                                         && die;
+    system("$mysql -e 'INSERT INTO coord_system VALUES (1,\"chromosome\",\"$version\",1,\"default_version,top_level\");' $db->{dbname}")       && die;
     system("$mysql -e 'INSERT INTO coord_system VALUES (2,\"superlink\",\"$version\",2,\"default_version,sequence_level\");' $db->{dbname}") && die;
-    system("$mysql -e 'INSERT INTO meta (meta_key,meta_value) VALUES (\"genebuild.version\",\"$version\");' $db->{dbname}") && die;
-    system("$mysql -e 'INSERT INTO analysis (created,logic_name,module) VALUES ( NOW(),\"wormbase\",\"wormbase\");' $db->{dbname}") && die;
-    system("$mysql -e 'INSERT INTO analysis_description (analysis_id,description,display_label) VALUES (1,\"imported from WormBase\",\"WormGene\");' $db->{dbname}") && die;
+    system("$mysql -e 'INSERT INTO coord_system VALUES (3,\"clone\",\"$version\",3,\"default_version\");' $db->{dbname}")                                   && die;
+    system("$mysql -e 'INSERT INTO meta (meta_key,meta_value) VALUES (\"genebuild.version\",\"$version\");' $db->{dbname}")                  && die;
+    system("$mysql -e 'INSERT INTO analysis (created,logic_name,module) VALUES ( NOW(),\"wormbase\",\"wormbase\");' $db->{dbname}")          && die;
+    system( "$mysql -e 'INSERT INTO analysis_description (analysis_id,description,display_label) VALUES (1,\"imported from WormBase\",\"WormGene\");' $db->{dbname}") && die;
     system("$mysql $db->{dbname} </nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/DataConversion/wormbase/attrib_type.sql") && die;
-    system("/usr/local/ensembl/bin/perl /nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/load_taxonomy.pl -name \"Caenorhabditis $species\" -taxondbhost ia64f -taxondbport 3306 -taxondbname ncbi_taxonomy -lcdbhost $db->{host} -lcdbport $db->{port} -lcdbname $db->{dbname} -lcdbuser $db->{user} -lcdbpass $db->{password}") && die;
-#    system("$mysql $db->{dbname} </nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/DataConversion/wormbase/taxonomy.sql") && die;
-#    system("$mysql -e 'INSERT INTO meta (meta_key,meta_value) VALUES (\"species.classification\",\"$species\");' $db->{dbname}") && die;
-#    system("$mysql -e 'INSERT INTO meta (meta_key,meta_value) VALUES (\"species.taxonomy_id\",\"$config->{taxon_id}\");' $db->{dbname}") && die;
- 
+    system("perl $cvsDIR/ensembl-pipeline/scripts/load_taxonomy.pl -name \"$config->{species}\" -taxondbhost ia64f -taxondbport 3306 -taxondbname ncbi_taxonomy -lcdbhost $db->{host} -lcdbport $db->{port} -lcdbname $db->{dbname} -lcdbuser $db->{user} -lcdbpass $db->{password}"
+      )
+      && die("cannot run taxondb update");
+
+    # in case the taxondb gives up uncomment below
+    #    system("$mysql $db->{dbname} </nfs/acari/wormpipe/ensembl/ensembl-pipeline/scripts/DataConversion/wormbase/taxonomy.sql") && die;
+    #    system("$mysql -e 'INSERT INTO meta (meta_key,meta_value) VALUES (\"species.classification\",\"$species\");' $db->{dbname}") && die;
+    #    system("$mysql -e 'INSERT INTO meta (meta_key,meta_value) VALUES (\"species.taxonomy_id\",\"$config->{taxon_id}\");' $db->{dbname}") && die;
+
     if ($status) { die("Error while building the database.") }
 }
 
@@ -93,6 +102,7 @@ sub load_dna {
     );
     my $cs = $db->get_CoordSystemAdaptor()->fetch_by_dbID(1);    # chromosome
     my $ss = $db->get_CoordSystemAdaptor()->fetch_by_dbID(2);    # superlink
+    my $cl = $db->get_CoordSystemAdaptor()->fetch_by_dbID(3);    # clone
     my $sa = $db->get_SliceAdaptor();
 
     foreach my $file ( glob $config->{fasta} ) {
@@ -102,27 +112,56 @@ sub load_dna {
         while ( my $seq = $seqs->next_seq() ) {
             print $seq->display_id(), "\t", $seq->length(), "\n" if $debug;
 
-            my $cslice=&store_slice( $db, $seq->display_name(), 1, $seq->length, 1, $cs );
+            my $cslice = &store_slice( $db, $seq->display_name(), 1, $seq->length, 1, $cs );
 
             # do something else if size > 16MB
-	    if ($seq->length > 16000000){
-		my $part1=substr($seq->seq(),0,($seq->length()/2));
-		my $part2=substr($seq->seq(),($seq->length()/2));
-		my $bslice1=&store_slice( $db, $seq->display_name().'_1', 1, (length $part1), 1, $ss, $part1);
-		my $bslice2=&store_slice( $db, $seq->display_name().'_2', 1, (length $part2), 1, $ss, $part2);
+            if ( $seq->length > 16000000 ) {
+                my $part1 = substr( $seq->seq(), 0, ( $seq->length() / 2 ) );
+                my $part2 = substr( $seq->seq(), ( $seq->length() / 2 ) );
+                my $bslice1 = &store_slice( $db, $seq->display_name() . '_1', 1, ( length $part1 ), 1, $ss, $part1 );
+                my $bslice2 = &store_slice( $db, $seq->display_name() . '_2', 1, ( length $part2 ), 1, $ss, $part2 );
 
-		&insert_agp_line($cslice->get_seq_region_id($cslice), (length $part1)+1,$seq->length, $bslice2->get_seq_region_id($bslice2), 1,(length $part2) , 1, $db);
-		&insert_agp_line($cslice->get_seq_region_id($cslice), 1, (length $part1), $bslice1->get_seq_region_id($bslice1), 1,(length $part1) , 1, $db);
-	    }
-	    else {
-		    my $bslice=&store_slice( $db, $seq->display_name(), 1, $seq->length, 1, $ss, $seq->seq());
-		    &insert_agp_line($cslice->get_seq_region_id($cslice), 1, $seq->length, $bslice->get_seq_region_id($bslice), 1,$seq->length , 1, $db);
-	    }
+                &insert_agp_line( $cslice->get_seq_region_id($cslice), ( length $part1 ) + 1,
+                    $seq->length, $bslice2->get_seq_region_id($bslice2), 1, ( length $part2 ), 1, $db);
+                &insert_agp_line( $cslice->get_seq_region_id($cslice), 1, ( length $part1 ),
+                    $bslice1->get_seq_region_id($bslice1), 1, ( length $part1 ), 1, $db);
+            }
+            else {
+                my $bslice = &store_slice( $db, $seq->display_name(), 1, $seq->length, 1, $ss, $seq->seq() );
+                &insert_agp_line( $cslice->get_seq_region_id($cslice), 1, $seq->length, $bslice->get_seq_region_id($bslice), 1, $seq->length, 1, $db );
+            }
 
         }
     }
 
-    $db->get_MetaContainer()->store_key_value( 'assembly.mapping', $cs->name . ":" . $cs->version . "|" . $ss->name );
+    # agp fun
+    if ($config->{agp}) {
+        foreach my $file ( glob $config->{agp} ) {
+	    my $bzhook= $file=~/.bz2$/ ? "bzcat $file|" : $file;
+            my $fh = new IO::File $bzhook or die "couldn't open " . $file . " $!";
+
+	    #### need a slice of a chromosome to get the id :o
+	    $file=~/(\w+)\.agp/;
+	    print "processing chromosome $1\n";
+	    my $chromosome = $db->get_SliceAdaptor()->fetch_by_region('chromosome',$1); # stupid assumption about the name of the file being the chromosome name
+            my %chr_hash = %{ &agp_parse( $fh, $chromosome->adaptor->get_seq_region_id($chromosome), $config->{version} ) };
+
+            foreach my $id ( keys(%chr_hash) ) {
+		my $length = $chr_hash{$id}->{'contig_end'}-$chr_hash{$id}->{'contig_start'}+1;
+		print "adding $1 $id - $chr_hash{$id}->{contig_start} - $chr_hash{$id}->{contig_end} length:$length\n" if $debug;
+		##############################
+                my $contig = &store_slice( $db, $id, 1, $length, 1, $cl );
+                $contig = $contig->adaptor->get_seq_region_id($contig);
+                my $agp    = $chr_hash{$id};
+                &insert_agp_line($agp->{'chromosome_id'},$agp->{'chr_start'},$agp->{'chr_end'},$contig,$agp->{'contig_start'},$agp->{'contig_end'},$agp->{'contig_ori'},$db);
+            }
+    	}
+    }
+
+    $db->get_MetaContainer()->store_key_value( 'assembly.mapping', $cs->name . ":" . $cs->version . "|" . $ss->name .':'.$cs->version );
+    $db->get_MetaContainer()->store_key_value( 'assembly.mapping', $cs->name . ":" . $cs->version . "|" . $cl->name .':'.$cs->version );
+    $db->get_MetaContainer()->store_key_value( 'assembly.mapping', $cl->name . ":" . $cs->version . "|" . $cs->name .':'.$cs->version."|".$ss->name .':'.$cs->version );
+
     $db->dbc->do('INSERT INTO seq_region_attrib (seq_region_id,attrib_type_id,value) SELECT seq_region_id,11,5 FROM seq_region WHERE Name LIKE "%Mt%"');
     $db->dbc->do('INSERT INTO seq_region_attrib (seq_region_id,attrib_type_id,value) SELECT seq_region_id,6,6 FROM seq_region WHERE coord_system_id=1');
 }
@@ -137,27 +176,28 @@ sub load_genes {
     );
     my $analysis = $db->get_AnalysisAdaptor()->fetch_by_logic_name('wormbase');
 
-	# elegans hack for build
-	if ($species eq 'elegans') {
-		foreach my $chr (glob $config->{fasta}){
-			my($path,$name)=($chr=~/(^.*)\/CHROMOSOMES\/(.*?)\.\w+/);
-			`mkdir /tmp/compara` if ! -e '/tmp/compara';
-			system("cat $path/GFF_SPLITS/${\$name}_gene.gff $path/GFF_SPLITS/${\$name}_curated.gff > /tmp/compara/${\$name}.gff") && die 'cannot concatenate GFFs';
-		}
-	}
+    # elegans hack for build
+    if ( $species eq 'elegans' ) {
+        foreach my $chr ( glob $config->{fasta} ) {
+            my ( $path, $name ) = ( $chr =~ /(^.*)\/CHROMOSOMES\/(.*?)\.\w+/ );
+            `mkdir /tmp/compara` if !-e '/tmp/compara';
+            system("cat $path/GFF_SPLITS/${\$name}_gene.gff $path/GFF_SPLITS/${\$name}_curated.gff > /tmp/compara/${\$name}.gff")
+              && die 'cannot concatenate GFFs';
+        }
+    }
 
     foreach my $file ( glob $config->{gff} ) {
         next if $file =~ /masked|CSHL|BLAT_BAC_END|briggsae|MtDNA/;
-        $file=~/.*\/(.*)\.gff/;
-	print "parsing $1 from $file\n";
-        my $slice = $db->get_SliceAdaptor->fetch_by_region('Scaffold',$1);
+        $file =~ /.*\/(.*)\.gff/;
+        print "parsing $1 from $file\n";
+        my $slice = $db->get_SliceAdaptor->fetch_by_region( 'chromosome', $1 );
         my $genes = &parse_gff( $file, $slice, $analysis );
         &write_genes( $genes, $db );
     }
     $db->dbc->do('UPDATE gene SET biotype="protein_coding"');
 
-	# elegans cleanup
-	(system('rm -rf /tmp/compara') && die "could not clean up /tmp/compara") if $species eq 'elegans';
+    # elegans cleanup
+    ( system('rm -rf /tmp/compara') && die "could not clean up /tmp/compara" ) if $species eq 'elegans';
 }
 
 package WormBase;
@@ -195,8 +235,8 @@ sub process_file {
             next LOOP;
         }
         elsif ( $line ne $gff_types ) { next LOOP }    # <= here goes the change needs tp become $line eq "$bla $blub"
-        $genes{$gene}||= []; 
-	push( @{ $genes{$gene} }, $element );
+        $genes{$gene} ||= [];
+        push( @{ $genes{$gene} }, $element );
     }
     print STDERR "Have " . keys(%genes) . " genes (CDS), " . keys(%five_prime) . " have 5' UTR and " . keys(%three_prime) . " have 3' UTR information\n";
     return \%genes, \%five_prime, \%three_prime;
