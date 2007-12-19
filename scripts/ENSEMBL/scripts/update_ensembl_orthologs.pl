b8076a94b90aa0272afd8dd8ad046f2e86e85f59
mh6 <mh6>
2007-12-19 10:22:53 +0000

    update compara orthologs from main compara database

---
+++ scripts/ENSEMBL/scripts/update_ensembl_orthologs.pl
@@ -0,0 +1,77 @@
+#!/usr/bin/perl -w
+#===============================================================================
+#   updates ortholog connections from the main Compara database
+#
+#       AUTHOR:   (), <>
+#      VERSION:  1.0
+#      CREATED:  03/08/06 13:26:19 BST
+#     REVISION:  ---
+#===============================================================================
+
+use strict;
+use IO::File;
+use Bio::EnsEMBL::Registry;
+Bio::EnsEMBL::Registry->load_registry_from_db(-host => 'ensembldb.ensembl.org', -user => 'anonymous',-port => 3306,-verbose => 1);
+
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
+
+
+my %cds2wbgene=%{&get_commondata('/nfs/disk100/wormpub/DATABASES/current_DB/COMMON_DATA/cds2wbgene_id.dat')};
+
+my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Caenorhabditis elegans','core','Slice');
+my $member_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Member');
+my $homology_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Homology');;
+
+my @slices = @{$slice_adaptor->fetch_all('toplevel')};
+foreach my $slice(@slices){
+	foreach my $gene (@{$slice->get_all_Genes}){
+		my $member = $member_adaptor->fetch_by_source_stable_id( 'ENSEMBLGENE',$gene->stable_id());
+		next unless $member;
+		my $homologies = $homology_adaptor->fetch_all_by_Member_method_link_type( $member, 'ENSEMBL_ORTHOLOGUES' );
+
+		my %homol_ids;
+
+		foreach my $homology ( @{$homologies} ) {
+			foreach my $ma ( @{ $homology->get_all_Member_Attribute } ) {
+				my ( $me, $at ) = @{$ma};
+				foreach my $pepm ( @{ $me->get_all_peptide_Members() } ) { 
+					if ($pepm->taxon_id != 6239){$homol_ids{ $pepm->gene->stable_id } = [$pepm->taxon,$homology->description,$homology->subtype] }
+				}
+
+			}
+		}
+
+
+		next unless %homol_ids;
+
+		my $gid=$cds2wbgene{$gene->stable_id}?$cds2wbgene{$gene->stable_id}:next;
+
+		print "Gene : \"$gid\"\n";
+		while (my ($k,$v)=each(%homol_ids)){
+				print "Ortholog_other EnsEMBL gene $k \"${\$$v[0]->name}\" Inferred_automatically compara\n";
+		}
+
+		print "\n";
+
+	}	
+}
+
+
+sub get_commondata {
+	my ($name)=@_;
+	my $file= new IO::File "< $name";
+	$/=undef;
+	my $data=<$file>;
+	$/="\n";
+	$file->close;
+	my $VAR1;
+	my %genehash;
+	eval($data);
+
+	while(my ($k,$v)=each(%{$VAR1})){
+		$k=~s/[a-z]$//;
+		$genehash{$k}=$v
+	}
+	return \%genehash;
+}
