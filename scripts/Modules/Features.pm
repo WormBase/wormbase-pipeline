4b8907131bd3212eb67b97c4d119aea9932148d8
pad <pad>
2008-01-14 16:40:43 +0000

    Added code to find buried polyA features, if a run of 10 A's are identified within the the read then the sequence post match is scored based on composition.  If the sequence scores over 33 or is shorter that 25bp then it is deemed poor quality and both the poor quality and the polyA are flagged.

--- scripts/Modules/Features.pm
+++ scripts/Modules/Features.pm
@@ -1,10 +1,10 @@
 ##############
 # Features.pm
 # to contain the filters for use before alignment to the genome
-# by Michael Han 
+# by Michael Han
 # 
-# Last updated by: $Author: mh6 $
-# Last updated on: $Date: 2005-10-11 09:09:29 $
+# Last updated by: $Author: pad $
+# Last updated on: $Date: 2008-01-14 16:40:43 $
 
 package Features;
 
@@ -35,25 +35,86 @@ struct Feature =>{
 # trim_polyA(sequence)
 
 sub trim_polyA{
-    my($seq,$id)=@_;
+  my($seq,$id)=@_;
+  my $flag = "0";
+  my $psize = length $seq;
+  my ($maskedb,$tail)=0;
+  $seq=~tr/acgt/ACGT/; #upcase sequence
+  
+  #Hidden polyAs and bad clipping.   AAAAAAAXYZPQR
+  my ($probseq,$poorqual,$polyAlength,$polyAstart,$polyAend,$rubstart,$badseq,$rubbish_ratio)="";
+  if ($seq=~/(AAAAAAAAAA+([^A]+\w+))/) {
+    $probseq = length $1;
+    $poorqual = length $2;
+    $badseq = $2;
     
-    my $psize = length $seq;
-    my ($maskedb,$tail)=0;
-    $seq=~tr/acgt/ACGT/; #upcase sequence
-
-    if ($seq=~/(A+)$/) {
-	$tail=$1;
-	$maskedb=length $1;
+    my $poorqualscore = &check_poor_qual($badseq);#uses a small subroutine to assign a score to the post polyA sequence.
+    if (($poorqualscore < 33) && ($poorqual > 50)) {
+      print "//$id contains internal polyA run longer than 10bp but does not look like a real polyA!!\n\n";
     }
-    # generate 2 features polyA site and tail if size > 5
-    if ($maskedb > 5) {
-
-	# masking
-	my $masked_tail= "n"x$maskedb;
+    elsif (($poorqualscore > 32) or ($poorqual < 25)) { #mask poor quality under 25bp scoring fails short seqs.
+      $polyAlength = ($probseq - $poorqual);
+      $polyAstart = ($psize - $probseq +1);
+      $polyAend = ($polyAstart + $polyAlength -1);
+      $rubstart = ($polyAend + 1);
+      $flag = "1";
+      
+      if ($flag > 0) {
 	my @features;
-	$seq=~s/$tail$/$masked_tail/i;
-
 	my $feature1=Feature->new(
+				  id          => $id,
+				  sequence    => $seq,
+				  start       => $polyAstart,
+				  stop        => $polyAend,
+				  psize       => $psize,
+				  length      => $polyAlength,
+				  type        => 'polyA',
+				  description => 'polyA tail sequence',
+				  type2       => 'polyA'
+				 );
+	my $feature2=Feature->new(
+				  id          => $id,
+				  sequence    => $seq,
+				  start       => $rubstart,
+				  stop        => $psize,
+				  psize       => $psize,
+				  length      => ($psize - $rubstart+1),
+				  type        => 'low',
+				  description => '['.$badseq.']'.'- low-complexity or poor quality/unclipped sequence(Score::'.$poorqualscore.'). Removed from BLAT pipeline.',
+				  type2       => 'low'
+				 );
+	my $feature3=Feature->new(
+				  id          => $id,
+				  sequence    => $seq,
+				  start       => ($polyAstart),
+				  stop        => ($polyAstart+1),
+				  psize       => $psize,
+				  length      => 1,
+				  type        => 'polyA_site',
+				  description => 'polyA site',
+				  type2       => 'polyA_site'
+				 );
+	return ($feature1,$feature2,$feature3);
+      }
+    }
+      elsif (($poorqualscore > 32) && ($poorqual > 150)) {
+	print "Warning, this sequence appears to be low quality/poly nucleotide runs and need investigating.\n"
+      }
+  }
+  
+  if (($seq=~/(A+)$/) && ($flag < 1)) {
+    $tail=$1;
+    $maskedb=length $1;
+    }
+  # generate 2 features polyA site and tail if size > 5
+  if ($maskedb > 5) {
+    
+    # masking
+    my $masked_tail= "n"x$maskedb;
+    my @features;
+    $seq=~s/$tail$/$masked_tail/i;
+    
+    my $feature1=Feature->new(
 			      id          => $id,
 			      sequence    => $seq,
 			      start       => ($psize - $maskedb+1),
@@ -64,7 +125,7 @@ sub trim_polyA{
 			      description => 'polyA tail sequence',
 			      type2       => 'polyA'
 			     );
-	my $feature2=Feature->new(
+    my $feature2=Feature->new(
 			      id          => $id,
 			      sequence    => $seq,
 			      start       => ($psize - $maskedb),
@@ -75,20 +136,20 @@ sub trim_polyA{
 			      description => 'polyA site',
 			      type2       => 'polyA_site'
 			     );
-################
-# in case we migth look for polyA motifs in future
-#
-#
-#	if ($seq=~/(AATAAA)\w{5,30}?n+$/i){
-#	    my $offset=length $`;
-#	    my $motif=$1;
-#	    my $prettyseq=$seq;
-#	    $prettyseq=~s/(AATAAA)(\w{5,30}?n+$)/ \[$1\] $2/;
-#	   print '='x20,"\nfound polyA motif $motif at $offset\n".'='x20,"\nin\n$prettyseq\n";
-#	}
-	return ($feature1,$feature2);
+    ################
+    # in case we migth look for polyA motifs in future
+    #
+    #
+    #	if ($seq=~/(AATAAA)\w{5,30}?n+$/i){
+    #	    my $offset=length $`;
+    #	    my $motif=$1;
+    #	    my $prettyseq=$seq;
+    #	    $prettyseq=~s/(AATAAA)(\w{5,30}?n+$)/ \[$1\] $2/;
+    #	   print '='x20,"\nfound polyA motif $motif at $offset\n".'='x20,"\nin\n$prettyseq\n";
+    #	}
+    return ($feature1,$feature2);
     }
-    else {return undef}
+  else {return undef}
 }
 
 
@@ -98,93 +159,128 @@ sub trim_polyA{
 # at /nfs/WWWdev/INTWEB_docs/cgi-bin/Projects/C_elegans/tsl_search
 
 sub get_tls{
-    my %SL    = (
-          'SL1',  'GGTTTAATTACCCAAGTTTGAG',
-          'SL2',  'GGTTTTAACCCAGTTACTCAAG',
-          'SL2a', 'GGTTTATACCCAGTTAACCAAG',
-          'SL2b', 'GGTTTTAACCCAGTTTAACCAAG',
-          'SL2c', 'GGTTTTAACCCAGTTACCAAG',
-          'SL2d', 'GGTTTTTACCCAGTTAACCAAG',
-          'SL2e', 'GGTTTAAAACCCAGTTAACAAG',
-          'SL2f', 'GGTTTTAACCCAGTTAACCAAG',
-          'SL2g', 'GGTTTTAACCAGTTAACTAAG',
-          'SL2h', 'GGTTTTAACCCATATAACCAAG',
-          'SL2i', 'GGTTTTAACCCAAGTTAACCAAG',
-          'SL2j', 'GGTTTAAAACCCAGTTACCAAG',
-          'SL2k', 'GGTTTTAACCCAGTTAATTGAG',
+  my %SL    = (
+	       'SL1',  'GGTTTAATTACCCAAGTTTGAG',
+	       'SL2',  'GGTTTTAACCCAGTTACTCAAG',
+	       'SL2a', 'GGTTTATACCCAGTTAACCAAG',
+	       'SL2b', 'GGTTTTAACCCAGTTTAACCAAG',
+	       'SL2c', 'GGTTTTAACCCAGTTACCAAG',
+	       'SL2d', 'GGTTTTTACCCAGTTAACCAAG',
+	       'SL2e', 'GGTTTAAAACCCAGTTAACAAG',
+	       'SL2f', 'GGTTTTAACCCAGTTAACCAAG',
+	       'SL2g', 'GGTTTTAACCAGTTAACTAAG',
+	       'SL2h', 'GGTTTTAACCCATATAACCAAG',
+	       'SL2i', 'GGTTTTAACCCAAGTTAACCAAG',
+	       'SL2j', 'GGTTTAAAACCCAGTTACCAAG',
+	       'SL2k', 'GGTTTTAACCCAGTTAATTGAG',
           );
-    my ($seq,$id)=@_;
-    $seq=~tr/acgt/ACGT/; #upcase sequence
-    my $psize=length $seq;
-
-    # searches for n-mers of the TLS (max 23)
-    for (my $i=23;$i>6;$i--){
-
-	# loops through the TLS sequences 
-	foreach my $slname (keys %SL){
-
-	    next if $i > length $SL{$slname};
-	    my $nmer=substr($SL{$slname},-$i);
-
-	    if ($seq=~ /^($nmer)/i) {
-		    my $stop=length $i;
-		    my $mask= 'n'x(length $nmer);
-
-		    $seq=~s/^$nmer/$mask/i; # trim sequence
-		    $slname=~/(SL\d)/; # extract type
-		    my $sltype=$1;
-		    my $feature=Feature->new(
-			      id          => $id,
-			      sequence    => $seq,
-			      start       => 1,
-			      stop        => (length $nmer),
-			      psize       => $psize,
-			      length      => (length $nmer),
-			      type        => $sltype,
-			      description => "$slname trans-spliced leader sequence",
-			      type2       => 'TSL'
-			     );
-
-		    return $feature;
-		}
+  my ($seq,$id)=@_;
+  $seq=~tr/acgt/ACGT/; #upcase sequence
+  my $psize=length $seq;
+  
+  # searches for n-mers of the TLS (max 23)
+  for (my $i=23;$i>6;$i--){
+    
+    # loops through the TLS sequences 
+    foreach my $slname (keys %SL){
+      
+      next if $i > length $SL{$slname};
+      my $nmer=substr($SL{$slname},-$i);
+      
+      if ($seq=~ /^($nmer)/i) {
+	my $stop=length $i;
+	my $mask= 'n'x(length $nmer);
+	
+	$seq=~s/^$nmer/$mask/i; # trim sequence
+	$slname=~/(SL\d)/; # extract type
+	my $sltype=$1;
+	my $feature=Feature->new(
+				 id          => $id,
+				 sequence    => $seq,
+				 start       => 1,
+				 stop        => (length $nmer),
+				 psize       => $psize,
+				 length      => (length $nmer),
+				 type        => $sltype,
+				 description => "$slname trans-spliced leader sequence",
+				 type2       => 'TSL'
+				);
+	
+	return $feature;
+      }
 	    }
-	}
-    return undef;
+  }
+  return undef;
 }
 
 ###################
 # generate ACE from Feature
 
 sub ace_feature{
-    my ($feature)=@_;
+  my ($feature)=@_;
+  
+  my $template="Sequence : \"%s\"\n".
+    "Feature_data \"%s:%s\" 1 %d\n".
+      "\n".
+	"Feature_data : \"%s:%s\"\n".
+	  "Feature %s  %d %d %d \"%s\"\n\n";
+  
+  my $id=$feature->id;
+  my $type2=$feature->type2;
+  
+  return sprintf($template,$id,
+		 $id,$type2,$feature->psize,
+		 $id,$type2,
+		 $feature->type,$feature->start,$feature->stop,$feature->length,$feature->description);
+}
 
-    my $template="Sequence : \"%s\"\n".
-                 "Feature_data \"%s:%s\" 1 %d\n".
-	         "\n".
-	         "Feature_data : \"%s:%s\"\n".
-	         "Feature %s  %d %d %d \"%s\"\n\n";
 
-    my $id=$feature->id;
-    my $type2=$feature->type2;
+#####################################################################
+# Sequence quality estimator.
+# Subroutine provided by ar2 to give a score for a piece of sequence.
+# Below 33 and the sequence is probably genomic.
 
-    return sprintf($template,$id,
-	   $id,$type2,$feature->psize,
-	   $id,$type2,
-	   $feature->type,$feature->start,$feature->stop,$feature->length,$feature->description);
+sub check_poor_qual {
+  my $dna = shift;
+  my $dna_length = length ($dna);
+  my $n = $dna =~ tr/N/N/ ;
+  my $q = sprintf("%2d",$n/$dna_length * 100);
+  
+  my $n100 = substr($dna,0,100) =~ tr/N/N/ ;
+  my $q100 = sprintf("%2d",$n100);
+  
+  my $rubbish =0;
+  foreach ( qw( A C T G N ) ) {
+    my @n = $dna =~ /$_+/g;
+    my @sn = sort { length($b) <=> length($a) } @n;
+    foreach (@sn) {
+      if ( length($_) > 5 ) {
+	$rubbish +=  length($_);
+      }
+    }
+  }
+  my $rubbish_ratio;
+  if ( $rubbish == 0 or $dna_length == 0) {
+    $rubbish_ratio = 0;
+  } 
+  else {
+    $rubbish_ratio = sprintf("%2d",$rubbish/$dna_length *100);
+  }
+  return $rubbish_ratio;
 }
 
 #################
 # create ACE from polyA and TSL
 
 sub annot{
-    my ($seq,$id)=@_;
-    my @features=Features::trim_polyA($seq,$id);
-    push(@features,Features::get_tls($seq,$id));
-    my $ace='';
-    foreach my $feature (@features){
-	 $ace=$ace.Features::ace_feature($feature) if $feature;
-    }
-    return $ace;
+  my ($seq,$id)=@_;
+  my @features=Features::trim_polyA($seq,$id);
+  push(@features,Features::get_tls($seq,$id));
+  my $ace='';
+  foreach my $feature (@features){
+    $ace=$ace.Features::ace_feature($feature) if $feature;
+  }
+  return $ace;
 }
 
 
@@ -225,7 +321,8 @@ the Features_data is stored in a Feature structure which also contains the maske
 
 =head3 trim_polyA($sequence,$id)
 
-returns: 2 Features, one for polyA tail and one for polyA site if found else undef
+returns: upto 3 Features, one for polyA tail, one for polyA site if found else 
+undef and a low-complexity feature if the read is poorly clipped.
 
 minimum length is 6 bases
 
@@ -254,10 +351,10 @@ returns an ace string of polyA sites/tails and TSL
 
 =head1 AUTHOR
 
-$Author: mh6 $
+$Author: pad $
 
 =head1 VERSION
 
-$Date: 2005-10-11 09:09:29 $
+$Date: 2008-01-14 16:40:43 $
 
 =cut
