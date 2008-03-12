dfb63a16b2db493dd620921710539470f85d5d89
mh6 <mh6>
2008-03-12 16:33:08 +0000

    that one replaces dump_TRF and dump_repeats ... and is faster.

--- scripts/BLAST_scripts/dump_repeats.pl
+++ scripts/BLAST_scripts/dump_repeats.pl
@@ -6,37 +6,49 @@ use lib $ENV{'CVS_DIR'};
 use Getopt::Long;
 use Wormbase;
 use Log_files;
+use Storable;
+use POSIX qw(ceil);
+
+use lib '/software/worm/lib/bioperl-live';
+use lib '/software/worm/ensembl/ensembl/modules';
+use Bio::EnsEMBL::DBSQL::DBAdaptor;
+
 
 #######################################
 # command-line options                #
 #######################################
-my ($test, $debug, $help, $store);
+my ($test, $debug, $help, $store,$database,$dumpdir);
 
 GetOptions ("debug:s" => \$debug,
 	    "test"    => \$test,
 	    "help"    => \$help,
 	    "store:s" => \$store,
+	    'database:s' => \$database,
+	    'dump_dir=s' => \$dumpdir,
            );
 
 my $wormbase;
 if ( $store ) {
-  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+  $wormbase = Storable::retrieve( $store ) or croak("Can't restore wormbase from $store\n");
 } else {
   $wormbase = Wormbase->new( 'debug'   => $debug,
                              'test'    => $test,
 			     );
 }
 
+my $dbobj = Bio::EnsEMBL::DBSQL::DBAdaptor->new(
+                                                       '-host'   => 'ia64d',
+                                                       '-user'   => 'wormro',
+                                                       '-dbname' => $database
+                                                      ) || die (@!);
+
+
 # establish log file.
 my $log = Log_files->make_build_log($wormbase);
 
-my $dump_dir = "/lustre/work1/ensembl/wormpipe/dumps";
-$dump_dir = glob("~wormpub/TEST_BUILD/") if $test;
-my $acedb_database;
+my $dump_dir = ($dumpdir || $wormbase->acefiles);
 my $output = "$dump_dir/repeat_homologies.ace";
 die &help if $help;
-
-$output .= "_test" if $test;
 
 $log->write_to("Dumping to $output\n");
 
@@ -45,117 +57,61 @@ open (OUT,">$output") or $log->log_and_die("cant open $output\n");
 
 # retrieve hash of acc2clone
 my %acc2clone;
-$wormbase->FetchData("accession2clone",\%acc2clone);
+$wormbase->FetchData("accession2clone",\%acc2clone) if (ref $wormbase eq 'Elegans');
 
-my %clonesize;
-$wormbase->FetchData("clonesize", \%clonesize);
+my $seq_level = ref $wormbase eq 'Elegans'? 'clone' : 'toplevel';
 
-# mysql database parameters
-my $dbhost = "ia64b";
-my $dbuser = "wormro";
-my $dbname = "worm_dna";
-my $dbpass = "";
+my $sa=$dbobj->get_SliceAdaptor();
+foreach my $seq ( @{$sa->fetch_all($seq_level)}){
+	my $acc=$seq->seq_region_name;
+	$acc=~s/\.\d+$//;
+	my $clone=($acc2clone{$acc}||$acc);
+        my $clonesize=$seq->seq_region_length;
 
-my $wormrepeats_DB = DBI -> connect("DBI:mysql:$dbname:$dbhost", $dbuser, $dbpass, {RaiseError => 1})
-    or  $log->log_and_die("cannot connect to db, $DBI::errstr");
+	my $repeats = $seq->get_all_RepeatFeatures('RepeatMask');
+        
+	if (scalar @$repeats) {
+         print OUT "\nSequence : \"$clone\"\n";
+         print OUT "Homol_data $clone:RepeatMasker 1 $clonesize\n\n";
+         print OUT "Homol_data : $clone:RepeatMasker\n";
+        }
 
+	foreach my $feature (@$repeats){
+	     printf OUT ("Motif_homol %s RepeatMasker %s %s %s ",
+	        $feature->display_id,$feature->score,$feature->seq_region_start,$feature->seq_region_end);
+	     if ($feature->strand>0){
+		     printf OUT ("%s %s\n",$feature->hstart,$feature->hend);
+	     } else { 
+		     printf OUT ("%s %s\n",$feature->hend,$feature->hstart);
+	     }
+	}
 
-#retrieves descriptive information about the repeats from the database
-my %repeat_info;
-&GetRepeatInfo;
+        $repeats = $seq->get_all_RepeatFeatures('TRF');
+	if (scalar @$repeats) {
+         print OUT "\nSequence : \"$clone\"\n";
+         print OUT "Feature_data $clone:TRF 1 $clonesize\n\n";
+         print OUT "Feature_data : $clone:TRF\n";
+        }
 
-# build hash of internal_id to clone via EMBL accession
-my $sth_f = $wormrepeats_DB->prepare ( q{SELECT
-					 clone_id , name
-					 FROM clone
-	  	  	     } );
-
-my %int_id2clone;
-
-$sth_f->execute;
-my $ref_results = $sth_f->fetchall_arrayref;
-
-foreach my $pair (@$ref_results) {
-  my $int_id = $$pair[0];
-  my $acc = $$pair[1];
-  my $clone = $acc2clone{$acc};
-
-  if ($clone) {
-    $int_id2clone{$int_id} = $clone;
-  }
-  else {
-    $log->write_to("no clone found for acc $acc : internal_id $int_id\n");
-  }
+	foreach my $feature (@$repeats){
+            my $copy_no=ceil(abs($feature->seq_region_start - $feature->seq_region_end) / length( $feature->repeat_consensus->seq ));
+	    printf OUT ("Feature tandem %s %s %s \"%i copies of %imer\"\n",
+		    $feature->seq_region_start,$feature->seq_region_end,$feature->score,$copy_no,length($feature->repeat_consensus->seq )); 
+	}
 }
 
-# now retrieve the data from wormrepeats database
-$sth_f = $wormrepeats_DB->prepare ( q{SELECT
-				      contig_id, contig_start, contig_end,
-				      score, contig_strand,
-				      repeat_start,repeat_end,repeat_consensus_id
-				      FROM repeat_feature
-				      WHERE analysis_id = 10
-				      AND score > 225
-				      ORDER BY contig_id
-				     }
-				  );
-
-$sth_f->execute;
-$ref_results = $sth_f->fetchall_arrayref;
-my ($contig, $seq_start, $seq_end, $score, $strand, $hstart, $hend, $hid);
-my $current_clone = "";
-my $clone;
-foreach my $repeat (@$ref_results) {
-  ($contig, $seq_start, $seq_end, $score, $strand, $hstart, $hend, $hid) = @$repeat;
-  
-  if($int_id2clone{$contig}) {
-     $clone = $int_id2clone{$contig};
-    if( "$clone" ne "$current_clone" ) {
-      $current_clone = $clone;
-      print OUT "\nSequence : \"$clone\"\n";
-      print OUT "Homol_data $clone:RepeatMasker 1 $clonesize{$clone}\n\n";
-      print OUT "Homol_data : $clone:RepeatMasker\n";      
-    }
-    print OUT "Motif_homol ",$repeat_info{$hid}->{'name'} ," RepeatMasker $score $seq_start $seq_end";
-    if( $strand == 1 ){
-      print OUT " $hstart $hend\n";
-    }
-    else {
-      print OUT " $hend $hstart\n";
-    }
-  }
-  else {
-    undef $clone;
-  }
-}
 
 close OUT;
 
 $log->mail;
 exit(0);
-
-sub GetRepeatInfo
-  {
-    my $info = $wormrepeats_DB->prepare ( q{ SELECT * from repeat_consensus} );
-    $info->execute;
-
-    my $info_data = $info->fetchall_arrayref;
-    foreach my $repeat ( @$info_data ) {
-      $repeat_info{$$repeat[0]}->{'name'} = $$repeat[1];
-      $repeat_info{$$repeat[0]}->{'class'} = $$repeat[2];
-    } 
-  }
-
-
-sub help
-  {
+sub help {
     print "===============================================\n$0\n
-Extract RepeatMasker data from worm_dna on ecs1f\n\n
+Extract RepeatMasker and TRF data from worm_ensembl databases on ia64d\n\n
 Writes ace file $output\n
-\t\t-test appends _test to output filename.
 
-Must be able to access Wormbase.pm\
+Must be able to access Wormbase.pm and /software/worm/ensembl/\
 Takes about 2 mins to dump whole genome\n\n
 ================================================\n\n";
-  }
+}
 
