5d57d03363ef4c5cc023e5dac7e8b7e2e6dc10f4
mh6 <mh6>
2008-03-13 16:34:25 +0000

    removed the brigpep files from loading, as they may or may not exist depending on if briggsae was built or not.

--- scripts/load_data_sets.pl
+++ scripts/load_data_sets.pl
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -w
 # Last updated by: $Author: mh6 $     
-# Last updated on: $Date: 2008-03-13 16:24:54 $      
+# Last updated on: $Date: 2008-03-13 16:34:25 $      
 
 use strict;
 use lib $ENV{'CVS_DIR'};
@@ -87,15 +87,15 @@ sub parse_homol_data {
   my @files2Load = (
 		    #BLAST data
 		    "worm_pep_blastp.ace",
-		    "worm_brigpep_blastp.ace",
+#		    "worm_brigpep_blastp.ace", # don't load briggsae files unless building briggsae
 		    "worm_dna_blastx.ace",
 		    #motif info
 		    "worm_pep_motif_info.ace",
-		    "worm_brigpep_motif_info.ace",
+#		    "worm_brigpep_motif_info.ace",
 		    #protein info
 		    "ensembl_protein_info.ace",
 		    "worm_pep_interpro_motif_info.ace",
-		    "worm_brigpep_interpro_motif_info.ace",
+#		    "worm_brigpep_interpro_motif_info.ace",
 		    #other data
 		    "repeat_homologies.ace",
 		    "waba.ace",
