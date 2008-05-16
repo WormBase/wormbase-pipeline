aaefb0142ed285f4acede0128887e91f4d763bb3
ar2 <ar2>
2008-05-16 12:52:47 +0000

    work for merging species databases

--- scripts/make_acefiles.pl
+++ scripts/make_acefiles.pl
@@ -8,7 +8,7 @@
 # autoace.
 #
 # Last updated by: $Author: ar2 $
-# Last updated on: $Date: 2008-01-25 09:23:43 $
+# Last updated on: $Date: 2008-05-16 12:52:47 $
 
 #################################################################################
 # Variables                                                                     #
@@ -36,7 +36,7 @@ my $database;
 my $basedir;
 my $store;
 my $species;
-my $syntax;
+my ($syntax, $merge);
 
 GetOptions (	"debug=s"    => \$debug,
 		"help"       => \$help,
@@ -45,7 +45,8 @@ GetOptions (	"debug=s"    => \$debug,
 		"test"       => \$test,
 		"store:s"    => \$store,
 		"species:s"	 => \$species,
-		"syntax"	 => \$syntax
+		"syntax"	 => \$syntax,
+		"merge"		 => \$merge
 	   	);
 
 my $wormbase;
@@ -61,82 +62,96 @@ else {
 
 my $log = Log_files->make_build_log($wormbase);
 my $elegans = $wormbase->build_accessor;
-my $config = $wormbase->basedir."/autoace_config/".$wormbase->species.".config";
-my $tace = $wormbase->tace;
-my $path = $wormbase->acefiles."/primaries";
-open (CFG,"<$config") or $log->log_and_die("cant open $config :$!\n");
 
-my $dbpath = "";
-while(<CFG>) {
-	next if /#/;
-	next unless /\w/;
-    my %makefile;
-    foreach my $pair (split(/\t+/,$_)) {
-		my($tag,$value) = ($pair =~ /(\w+)=(.*)/);
-		if( $tag and $value) {
-			if($tag eq 'delete') {
-				push(@{$makefile{$tag}},$value);
-			}else {
-	    		$makefile{$tag} = $value;
-	    	}
-		}
-		else {
-	    	$log->log_and_die("Ill formed config line $_ :\n");
-	    }
-    }
-    next if $syntax;
-    my $query = "nosave\nquery ";
-    if( $makefile{'class'} and  $makefile{'db'} and  $makefile{'file'} ) {
-    	mkpath("$path/".$makefile{'db'}) unless -e "$path/".$makefile{'db'};
-    	my $file = $path."/".$makefile{'db'}."/".$makefile{'file'};
-    	open(ACE,">$file") or $log->log_and_die("cant open file $file : $!\n");
-    	
-    	if($makefile{'class'} eq 'DNA') {
- 			$query .= "find Sequence";
- 		}else {
-	    	$query .= "find ".$makefile{'class'};
-	    }
-	    if( $makefile{'query'} ) {
-	    	$query .= " WHERE ".$makefile{'query'};
-		}
-		$query .= "\n";    
- 		if( $makefile{'delete'} ) {	 
- 			foreach my $del (@{$makefile{'delete'}}) {   
-	 			$query .= "eedit -D $del\n";
-	 		}
- 		}
- 		if($makefile{'class'} eq 'DNA') {
- 			$query .= "FOLLOW DNA\n";
- 		}
-		if( $makefile{'follow'} ) {
-			$query .= "show -T ".$makefile{'follow'}." -a\n"; #output parent + followed tag
-			$query .= "FOLLOW ".$makefile{'follow'}."\n";
-		}
-		$query .= "show -T -a ";
-		if( $makefile{'tag'} ) {
-			$query .= " -t ".$makefile{'tag'};
-		}
-		$query .= "\n";
-		my $acedb = $dbpath."/".$makefile{'db'};
-		$log->write_to("dumping $makefile{'class'} from $acedb\n");
-		open(TACE,"echo '$query' | $tace $acedb | ") or $log->log_and_die("cant do query : $!\n");
-		LINE: while(<TACE>) {
-			next if (/acedb>/ or /\/\//);
-			if( $makefile{'regex'} ) {	
-				unless (/[^\w]/ or /$makefile{'class'}\s+\:\s+/ or /$makefile{'follow'}\s+\:\s+/) {
-					next LINE unless /$makefile{'regex'}/;
-				}			
-    		}
-    		print ACE;
-    	}
-    	close TACE;
-    	close ACE;
-	}
-	elsif ($makefile{'path'}) {
-		my $sub = $makefile{'path'};
-		$dbpath = $wormbase->$sub;
-	}
+my $config = $wormbase->basedir."/autoace_config/".$wormbase->species;
+$config .= ".merge" if $merge;
+$config .=".config";
+
+my $tace = $wormbase->tace;
+my $path = $wormbase->acefiles.($merge ? "/MERGE" :"/primaries");
+mkpath $path unless -e $path;
+
+unless (-e $config) {
+	$log->write_to("merge config file absent - database being skipped\n");
 }
+else {
+	open (CFG,"<$config") or $log->log_and_die("cant open $config :$!\n");
+
+	my $dbpath = "";
+	while(<CFG>) {
+		next if /#/;
+		next unless /\w/;
+    	my %makefile;
+    	foreach my $pair (split(/\t+/,$_)) {
+			my($tag,$value) = ($pair =~ /(\w+)=(.*)/);
+			if( $tag and $value) {
+				if($tag eq 'delete') {
+					push(@{$makefile{$tag}},$value);
+				}else {
+		    		$makefile{$tag} = $value;
+		    	}
+			}
+			else {
+		    	$log->log_and_die("Ill formed config line $_ :\n");
+		    }
+    	}
+    	next if $syntax;
+    	my $query = "nosave\nquery ";
+    	if( $makefile{'class'} and  $makefile{'db'} and  $makefile{'file'} ) {
+    		if ($db) {
+		    	next unless ($makefile{'db'} eq $db);
+    		}
+    		mkpath("$path/".$makefile{'db'}) unless -e "$path/".$makefile{'db'};
+    		my $file = $path."/".$makefile{'db'}."/".$makefile{'file'};
+    		open(ACE,">$file") or $log->log_and_die("cant open file $file : $!\n");
+    		
+    		if($makefile{'class'} eq 'DNA') {
+ 				$query .= "find Sequence";
+ 			}else {
+		    	$query .= "find ".$makefile{'class'};
+		    }
+		    if( $makefile{'query'} ) {
+		    	$query .= " WHERE ".$makefile{'query'};
+			}
+			$query .= "\n";    
+ 			if( $makefile{'delete'} ) {	 
+ 				foreach my $del (@{$makefile{'delete'}}) {   
+		 			$query .= "eedit -D $del\n";
+		 		}
+ 			}
+ 			if($makefile{'class'} eq 'DNA') {
+ 				$query .= "FOLLOW DNA\n";
+ 			}
+			if( $makefile{'follow'} ) {
+				$query .= "show -T ".$makefile{'follow'}." -a\n"; #output parent + followed tag
+				$query .= "FOLLOW ".$makefile{'follow'}."\n";
+			}
+			$query .= "show -T -a ";
+			if( $makefile{'tag'} ) {
+				$query .= " -t ".$makefile{'tag'};
+			}
+			$query .= "\n";
+			my $acedb = $dbpath."/".$makefile{'db'};
+			$log->write_to("dumping $makefile{'class'} from $acedb\n");
+			open(TACE,"echo '$query' | $tace $acedb | ") or $log->log_and_die("cant do query : $!\n");
+			LINE: while(<TACE>) {
+				next if (/acedb>/ or /\/\//);
+				if( $makefile{'regex'} ) {	
+					unless (/[^\w]/ or /$makefile{'class'}\s+\:\s+/ or /$makefile{'follow'}\s+\:\s+/) {
+						next LINE unless /$makefile{'regex'}/;
+					}			
+    			}
+    			print ACE;
+    		}
+    		close TACE;
+    		close ACE;
+		}
+		elsif ($makefile{'path'}) {
+			my $sub = $makefile{'path'};
+			$dbpath = $wormbase->$sub;
+		}
+	}
+}	
 
 $log->write_to("syntax ok\n") if $syntax; #will have died before here if not ok
 $log->mail;
