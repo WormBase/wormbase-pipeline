53c0974f7ff4ef3b1b0f9fdba4dd44632c6dc762
gw3 <gw3>
2008-06-12 13:53:11 +0000

    script to add Interpolated_map_position to Gene and Variation object lines in GFF files

---
+++ scripts/Map_pos_GFFprocess.pl
@@ -0,0 +1,100 @@
+#!/software/bin/perl -w
+
+use strict;                                      
+use lib $ENV{'CVS_DIR'};
+use Wormbase;
+use Getopt::Long;
+use Log_files;
+use Storable;
+use Ace;
+use File::Copy;
+
+my ($help, $debug, $test, $verbose, $store, $wormbase, $species);
+my( $dbpath);
+GetOptions (
+            "help"      => \$help,
+            "debug=s"	=> \$debug,
+	    "test"	=> \$test,
+	    "store:s"	=> \$store,
+	    "species:s" => \$species,
+	    );
+
+if ( $store ) {
+  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+} else {
+  $wormbase = Wormbase->new( -debug    => $debug,
+                             -test     => $test,
+                             -organism => $species
+			     );
+}
+
+# establish log file.
+my $log = Log_files->make_build_log($wormbase);
+
+$dbpath = $wormbase->orgdb;
+my $db = Ace->connect('-path' => $dbpath) or $log->log_and_die("cant open Ace connection to $dbpath\n".Ace->error."\n");
+
+
+# get the physical mapping position of the Variations
+print "Reading Variation mapping data\n";
+my %variation;
+my $query = "find Variation where Interpolated_map_position";
+my $vars = $db->fetch_many('-query' => $query);
+while (my $var = $vars->next){
+  $variation{$var->name} = $var->Interpolated_map_position(2);
+}
+
+
+# get the physical mapping position of the Genes
+print "Reading Gene mapping data\n";
+my %gene;
+$query = "find Gene where Interpolated_map_position";
+my $genes = $db->fetch_many('-query' => $query);
+while (my $gene = $genes->next){
+  $gene{$gene->name} = $gene->Interpolated_map_position(2);
+}
+
+undef $db;
+
+#read GFF files
+my @chroms = @{$wormbase->get_binned_chroms};
+
+foreach my $chroms (@chroms) {
+  print "Processing chromosome $chroms\n";
+  my $gff_file = $wormbase->gff."/".$chroms.".gff";
+  open (GFF,"<$gff_file") or $log->log_and_die("cant open GFF $gff_file: $!\n");
+  open (NEW,">$gff_file.new") or $log->log_and_die("cant write new GFF file: $!\n");
+  while (my $line = <GFF>) {
+    chomp $line;
+    if ($line =~ /^#/) {next;}
+
+    my @f = split(/\t/, $line);
+
+    if (defined $f[8] && $f[1] eq 'Allele') {
+      my ($allele) = $f[8] =~ /Variation\s+\"(\S+)\"/;
+      #if (!defined $allele) {die "no allele ID found Line: $line";}
+      $f[8] .= " ; Interpolated_map_position \"$variation{$allele}\"" if (exists $variation{$allele});
+	
+      print NEW join("\t",@f) . "\n";
+      #print join("\t",@f) . "\n";
+
+    } elsif ($f[1] eq 'gene' && $f[2] eq 'gene') {
+      my ($gene) = $f[8] =~ /Gene\s+\"(\S+)\"/;
+      #if (!defined $gene) {die "no gene ID found Line: $line";}
+      $f[8] .= " ; Interpolated_map_position \"$gene{$gene}\""if (exists $gene{$gene});
+	
+      print NEW join("\t",@f) . "\n";
+      #print join("\t",@f) . "\n";
+
+    } else {
+      print NEW "$line\n";
+    }
+  }
+  close GFF;
+  close NEW;
+  move("$gff_file.new","$gff_file") or $log->error("cant move $gff_file.new :$!\n") ;
+}
+
+
+$log->mail();
+exit(0);
