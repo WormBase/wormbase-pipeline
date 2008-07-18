b0463da2773d4523d3e0ec27fee123188e52dec9
ar2 <ar2>
2008-07-18 15:35:58 +0000

    exclude remanei too

--- scripts/BLAST_scripts/swiss_trembl2slim.pl
+++ scripts/BLAST_scripts/swiss_trembl2slim.pl
@@ -23,6 +23,7 @@ $exclude{'Saccharomyces cerevisiae'}     = 1;
 $exclude{'Homo sapiens'}                 = 1;
 $exclude{'Human immunodeficiency virus'} = 1;
 $exclude{'Caenorhabditis briggsae'}      = 1;
+$exclude{'Caenorhabditis remanei'}       = 1;
 
 our $output; # file to write
 my $output_dir = "/lustre/work1/ensembl/wormpipe/swall_data";
