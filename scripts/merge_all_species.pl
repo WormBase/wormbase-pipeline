83e2b589a48e9d73407e1643a41dbc521a71f3b2
gw3 <gw3>
2008-11-25 14:21:24 +0000

    this now reads in data from several sources

--- scripts/merge_all_species.pl
+++ scripts/merge_all_species.pl
@@ -1,7 +1,7 @@
 #/software/bin/perl -w
 #
-# Last updated by: $Author: pad $
-# Last updated on: $Date: 2008-08-27 09:34:10 $
+# Last updated by: $Author: gw3 $
+# Last updated on: $Date: 2008-11-25 14:21:24 $
 
 #################################################################################
 # Variables                                                                     #
@@ -22,14 +22,10 @@ use LSF::JobManager;
 
 my $debug;      # Debug mode, verbose output to runner only
 my $test;        # If set, script will use TEST_BUILD directory under ~wormpub
-my $db;
-my $database;
 my $basedir;
 my $store;
 
 GetOptions (	"debug=s"    => \$debug,
-		"database=s" => \$database,
-		"db:s"       => \$db,
 		"test"       => \$test,
 		"store:s"    => \$store,
 	   	);
@@ -47,43 +43,103 @@ else {
 
 my $log = Log_files->make_build_log($wormbase);
 
+$log->write_to("hacked version of merge_all_species\n\n");
+
 my %accessors = $wormbase->species_accessors;
+
+# move all the old MERGE files out of the way
+$log->write_to("\nRemove the old MERGE ace files . . .\n");
+foreach my $spDB (values %accessors) {
+  my $dir = $spDB->acefiles."/MERGE/".$spDB->species."/";
+  if (-e $dir) {
+    foreach my $file ( &read_dir($dir) ) {
+      $wormbase->run_command("mv $file $file.old", $log);
+    }
+  }
+}
+
 
 # dump out the files in parallel.
 my $lsf =  LSF::JobManager->new();
-$log->write_to("Dumping acefile from . . .\n");
+$log->write_to("\nDumping acefile from . . .\n");
 foreach my $spDB (values %accessors) {
   $log->write_to("\t".$spDB->full_name('-short' => 1));
   $lsf->submit(-J => $spDB->species, $spDB->build_cmd("make_acefiles.pl -merge"));
 }
 
 $lsf->wait_all_children( history => 1 );
+$log->write_to("\nFinished writing acefiles\n");
 
-$log->write_to("\nFinished writing acefiles\nAbout to load . .\n");
+$log->write_to("\nDelete the homol_data files . . .\n");
+foreach my $spDB (values %accessors) {
+  my $dir = $spDB->acefiles."/MERGE/".$spDB->species."/";
+  unlink "$dir".$spDB->species."_Homol_data.ace";
+}
+
+
+$log->write_to("\nAbout to load . . .\n");
 #and then load then one after another.
 foreach my $spDB (values %accessors) {
+  my $species = $spDB->species;
+  $log->write_to("  Load from $species . . .\n");
   my @loaded;
   my $dir = $spDB->acefiles."/MERGE/".$spDB->species."/";
   next unless -e $dir;
   push(@loaded,$spDB->species);
   foreach my $file ( &read_dir($dir) ) {
+    $log->write_to("    loading $file\n");
     $wormbase->load_to_database($wormbase->orgdb, $file, "merge_all_species", $log);
   }
-  $log->write_to("\tloaded ".join(', ',@loaded)." in to ".$wormbase->orgdb."\n");
+  $log->write_to("  Loaded ".join(', ',@loaded)." in to ".$wormbase->orgdb."\n");
 }
 
-$log->write_to("\nNow loading blast data\n");
+# look to see which BLAT files were loaded into the BUILD/species database and load them into autoace
+$log->write_to("\nNow loading BLAT data . . .\n");
 foreach my $spDB (values %accessors) {
-  my @blastfiles = qw( SPECIES_blastp.ace SPECIES_blastx.ace worm_ensembl_SPECIES_interpro_motif_info.ace worm_ensembl_SPECIES_motif_info.ace);
+  my $species = $spDB->species;
+  $log->write_to("  Load BLAT from $species . . .\n");
+
+  my $logfile = $spDB->orgdb."/database/log.wrm";
+  open (LOG, "< $logfile") || die "Can't open $logfile : $!";
+  my @acelist;
+  while (my $line = <LOG>) {
+    chomp $line;
+    if ($line =~ /Parsing\s+file\s+(.+)/) {
+      my $acefile = $1;
+      # we only want the BLAT files and not duplicate files
+      if ($acefile =~ /\/BLAT\// && !grep /$acefile/, @acelist) {
+	if (-e $acefile) {
+	  push @acelist, $acefile;
+	} else {
+	  $log->error("ERROR: The file $acefile does not exist!\n");
+	}
+      }
+    }
+  }
+  close (LOG);
+
+  foreach my $file ( @acelist ) {
+    $log->write_to("    loading $file\n");
+    $wormbase->load_to_database($wormbase->orgdb, $file, "merge_all_species", $log);
+  }
+}
+
+
+
+
+$log->write_to("\nNow loading BLAST and repeat data . . .\n");
+foreach my $spDB (values %accessors) {
+  my $species = $spDB->species;
+  $log->write_to("  Load BLAST from $species . . .\n");
+  my @blastfiles = qw( SPECIES_blastp.ace SPECIES_blastx.ace worm_ensembl_SPECIES_interpro_motif_info.ace worm_ensembl_SPECIES_motif_info.ace repeat_homologies.ace inverted_repeats.ace );
   foreach my $f (@blastfiles){
     my $file = $f;		# don't use $f as it is a reference to the array element
-    my $species = $spDB->species;
     $file =~ s/SPECIES/$species/;
     if (-e $spDB->acefiles."/$file") {
-      $log->write_to("Loading: $file\n");
+      $log->write_to("    loading $file\n");
       $wormbase->load_to_database($wormbase->orgdb, $spDB->acefiles."/$file", "merge_all_species", $log);
     } else {
-      $log->write_to("WARNING: Can't find $file\n");
+      $log->error("ERROR: Can't find $file\n");
     }
   }
 }
@@ -92,10 +148,12 @@ $log->mail;
 
 exit;
 	
+###################################################################################################
+
 sub read_dir {
   my $dir = shift;
   opendir (DIR,$dir) or $log->log_and_die("cant open directory $dir\n");
-  $log->write_to("\treading $dir\n");
+  $log->write_to("    Reading $dir\n");
   my @files = readdir DIR;
   my @to_load;
   foreach my $file ( @files ) {
