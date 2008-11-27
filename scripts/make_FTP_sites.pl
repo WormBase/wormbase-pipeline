ebdaaa246e44c9de2d1dd5902043c2b866dff472
gw3 <gw3>
2008-11-27 12:15:37 +0000

    sorted out filenames in copying the dna files

--- scripts/make_FTP_sites.pl
+++ scripts/make_FTP_sites.pl
@@ -8,7 +8,7 @@
 # Originally written by Dan Lawson
 #
 # Last updated by: $Author: gw3 $
-# Last updated on: $Date: 2008-11-27 11:48:44 $
+# Last updated on: $Date: 2008-11-27 12:15:37 $
 #
 # see pod documentation (i.e. 'perldoc make_FTP_sites.pl') for more information.
 #
@@ -297,10 +297,10 @@ sub copy_dna_files{
       #todd wants all species to have whole genome in one file
       if ($wb->assembly_type eq 'contig') {
 	my $dna_file = "$chromdir/supercontigs.fa";
-	my $masked_file = "$chromdir/".$gspecies."_masked.dna";
-	my $soft_file = "$chromdir/".$gspecies."_masked.dna";
+	my $masked_file = "$chromdir/".$gspecies."_masked.dna.gz";
+	my $soft_file = "$chromdir/".$gspecies."_softmasked.dna.gz";
 		
-	$wormbase->run_command("/bin/gzip -f $dna_file > $dna_dir/".$gspecies."$WS_name.dna.fa",$log);
+	$wormbase->run_command("/bin/gzip -c $dna_file >! $dna_dir/".$gspecies."$WS_name.dna.fa.gz",$log);
 	$wormbase->run_command("cp -f $soft_file $dna_dir/".$gspecies."_softmasked.$WS_name.dna.fa.gz", $log);
 	$wormbase->run_command("cp -f $masked_file $dna_dir/".$gspecies."_masked.$WS_name.dna.fa.gz", $log);
 		
