95ce3815ccf5a0d730183a5354f9f00b2a2c81c6
ar2 <ar2>
2008-12-18 15:51:21 +0000

    options, checks AND pod!

--- scripts/MISC_SCRIPTS/get_flanking_sequences.pl
+++ scripts/MISC_SCRIPTS/get_flanking_sequences.pl
@@ -4,31 +4,88 @@ use lib $ENV{'CVS_DIR'};
 use Sequence_extract;
 use Wormbase;
 use Storable;
+use Getopt::Long;
 
-#my $database = glob("~wormpub/DATABASES/");
-my $database = '/nfs/disk100/wormpub/BUILD/autoace';
+my ($help,$database, $input_file);
+my ($seq,$start, $end, $length);
+GetOptions ("help"       => \&help,
+	    "database=s" => \$database,
+	    "file:s"	 => \$input_file,
+	    "seq:s"      => \$seq,
+	    "start:i"    => \$start,
+	    "end:i"      => \$end,
+	    "length:i"   => \$length
+	   );
 
-#my $wb = Wormbase->new( '-test' => 1,
-#			'-debug' => 'ar2'
-#);
+$database = '/nfs/disk100/wormpub/DATABASES/current_DB' unless $database;
 
 my $wb = retrieve("$database/Elegans.store");
-
 my $seq_obj      =  Sequence_extract->invoke($database,undef,$wb);
+$length = 30 unless $length;
 
-my ($seq,$start, $end) = @ARGV;
-my ($c_seq, $c_start, $c_end) = $seq_obj->LocateSpan($seq, $start, $end);
-
-my ($flank1, $flank2);
-if($start < $end) {
-    $flank1 = $seq_obj->Sub_sequence($c_seq, $c_start-31, 30);
-    $flank2 = $seq_obj->Sub_sequence($c_seq, $c_end,30);
+if(defined $input_file){
+    open(IF,"<$input_file") or die "cant open $input_file: $!\n";
+    while(<IF>){
+	($seq,$start, $end) = split(/\s+/,$_);
+	&get_flanks;
+    }
+    close IF;
 }
 else {
-    $flank1 = $wb->DNA_string_reverse($seq_obj->Sub_sequence($c_seq, $c_end+1, 30));
-    $flank2 = $wb->DNA_string_reverse($seq_obj->Sub_sequence($c_seq, $c_start-31,30));
+    &get_flanks;
+} 
+
+
+
+sub get_flanks {
+    &check_data($seq, $start, $end) or do{print "FAILED :\n";return};
+    my ($c_seq, $c_start, $c_end) = $seq_obj->LocateSpan($seq, $start, $end);
+    
+    my ($flank1, $flank2);
+    if($start < $end) {
+	$flank1 = $seq_obj->Sub_sequence($c_seq, $c_start-($length+1), $length);
+	$flank2 = $seq_obj->Sub_sequence($c_seq, $c_end,$length);
+    }
+    else {
+	$flank1 = $wb->DNA_string_reverse($seq_obj->Sub_sequence($c_seq, $c_end+1, $length));
+	$flank2 = $wb->DNA_string_reverse($seq_obj->Sub_sequence($c_seq, $c_start-($length+1),$length));
+    }
+    print "$seq\t$start\t$end\t$flank1\t$flank2\n";
 }
-print "$seq\t$start\t$flank1\t$flank2\n";
 
+sub check_data {
+    my ($seq, $start, $end) = @_;
+    return unless ($seq =~ /[a-zA-Z]/ and  $start =~ /\d/ and $end =~ /\d/);
+    return 1;
+}
 
-	
+sub help {
+    system ("perldoc $0");
+    exit;
+}
+
+__END__
+
+=pod
+
+=head1 get_flanking_sequences.pl
+
+This is to get flanking sequences for a given piece of the genome.
+
+=head1 Options
+
+=over 4
+
+    -database   Specificy which database to use. Defaults to current_DB
+    -seq        Genomic sequence
+    -start      start coordinate 
+    -end        end coordinate
+    -length     length of flanking sequences required
+    -file       pass a file of coordinates to find flanks for.  File needs to be 3 columns (seq, start, end)
+    -help       show this pod
+
+=back
+
+    e.g. perl  get_flanking_sequences.pl -seq V -start 39000 -end 40000 -length 35
+
+=cut
