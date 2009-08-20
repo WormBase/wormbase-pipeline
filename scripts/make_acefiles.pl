406985867d54aa85b7a9366c60bbea6c75b83e6c
gw3 <gw3>
2009-08-20 10:56:35 +0000

    added error checks for spaces except when in value fields containing a (

--- scripts/make_acefiles.pl
+++ scripts/make_acefiles.pl
@@ -7,8 +7,8 @@
 # Generates the .acefiles from the primary databases as a prelim for building
 # autoace.
 #
-# Last updated by: $Author: ar2 $
-# Last updated on: $Date: 2009-01-06 17:34:51 $
+# Last updated by: $Author: gw3 $
+# Last updated on: $Date: 2009-08-20 10:56:35 $
 
 #################################################################################
 # Variables                                                                     #
@@ -70,87 +70,90 @@ $config .=".config";
 my $tace = $wormbase->tace;
 my $path = $wormbase->acefiles.($merge ? "/MERGE" :"/primaries");
 mkpath $path unless -e $path;
-
+ 
 unless (-e $config) {
 	$log->write_to("merge config file absent - database being skipped\n");
 }
 else {
-	open (CFG,"<$config") or $log->log_and_die("cant open $config :$!\n");
+  open (CFG,"<$config") or $log->log_and_die("cant open $config :$!\n");
 
-	my $dbpath = "";
-	while(<CFG>) {
-		next if /#/;
-		next unless /\w/;
-    	my %makefile;
-    	foreach my $pair (split(/\t+/,$_)) {
-			my($tag,$value) = ($pair =~ /(\w+)=(.*)/);
-			if( $tag and $value) {
-				if($tag eq 'delete') {
-					push(@{$makefile{$tag}},$value);
-				}else {
-		    		$makefile{$tag} = $value;
-		    	}
-			}
-			else {
-		    	$log->log_and_die("Ill formed config line $_ :\n");
-		    }
-    	}
-    	next if $syntax;
-    	my $query = "nosave\nquery ";
-    	if( $makefile{'class'} and  $makefile{'db'} and  $makefile{'file'} ) {
-    		if ($db) {
-		    	next unless ($makefile{'db'} eq $db);
-    		}
-    		mkpath("$path/".$makefile{'db'}) unless -e "$path/".$makefile{'db'};
-    		my $file = $path."/".$makefile{'db'}."/".$makefile{'file'};
-    		open(ACE,">$file") or $log->log_and_die("cant open file $file : $!\n");
-    		
-    		if($makefile{'class'} eq 'DNA') {
- 				$query .= "find Sequence";
- 			}else {
-		    	$query .= "find ".$makefile{'class'};
-		    }
-		    if( $makefile{'query'} ) {
-		    	$query .= " WHERE ".$makefile{'query'};
-			}
-			$query .= "\n";    
- 			if( $makefile{'delete'} ) {	 
- 				foreach my $del (@{$makefile{'delete'}}) {   
-		 			$query .= "eedit -D $del\n";
-		 		}
- 			}
- 			if($makefile{'class'} eq 'DNA') {
- 				$query .= "FOLLOW DNA\n";
- 			}
-			if( $makefile{'follow'} ) {
-				$query .= "show -T ".$makefile{'follow'}." -a\n"; #output parent + followed tag
-				$query .= "FOLLOW ".$makefile{'follow'}."\n";
-			}
-			$query .= "show -T -a ";
-			if( $makefile{'tag'} ) {
-				$query .= " -t ".$makefile{'tag'};
-			}
-			$query .= "\n";
-			my $acedb = $dbpath."/".$makefile{'db'};
-			$log->write_to("dumping $makefile{'class'} from $acedb\n");
-			open(TACE,"echo '$query' | $tace $acedb | ") or $log->log_and_die("cant do query : $!\n");
-			LINE: while(<TACE>) {
-				next if (/acedb>/ or /^\/\//);
-				if( $makefile{'regex'} ) {	
-					unless (/[^\w]/ or /$makefile{'class'}\s+\:\s+/ or /$makefile{'follow'}\s+\:\s+/) {
-						next LINE unless /$makefile{'regex'}/;
-					}			
-    				}
-    				print ACE;
-    			}
-    			close TACE;
-    			close ACE;
-		}
-		elsif ($makefile{'path'}) {
-			my $sub = $makefile{'path'};
-			$dbpath = $wormbase->$sub;
-		}
+  my $dbpath = "";
+  while(<CFG>) {
+    next if /#/;
+    next unless /\w/;
+    my %makefile;
+    foreach my $pair (split(/\t+/,$_)) {
+      my($tag,$value) = ($pair =~ /(\w+)=(.*)/);
+      if( $tag and $value) {
+	if ($tag =~ /\ / || ($value =~ /\ / && $value !~ /\(/)) {
+	  $log->log_and_die("Ill formed config line with space instead of TAB around $tag=$value:\n$_\n");
+	} else {print "DEBUG $tag and $value OK\n";}
+	if($tag eq 'delete') {
+	  push(@{$makefile{$tag}},$value);
+	}else {
+	  $makefile{$tag} = $value;
 	}
+      }
+      else {
+	$log->log_and_die("Ill formed config line:\n$_\n");
+      }
+    }
+    next if $syntax;
+    my $query = "nosave\nquery ";
+    if( $makefile{'class'} and  $makefile{'db'} and  $makefile{'file'} ) {
+      if ($db) {
+	next unless ($makefile{'db'} eq $db);
+      }
+      mkpath("$path/".$makefile{'db'}) unless -e "$path/".$makefile{'db'};
+      my $file = $path."/".$makefile{'db'}."/".$makefile{'file'};
+      open(ACE,">$file") or $log->log_and_die("cant open file $file : $!\n");
+      
+      if($makefile{'class'} eq 'DNA') {
+	$query .= "find Sequence";
+      }else {
+	$query .= "find ".$makefile{'class'};
+      }
+      if( $makefile{'query'} ) {
+	$query .= " WHERE ".$makefile{'query'};
+      }
+      $query .= "\n";    
+      if( $makefile{'delete'} ) {	 
+	foreach my $del (@{$makefile{'delete'}}) {   
+	  $query .= "eedit -D $del\n";
+	}
+      }
+      if($makefile{'class'} eq 'DNA') {
+	$query .= "FOLLOW DNA\n";
+      }
+      if( $makefile{'follow'} ) {
+	$query .= "show -T ".$makefile{'follow'}." -a\n"; #output parent + followed tag
+	$query .= "FOLLOW ".$makefile{'follow'}."\n";
+      }
+      $query .= "show -T -a ";
+      if( $makefile{'tag'} ) {
+	$query .= " -t ".$makefile{'tag'};
+      }
+      $query .= "\n";
+      my $acedb = $dbpath."/".$makefile{'db'};
+      $log->write_to("dumping $makefile{'class'} from $acedb\n");
+      open(TACE,"echo '$query' | $tace $acedb | ") or $log->log_and_die("cant do query : $!\n");
+    LINE: while(<TACE>) {
+	next if (/acedb>/ or /^\/\//);
+	if( $makefile{'regex'} ) {	
+	  unless (/[^\w]/ or /$makefile{'class'}\s+\:\s+/ or /$makefile{'follow'}\s+\:\s+/) {
+	    next LINE unless /$makefile{'regex'}/;
+	  }			
+	}
+	print ACE;
+      }
+      close TACE;
+      close ACE;
+    }
+    elsif ($makefile{'path'}) {
+      my $sub = $makefile{'path'};
+      $dbpath = $wormbase->$sub;
+    }
+  }
 }	
 
 $log->write_to("syntax ok\n") if $syntax; #will have died before here if not ok
