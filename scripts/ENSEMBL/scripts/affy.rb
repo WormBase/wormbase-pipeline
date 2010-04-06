9348050847c3e3eabb065a02a61dc1d9e0968996
mh6 <mh6>
2010-04-06 15:39:02 +0000

    adapted to the newish funcgen schema

--- scripts/ENSEMBL/scripts/affy.rb
+++ scripts/ENSEMBL/scripts/affy.rb
@@ -14,10 +14,9 @@ data.each{|array_name,v|
 	v.each{|hit|
 		print <<HERE
 
-INSERT IGNORE INTO object_xref (ensembl_object_type,ensembl_id,xref_id) SELECT 'ProbeSet', probe_set_id,xref_id FROM probe_set p, xref x  WHERE x.dbprimary_acc like "#{hit}%" AND p.name="#{array_name}";
-INSERT IGNORE INTO object_xref (ensembl_object_type,ensembl_id,xref_id) SELECT 'Probe', probe_id,xref_id FROM probe join probe_set p using (probe_set_id), xref x  WHERE x.dbprimary_acc like "#{hit}%" AND p.name="#{array_name}";
+INSERT IGNORE INTO object_xref (ensembl_object_type,ensembl_id,xref_id,linkage_annotation,analysis_id) SELECT 'ProbeSet', probe_set_id,xref_id,'[imported from WormBase]',5 FROM probe_set p, xref x  WHERE x.dbprimary_acc like "#{hit}%" AND p.name="#{array_name}";
+INSERT IGNORE INTO object_xref (ensembl_object_type,ensembl_id,xref_id,xref_id,linkage_annotation,analysis_id) SELECT 'Probe', probe_id,xref_id,'[imported from WormBase]',5 FROM probe join probe_set p using (probe_set_id), xref x  WHERE x.dbprimary_acc like "#{hit}%" AND p.name="#{array_name}";
+INSERT IGNORE INTO object_xref (ensembl_object_type,ensembl_id,xref_id,linkage_annotation,analysis_id) SELECT 'ProbeFeature', probe_feature_id,xref_id,'[imported from WormBase]',5 FROM probe_feature join probe using (probe_id) join probe_set p using (probe_set_id), xref x  WHERE x.dbprimary_acc like "#{hit}%" AND p.name="#{array_name}";
 HERE
-
-#had: INSERT IGNORE INTO object_xref (ensembl_object_type,ensembl_id,xref_id) SELECT 'ProbeFeature', probe_feature_id,xref_id FROM probe_feature join probe using (probe_id) join probe_set p using (probe_set_id), xref x  WHERE x.dbprimary_acc like "#{hit}%" AND p.name="#{array_name}";
-	}
+  }
 }
