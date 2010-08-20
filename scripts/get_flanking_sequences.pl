55910949396c5293048f7b9d46ba04b737cbdc94
gw3 <gw3>
2010-08-20 09:01:34 +0000

    added some perdocs

--- scripts/get_flanking_sequences.pl
+++ scripts/get_flanking_sequences.pl
@@ -5,8 +5,8 @@
 # Script to grab two unique flanking sequences after specifying a sequence 
 # and two coordinates
 #
-# Last updated by: $Author: mh6 $     
-# Last updated on: $Date: 2010-06-08 09:36:01 $      
+# Last updated by: $Author: gw3 $     
+# Last updated on: $Date: 2010-08-20 09:01:34 $      
 
 use strict;
 use lib $ENV{'CVS_DIR'};                  
@@ -16,15 +16,20 @@ use Storable;
 use Getopt::Long;
 
 
-my ($store, $wormbase, $species);
+my ($help, $store, $wormbase, $species);
 my ($seq,$x,$y,$db);
 GetOptions ("store:s" => \$store,
 	    "x:i"     => \$x,
 	    "y:i"     => \$y,
 	    "db:s"    => \$db,
 	    "seq:s"   => \$seq,
-	    "species:s"=>\$species
+	    "species:s"=>\$species,
+	    "help"    => \$help,
 	    );
+
+# Display help if required
+&usage("Help") if ($help);
+
 
 my $flanking_seq_length    = 30;
 
@@ -47,3 +52,109 @@ if($l and $r) {print "Left  flank: $l\nRight flank: $r\n";}
 else { print "ERROR: cant find flanks for $seq:$x-$y\n";}
 
 exit(0);
+
+
+##############################################################
+#
+# Subroutines
+#
+##############################################################
+
+
+
+##########################################
+
+sub usage {
+  my $error = shift;
+
+  if ($error eq "Help") {
+    # Normal help menu
+    system ('perldoc',$0);
+    exit (0);
+  }
+}
+
+##########################################
+
+
+
+
+# Add perl documentation in POD format
+# This should expand on your brief description above and 
+# add details of any options that can be used with the program.  
+# Such documentation can be viewed using the perldoc command.
+
+
+__END__
+
+=pod
+
+=head2 NAME - get_flanking_sequence.pl
+
+=head1 USAGE
+
+=over 4
+
+=item get_flanking_sequence.pl  [-options]
+
+=back
+
+This script finds the flanking sequences of a region on the genome.
+
+script_template.pl MANDATORY arguments:
+
+=over 4
+
+=item -x, starting coordinate in the sequence of your object
+
+=back
+
+=over 4
+
+=item -y, ending coordinate in the sequence of your object
+
+=back
+
+=over 4
+
+=item -seq, the sequence that you are specifying the coordinates of - this can be a chomosome, a superlink or a clone.
+
+=back
+
+script_template.pl  OPTIONAL arguments:
+
+=over 4
+
+=item -h, Help
+
+=back
+
+=over 4
+ 
+=item -species, the species you are working on
+ 
+=back
+
+=over 4
+
+=item -db, the database you wisk to look in - the default is autoace
+
+=back
+
+=head1 REQUIREMENTS
+
+=over 4
+
+=item None at present.
+
+=back
+
+=head1 AUTHOR
+
+=over 4
+
+=item mh6 (mh6@sanger.ac.uk)
+
+=back
+
+=cut
