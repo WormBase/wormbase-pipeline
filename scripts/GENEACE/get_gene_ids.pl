7577cca3dd05e9355fb9e2bc24ffd503360010cb
mh6 <mh6>
2010-11-02 12:13:54 +0000

    script to batch_create genes

---
+++ scripts/GENEACE/get_gene_ids.pl
@@ -0,0 +1,56 @@
+#!/software/bin/perl -w
+
+use lib $ENV{'CVS_DIR'};
+use lib '/nfs/WWWdev/SANGER_docs/lib/Projects/C_elegans';
+use lib '/software/worm/lib/perl';
+use NameDB_handler;
+use Wormbase;
+use Storable;
+use Getopt::Long;
+
+#connect to name server and set domain to 'Gene'
+
+my $PASS;
+my $USER;
+my $DOMAIN  = 'Gene';
+my ($debug, $test, $store, $species, $database);
+
+GetOptions (
+	    "debug=s"    => \$debug,
+	    "test"       => \$test,
+	    "store:s"    => \$store,
+	    "species:s"  => \$species,
+	    "database:s" => \$database,
+	    "user:s"	 => \$USER,
+	    "pass:s"	 => \$PASS,
+	   );
+
+
+my $wormbase;
+
+die "Species option is mandatory\n" unless $species;
+
+
+if ( $store ) {
+  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+} else {
+  $wormbase = Wormbase->new( -debug   => $debug,
+                             -test    => $test,
+                             -organism => $species
+			     );
+}
+# establish log file.
+my $log = Log_files->make_build_log($wormbase);
+my $DB = $wormbase->test ? 'test_wbgene_id;mcs4a:3307' : 'wbgene_id;shap:3303';
+my $db = NameDB_handler->new($DB,$USER,$PASS);
+$db->setDomain($DOMAIN);
+
+while (<>) {
+	chomp;
+	@F=split;
+	$gene_id = $db->new_gene($F[0], 'Sequence', $species);
+	print "$F[0] $gene_id\n";
+}
+
+$log->mail;
+exit;
