951b20cbbdcd6193e23bee24435636f49ba2badd
mh6 <mh6>
2010-11-02 12:33:27 +0000

    almost in the correct method

--- scripts/GENEACE/get_gene_ids.pl
+++ scripts/GENEACE/get_gene_ids.pl
@@ -42,13 +42,13 @@ if ( $store ) {
 # establish log file.
 my $log = Log_files->make_build_log($wormbase);
 my $DB = $wormbase->test ? 'test_wbgene_id;mcs4a:3307' : 'wbgene_id;shap:3303';
-my $db = NameDB_handler->new($DB,$USER,$PASS);
+my $db = NameDB_handler->new($DB,$USER,$PASS,'/nfs/WWWdev/SANGER_docs/htdocs/');
 $db->setDomain($DOMAIN);
 
 while (<>) {
 	chomp;
 	my @F=split;
-	my $gene_id = $db->new_gene($F[0], 'Sequence', $species,'/nfs/WWWdev/SANGER_docs/htdocs/');
+	my $gene_id = $db->new_gene($F[0], 'Sequence', $species);
 	print "$F[0] $gene_id\n";
 }
 
