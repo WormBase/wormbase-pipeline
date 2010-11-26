80d8d6a45f6e926edb30c2cbc767accc251ec581
mh6 <mh6>
2010-11-26 12:03:47 +0000

    that one uses now the wquery directory from CVS and some other random cleanups

--- scripts/NAMEDB/variation_server_compare.pl
+++ scripts/NAMEDB/variation_server_compare.pl
@@ -12,22 +12,20 @@ use Carp;
 use Getopt::Long;
 use Storable;
 
-my ($help, $debug, $test, $store, $database, $def);
+my ($help, $debug, $test, $store, $database);
 
-GetOptions (	"help"       => \$help,
-            	"debug=s"    => \$debug,
-		"test"       => \$test,
-		"store:s"    => \$store,
-		"database:s" => \$database,
-		);
+GetOptions (    'help'       => \$help,
+                'debug=s'    => \$debug,
+                'test'       => \$testt,
+                'store:s'    => \$store,
+                'database:s' => \$database,
+           );
 
 my $wormbase;
-if ( $store ) {
+if ($store) {
     $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
 } else {
-    $wormbase = Wormbase->new( -debug   => $debug,
-			       -test    => $test,
-			       );
+    $wormbase = Wormbase->new(-debug => $debug, -test => $test);
 }
 
 
@@ -38,7 +36,8 @@ my $log = Log_files->make_build_log($wormbase);
 my $acedb = ($database or $wormbase->database('geneace'));
 $log->write_to("Checking $acedb for errors\n");
 
-$def = $wormbase->database('geneace')."/wquery/variation_nameserver_comm.def" unless $def;
+my $def = "$ENV{CVS_DIR}/wquery/geneace/variation_nameserver_comm.def";
+
 my $TABLE = $wormbase->table_maker_query($acedb, $def);
 my %ace_ids;
 while(<$TABLE>) {
@@ -51,20 +50,20 @@ delete $ace_ids{'acedb>'};
 close $TABLE;
 
 #connect to name server and set domain to 'Gene'
-my $DB    	= 'wbgene_id;shap;3303';
-my $PASS 	= "wormpub";
-my $USER 	= "wormpub";
+my $DB      = 'wbgene_id;shap;3303';
+my $PASS    = "wormpub";
+my $USER    = "wormpub";
 my $DOMAIN  = 'Variation';
 my $db = NameDB_handler->new($DB,$USER,$PASS,'/nfs/WWWdev/SANGER_docs/htdocs');
 
 # get nameserver data
-my $query = "SELECT primary_identifier.object_public_id, 
+my $query = 'SELECT primary_identifier.object_public_id, 
                     secondary_identifier.object_name
              FROM primary_identifier,secondary_identifier 
              WHERE  primary_identifier.object_id = secondary_identifier.object_id 
              AND   (secondary_identifier.name_type_id = 5) 
              AND   (primary_identifier.object_live = 1) 
-             ORDER BY object_public_id";
+             ORDER BY object_public_id';
 
 #RESULTS . . 
 #+------------------+-------------+
@@ -78,7 +77,6 @@ my $query = "SELECT primary_identifier.object_public_id,
 my $sth = $db->dbh->prepare($query);
 $sth->execute();
 
-
 my %server_ids;
 while (my ( $var, $name ) = $sth->fetchrow_array){
     $server_ids{$var} = $name;
@@ -86,15 +84,15 @@ while (my ( $var, $name ) = $sth->fetchrow_array){
 
 foreach my $var (keys %ace_ids) {
     if($server_ids{$var}){
-	if($server_ids{$var} eq $ace_ids{$var}) {
-	    #print "$var ok\n";
-	}
-	else {
-	    $log->error("$var - acedb = $ace_ids{$var} : var-server = $server_ids{$var}\n");
-	}
-	delete($server_ids{$var});
+      if($server_ids{$var} eq $ace_ids{$var}) {
+        #print "$var ok\n";
+      }
+      else {
+        $log->error("$var - acedb = $ace_ids{$var} : var-server = $server_ids{$var}\n");
+      }
+      delete($server_ids{$var});
     } else {
-	$log->error("$var missing from var-server\n");
+        $log->error("$var missing from var-server\n");
     }
 }
 
@@ -105,5 +103,3 @@ foreach my $var (keys %server_ids) {
 $log->write_to("Work Done!\n");
 $log->mail();
 exit(0);
-
-
