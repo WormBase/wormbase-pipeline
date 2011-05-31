faffbb24f5f1abcef7c29de87186941a0cf5f14a
gw3 <gw3>
2011-05-31 13:11:29 +0000

    added modENCODE_aggregate_transcripts.ace

--- scripts/load_data_sets.pl
+++ scripts/load_data_sets.pl
@@ -1,6 +1,6 @@
 #!/usr/local/bin/perl -w
-# Last updated by: $Author: klh $     
-# Last updated on: $Date: 2011-03-01 10:07:45 $      
+# Last updated by: $Author: gw3 $     
+# Last updated on: $Date: 2011-05-31 13:11:29 $      
 
 use strict;
 use lib $ENV{'CVS_DIR'};
@@ -63,7 +63,8 @@ sub parse_misc_elegans_files {
 		       $wormbase->misc_dynamic."/misc_genefinder.ace"           => "genefinder_predictions",
 		       $wormbase->misc_dynamic."/misc_twinscan.ace"             => "twinscan_predictions"  ,
 		       $wormbase->misc_dynamic."/misc_mgene.ace"                => "mgene_predictions"     ,
-		       $wormbase->misc_dynamic."/misc_RNASEQ_CDS.ace"           => "RNASEQ_CDS_predictions"     ,
+		       $wormbase->misc_dynamic."/misc_RNASEQ_CDS.ace"           => "RNASEQ_CDS_predictions",
+		       $wormbase->misc_dynamic."/misc_modENCODE_aggregate_transcripts.ace" => "modENCODE_aggregate_transcripts",
 		       $wormbase->misc_dynamic."/misc_jigsaw.ace"               => "jigsaw_predictions"    ,
 		       $wormbase->misc_dynamic."/misc_TEC_RED_homol_data.ace"   => "TEC_RED"               ,
 		       $wormbase->misc_dynamic."/misc_TEC_RED_homol.ace"        => "TEC_RED"               ,
