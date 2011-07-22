ab612f3a91a55c41d6c20697277c1bbf50cc0e1f
klh <klh>
2011-07-22 12:22:22 +0000

    Updated to use new, more intuitive get_flanking_sequences_for_feature method. Now also accepts a length arg, which does away with the need for get_flanking_sequences_50

--- scripts/get_flanking_sequences.pl
+++ scripts/get_flanking_sequences.pl
@@ -5,8 +5,8 @@
 # Script to grab two unique flanking sequences after specifying a sequence 
 # and two coordinates
 #
-# Last updated by: $Author: gw3 $     
-# Last updated on: $Date: 2010-08-20 09:01:34 $      
+# Last updated by: $Author: klh $     
+# Last updated on: $Date: 2011-07-22 12:22:22 $      
 
 use strict;
 use lib $ENV{'CVS_DIR'};                  
@@ -16,73 +16,46 @@ use Storable;
 use Getopt::Long;
 
 
-my ($help, $store, $wormbase, $species);
+my ($store, $wormbase, $species, $min_length, $test, $is_zero);
+
 my ($seq,$x,$y,$db);
-GetOptions ("store:s" => \$store,
-	    "x:i"     => \$x,
-	    "y:i"     => \$y,
-	    "db:s"    => \$db,
-	    "seq:s"   => \$seq,
-	    "species:s"=>\$species,
-	    "help"    => \$help,
+
+&GetOptions ("store:s"   => \$store,
+             "test"      => \$test,
+             "species:s" => \$species,
+             "start:i"   => \$x,
+             "end:i"     => \$y,
+             "db:s"      => \$db,
+             "zero"      => \$is_zero,
+             "seq:s"     => \$seq,
+             "length:s"  => \$min_length, 
 	    );
 
-# Display help if required
-&usage("Help") if ($help);
-
-
-my $flanking_seq_length    = 30;
-
-# if you only provide one coordinate, use the x again (i.e. 1 bp feature)
-if (!defined $y) {$y = $x+1;$x--}
-
-print "Looking for flanking sequences to $x - $y in $seq\n";
+$min_length = 30 if not defined $min_length;
 
 if ( $store ) {
   $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
 } else {
-  $wormbase = Wormbase->new(-organism => $species
-			     );
+  $wormbase = Wormbase->new(
+    -organism => $species,
+    -test => $test);
 }
+
+$y = $x if not defined $y;
+
+print STDERR "Looking for flanking sequences to $x - $y in $seq\n";
 
 $db ||= $wormbase->orgdb;
 my $mapper = Feature_mapper->new($db,0,$wormbase);
-my($l, $r) = $mapper->get_flanking_sequence($seq, $x, $y);
-if($l and $r) {print "Left  flank: $l\nRight flank: $r\n";}
-else { print "ERROR: cant find flanks for $seq:$x-$y\n";}
-
-exit(0);
-
-
-##############################################################
-#
-# Subroutines
-#
-##############################################################
-
-
-
-##########################################
-
-sub usage {
-  my $error = shift;
-
-  if ($error eq "Help") {
-    # Normal help menu
-    system ('perldoc',$0);
-    exit (0);
-  }
+my($l, $r) = $mapper->get_flanking_sequence_for_feature($seq, $x, $y, $is_zero, $min_length);
+if($l and $r) {
+  print "Left  flank: $l\nRight flank: $r\n";
+}
+else { 
+  print "ERROR: cant find flanks for $seq:$x-$y\n";
 }
 
-##########################################
-
-
-
-
-# Add perl documentation in POD format
-# This should expand on your brief description above and 
-# add details of any options that can be used with the program.  
-# Such documentation can be viewed using the perldoc command.
+exit(0);
 
 
 __END__
@@ -101,41 +74,17 @@ __END__
 
 This script finds the flanking sequences of a region on the genome.
 
-script_template.pl MANDATORY arguments:
-
 =over 4
 
-=item -x, starting coordinate in the sequence of your object
+=item -seq - the sequence that you are specifying the coordinates of - this can be a chomosome, a superlink or a clone.
 
-=back
+=item -start - starting coordinate in the sequence of your object
 
-=over 4
+=item -end - ending coordinate in the sequence of your object
 
-=item -y, ending coordinate in the sequence of your object
+=item -zero - raise this if your extent is a 2bp one defining a 0-bp object (i.e. between bases)
 
-=back
-
-=over 4
-
-=item -seq, the sequence that you are specifying the coordinates of - this can be a chomosome, a superlink or a clone.
-
-=back
-
-script_template.pl  OPTIONAL arguments:
-
-=over 4
-
-=item -h, Help
-
-=back
-
-=over 4
- 
-=item -species, the species you are working on
- 
-=back
-
-=over 4
+=item -species - the species you are working on
 
 =item -db, the database you wisk to look in - the default is autoace
 
