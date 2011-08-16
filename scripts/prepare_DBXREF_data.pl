94961d8886c973edacddf1ffb59da95040011e79
pad <pad>
2011-08-16 09:58:56 +0000

    added command line options rather than slurping variables from the line, standardised paths and added functionality to specify the release or utilise the ongoing build.

--- scripts/prepare_DBXREF_data.pl
+++ scripts/prepare_DBXREF_data.pl
@@ -4,21 +4,68 @@ use lib $ENV{'CVS_DIR'};
 use Wormbase;
 use Ace;
 use Storable;
+use Getopt::Long;
 
 #################################################
 # script usage
 #
 # cd ~/DATABASES
 #
-# prepare_DBXREF_data.pl ~wormpub/BUILD/autoace/Elegans.store WS220 Wormbase_WS220_mapping.txt
+#
+# prepare_DBXREF_data.pl -database WS227 -out Wormbase_WS220_mapping.txt
 #
 #################################################
 
-my $store = shift;#'/nfs/disk100/wormpub/BUILD/autoace/wormbase.store';
-my $wormbase = retrieve( $store );
-my $database = shift;
-my $output = "./DB_xref_files/".shift;
+
+my ($database,$store,$out);
+
+GetOptions ("database:s" => \$database,
+	    "store:s" => \$store,
+	    "out:s" => \$out,
+	    "build" => \$build
+	   );
+
+#Minimum requirements
+if ((!defined $database) && (!defined $build)) {die "ERROR: You must specify either -database or -build as a mandatory option\n";}
+
+unless (defined $store) {
+  if (defined $build) {
+    print "Using BUILD database storable\n";
+    $store = "/nfs/users/nfs_w/wormpub/BUILD/autoace/Elegans.store";
+  }
+  elsif (-e "/nfs/users/nfs_w/wormpub/DATABASES/".$database."Elegans.store") {
+    print "Using $database storable\n";
+    $store = "~wormpub/DATABASES/".$database."Elegans.store";
+  }
+  else {
+    print "No storable specified and cannot retrieve $database store therefore defaulting to using current_DB storable\n";
+    $store = "/nfs/users/nfs_w/wormpub/DATABASES/current_DB/Elegans.store";
+  }
+}
+
+my $wormbase = retrieve($store);
+my $version;
+
+unless (defined $out) {
+  if (defined $build) {
+    $version = $wormbase->get_wormbase_version;
+    $out = "Wormbase_WS".$version."_mapping.txt";
+  }
+  if (defined $database) {
+    $out = "Wormbase_".$database."_mapping.txt";
+    $version = $database;
+  }
+}
+
+
+
+my $base = $wormbase->wormpub;
+my $output = $base."/DATABASES/DB_xref_files/".$out;
 my $error_file = $output."_err";
+my $dbpath;
+
+$dbpath = $base."/DATABASES/$database" unless (defined $build);
+$dbpath = $wormbase->autoace unless (defined $database);
 
 if (-e $output) {$wormbase->run_command("rm $output");
 print "Removing old output files and creating new ones.\n";
@@ -27,18 +74,16 @@ print "Removing old output files and creating new ones.\n";
 open (ERR, ">$error_file") or die "Cannot open output file $error_file\n";
 open (OUT, ">$output") or die "Cannot open output file $output\n";
 
-$database ||= $wormbase->autoace;#$ENV{'CURRENT'}; if the database isn't specified then autoace is used
-
 print OUT "//----------------------------------------------------------------------\n";
-print OUT "// Updated mappings between the ENA entries and WormBase release $database\n";
+print OUT "// Updated mappings between the ENA entries and WormBase release $version\n";
 print OUT "// Mappings for coding, non-coding and pseudogenic loci. \"na\" is used when a protein ID is not available.\n";
 print OUT "// File Format: <Parent Accession> Protein_ID WBGene_ID Standard_name\n//\n";
 print OUT "// Examples:\n//\n//\tCDS:\t\tAF025472	AAY43980	WBGene00022688	ZK250.5b\n//\n//\tncRNA:\t\tAL022288        na      WBGene00199582  ZK1025.16\n//\n//\tPseudogene:\tAC199241	na	WBGene00044951	2RSSE.3\n//----------------------------------------------------------------------\n\n";
 
 my %clone2acc;
-$wormbase->FetchData('clone2accession',\%clone2acc,"$database/COMMON_DATA");
+$wormbase->FetchData('clone2accession',\%clone2acc,"$dbpath/COMMON_DATA");
 
-my $db = Ace->connect( -path=> $database) or die Ace->error;
+my $db = Ace->connect( -path=> $dbpath) or die Ace->error;
 
 my @genes = $db->fetch(-query => 'Find worm_genes'); 
 # testing only
@@ -96,12 +141,31 @@ __END__
 
 =head2 EMBL XREF generation
 
+
   This script will generate the file that is needed for the EMBL release XREFS
 
-  An email will be sent to wormbase-help requesting this data.   As of Aug 2006 it is from Alastair Baldwin <abaldwin@ebi.ac.uk>
+  An email will be sent to wormbase-help requesting this data.   As of Aug 2011 it is from Datasubs <datasubs@ebi.ac.uk>
 
-  The script runs against autoace so make sure that the protein_ids have been loaded (make_wormpep.pl -final)
+  The script runs against the specified database so make sure that the protein_ids have been loaded (make_wormpep.pl -final)
+  
+  And there are other limitations like a storable object and COMMON_DATA
 
-  Dont gzip the file to send it as EBI autorejects files with a .gz extension
+  Dont gzip the file to send it as EBI.
+
+  
+
+=head2 Mandatory arguments
+
+ -database WS227 // This option allows you to specify a release under ther DATABASES dir.
+
+ or
+
+ -build // this grabs the xref data from the ongoing build, useful if the build is in the last phases.
+
+=head2 Optional arguments
+
+ -out Wormbase_WS220_mapping.txt // allows you to specify a different output file name.
+
+ -store ~wormpub/DATABASES/current_DB/Elegans.store //allows you to give a storable object for retrieval.
 
 =cut
