676cb8b6807699b5f7b9baa7849c722750a67751
mh6 <mh6>
2011-08-17 10:29:49 +0000

    name of the database alias in the Registry changed

--- scripts/ENSEMBL/scripts/dump_all_clustal.pl
+++ scripts/ENSEMBL/scripts/dump_all_clustal.pl
@@ -28,10 +28,10 @@ use Bio::LocatableSeq;
 Bio::EnsEMBL::Registry->load_all();
 
 # adaptors setup
-my $mlss_a = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara', 'MethodLinkSpeciesSet' );
-my $gdb_a  = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara', 'GenomeDB' );
-my $ga_a   = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara', 'GenomicAlignBlock' );
-my $dnaf_a = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara', 'DnaFrag' );
+my $mlss_a = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara_alignments', 'MethodLinkSpeciesSet' );
+my $gdb_a  = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara_alignments', 'GenomeDB' );
+my $ga_a   = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara_alignments', 'GenomicAlignBlock' );
+my $dnaf_a = Bio::EnsEMBL::Registry->get_adaptor( 'Compara', 'compara_alignments', 'DnaFrag' );
 
 # genome databases setup
 my $edb = $gdb_a->fetch_by_name_assembly('Caenorhabditis elegans');
