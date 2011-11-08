36dc4555acaac2761e6bc6be51956b2e50a15f62
klh <klh>
2011-11-08 16:19:06 +0000

    More flexible use (another database, nowrite etc)

--- scripts/get_EMBL_data.pl
+++ scripts/get_EMBL_data.pl
@@ -7,7 +7,7 @@
 # Reads protein ids and gets SwissProt IDs
 #
 # Last updated by: $Author: klh $
-# Last updated on: $Date: 2011-03-25 11:40:53 $
+# Last updated on: $Date: 2011-11-08 16:19:06 $
 
 use strict;                                      
 use lib $ENV{'CVS_DIR'};
@@ -21,20 +21,17 @@ use Storable;
 # command-line options       #
 ##############################
 
-my ($help, $debug, $test, $verbose, $store, $wormbase);
-
-my $initial;             # run script in initial mode at start of build
-my $final;               # for full run at end of build
-my $table;
-my $species = 'elegans';
-my $mail;
+my ($help, $debug, $test, $verbose, $store, $wormbase, $species, $table, $infile, $autoace, $no_load, $acefile);
 
 GetOptions (
-            "debug=s"    => \$debug,
-            "test"       => \$test,
-            "store:s"    => \$store,
-            "species:s"  => \$species,
-            "mail:s"	 => \$mail
+  "debug=s"    => \$debug,
+  "test"       => \$test,
+  "store:s"    => \$store,
+  "species:s"  => \$species,
+  "infile:s"   => \$infile,
+  "autoace=s"  => \$autoace,
+  "noload"     => \$no_load,
+  "acefile=s"  => \$acefile,
            );
 
 
@@ -44,7 +41,8 @@ if ( $store ) {
 } else {
   $wormbase = Wormbase->new( -debug   => $debug,
                              -test    => $test,
-                             -organism=> $species
+                             -organism=> $species,
+                             -autoace => $autoace,
 			     );
 }
 
@@ -55,19 +53,17 @@ my $log = Log_files->make_build_log($wormbase);
 my %cds2gene_id = $wormbase->FetchData('cds2wbgene_id');
 my %cds2wormpep = $wormbase->FetchData('cds2wormpep');
 my %accession2clone   = $wormbase->FetchData('accession2clone');
-my %uac2cds;  #uniprot acc to cds
-my %uac2uid;  # acc to ID
-&getIDs;
+
+$acefile = $wormbase->acefiles."/cds_embl_data.ace" if not defined $acefile;
+$infile = $wormbase->autoace .  "/protein_ID.mail" if not defined $infile;
+
+my $acc2idmap = &get_acc_to_id_map();
 
 #open output
-my $aceoutput = $wormbase->acefiles."/cds_embl_data.ace";
-open(ACE,">$aceoutput") or $log->log_and_die("cant write $aceoutput : $!\n");
+open(ACE,">$acefile") or $log->log_and_die("cant write $acefile : $!\n");
+open (IN,"<$infile") or $log->log_and_die("cant read $infile\n");
 
-#read protein id mail
-$mail = $mail ? $mail : $wormbase->autoace."/protein_ID.mail";
-open (MAIL,"<$mail") or $log->log_and_die("cant read $mail\n");
-
-while(<MAIL>){
+while(<IN>){
   #U10401  105     AAA19054        4       1408728171      T20B12.1        P41842  T20B12.1
   s/[\n\r]//g;
   my @data = split("\t",$_);
@@ -76,47 +72,58 @@ while(<MAIL>){
   my($cloneacc, $pid, $version, $cds, $uniprot) = ($data[0],$data[2],$data[3],$data[-1],$data[-2]);  
   
   next unless (defined $pid);
-  print "Potential New Protein: $_\n" unless ($cloneacc and $pid and $version and $cds and $uniprot);
+  print "Potential New Protein: $_\n" if $uniprot eq 'UNDEFINED';
   
   next unless $accession2clone{$cloneacc}; #mail includes some mRNAs
   
   print ACE "\nCDS : \"$cds\"\n";
   print ACE "Protein_id ".$accession2clone{$cloneacc}." $pid $version\n";
-  if($cds2wormpep{$cds}) {
+  if($cds2wormpep{$cds} and defined $uniprot and $uniprot ne 'UNDEFINED') {
+
     print ACE "\nProtein : \"WP:".$cds2wormpep{$cds}."\"\n";
     print ACE "Database UniProt UniProtAcc $uniprot\n" if $uniprot;
-    print ACE "Database UniProt UniProtID ".$uac2uid{$uniprot}."\n" if ($uac2uid{$uniprot});
+    if (exists $acc2idmap->{$uniprot}) {
+      print ACE "Database UniProt UniProtID ".$acc2idmap->{$uniprot}."\n";
+    }
     
     print ACE "\nCDS : \"$cds\"\n";
     print ACE "Database UniProt UniProtAcc $uniprot\n" if $uniprot;
-    print ACE "Database UniProt UniProtID ".$uac2uid{$uniprot}."\n" if ($uac2uid{$uniprot});
+    if (exists $acc2idmap->{$uniprot}) {
+      print ACE "Database UniProt UniProtID ".$acc2idmap->{$uniprot}."\n";
+    }
   }
   else {
     $log->write_to("no ".$wormbase->pepdir_prefix."pep for $cds\n");
   }
 }	
 
-close MAIL or $log->error("didn't close mail properly\n");
+close(ACE) or $log->log_and_die("Could not close $acefile properly\n");
 
-$wormbase->load_to_database($wormbase->autoace, $aceoutput, 'EMBL_ids',$log) unless $test;
+unless ($no_load or $test) {
+  $wormbase->load_to_database($wormbase->autoace, $acefile, 'EMBL_ids',$log);
+}
 
 $log->mail;
 
-sub getIDs {
+
+###################
+sub get_acc_to_id_map {
   my $query = "mfetch -d uniprot -i \"org:".$wormbase->full_name."\" -f \"div acc\"";
   open (IDS ,"$query |") or $log->log_and_die("mfetch id query failed : $!\n");
-  #open(IDS,"</tmp/elegans_uniprot");
-  #ID   ADD1_CAEEL              Reviewed;         732 AA.
-  #AC   Q9U9K0; O44581; Q95X64; Q9U9J9;
-  my $id;
+
+  my ($id, %uac2uid);
+
   while(<IDS>){
     if(/^ID\s+(\w+)\s+/) {
       $id = $1;
       next;
     }
-    foreach my $acc (/(\w+);/g) {
-      $uac2uid{$acc} = $id;
+    if (/^AC/) {
+      foreach my $acc (/(\w+);/g) {
+        $uac2uid{$acc} = $id;
+      }
     }
-    undef $id;
   }
+
+  return \%uac2uid;
 }
