905c2258a58842f7f62ef95040d4334bb32484f2
klh <klh>
2011-12-12 10:18:23 +0000

    New script to wrap our LSF interactive job submissions. Main point of this script is to raise the alarm if the job is killed due to exceeding memory requirements

---
+++ scripts/lsfsubmit.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/env perl 
+
+use Getopt::Long qw(:config pass_through);
+
+my $bsubmem = 2;
+
+&GetOptions('bsubmem=s' => \$bsubmem);
+
+my $minus_M = $bsubmem * 1024 * 1024;
+my $minus_R = sprintf("'select[mem>%d] rusage[mem=%d]'", $bsubmem * 1024, $bsubmem * 1024);
+
+unshift @ARGV, ("bsub", "-I", "-M $minus_M", , "-R $minus_R");
+
+print "@ARGV\n";
+my $exit_code = system(@ARGV);
+
+if ($exit_code) {
+  die "EXITED with code $exit_code\n";
+}
+
+
