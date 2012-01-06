5f8064eda2de18f8db9a2c1bc7ecf3696859fb30
klh <klh>
2012-01-06 12:58:17 +0000

    Now working...

--- scripts/make_wormpep.pl
+++ scripts/make_wormpep.pl
@@ -7,7 +7,7 @@
 # Builds a wormpep data set from the current autoace database
 #
 # Last updated by: $Author: klh $
-# Last updated on: $Date: 2012-01-06 11:51:40 $
+# Last updated on: $Date: 2012-01-06 12:58:17 $
 
 use strict;                                      
 use lib $ENV{'CVS_DIR'};
@@ -84,17 +84,18 @@ my $new_wpdir = $wormbase->wormpep;
 my $release   = $wormbase->get_wormbase_version;
 my $tace      = $wormbase->tace;
 
+my $PEP_FILE_STEM = $wormbase->pepdir_prefix . "pep";
+my $PEP_PREFIX    = $wormbase->pep_prefix;
+
 # need to get previous build WORMPEP
-my ($stem, $old_release ) = $new_wpdir =~ /(.*pep)(\d+)/;
+my ($pepdir, $old_release ) = $new_wpdir =~ /^(\S+)\/${PEP_FILE_STEM}(\d+)/;
 my ($wpdir, $old_wpdir_test_file);
 do {
   $old_release--;
-  $wpdir = "$stem${old_release}";
-  $old_wpdir_test_file = "$wpdir/${stem}.fasta${old_release}";
+  $wpdir = "$pepdir/${PEP_FILE_STEM}${old_release}";
+  $old_wpdir_test_file = "$wpdir/${PEP_FILE_STEM}.fasta${old_release}";
 } while (not -e $old_wpdir_test_file);
-  
-my $PEP_FILE_STEM = $wormbase->pepdir_prefix . "pep";
-my $PEP_PREFIX    = $wormbase->pep_prefix;
+
 
 # Make new directory for current release         
 if (-e $new_wpdir){
