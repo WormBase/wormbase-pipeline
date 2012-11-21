74a05c7d721d7e6eee6b917e6d8eab04c9f97521
klh <klh>
2012-11-21 13:26:59 +0000

    Made *much* more robust. Should now complain loudly when build jobs is killed by LSF

--- scripts/lsfsubmit.pl
+++ scripts/lsfsubmit.pl
@@ -12,13 +12,71 @@ my $queue = 'normal';
 my $minus_M = $bsubmem * 1000 * 1000;
 my $minus_R = sprintf("'select[mem>%d] rusage[mem=%d]'", $bsubmem * 1000, $bsubmem * 1000);
 
-unshift @ARGV, ("bsub", "-q $queue", "-I", "-M $minus_M", , "-R $minus_R");
+#unshift @ARGV, ("bsub", "-q $queue", "-I", "-M $minus_M", , "-R $minus_R");
+my $command = "bsub -q $queue -I -M $minus_M -R $minus_R @ARGV";
 
-print "@ARGV\n";
-my $exit_code = system(@ARGV);
+my ($job_id) = open_command_line($command);
 
-if ($exit_code) {
-  die "EXITED with code $exit_code\n";
+if (not $job_id) {
+  print STDERR "ERROR: Could not get job_id from job, so unable to determine exit status - investigate\n";
+} else {
+  my $found = 0;
+  
+  while(not $found) {
+    open(my $blist, "bjobs -d |");
+    while(<$blist>) {
+      /^(\d+)/ and do {
+        if ($1 eq $job_id) {
+          $found = 1;
+          last;
+        }
+      };
+    }
+    sleep(1);
+  }
+  
+  my $error_code;
+
+  open(my $bjobs, "bjobs -l $job_id |");
+  while(<$bjobs>) {
+    /Exited with exit code (\d+)/ and do {
+      $error_code = $1;
+      last;
+    };
+    /Done successfully/ and do {
+      $error_code = 0;
+      last;
+    };
+  }
+  
+  if (not defined $error_code) {
+    print STDERR "ERROR: Unable to determine exit code for submitted job - investigate\n";
+  } elsif ($error_code) {
+    print STDERR "ERROR: job terminated abnormally (exit code $error_code)\n";
+  } else {
+    print STDERR "SUCCESSFULLY COMPLETE\n";
+  }
 }
 
 
+sub open_command_line {
+  my ( $bsub_command ) = @_;
+  
+  my $lsf = 0;
+  
+  if ( open( my $pipe, '-|' ) ) {
+    while (<$pipe>) {
+      if (/Job <(\d+)>/) {
+        $lsf = $1;
+      } 
+      print;
+    }
+    
+    return $lsf;
+  } else {
+    # We want STDERR and STDOUT merged for the bsub process
+    # open STDERR, '>&STDOUT';
+    # probably better to do with shell redirection as above can fail
+    exec( $bsub_command . ' 2>&1' ) || die("Could not run bsub");
+  }
+} ## end sub open_command_line
