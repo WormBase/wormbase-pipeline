f781fc8e092ab6f63b7571055ebf4c4288a14c36
klh <klh>
2012-11-21 16:23:32 +0000

    Can now use autoace rather than current-db (for use during the build)

--- scripts/ENSEMBL/scripts/update_ensembl_orthologs.pl
+++ scripts/ENSEMBL/scripts/update_ensembl_orthologs.pl
@@ -8,99 +8,143 @@
 #     REVISION:  ---
 #===============================================================================
 
+use lib '/software/worm/ensembl/bioperl-live';
+use lib $ENV{CVS_DIR};
+
 use strict;
 use IO::File;
-use lib '/software/worm/ensembl-64/ensembl/modules';
-use lib '/software/worm/ensembl-64/ensembl-compara/modules';
-use lib '/software/worm/ensembl/bioperl-live';
+use Getopt::Long;
+use Storable;
+use Wormbase;
+
+
+
+#use lib '/software/worm/ensembl-64/ensembl/modules';
+#use lib '/software/worm/ensembl-64/ensembl-compara/modules';
 
 use Bio::EnsEMBL::Registry;
-Bio::EnsEMBL::Registry->load_registry_from_db(-host => 'ens-livemirror', -user => 'wormro',-port => 3306,-verbose => 0,-db_version => 64);
-
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
 
-my %speciesOfInterest = ( 
-           4932 => 1, # yeast
-           7227 => 1, # fly
-           9606 => 1, # human
-           10090=> 1,# mouse
+my %speciesOfInterest = (
+  'saccharomyces_cerevisiae'  => 'yeast',
+  'drosophila_melanogaster' => 'fly',
+  'homo_sapiens'            => 'human',
+  'mus_musculus'            => 'mouse',
 );
 
-my %cds2wbgene=%{&get_commondata('/nfs/wormpub/DATABASES/current_DB/COMMON_DATA/cds2wbgene_id.dat')};
 
-my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor('C.elegans','core','Slice');
+my ($wormbase, $debug, $test, $verbose, $store, $species, $use_current_db, %cds2wbgene, %current_genes);
+
+GetOptions (
+  "debug=s"    => \$debug,
+  "test"       => \$test,
+  "verbose"    => \$verbose,
+  "store:s"    => \$store,
+  "species:s"  => \$species,
+  "usecurrentdb" => \$use_current_db,
+            );
+
+if ( $store ) {
+  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+} else {
+  $wormbase = Wormbase->new( -debug   => $debug,
+                             -test    => $test,
+                             -organism => $species
+                             );
+}
+
+my $species_name = $wormbase->full_name;
+
+Bio::EnsEMBL::Registry->load_registry_from_db(-host => 'ens-livemirror', 
+                                              -user => 'wormro',
+                                              -port => 3306,
+                                              -verbose => 0);
+
+
+my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species_name,'core','Slice');
 my $member_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Member');
 my $homology_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Homology');;
+
+
+if ($use_current_db) {
+  %cds2wbgene = %{&get_commondata( "/nfs/DATABASES/current_db/COMMON_DATA/cds2wbgene_id.dat" )};
+} else {
+  %cds2wbgene = %{&get_commondata( $wormbase->common_data . "/cds2wbgene_id.dat" )};
+}
+map { $current_genes{$_} = 1 } values %cds2wbgene;
+
 
 my @slices = @{$slice_adaptor->fetch_all('toplevel')};
 
 
 foreach my $slice(@slices){
-	my $genes = $slice->get_all_Genes;
-	while (my $gene = shift @{$genes}){
+  my $genes = $slice->get_all_Genes;
+  while (my $gene = shift @{$genes}){
+    my ($db_entry) = @{$gene->get_all_DBLinks('wormbase_gene')};
+    my $wb_gene_id = $db_entry->primary_id;
 
-		my $gid=$cds2wbgene{$gene->stable_id}?$cds2wbgene{$gene->stable_id}:next; # don't import outdated genes
-
-		my $member = $member_adaptor->fetch_by_source_stable_id( 'ENSEMBLGENE',$gene->stable_id());
-		next unless $member;
-		my $homologies = $homology_adaptor->fetch_all_by_Member_method_link_type( $member, 'ENSEMBL_ORTHOLOGUES' );
-
-		my %homol_ids;
-		my %omims;
-		my %omim_genes;
-
-		foreach my $homology ( @{$homologies} ) {
-			foreach my $ma ( @{ $homology->get_all_Member_Attribute } ) {
-				my ( $me, $at ) = @{$ma};
-				my $pepm = $me->get_canonical_peptide_Member(); 
-				if ($speciesOfInterest{$pepm->taxon_id}){
-					$homol_ids{ $pepm->stable_id } = $pepm; #$homology->description,$homology->subtype];
-				}
-			}
-		}
-
-		next unless %homol_ids;
-
-
-		print "Gene : \"$gid\"\n";
-		while (my ($k,$v)=each(%homol_ids)){
-#				print "Ortholog_other EnsEMBL gene $k \"${\$$v[0]->name}\"  From_analysis EnsEMBL-Compara\n";
-				print "Ortholog_other ENSEMBL:$k  From_analysis EnsEMBL-Compara\n";
-		}
-		print "\n";
-
-		while (my ($k,$v)=each(%homol_ids)){
-					# put the OMIM orthologs into another hash based on the MIM_GENE xref
-					print "Protein : ENSEMBL:$k\n";
-					print "Species \"${\$v->taxon->name}\"\n";
-					print "DB_info Database EnsEMBL ENSEMBL_proteinID $k\n";
-					if ($v->taxon_id == 9606){ # meaning if human
-					        # uses an undocumented function of get_all_DBEntries, so lets hope it stays
-						map {printf "DB_info Database OMIM disease %s\n",$_->primary_id} @{$v->gene->get_all_DBLinks('MIM_MORBID')};
-						map {printf "DB_info Database OMIM gene %s\n",$_->primary_id} @{$v->gene->get_all_DBLinks('MIM_GENE')};
-					}
-					print "\n";
-		}
-	}	
+    next if not exists $current_genes{$wb_gene_id};
+    
+    my $member = $member_adaptor->fetch_by_source_stable_id( 'ENSEMBLGENE',$gene->stable_id());
+    next unless $member;
+    my $homologies = $homology_adaptor->fetch_all_by_Member_method_link_type( $member, 'ENSEMBL_ORTHOLOGUES' );
+    
+    my %homol_ids;
+    my %omims;
+    my %omim_genes;
+    
+    foreach my $homology ( @{$homologies} ) {
+      foreach my $ma ( @{ $homology->get_all_Member_Attribute } ) {
+        my ( $me, $at ) = @{$ma};
+        if (exists $speciesOfInterest{$me->genome_db->name}){
+          my $pepm = $me->get_canonical_Member(); 
+          $homol_ids{ $pepm->stable_id } = $pepm; #$homology->description,$homology->subtype];
+        }
+      }
+    }
+    
+    next unless %homol_ids;
+        
+    print "Gene : \"$wb_gene_id\"\n";
+    while (my ($k,$v)=each(%homol_ids)){
+      print "Ortholog_other ENSEMBL:$k  From_analysis EnsEMBL-Compara\n";
+    }
+    print "\n";
+    
+    while (my ($k,$v)=each(%homol_ids)){
+      # put the OMIM orthologs into another hash based on the MIM_GENE xref
+      print "Protein : ENSEMBL:$k\n";
+      print "Species \"${\$v->taxon->name}\"\n";
+      print "DB_info Database EnsEMBL ENSEMBL_proteinID $k\n";
+      if ($v->taxon_id == 9606){ # meaning if human
+        # uses an undocumented function of get_all_DBEntries, so lets hope it stays
+        map {printf "DB_info Database OMIM disease %s\n",$_->primary_id} @{$v->get_Gene->get_all_DBLinks('MIM_MORBID')};
+        map {printf "DB_info Database OMIM gene %s\n",$_->primary_id} @{$v->get_Gene->get_all_DBLinks('MIM_GENE')};
+      }
+      print "\n";
+    }
+  }	
 }
 
 
 sub get_commondata {
-	my ($name)=@_;
-	my $file= new IO::File "< $name";
-	$/=undef;
-	my $data=<$file>;
-	$/="\n";
-	$file->close;
-	my $VAR1;
-	my %genehash;
-	eval($data);
+  my ($fname)=@_;
+  
+  open(my $fh, $fname) or die "could not open $fname for reading\n";
+  $/=undef;
+  my $data=<$fh>;
+  $/="\n";
+  close($fh);
 
-	while(my ($k,$v)=each(%{$VAR1})){
-		$k=~s/[a-z]$//;
-		$genehash{$k}=$v
-	}
-	return \%genehash;
+  my $VAR1;
+  my %genehash;
+  eval($data);
+  
+  while(my ($k,$v)=each(%{$VAR1})){
+    $k=~s/[a-z]$//;
+    $genehash{$k}=$v
+  }
+  return \%genehash;
 }
