d170cd9e03b2db23716038979ecb133d7b9a019e
gw3 <gw3>
2013-03-05 11:31:14 +0000

    small module to pull some sequence information out of EBI SRS server

---
+++ scripts/Modules/WBSRS.pm
@@ -0,0 +1,176 @@
+#!/usr/bin/env perl
+
+package WBSRS;
+
+use strict;
+use LWP::UserAgent;
+
+
+
+###########################
+# return hash-ref of the following information for each Accession in Uniprot
+# key is 'acc' value is hash of 'id', 'des', 'gene', 'key', 'len'
+sub uniprot {
+  my ($species) = @_;
+
+  my ($ggenus, $gspecies) = $species =~ /^(\S+)\s+(\S+)/;
+  if (! defined $gspecies) {die("SRS: Full species name not given\n")};
+
+  my %acc2ids;
+
+
+  my $ua       = LWP::UserAgent->new;
+  
+  my $base     = 'http://srs.ebi.ac.uk/srsbin/cgi-bin/wgetz?-noSession';
+  my $query    = "+[uniprot-org:$ggenus]&[uniprot-org:$gspecies]";
+  
+  my $cResult  = '+-page+cResult+-ascii';
+  my $fullview = '+-view+UniprotView+-ascii+-lv+';
+  
+  # Doing query: ${base}${query}${cResult}
+  # getting the number of entries in the set of results
+  
+  my $qa1 = $ua->get($base.$query.$cResult);
+  die("SRS: Can't get URL -- " . $qa1->status_line) unless $qa1->is_success;
+  
+  if($qa1->content =~/^(\d+)/) {
+    my $lv = $1;
+    #print "EBI SRS server Uniprot query returned $lv entries; fetching...\n";
+    
+    my $tmp_file = "/tmp/srs_results.$$.txt";
+    my $qa2 = $ua->get($base.$query.$fullview.$lv, ':content_file' => $tmp_file);
+    die("SRS: Could not fetch Uniprot entries using EBI SRS server") 
+      if not $qa2->is_success;
+    
+    open(my $f, $tmp_file);
+    while(<$f>) {
+      my @f = split /\t/;
+      $f[0] =~ s/UNIPROT:(\S+)/$1/;
+      $acc2ids{$f[1]}{'id'} = $f[0];
+      $acc2ids{$f[1]}{'des'} = $f[3];
+      $acc2ids{$f[1]}{'gene'} = $f[4];
+      $acc2ids{$f[1]}{'key'} = $f[6];
+      $acc2ids{$f[1]}{'len'} = $f[7];
+    }
+
+  } else {
+    die("SRS: Unexpected content from SRS query\n");
+  }
+   
+  return \%acc2ids;
+}
+###########################
+# return hash-ref of the following information for each Accession in EMBL
+# this will probably often fail if done in one go, so the returned result has been chunked
+# key is 'acc' value is hash of 'id', 'des', 'len'
+sub embl {
+  my ($species) = @_;
+
+  my ($ggenus, $gspecies) = $species =~ /^(\S+)\s+(\S+)/;
+  if (! defined $gspecies) {die("SRS: Full species name not given\n")};
+
+  my %acc2ids;
+
+
+  my $ua       = LWP::UserAgent->new;
+
+  my $base     = 'http://srs.ebi.ac.uk/srsbin/cgi-bin/wgetz?-noSession';
+  my $query    = "+[embl-org:$ggenus]&[embl-org:$gspecies]";
+
+  my $cResult  = '+-page+cResult+-ascii';
+  my $fullview = '+-view+EMBLSeqSimpleView+-ascii';
+
+  # Doing query: ${base}${query}${cResult}
+  # getting the number of entries in the set of results
+
+  my $qa1 = $ua->get($base.$query.$cResult);
+  die("SRS: Can't get URL -- " . $qa1->status_line) unless $qa1->is_success;
+
+  if($qa1->content =~/^(\d+)/) {
+    my $total = $1;
+    # print "EBI SRS server Uniprot query returned $total entries; fetching...\n";
+    
+    my $tmp_file = "/tmp/srs_results.$$.txt";
+    
+    my $chunksize = 10000;
+    my $chunkstart = 1; # start of next chunk
+    while ($chunkstart < $total) {
+      my $bv = "+-bv+${chunkstart}";
+      my $lv = "+-lv+${chunksize}";
+      # print "EBI SRS server Uniprot query returned $total entries; fetching no. $chunkstart...\n";
+      my $qa2 = $ua->get($base.$query.$fullview.$lv.$bv, ':content_file' => $tmp_file);
+      die("SRS: Could not fetch EMBL entries using EBI SRS server") 
+        if not $qa2->is_success;
+      
+      open(my $f, $tmp_file);
+      while(<$f>) {
+	my @f = split /\t/;
+	$f[0] =~ s/EMBL:(\S+)/$1/;
+	$acc2ids{$f[1]}{'id'} = $f[0];
+	$acc2ids{$f[1]}{'des'} = $f[3];
+	$acc2ids{$f[1]}{'len'} = $f[4];
+      }
+      $chunkstart+=$chunksize;
+    }
+  } else {
+    die("SRS: Unexpected content from SRS query\n");
+  }
+  
+  return \%acc2ids;
+}
+
+# e.g.
+#http://srs.ebi.ac.uk/srsbin/cgi-bin/wgetz?-id+PERMtestSession+[embl-id:AK033621|AK078912]+-f+id%20acc%20div%20mol%20des%20key%20org+-ascii+-view+EMBLSeqSimpleView
+
+###########################
+# return text of the specified entry
+# http://srs.ebi.ac.uk/cgi-bin/wgetz?-noSession+-e+[EMBL-id:V01512]+-ascii
+# example databases: EMBL, SWISSPROT, UNIPROT
+# example types of query: 'id', 'acc', (more general types like 'des, 'all' will probably crash your script)
+sub entry {
+
+  my ($database, $type, $entry) = @_;
+
+  my $text;
+  my $ua       = LWP::UserAgent->new;
+
+  my $base     = 'http://srs.ebi.ac.uk/srsbin/cgi-bin/wgetz?-noSession';
+  my $query    = "+-e+[${database}-${type}:${entry}]";
+  my $fullview = '+-ascii';
+
+  # Doing query: ${base}${query}${fullview}
+
+  my $qa1 = $ua->get($base.$query.$fullview);
+  die("SRS: Can't get URL -- " . $qa1->status_line) unless $qa1->is_success;
+  $text = $qa1->content;
+
+  return $text;
+}
+###########################
+# return fasta sequences
+# http://srs.ebi.ac.uk/srsbin/cgi-bin/wgetz?-noSession+[uniprot-id:pax6_*]+-view+FastaSeqs
+# example databases: EMBL, SWALL (same as UNIPROT), UNIPROT, REFSEQ
+# example types of query: 'id', 'acc'
+
+sub fasta {
+
+  my ($database, $type, $entry) = @_;
+
+  my $text;
+  my $ua       = LWP::UserAgent->new;
+
+  my $base     = 'http://srs.ebi.ac.uk/srsbin/cgi-bin/wgetz?-noSession';
+  my $query    = "+[${database}-${type}:${entry}]";
+  my $fullview = '+-ascii+-view+FastaSeqs';
+
+  # Doing query: ${base}${query}${fullview}
+
+  my $qa1 = $ua->get($base.$query.$fullview);
+  die("SRS: Can't get URL -- " . $qa1->status_line) unless $qa1->is_success;
+  $text = $qa1->content;
+
+  return $text;
+
+}
+
+1;
