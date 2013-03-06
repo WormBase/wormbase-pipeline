4652b9f8d7d2788c216225d7ba49bfa5586d0be0
klh <klh>
2013-03-06 13:10:32 +0000

    New way of mapping Oligo_sets to genes (fast)

--- scripts/Modules/Map_Helper.pm
+++ scripts/Modules/Map_Helper.pm
@@ -1,163 +1,200 @@
-#!/usr/bin/env perl
-#===============================================================================
-#
-#         FILE:  Map_Helper.pm
-#
-#  DESCRIPTION:
-#
-#        FILES:  ---
-#         BUGS:  ---
-#        NOTES: heavily depending on some structs as explained in perldoc
-#      $Author: ar2 $
-#      COMPANY:
-#     $Version:  $
-#      CREATED:  14/11/05 14:33:43 GMT
-#        $Date: 2007-05-23 13:33:11 $
-#===============================================================================
+
 package Map_Helper;
 
 use strict;
-use warnings;
 
-##########################################################
-# gff input file helper moved over from mapping scripts
-# will update $genes (hashref of Genes Exons)
-#
-sub get_from_gff {
-    my ( $file, $type, $unless, $genes ) = @_;
-    open( GFF_in, "<$file" )
-      || die "Failed to open gff file\n\n";
-    while (<GFF_in>) {
-        chomp;
-        s/\#.*//;
-	next if /^\#/;
-	next if /Link/;
-        next unless /$unless/;
-        my @f = split /\t/;
+########################
+sub new {
+  my $class = shift;
 
-        my ($name) = ( $f[8] =~ /\"(\S+)\"/ );
-        my $x = new Exon;
-        $x->id($name);
-        $x->start( $f[3] );
-        $x->stop( $f[4] );
-        $x->type($type);
-        if ( $$genes{ $x->id } ) {
-            push @{ $$genes{ $x->id }->exons }, $x;
-            $$genes{ $x->id }->start( $x->start )
-              if $x->start < $$genes{ $x->id }->start;
-            $$genes{ $x->id }->stop( $x->stop )
-              if $x->stop > $$genes{ $x->id }->stop;
-        }
-        else {
-            $$genes{ $x->id } = new Gene;
-            push @{ $$genes{ $x->id }->exons }, $x;
-            $$genes{ $x->id }->start( $x->start );
-            $$genes{ $x->id }->stop( $x->stop );
-	    $$genes{ $x->id }->orientation($f[6]);
-	    $$genes{ $x->id }->parent($f[0]);
-        }
+  my $self = {
+    _features         => {},
+    _indexed_features => {},
+  };
+  bless $self, $class;
+
+  return $self;
+}
+
+
+
+# stores genes to a bdb
+sub register_segment {
+  my ($self, $seq,$start,$end,$strand,$name)=@_;
+
+  my $feats = $self->_features;
+
+  if (exists $feats->{$name}) {
+    foreach my $seg (@{$feats->{$name}}) {
+      if ($seg->{seq} ne $seq) {
+        die "Could feature $name cannot be registered to two different sequences\n";
+      } elsif ($seg->{start} <= $end and
+               $seg->{end}   >= $start) {
+        die "Attempt to register overlapping segments for the same feature $name\n";
+      }
     }
-    close(GFF_in);
+  }
+
+  push @{$feats->{$name}}, {
+    seq    => $seq,
+    start  => $start,
+    end    => $end,
+    strand => $strand,
+    name   => $name,
+  };
 
 }
 
-###########################################
-# helper function to search for overlaps
-# will update %$output with Exons
-#
-sub map_it {
-    my ( $output, $pcr, $sorted_genes, $genes ) = @_;
-    foreach my $testPCR ( keys %$pcr ) {
-        my $PCRstart = $$pcr{$testPCR}->[0];
-        my $PCRstop  = $$pcr{$testPCR}->[1];
+##################################
+sub build_index{
+  my ($self) = @_;
 
-        foreach my $gene (@$sorted_genes) {
-            if ( $PCRstart > $$genes{$gene}->stop ) {
-                next;
-            }
+  my $feats = $self->_features;
+  my $indexed_feats = $self->_indexed_features;
 
-            elsif ( $PCRstop < $$genes{$gene}->start ) {
-                last;
-            }
+  foreach my $fid (keys %$feats) {
+    my @segs = sort { $a->{start} <=> $b->{start} } @{$feats->{$fid}};
+    $feats->{$fid} = \@segs;
 
-            else {
-                my %added_exons;
-                foreach my $exon ( @{ $$genes{$gene}->exons } ) {
-                    if (   $PCRstart <= $exon->stop
-                        && $PCRstop >= $exon->start )
-                    {
-                        next if $added_exons{ $exon->type . $exon->id };
-                        push @{ $$output{$testPCR} }, $exon;
-                        $added_exons{ $exon->type . $exon->id } = 1;
-                    }
-                }
-            }
-        }
+    push @{$indexed_feats->{$segs[0]->{seq}}}, {
+      start => $segs[0]->{start},
+      end   => $segs[-1]->{end},
+      strand => $segs[0]->{strand},
+      name   => $fid,
+    };
+  }
+
+  foreach my $seq (keys %$indexed_feats) {
+    my @tmp = sort {$a->{start}<=>$b->{start} or $a->{end} <=> $b->{end}} @{$indexed_feats->{$seq}};
+    my $max_end = -1;
+    for(my $i=0; $i < scalar(@tmp); $i++) {
+      if ($tmp[$i]->{end} > $max_end) {
+        $max_end = $tmp[$i]->{end};
+      }
+      $tmp[$i]->{max_end_left} = $max_end;
     }
+    $indexed_feats->{$seq} = \@tmp;
+  }
 }
 
 
+#################################
+sub search_feature_spans {
+  my ($self,$chromosome,$start,$end,$strand)=@_;
 
-#######################
-# gff description line extraction
-#
+  my @list = @{$self->_indexed_features->{$chromosome}}; 
+  my @hits = @{$self->_bin_search($chromosome, $start, $end, \@list)};
 
-sub desc_line {
-    my ($line) = @_;
-    $line =~ /(\w+) \"(\S+)\"/;
-    my $type = $1;
-    my $id   = $2;
-    return ( $id, $type );
+  if (defined $strand) {
+    @hits = grep { $_->{strand} eq $strand } @hits;
+  }
+
+  return [map  { $_->{name} } @hits];
 }
+
+###############################
+sub search_feature_segments {
+  my ($self,$chromosome,$start,$end,$strand)=@_;
+
+  my @list; 
+  if (exists $self->_indexed_features->{$chromosome}) {
+    @list = @{$self->_indexed_features->{$chromosome}} 
+  }
+
+  my @hits = @{$self->_bin_search($chromosome, $start, $end, \@list)};
+
+
+  if (defined $strand) {
+    @hits = grep { $_->{strand} eq $strand } @hits;
+  }
+
+  my @seg_hits;
+
+  if (@hits) {
+
+    foreach my $hit (@hits) {
+
+      my $match = 0;
+
+     foreach my $seg (@{$self->_features->{$hit->{name}}}) {
+       if ($start <= $seg->{end} and $end >= $seg->{start}) {
+          $match = 1;
+          last;
+        }
+      }
+
+      if ($match) {
+        push @seg_hits, $hit; 
+      }
+    }
+  }
+
+  return [map { $_->{name} } @seg_hits];
+  
+}
+
+
+#################################
+sub populate_from_GFF {
+  my ($self, $file, $method, $type, $name_regexp) = @_;
+  
+  open(my $fh, $file) or die "Could not open $file for reading\n";
+
+  while(<$fh>) {
+    next if /^\#/;
+    my @l = split(/\t+/, $_);
+    
+    next if defined $type and $type ne $l[2];
+    next if defined $method and $method ne $l[1];
+    
+    my ($fid) = ($l[8] =~ /$name_regexp/);
+    
+    next if not defined $fid;
+
+    $self->register_segment($l[0], $l[3], $l[4], $l[6], $fid); 
+
+  }
+}  
+
+#################################
+sub _bin_search {
+  my ($self, $chromosome, $start, $end, $list) = @_;
+
+  my ($low, $high) = (0, scalar(@$list));
+  while($low < $high) {
+    my $mid = int(($low + $high) / 2);
+
+    if ($list->[$mid]->{start} <= $end) {
+      $low = $mid + 1;
+    } else {
+      $high = $mid;
+    }
+  }
+
+  my @hits;
+  for(my $i=$low-1; $i >= 0 and $list->[$i]->{max_end_left} >= $start; $i--) {
+    if ($start <= $list->[$i]->{end} and $end >= $list->[$i]->{start}) {
+      push @hits, $list->[$i];
+    }
+  }
+
+  return \@hits;
+}
+
+##########################
+sub _features {
+  my ($self) = @_;
+
+  return $self->{_features};
+}
+
+##########################
+sub _indexed_features {
+  my ($self) = @_;
+
+  return $self->{_indexed_features};
+}
+
+
 
 1;
-
-__END__
-
-=pod
-
-=head2 NAME - Map_Helper.pm
-
-=head2 USAGE
-
- use Map_Helper
- Map_Helper::Function()
-
-=over 4
-
-=head2 DESCRIPTION 
-
-Helper module to be included in mapping scripts
-
-=back
-
-=head3 FUNCTIONS
-
-=over 4
-
-=item map_it($output,$pcr,$sorted_genes,$genes)
-
-maps $pcr s to overlapping $genes.
-
-$output is a hashref containing item -> matched exons
-
-$pcr    is a hashref containing the to be queried arrays id->[start,stop]
-
-$sorted_genes is a arrayref containing genenames sorted by start then stop
-
-$genes is a hashref containing the genes/transcripts/whatever
-
-=item  get_from_gff( $file, $type, $unless, $genes )
-
-reads a gff into a hash of Genes containing Exons (%$genes), key is the Exon->id
-
-struct( Exon => [ start => '$', stop => '$', type => '$', id => '$' ] );
-struct( Gene => [ start => '$', stop => '$', exons => '@' ] );
-
-
-=back
-
-=head3 AUTHOR
-$Author: ar2 $
-
-=cut
--- scripts/map_Oligo_set.pl
+++ scripts/map_Oligo_set.pl
@@ -1,14 +1,7 @@
-#!/usr/local/bin/perl
+#!/usr/bin/env perl
 #
-# map_Oligo_set
+# map_Oligo_sets
 #
-# Usage: map_Oligo_set.pl [-options]
-#
-# 010927 :  dl : modified output print line to include speech marks. this prevents any acefile
-#                parsing problems
-# 010717 :  kj : modified to (hopefully) run faster and produce the final output files in one go
-#
-# 051114 :  mh6 : rewrote it for better readability
 #
 #############################################################################################
 
@@ -17,20 +10,21 @@ use strict;
 use Wormbase;
 use Getopt::Long;
 use warnings;
-use Class::Struct;
 use Modules::Map_Helper;
 
-##########################
-# Script variables (run) #
-##########################
 
-my $maintainers = "All";
-my %output      = ();
-my %finaloutput = ();
+my $config = {
+  CDS => [ ['curated', 'curated', 'exon'] ],
 
-########################
-# command-line options #
-########################
+  Transcript => [ ['Coding_transcript',     'Coding_transcript',     'exon'],
+                  ['Non_coding_transcript', 'Non_coding_transcript', 'exon'],
+                  ['ncRNA',                 'ncRNA',                 'exon'],
+                  ['Pseudogene',            'Pseudogene',            'exon'],
+ ],
+
+  Pseudogene => [ ['Pseudogene', 'Pseudogene', 'exon'] ],
+};
+
 
 my $help;       # Help perldoc
 my $test;       # Test mode
@@ -38,256 +32,121 @@ my $debug;      # Debug mode, output only goes to one user
 my $verbose;    # verbose mode, more command line outout
 my $acename;    # specify a custom acefile
 my $store;      # specify a frozen configuration file
-my $no_parse;   # don't parse the acefile
+my $noload;   # don't parse the acefile
+my $species;
+my $pre_ws237_model;
 
 GetOptions(
-    'debug=s'   => \$debug,
-    'verbose'   => \$verbose,
-    'test'      => \$test,
-    'help'      => \$help,
-    'acefile=s' => \$acename,
-    'store=s'   => \$store,
-    'no_parse'  => \$no_parse
+  'debug=s'   => \$debug,
+  'verbose'   => \$verbose,
+  'test'      => \$test,
+  'help'      => \$help,
+  'acefile=s' => \$acename,
+  'store=s'   => \$store,
+  'noload'    => \$noload,
+  'species=s' => \$species,
+  'prews237'  => \$pre_ws237_model,
 );
 
-# Display help if required
-&usage("Help") if ($help);
+my $oligo_set_file_prefix = ($pre_ws237_model) 
+    ? "Oligo_set" 
+    : "Oligo_set_mapping";
 
 ############################
 # recreate configuration   #
 # ##########################
 my $wb;
-if ($store) { $wb = Storable::retrieve($store) or croak("cant restore wormbase from $store\n") }
-else { $wb = Wormbase->new( -debug => $debug, -test => $test, ) }
-
-###########################################
-# Variables Part II (depending on $wb)    #
-###########################################
-
-$test  = $wb->test  if $wb->test;     # Test mode
-$debug = $wb->debug if $wb->debug;    # Debug mode, output only goes to one user
-
-# Use debug mode?
-if ($debug) {
-    print "DEBUG = \"$debug\"\n\n";
-    ( $maintainers = $debug . '\@sanger.ac.uk' );
+if ($store) { 
+  $wb = Storable::retrieve($store) or croak("cant restore wormbase from $store\n"); 
+} else { 
+  $wb = Wormbase->new( -debug    => $debug, 
+                       -test     => $test,
+                       -organism => $species);
 }
 
-#############
-# Paths etc #
-#############
-my $tace    = $wb->tace;                                                    # tace executable path
-my $dbdir   = $wb->autoace;                                                 # Database path
-my $gffdir  = $wb->gff_splits;                                              # GFF splits directory
-my $acefile = $acename ? $acename : "$dbdir/acefiles/Oligo_mappings.ace";
+my $gffdir  = $wb->gff_splits;
+my $acefile = $acename ? $acename : $wb->acefiles . "/Oligos_to_genes.ace";
 
-my @chromosomes = $test
-  ? qw( III )
-  : qw( I II III IV V X );    # chromosomes to parse (TEST_BUILD should be III)
-
-################
-# Structs      #
-################
-use Class::Struct;
-struct( Exon => [ start => '$', stop => '$', type => '$', id => '$' ] );
-struct( Gene => [ start => '$', stop => '$', orientation => '$',parent => '$', exons => '@' ] );
-
-################
-# Open logfile #
-################
 my $log = Log_files->make_build_log($wb);
 
-#####################################################
-# get exons and Oligo_set info out of the gff files #
-#####################################################
-
-foreach my $chromosome (@chromosomes) {
-    $log->write_to("Processing chromosome $chromosome\n");
-    print "\nProcessing chromosome $chromosome\n" if ($verbose);
-    my %genes = ();
-    my %Oligo = ();
-
-    # Get Oligo set info from split GFF file
-    open( GFF, "<$gffdir/CHROMOSOME_${chromosome}_Oligo_set.gff" )
-      || die "Failed to open $gffdir/CHROMOSOME_${chromosome}_Oligo_set.gff : $!\n\n";
-    while (<GFF>) {
-        chomp;
-        s/\#.*//;
-        next unless /Oligo_set/;
-#for WS156	next unless /exon/
-        my @f = split /\t/;
-
-        my ($name) = ( $f[8] =~ /Oligo_set \"(.*)\"$/ );
-        unless ($name) {
-            $log->write_to("ERROR: Cant get name from $f[8]\n");
-            next;
-        }
-        $Oligo{$name} = [ $f[3], $f[4] ];
-        print "Oligo_set : '$name'\n" if ($verbose);
-    }
-    close(GFF);
-
-#####################################
-# extract data from GFF_split files #
-#####################################
-
-    #Check that the gff files exists so as to not waste time.
-    $log->log_and_die("ERROR: $gffdir/CHROMOSOME_${chromosome}_UTR.gff does not exist\n") unless (-e "$gffdir/CHROMOSOME_${chromosome}_UTR.gff");
-    $log->log_and_die("ERROR: $gffdir/CHROMOSOME_${chromosome}_curated.gff does not exist\n") unless (-e "$gffdir/CHROMOSOME_${chromosome}_curated.gff");
-    $log->log_and_die("ERROR: $gffdir/CHROMOSOME_${chromosome}_Pseudogene.gff does not exist\n") unless (-e "$gffdir/CHROMOSOME_${chromosome}_Pseudogene.gff");
-    $log->log_and_die("ERROR: $gffdir/CHROMOSOME_${chromosome}_Non_coding_transcript.gff does not exist\n") unless (-e "$gffdir/CHROMOSOME_${chromosome}_Non_coding_transcript.gff");
-    $log->log_and_die("ERROR: $gffdir/CHROMOSOME_${chromosome}_ncRNA.gff does not exist\n") unless (-e "$gffdir/CHROMOSOME_${chromosome}_ncRNA.gff");
-
-    # Get exon info from split UTR GFF files
-    Map_Helper::get_from_gff( "$gffdir/CHROMOSOME_${chromosome}_UTR.gff", 'Transcript', 'UTR', \%genes );
-
-    # Get exon info from split exon GFF files
-    Map_Helper::get_from_gff( "$gffdir/CHROMOSOME_${chromosome}_curated.gff", 'CDS', qw{exon}, \%genes );
-
-    # Get exon info from split pseudogene exon GFF files
-    Map_Helper::get_from_gff( "$gffdir/CHROMOSOME_${chromosome}_Pseudogene.gff", 'Pseudogene', qw{exon}, \%genes );
-
-    # Get exon info from split transcript exon GFF file
-    Map_Helper::get_from_gff( "$gffdir/CHROMOSOME_${chromosome}_Non_coding_transcript.gff", 'Transcript', qw{exon}, \%genes );
-
-    # Get exon info from split ncRNA exon GFF file
-    Map_Helper::get_from_gff( "$gffdir/CHROMOSOME_${chromosome}_ncRNA.gff", 'Transcript', qw{exon}, \%genes );
-
-    print "Finished GFF loop\n" if ($verbose);
-
-    ###################
-    # make indexlists #
-    ###################
-
-    print " sorting genes\n" if ($verbose);
-
-    my @sorted_genes =
-      sort { $genes{$a}->start <=> $genes{$b}->start || $genes{$a}->stop <=> $genes{$b}->stop } keys %genes;
-
-    ##########
-    # map it #
-    ##########
-    Map_Helper::map_it( \%output, \%Oligo, \@sorted_genes, \%genes );
+my @oligo_set_files;
+if ($wb->assembly_type eq 'contig') {
+  @oligo_set_files = ($gffdir . "/${oligo_set_file_prefix}.gff");
+} else {
+  foreach my $chr_name ($wb->get_chromosome_names(-mito => 1, -prefix => 1)) {
+    my $file = $gffdir . "/${chr_name}_${oligo_set_file_prefix}.gff";
+    push @oligo_set_files, $file;
+  }
 }
-#foreach chromosome loop end
 
-########################
-# produce output files #
-########################
-open( OUTACE, ">$acefile" ) || die "cannot open file $acefile\n";
+foreach my $class (keys %$config) {
+  my $fm = Map_Helper->new();
 
-foreach my $oligoname ( keys %output ) {
-    print "mapped $oligoname\tto ", join ' ', ( map { $_->id } @{ $output{$oligoname} } ), "\n"
-      if ($verbose);
+  foreach my $stanza (@{$config->{$class}}) {
+    my ($file_prefix, $method, $type) = @$stanza;
 
-    foreach my $exon ( @{ $output{$oligoname} } ) {
-        if ( $exon->type eq "CDS" ) {
-            print OUTACE "CDS : \"", $exon->id, "\"\n";
-            print OUTACE "Corresponding_oligo_set \"$oligoname\"\n\n";
-        }
-        elsif ( $exon->type eq "Pseudogene" ) {
-            print OUTACE "Pseudogene : \"", $exon->id, "\"\n";
-            print OUTACE "Corresponding_oligo_set \"$oligoname\"\n\n";
-        }
-        elsif ( $exon->type eq "Transcript" ) {
-            print OUTACE "Transcript : \"", $exon->id, "\"\n";
-            print OUTACE "Corresponding_oligo_set \"$oligoname\"\n\n";
-        }
+    my @files;
+    if ($wb->assembly_type eq 'contig') {
+      my $file = $gffdir . "/${file_prefix}.gff";
+      push @files, $file;
+    } else {
+      foreach my $chr_name ($wb->get_chromosome_names(-mito => 1, -prefix => 1)) {
+        my $file = $gffdir . "/${chr_name}_${file_prefix}.gff";
+        push @files, $file;
+      }
     }
+
+    foreach my $file (@files) {
+      if (not -e $file) {
+        $log->log_and_die("Could not find file $file\n");
+      }
+      $fm->populate_from_GFF($file, $method, $type, sprintf('%s \"(\S+)\"', $class));
+    }
+  }
+
+  $fm->build_index;
+
+  my %object_map;
+  
+  foreach my $file (@oligo_set_files) {
+    open( my $fh, $file) or $log->log_and_die("Could not open $file for reading\n");
+    while(<$fh>) {
+      next if /^\#/;
+      my @l = split(/\t+/, $_);
+      
+      next if $l[1] ne 'Oligo_set' or $l[2] ne 'reagent';
+      my ($name) = $l[8] =~ /Oligo_set \"(\S+)\"/;
+      if (not defined $name) {
+        ($name) = $l[8] =~ /Target \"Oligo_set:(\S+)\"/;
+      }
+
+      next if not defined $name;
+      
+      my @matches = @{$fm->search_feature_segments($l[0], $l[3], $l[4])};
+      map { $object_map{$_}->{$name} = 1 } @matches;
+    }
+  }
+  
+  foreach my $obj_name (sort keys %object_map) {
+    print "\n$class : \"$obj_name\"\n";
+    foreach my $os (sort keys %{$object_map{$obj_name}}) {
+      print "Corresponding_oligo_set $os\n";
+    }
+  }
 }
-close(OUTACE);
 
-##############################
-# read acefiles into autoace #
-##############################
-
-unless ($test||$no_parse) {
-    $wb->load_to_database( $dbdir, $acefile, 'map_Oligo_products', $log );
+if (not $noload) {
+  $wb->load_to_database( $wb->autoace, $acefile, 'map_Oligo_set', $log );
 }
 
 ###############
 # hasta luego #
 ###############
 
-$log->mail( "$maintainers", "BUILD REPORT: $0" );
+$log->mail();
 
 exit(0);
 
-##############################################################
-##### Subroutines #####
-##############################################################
-
-sub usage {
-    my $error = shift;
-
-    if ( $error eq "Help" ) {
-
-        # Normal help menu
-        system( 'perldoc', $0 );
-        exit(0);
-    }
-}
-
-################################
-# hit to exon converter
-# including: adding types based on source/features
-sub to_exon {
-    my $hit = shift;
-    my $type;
-
-    if    ( $hit->{feature} eq 'curated'               && $hit->{source} eq 'exon' ) { $type = 'CDS' }
-    elsif ( $hit->{feature} eq 'Pseudogene'            && $hit->{source} eq 'exon' ) { $type = 'Pseudogene' }
-    elsif ( $hit->{feature} eq 'Non_coding_transcript' && $hit->{source} eq 'exon' ) { $type = 'Transcript' }
-    elsif ( $hit->{feature} eq 'Coding_transcript'     && $hit->{source} eq 'exon' ) { $type = 'Transcript' }
-    
-    else { $type = "feature:" . $hit->{feature} . " source:" . $hit->{source} }
-    my $exon = Exon->new( id => get_id( $hit->{fluff} ), start => $hit->{start}, stop => $hit->{stop}, type => $type );
-
-    return \$exon;
-}
 
 __END__
 
-=pod
-
-=head2 NAME - map_Oligo_products.pl
-
-=head1 USAGE
-
-=over 4
-
-=item map_Oligo_products.pl [-options]
-
-=back
-
-map_Oligo_products.pl calculates the overlap between the mapped Oligo_products
-and the CDS, transcript and pseudogene coordinates in the WS database release. 
-It will generate an acefile for this data and upload into /wormsrv2/autoace
-
-map_Oligo_products mandatory arguments:
-
-=over 4
-
-=item none
-
-=back
-
-map_Oligo_products optional arguments:
-
-=over 4
-
-=item -debug username, debug mode email goes to user specified by -debug
-
-=item -verbose, toggles extra output
-
-=item -test, Test mode, generate the acefile but do not upload 
-
-=item -acefile, custom name for the acefile
-
-=item -help, Help pages
-
-=item -store, specify configuration file
-
-=back
-
-=cut
