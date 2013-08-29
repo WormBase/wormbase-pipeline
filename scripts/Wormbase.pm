a192ba87ff83e7f80fe781efb2ee01cc7d81a760
mh6 <mh6>
2013-08-29 10:11:43 +0000

    added Panagrellus to the Tier III species

--- scripts/Wormbase.pm
+++ scripts/Wormbase.pm
@@ -17,7 +17,7 @@ use Digest::MD5 qw(md5_hex);
 use Species;
 
 our @core_organisms=qw(Elegans Briggsae Remanei Brenneri Japonica Pristionchus Brugia);
-our @tier3_organisms=qw(Mhapla Mincognita Heterorhabditis Hcontortus Hcontortus_gasser Cangaria Tspiralis Sratti Csp11 Asuum Bxylophilus Csp5 Loaloa Asuum_davis);
+our @tier3_organisms=qw(Mhapla Mincognita Heterorhabditis Hcontortus Hcontortus_gasser Cangaria Tspiralis Sratti Csp11 Asuum Bxylophilus Csp5 Loaloa Asuum_davis Panagrellus);
 our @provisional_organisms = qw(Hcontortus);
 our @allowed_organisms=(@core_organisms, @tier3_organisms,@provisional_organisms); #class data
 
