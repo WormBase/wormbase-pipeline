9b2ab6c87618b4bbb8430164c4adf390755a559d
mh6 <mh6>
2013-11-07 13:45:12 +0000

    cleaned up a bit

--- scripts/ENSEMBL/scripts/get_omim2ensemblprotein.pl
+++ scripts/ENSEMBL/scripts/get_omim2ensemblprotein.pl
@@ -20,30 +20,9 @@ use Wormbase;
 
 
 #use lib '/software/worm/ensembl-64/ensembl/modules';
-#use lib '/software/worm/ensembl-64/ensembl-compara/modules';
 
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
-
-
-my %speciesOfInterest = (
-  'saccharomyces_cerevisiae'  => 'yeast',
-  'drosophila_melanogaster' => 'fly',
-  'homo_sapiens'            => 'human',
-  'mus_musculus'            => 'mouse',
-);
-
-
-my ($wormbase, $debug, $test, $verbose, $store, $species, $use_current_db, %cds2wbgene, %current_genes);
-
-GetOptions (
-  "debug=s"    => \$debug,
-  "test"       => \$test,
-  "verbose"    => \$verbose,
-  "store:s"    => \$store,
-  "species:s"  => \$species,
-  "usecurrentdb" => \$use_current_db,
-            );
 
 
 Bio::EnsEMBL::Registry->load_registry_from_db(-host => 'ens-livemirror', 
@@ -72,7 +51,6 @@ foreach my $gene (@{$gene_adaptor->fetch_all_by_biotype('protein_coding')}) {
     foreach my $od (@omim_disease) {
       printf "DB_info Database OMIM disease %s\n", $od->primary_id;
     }
-
     print "\n";
   }	
 }
