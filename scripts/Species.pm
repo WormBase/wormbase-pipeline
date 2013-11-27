c927d8a66c0375f0b3b726f078056ba2b69f1396
klh <klh>
2013-11-27 14:33:07 +0000

    Changed CDS/pep prefixes for O.vol

--- scripts/Species.pm
+++ scripts/Species.pm
@@ -527,11 +527,11 @@ sub full_name {
 	};
 }
 sub chromosome_prefix {''}
-sub seq_name_regex{qr/^Ov\d+/};
-sub pep_prefix {'OV'}
+sub seq_name_regex{qr/^OVOC\d+/};
+sub pep_prefix {'OVP'}
 sub pepdir_prefix{'ovol'};
 sub cds_regex_noend{qr/Ov\d+[a-z]*/}; # for getting the CDS part of a Transcript name
-sub cds_regex{qr/Ov\d+[a-z]*/};
+sub cds_regex{qr/OVOC\d+[a-z]*/};
 sub ncbi_tax_id {6282};
 sub ncbi_bioproject {'PRJEB513'};
 sub bioproject_description { 'Wellcome Trust Sanger Institute, O.volvulus project' }
