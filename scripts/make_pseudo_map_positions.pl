29bdb2316f9533e0cb14e13ad615fa7fdc96a44e
khowe <kevin.howe@wormbase.org>
2013-12-12 18:58:42 +0000

    Now only reports once for each gene, in a more user-friendly output

--- scripts/make_pseudo_map_positions.pl
+++ scripts/make_pseudo_map_positions.pl
@@ -1,17 +1,15 @@
-#!/usr/local/bin/perl5.8.0
+#!/usr/bin/env perl
 # 
 # make_pseudo_map_positions.pl
-#
-# Written by Keith Bradnam aged 12 and three quarters
 #
 # Script to identify genes which can have their Interpolated_map_position tag promoted to a Map position
 #
 # Last updated by: $Author: klh $
-# Last updated on: $Date: 2013-10-14 10:16:24 $
+# Last updated on: $Date: 2013-12-12 18:58:42 $
 
 use strict;
 use warnings;
-use lib $ENV{'CVS_DIR'};
+use lib $ENV{CVS_DIR};
 use Wormbase;
 use Ace;
 use Getopt::Long;
@@ -44,13 +42,13 @@ GetOptions (
 
 # recreate configuration
 my $wb;
-if ($store) { $wb = Storable::retrieve($store) or croak("cant restore wormbase from $store\n")
+if ($store) { 
+  $wb = Storable::retrieve($store) or croak("cant restore wormbase from $store\n")
+} else { 
+  $wb = Wormbase->new( 
+    -debug => $debug, 
+    -test => $test, ); 
 }
-else { $wb = Wormbase->new( -debug => $debug, -test => $test, ) }
-
-# Variables Part II (depending on $wb)
-$test  = $wb->test  if $wb->test;     # Test mode
-$debug = $wb->debug if $wb->debug;    # Debug mode, output only goes to one user
 
 # Set up top level base directory which is different if in test mode
 # Make all other directories relative to this
@@ -59,10 +57,8 @@ my $basedir   = $wb->basedir;
 # create log file, open output file handles
 my $log = Log_files->make_build_log($wb);
 
-
-my $tace = $wb->tace;                               # tace executable path
 $database = $wb->autoace if (!$database);           # specify autoace as the default database
-$acefile = $wb->acefiles."/pseudo_map_positions.ace" if not defined $acefile;
+$acefile  = $wb->acefiles."/pseudo_map_positions.ace" if not defined $acefile;
 
 
 ###############################################################################################
@@ -71,9 +67,53 @@ $acefile = $wb->acefiles."/pseudo_map_positions.ace" if not defined $acefile;
 # inferred multipoint data for Jonathan Hodgkin to approve
 ###############################################################################################
 
+$log->write_to("Genes with newly promoted Map positions:\n\n");
 
-# keep track of how many map positions are 'promoted'
-my $count = 0;
+# open a connection to database
+my $db = Ace->connect(-path  => $database ) ||  $log->log_and_die("Connection failure: ",Ace->error);
+
+# build query to find candidate genes
+my $query  = "find Gene * WHERE !Mapping_data & Allele & CGC_name & Sequence_name & Interpolated_map_position & Species =\"*elegans\"";
+push(my @candidate_genes, $db->find($query) );
+
+my @to_promote;
+foreach my $gene (@candidate_genes){
+
+  # get basic details from gene
+  my $map      = $gene->Interpolated_map_position(1);
+  my $position = $gene->Interpolated_map_position(2);
+  my $cgc_name = $gene->CGC_name;
+  my @alleles  = $gene->Allele(1);
+
+  my @supporting_alleles;
+  foreach my $allele (@alleles){
+
+    # only want to consider alleles which are not Transposon insertions or SNPs
+    next if(defined($allele->Transposon_insertion));
+
+    my $method = $allele->Method;
+    next if not grep { $method eq $_ } ('Allele',
+                                        'Deletion_allele',
+                                        'Deletion_and_insertion_allele',
+                                        'Insertion_allele',
+                                        'Substitution_allele',
+                                        'KO_consortium_allele',
+                                        'NBP_knockout_allele',
+                                        'NemaGENETAG_consortium_allele');
+
+    push @supporting_alleles, $allele;
+  }
+  if (@supporting_alleles) {
+    push @to_promote, {
+      gene => $gene,
+      cgc  => $cgc_name,
+      map  => $map,
+      pos  => $position,
+      alleles => \@supporting_alleles,
+    };
+  }
+}
+
 
 # open main output file to be loaded into autoace (and subsequently geneace)
 open(OUT, ">$acefile") || die $!;
@@ -90,55 +130,27 @@ print OUT "// genes with map positions need to have mapping data).\n\n";
 print OUT "// This file should also be uploaded back into geneace...normally this\n";
 print OUT "// happen automatically by a later build script\n\n";
 
-
-$log->write_to("Genes with newly promoted Map positions ");
-$log->write_to("(this file will be loaded to geneace by a later build script):\n\n");
-
-# open a connection to database
-my $db = Ace->connect(-path  => $database ) ||  $log->log_and_die("Connection failure: ",Ace->error);
-
-# build query to find candidate genes
-my $query  = "find Gene * WHERE !Mapping_data & Allele & CGC_name & Sequence_name & Interpolated_map_position & Species =\"*elegans\"";
-
-# create a list of Genes which should gain a 'pseudo' map position
-push(my @candidate_genes, $db->find($query) );
-
-# cycle through list of candidate genes making ace file
-foreach my $gene (@candidate_genes){
-
-  # get basic details from gene
-  my $map      = $gene->Interpolated_map_position(1);
-  my $position = $gene->Interpolated_map_position(2);
-  my $cgc_name = $gene->CGC_name;
-  my @alleles  = $gene->Allele(1);
-
-  foreach my $allele (@alleles){
-
-    # only want to consider alleles which are not Transposon insertions or SNPs
-    next if(defined($allele->Transposon_insertion));
-
-    my $method = $allele->Method;
-    next if(defined($method) && ($method eq "SNP" || $method eq "Transposon_insertion"));
-
-    print "$gene\t$cgc_name\t$allele\t$map\t$position\n" if ($verbose);
-    
-    $count++;
-    $log->write_to("$gene\t$cgc_name\tallele = $allele\t'promoted' map position: $map\t$position\n");
-    
-    # first remove existing Interpolated_map_position before adding new data
-    print OUT "\nGene : \"$gene\" \/\/ $cgc_name\n";
-    print OUT "-D Interpolated_map_position\n\n";
-    
-    print OUT "Gene : \"$gene\"\n";
-    print OUT "Map \"$map\" Position $position\n";
-    print OUT "Pseudo_map_position\n";
-    print OUT "Remark \"Map position created from combination of previous interpolated map position (based on known location of sequence) and allele information.  Therefore this is not a genetic map position based on recombination frequencies or genetic experiments.  This was done on advice of the CGC.\" CGC_data_submission\n\n\n";
-  }
+foreach my $obj (sort { $a->{map} cmp $b->{map} or $a->{pos} <=> $b->{pos} } @to_promote) {
+  my @allele_names = map { $_->Public_name } @{$obj->{alleles}};
+  $log->write_to(sprintf("%-10s\t%-6s\t'promoted' map position: %3s\t%10.5f\talleles: %s\n", 
+                         $obj->{gene}, 
+                         $obj->{cgc},
+                         $obj->{map}, 
+                         $obj->{pos}, 
+                         join(",", sort @allele_names)));
+  # first remove existing Interpolated_map_position before adding new data
+  print OUT "\nGene : \"$obj->{gene}\" \/\/ $obj->{cgc}\n";
+  print OUT "-D Interpolated_map_position\n\n";
+  
+  print OUT "Gene : \"$obj->{gene}\"\n";
+  print OUT "Map \"$obj->{map}\" Position $obj->{pos}\n";
+  print OUT "Pseudo_map_position\n";
+  print OUT "Remark \"Map position created from combination of previous interpolated map position (based on known location of sequence) and allele information.  Therefore this is not a genetic map position based on recombination frequencies or genetic experiments.  This was done on advice of the CGC.\" CGC_data_submission\n\n\n";
 }
 close(OUT);
 $db->close;
 
-$log->write_to("Total: $count to become inferred genetic marker(s)\n\n");
+$log->write_to("\nTotal: ". scalar(@to_promote) . " genes to become inferred genetic marker(s)\n\n");
 
 # we do not wish to throw an error if there are large differences in
 # the number of objects loaded from this file between on Build and the
