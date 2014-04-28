928fcc2411e44855474bd108b6cf681462d3db14
epaule <michael.paulini@wormbase.org>
2014-04-28 15:03:44 +0000

    minimalistic cleanup

--- scripts/NAMEDB/geneace_nameDB_comm.pl
+++ scripts/NAMEDB/geneace_nameDB_comm.pl
@@ -14,11 +14,11 @@ use Storable;
 
 my ($help, $debug, $test, $store, $database);
 
-GetOptions (	"help"       => \$help,
-            	"debug=s"    => \$debug,
-			    	"test"       => \$test,
-	    			"store:s"    => \$store,
-                "database=s" => \$database,
+GetOptions (	'help'       => \$help,
+            	'debug=s'    => \$debug,
+	    	'test'       => \$test,
+  		'store:s'    => \$store,
+                'database=s' => \$database,
            );
 
 my $wormbase;
@@ -29,7 +29,6 @@ if ( $store ) {
                              -test    => $test,
 			     );
 }
-
 
 # establish log file.
 my $log = Log_files->make_build_log($wormbase);
@@ -52,21 +51,19 @@ while( <$TABLE> ){
 }
            
 #connect to name server and set domain to 'Gene'
-my $DB    	= 'wbgene_id;shap;3303';
-my $PASS 	= "wormpub";
-my $USER 	= "wormpub";
+my $DB      = 'wbgene_id;shap;3303';
+my $PASS    = 'wormpub';
+my $USER    = 'wormpub';
 my $DOMAIN  = 'Gene';
 my $db = NameDB_handler->new($DB,$USER,$PASS,$wormbase->wormpub . '/DATABASES/NameDB');
 
 # get nameserver data
-my $query = 'SELECT primary_identifier.object_public_id,
-						  secondary_identifier.name_type_id,
-						  secondary_identifier.object_name
-				 FROM   primary_identifier,secondary_identifier 
-				 WHERE  primary_identifier.object_id = secondary_identifier.object_id 
-				 AND   (secondary_identifier.name_type_id = 3 OR secondary_identifier.name_type_id = 1) 
-				 AND    primary_identifier.domain_id = 1
-				 ORDER BY object_public_id';
+my $query = 'SELECT pi.object_public_id, si.name_type_id, si.object_name
+             FROM   primary_identifier pi,secondary_identifier si
+             WHERE  pi.object_id = si.object_id 
+             AND   (secondary_identifier.name_type_id = 3 OR secondary_identifier.name_type_id = 1) 
+             AND    primary_identifier.domain_id = 1
+             ORDER BY object_public_id';
 # results
 #| object_public_id | name_type_id | name
 #| WBGene00044331   |            1 | clec-25       |
@@ -75,10 +72,8 @@ my $query = 'SELECT primary_identifier.object_public_id,
 my $sth = $db->dbh->prepare($query);
 $sth->execute();
 
-my %name_types = (
-						 '1' => 'cgc',
-						 '3' => 'seq'
-						);
+my %name_types = ( '1' => 'cgc', '3' => 'seq');
+
 my %server_genes;					 
 while (my ( $gene, $name_type, $name ) = $sth->fetchrow_array){
 	$server_genes{$gene}->{ $name_types{$name_type} } = $name;
@@ -86,9 +81,9 @@ while (my ( $gene, $name_type, $name ) = $sth->fetchrow_array){
 
 #This is to get dead and briggsae genes that dont have names.
 $query = 'SELECT object_public_id, object_live
-			 FROM   primary_identifier 
-			 WHERE domain_id = 1
-			 ORDER BY object_public_id';
+          FROM   primary_identifier 
+          WHERE domain_id = 1
+          ORDER BY object_public_id';
 
 $sth = $db->dbh->prepare($query);
 $sth->execute();
@@ -112,50 +107,49 @@ $log->write_to("No errors found\n") if ($log->report_errors == 0);
 $log->mail;
 exit(0);
 
-
 sub check_gene {
 	my $gene = shift;
 	#check presence in both sets
 	if( $ace_genes{"$gene"} ){
-		if( $server_genes{"$gene"} ){
-			# check Live 
-			if ($ace_genes{"$gene"}->{'sts'} != $server_genes{"$gene"}->{'sts'}){
-				$log->error("ERROR: $gene live or dead ? ns".$ace_genes{"$gene"}->{'sts'}." g".$server_genes{"$gene"}->{'sts'}."\n");
-				return;
-			}
-			return if ($ace_genes{"$gene"}->{'sts'} == 0) ; #dont check dead genes.
-			# for each name type eg cgc, seq
-			my ($i,$type);	
-			while( ($i,$type) = each(%name_types) ){
-				#confirm both have them
-				if ($ace_genes{"$gene"}->{$type} or $server_genes{"$gene"}->{$type}) {
-					#determine if either doesn't
-					unless ($ace_genes{"$gene"}->{$type}) {
-						$log->error("ERROR: Geneace - $gene missing $type name ".$server_genes{"$gene"}->{$type}."\n");
-						next;
-					}			
-					unless ($server_genes{"$gene"}->{$type}) {
-						$log->error("ERROR: Server - $gene missing $type name ".$ace_genes{"$gene"}->{$type}."\n");
-						next;
-					}
-					# confirm they are the same if both have name type	
-					if ($ace_genes{"$gene"}->{$type} ne $server_genes{"$gene"}->{$type}) {
-						$log->error("ERROR: $gene different $type names for $gene G:".$ace_genes{"$gene"}->{$type}." N:".$server_genes{"$gene"}->{$type}."\n");
-						next;
-					}
-				}
-			}
-			#print STDERR "$gene ok\n" if $debug;
-			return;
+	  if( $server_genes{"$gene"} ){
+	      # check Live 
+	      if ($ace_genes{"$gene"}->{'sts'} != $server_genes{"$gene"}->{'sts'}){
+		$log->error("ERROR: $gene live or dead ? ns".$ace_genes{"$gene"}->{'sts'}." g".$server_genes{"$gene"}->{'sts'}."\n");
+		return;
+	      }
+	      return if ($ace_genes{"$gene"}->{'sts'} == 0) ; #dont check dead genes.
+	      # for each name type eg cgc, seq
+	      my ($i,$type);	
+	      while( ($i,$type) = each(%name_types) ){
+		#confirm both have them
+		if ($ace_genes{"$gene"}->{$type} or $server_genes{"$gene"}->{$type}) {
+                  #determine if either doesn't
+                  unless ($ace_genes{"$gene"}->{$type}) {
+                    $log->error("ERROR: Geneace - $gene missing $type name ".$server_genes{"$gene"}->{$type}."\n");
+                    next;
+                  }			
+                  unless ($server_genes{"$gene"}->{$type}) {
+                    $log->error("ERROR: Server - $gene missing $type name ".$ace_genes{"$gene"}->{$type}."\n");
+                    next;
+                  }
+                  # confirm they are the same if both have name type	
+                  if ($ace_genes{"$gene"}->{$type} ne $server_genes{"$gene"}->{$type}) {
+                    $log->error("ERROR: $gene different $type names for $gene G:".$ace_genes{"$gene"}->{$type}." N:".$server_genes{"$gene"}->{$type}."\n");
+                    next;
+                  }
 		}
-		else {
-			# nameserver query will not return genes imported without a valid name.
-			$log->error("ERROR: $gene missing from server\n");
-			return;
-		}	
+	      }
+	      #print STDERR "$gene ok\n" if $debug;
+	      return;
 	}
 	else {
-		$log->error("ERROR: $gene missing from acedb\n");
-		return;
-	}
+	  # nameserver query will not return genes imported without a valid name.
+	  $log->error("ERROR: $gene missing from server\n");
+	  return;
+	}	
+      }
+      else {
+	$log->error("ERROR: $gene missing from acedb\n");
+	return;
+      }
 }
