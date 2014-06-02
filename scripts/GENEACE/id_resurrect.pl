237b3ed075a2d8fa4d5c362e2584cb496b9d5d67
Paul-Davis <paul.davis@wormbase.org>
2014-06-02 14:28:08 +0000

    Script now generates and loads an ace file into geneace that populates the standard tags for a live gene, it also creates a stub comment that should probably be manually added to.

--- scripts/GENEACE/id_resurrect.pl
+++ scripts/GENEACE/id_resurrect.pl
@@ -1,5 +1,5 @@
 #!/usr/local/bin/perl -w
-
+use lib $ENV{'CVS_DIR'};
 use lib '/software/worm/lib/perl/';
 use lib '/nfs/WWWdev/SANGER_docs/lib/Projects/C_elegans/';
 use strict;
@@ -7,6 +7,10 @@ use lib '../blib/lib';
 use lib '../lib';
 use NameDB;
 use Getopt::Long;
+use Ace;
+use Wormbase;
+use Storable;
+
 
 my $USAGE = <<END;
 Usage: $0 <options> domain id nametype1=name1 nametype2=name2...
@@ -18,7 +22,7 @@ Options:
   --user      username
   --password  password
 
-  id_resurrect.pl -user ar2 -password <ar2 pswd> -id WBGene00000001 -domain Gene 
+  id_resurrect.pl -user ar2 -password <ar2 pswd> -id WBGene00000001 -domain Gene -species brugia -load
   optional  (-test -database).  Will default to live nameserver, -test will change to test database.
   
   Valid DOMAINS are domains currently in the nameserver (ie 'Gene' and 'Variation' at time of writing)  
@@ -26,18 +30,21 @@ Options:
 END
 
 my ($DB,$USER,$PASS);
-my ($domain, $id, $test);
+my ($domain, $id, $test, $output, $species, $load);
 GetOptions('database:s' => \$DB,
 	   'user:s'     => \$USER,
 	   'password:s' => \$PASS,
 	   'id:s'       => \$id,
 	   'test'       => \$test,
-	   'domain:s'   => \$domain
+	   'domain:s'   => \$domain,
+	   'output:s'   => \$output,
+	   'species:s'  => \$species,
+	   'load'       => \$load,
 	  ) or die $USAGE;
 
 unless ($DB) {
-	$DB = 'wbgene_id;shap;3303';
-	$DB = 'test_wbgene_id;mcs11;3307' if $test;
+  $DB = 'wbgene_id;shap;3303';
+  $DB = 'test_wbgene_id;mcs11;3307' if $test;
 }
 $USER ||= $ENV{NAMEDB_USER};
 $PASS ||= $ENV{NAMEDB_PASS};
@@ -45,9 +52,44 @@ $PASS ||= $ENV{NAMEDB_PASS};
 $DB or die "Must provide a --database option";
 $USER ||= $ENV{USER};
 
-die $USAGE unless ($id and $domain and $USER and $PASS and $DB);
+die $USAGE unless ($id and $domain and $USER and $PASS and $DB and $species);
+
+my $wormbase;
+$wormbase = Wormbase->new( -test => $test,
+			   -organism => $species
+			 );
+
+my $rundate     = `date +%y%m%d`; chomp $rundate;
+my $tace = $wormbase->tace;
+
+my $WBUSERS = {
+	    # these are the users WBPerson id
+	    'klh'           => 3111,
+	    'pad'           => 1983,
+	    'mt3'           => 2970,
+	    'gw3'           => 4025,
+	    'mh6'           => 4055,
+	   };
+
+my $acedb = "/nfs/wormpub/DATABASES/geneace";
 my $db = NameDB->connect($DB,$USER,$PASS, '/nfs/WWWdev/SANGER_docs/htdocs');
 
+my $ace = Ace->connect('-path', $acedb) or die("cant open $acedb: $!\n");
+my $idObj = $ace->fetch('Gene', $id);
+
+my $ver = $idObj->Version->name;
+$ver++;
+
+my $clone;
+
+my $seq_name = $idObj->Sequence_name->name;
+if ($seq_name =~ /(\S+)\.\d+/) {
+  $clone = $1;
+}
+else {$clone = $seq_name;}
+
+unless ($output) {$output = "/tmp/idressurect_${id}.ace";}
+open (OUT,">$output") or die "Can't open $output\n";
 my @doms = $db->getDomains;
 #die ("$domain is invalid\n Use one of".join(@doms,', ')."\n") unless (grep $domain @doms);
 
@@ -58,11 +100,52 @@ unless ($db->idExists($id)) {
   die "$id: not found\n";
 }
 
-die( "$id is already alive!\n") if $db->idLive($id);
+#die( "$id is already alive!\n") if $db->idLive($id);
 
 if ($db->idResurrect($id)) {
 	print "$id resurrected\n";
 }
+print OUT "Gene : \"$id\"\n";
+print OUT "Version $ver\n";
+print OUT "Live\n";
+
+print OUT "Sequence_name $seq_name\n";
+if (defined $$WBUSERS{$USER}) {
+print OUT "History Version_change $ver now WBPerson". $$WBUSERS{$USER}." Event Resurrected\n";
+}
+else {
+print OUT "History Version_change $ver now WBPerson1983 Event Resurrected\n";
+}
+
+if ($species eq "elegans") {
+  print OUT "Positive_clone $clone Inferred_automatically \"From sequence, transcript, pseudogene data\"\n";
+}
+print OUT "Remark \"[$rundate ${USER}] Gene Resurrected via id_ressurect.pl Additional comments:\"\n";
+print OUT "Method Gene";
+
+if ($load) {
+  print "Output file created and loaded into $acedb : $output\n";
+  &loadace("$output", "id_ressurect", $acedb) or die "Failed to load $output\n";
+  #&loadace("$output", "id_ressurect", "/nfs/wormpub/DATABASES/TEST_DATABASES/NewModels") or die "Failed to load $output\n";
+}
+else {
+print "Output file created : $output\nYou need to manuall load this into geneace\n\n";
+}
 
 exit(0);
 
+
+
+sub loadace {
+  my $filepath = shift;
+  my $tsuser   = shift;
+  my $ldb = shift;  
+
+  my $command = "pparse $filepath\nsave\nquit\n";
+
+  open (TACE, "| $tace $ldb -tsuser $tsuser") || die "Couldn't open $ldb\n";
+  print TACE $command;
+  close TACE;
+  print ("SUCCESS! Loaded $filepath into ${ldb}\n\n");
+  return 1;
+}
