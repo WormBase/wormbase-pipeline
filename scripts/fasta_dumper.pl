992261f2720e60998759235b0bc289529b6dd34f
khowe <kevin.howe@wormbase.org>
2014-06-17 15:27:47 +0000

    Generalised so that you can dump multiple classes/methods into a single file

--- scripts/fasta_dumper.pl
+++ scripts/fasta_dumper.pl
@@ -1,15 +1,13 @@
 #!/software/bin/perl -w
 #
 # fasta_dumper.pl
-# 
-# by Paul Davis
 #
 # Script to dump a fasta file for a specified species/class/method
 # Option to add WBGeneIDs to the header of the file.
 # >2L52.1 gene=WBGene00007063
 #
 # Last updated by: $Author: klh $
-# Last updated on: $Date: 2013-12-02 15:51:12 $
+# Last updated on: $Date: 2014-06-17 15:27:47 $
 use strict;
 use lib $ENV{'CVS_DIR'};
 use Getopt::Long;
@@ -17,21 +15,20 @@ use Wormbase;
 use File::Path;
 use Storable;
 
-my ($debug, $store, $verbose, $database, $test, $wormbase, $species, $class, $method, $out_file, $target_dir, $geneid, $process, $version);
+use Bio::PrimarySeq;
+use Bio::SeqIO;
 
-GetOptions ( "debug:s"      => \$debug,      #email a specified user only, so you don't flood all your colleagues with test run spam.
-	     "verbose"      => \$verbose,    #verbose quces a little more info to screen
-	     "class:s"      => \$class,      #specify an acedb class
-	     "method:s"     => \$method,     #specify an acedb method
-	     "database:s"   => \$database,   #specify a database to query
-	     "output:s"     => \$out_file,   #specify an output file name
-	     "outdir:s"     => \$target_dir, #specify an output location or else the file will be written to BUILD/species/SEQUENCES/
-	     "test"         => \$test,       #invoke test env
-	     "store:s"      => \$store,      #supply a storable
-	     "species:s"    => \$species,    #needed to work out what species is being processed
-	     "gene"         => \$geneid,     #adds the WBGeneID to the header line (retrieved via a wormpep mapping)
-	     "processonly"  => \$process,    #process the specified output file without all the delay of dumping
-	     "version:s"    => \$version,    #adds flexibility 
+my ($debug, $store, $verbose, $database, $test, $wormbase, $species, 
+    @classmethodlabel, $out, @seqs);
+
+GetOptions ( "debug:s"               => \$debug, 
+	     "verbose"               => \$verbose,           #verbose quces a little more info to screen
+	     "classmethodlabel=s@"   => \@classmethodlabel,  #specify an acedb class
+	     "database:s"            => \$database,   #specify a database to query
+	     "output:s"              => \$out,        #specify an output file 
+	     "test"                  => \$test,       #invoke test env
+	     "store:s"               => \$store,      #supply a storable
+	     "species:s"             => \$species,    #needed to work out what species is being processed
 	   ) ;
 
 
@@ -48,183 +45,67 @@ if ( $store ) {
 my $log = Log_files->make_build_log($wormbase);
 
 # Define database to be queried
-my $db;
-if (not defined $database or $database eq "autoace") {
-  $db = $wormbase->autoace;
-}else{$db = $database}
+$database = $wormbase->autoace if not defined $database;
+$out = $wormbase->sequences . "/coding_transcripts.dna" if not defined $out;
 
-my $runtime = $wormbase->runtime;
-my $tace = $wormbase->tace;
-my (%gene_ids);
+my %gene_ids = $wormbase->FetchData('worm_gene2geneID_name');
 
-unless (defined $class) {$log->write_to("!!!!Warning - No class specified so defaulting to Transcript\n");
-			 $class = "Transcript";}
-unless (defined $method) {$log->write_to("!!!!Warning - No method specified so defaulting to Coding_transcript\n");
-			  $method = "Coding_transcript";}
-
-unless (defined $target_dir) {$target_dir = $wormbase->sequences;}
-unless (defined $out_file){$out_file     = "coding_transcripts.dna";}
-if ($geneid) {
-  unless (defined $version) {$log->write_to("The current build wormpep will be used for WBGene data\n")}
+if (not @classmethodlabel) {
+  $log->write_to("!!!!Warning - No class:method specified so defaulting to Transcript:Coding_transcript\n");
+  push @classmethodlabel,  "Transcript:Coding_transcript";
 }
-my $out = "$target_dir/$out_file";
-my $target= "${out}.mod";
-my $gspecies = $wormbase->full_name('-g_species' => 1);
+
 my $full_name = $wormbase->full_name;
 
-&fetch_gene_ids if $geneid;
-my $cds_regex = $wormbase->cds_regex_noend;
+my $connection = Ace->connect(-path => $database) || die (Ace->error);
+foreach my $classmethodlabel (@classmethodlabel) {
+  my ($class, $method, $label) = split(/:/, $classmethodlabel);
 
-############
-# Main body
-############
-
-
-if ($process) {
-  $log->write_to("\n$runtime: Only processing $out as -processonly option used on command line\n\n");
-}
-
-unless ($process) {
-  $log->write_to("\n$runtime: Started making $class($method) fasta file:\n$out\n");
-  if (-e "$out") {
-    $log->write_to("\nOld $out exists in this location, removing.......\n");
-    $wormbase->run_command("rm -f $out", $log);
+  my $query = "Find $class WHERE Species=\"${full_name}\""; 
+  if (defined $method) {
+    $query .= " AND Method=\"$method\"";
   }
-  open FAOUT,">$out" ||die($!);
 
-  my $object;
-  my $connection = Ace->connect(-path => "$db") || die (Ace->error);
-  my $object_it = $connection->fetch_many(-query => "Find $class; Species=\"${full_name}\"; Method=\"$method\";");
-  while(my $object=$object_it->next){
-    my $dna =$object->asDNA();
-    my @lines = split(/\n/, "$dna");
-    @lines = grep { /\S/ } @lines;
-    map { print FAOUT "$_\n" } @lines;
-  }
-  close FAOUT;
-}
-
-if ($geneid){
-
-  if (-e $out) {
-    open(out_fh, "<$out") || die "Failed to open $out\n" ;
-    open(target_fh, ">$target") || die "Failed to open $target\n";
-    while(<out_fh>) {
-      chomp;
-      s/>//;
-      if (/($cds_regex)/) {
-	if (exists $gene_ids{$1}) {
-	  print target_fh ">$_ gene=$gene_ids{$1}\n";
-	} else {
-	  print target_fh ">$_\n";
-	}
-      } elsif (/(\S+)/) {
-	print target_fh "$_\n";
-      }
+  my $object_it = $connection->fetch_many(-query => $query);
+  while(my $object = $object_it->next){
+    my $dna = $object->asDNA();
+    my @dna = split(/\n/, $dna);
+    shift @dna;
+    $dna = join("", @dna);
+    chomp($dna);
+    
+    my $seq = Bio::PrimarySeq->new(-seq => uc($dna),
+                                   -id  => $object->name);
+    my @desc;
+    if (defined $label) {
+      push @desc, "type=$label";
     }
-    close(target_fh) or $log->log_and_die("Could not successfully close target_fh\n");
-    close(out_fh)or $log->log_and_die("Could not successfully close out_fh\n");
+    if (exists $gene_ids{$object->name}) {
+      push @desc, sprintf("gene=%s", $gene_ids{$object->name})
+    }
+    if (@desc) {
+      $seq->description(join(" ", @desc));
+    }
+
+    push @seqs, $seq; 
   }
-  else {
-    $log->error("ERROR: Could not find transcript file for $gspecies ($out)\n");
-    die;
-  }
-  $wormbase->run_command("mv $out ${out}_bk", $log);
-  $log->write_to("\nMoving $out -> ${out}_bk\n");
-  $wormbase->run_command("mv ${out}.mod $out", $log);
-  $log->write_to("\nMoving ${out}.mod -> $out\n");
 }
 
-
-if (-e "${out}.gz") {
-  $log->write_to("\nOld zipped data exists ${out}.gz exists in this location, removing.......\n");
-  $wormbase->run_command("rm -f ${out}.gz", $log);
+if (@seqs) {
+  my $seqio = Bio::SeqIO->new(-format => 'fasta',
+                              -file => ">$out");
+  foreach my $seq (sort { $a->id cmp $b->id } @seqs) {
+    $seqio->write_seq($seq);
+  }
+  $seqio->close();
+  $log->write_to("Wrote " . scalar(@seqs) . " seqs for @classmethodlabel\n");
+} else {
+  $log->write_to("No seqs for @classmethodlabel\n");
 }
 
-
-$wormbase->run_command("gzip -9 -f $out", $log);
-$runtime = $wormbase->runtime;
-$log->write_to("$runtime: Finished making $class($method) fasta file\n\nFile location:$out\n\n");
+$connection->close();
 
 $log->mail();
 exit(0);
 
-sub fetch_gene_ids {
-  my $source_pepfile = $wormbase->common_data."/cds2wbgene_id.dat";
-  if (-e $source_pepfile) {
-    open(my $source_pep_fh, $source_pepfile);
-    while(<$source_pep_fh>) {
-      /^\s+\'(\S+)\'\s+\S+\s+\'(\S+)\'\,?/ and do {        
-	$gene_ids{$1} = $2;
-      };
-    }
-    close($source_pep_fh);
-  }
-  else {
-    $log->log_and_die("Failed to open $source_pepfile\n");
-  }
-}
-
 __END__
-
-=pod
-
-=head2 NAME - fasta_dumper.pl
-
-=head3 USAGE
-
-=head2 DESCRIPTION
-
-This script "does exactly what it says on the tin". ie it dumps fasta sequences for a given method class combination.
-
-=head2 OPTIONS:
-
-=over 4
-
-B<-verbose:> 
-
-Terminal output
-
-B<-databases:> 
-
-Either use autoace if used in build process or give the full database path. Basically retrieves paired read info for ESTs from that database.
-
-B<-gene:> 
-
-This option relies upon wormpep to get a mapping between CDS ID and WBGene ID.
-
-B<-processonly:> 
-
-This will take the output file and add the "gene=WBGene00012345" to the headers.
-
-B<-class:> 
-
-Specify an acedb class for data to be dumped
-
-B<-method:> 
-
-Narrows the keyset based on method
-
-B<-output:> 
-
-Specify a filename for the results to be saved in
-
-B<-debug:> 
-
-Sends the script log to the specifies user.
-
-B<-species:> 
-
-this option is necessary to establish the source of information
-
-=back
-
-=head1 AUTHOR
-
-=over 4
-
-=item Paul Davis (paul.davis@ebi.ac.uk)
-
-=back
-
-=cut
