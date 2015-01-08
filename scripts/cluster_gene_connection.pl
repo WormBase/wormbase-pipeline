32d5d7b6f4dc82756adf994810cfe4ebb5a6a381
khowe <kevin.howe@wormbase.org>
2015-01-08 14:08:14 +0000

    Extended to fo the same for Expr_pattern as well as Expression_cluster

--- scripts/cluster_gene_connection.pl
+++ scripts/cluster_gene_connection.pl
@@ -4,7 +4,8 @@
 
 =head2 NAME - cluster_gene_connection.pl
 
-This script makes connections between microarray expression clusters and Genes (original by Igor). Requires Expression_cluster data is in database
+This script makes connections between microarray expression clusters/patterns and Genes.
+Requires Expression_cluster and Expr_pattern data is in database
 
 
 =cut 
@@ -17,11 +18,14 @@ use Getopt::Long;
 use Storable;
 use Log_files;
 
-my ($debug, $store, $test,$no_load);
-GetOptions ( "debug:s" => \$debug,
-	     "test"    => \$test,
-	     "store:s" => \$store,
-             "noload"  => \$no_load,
+my ($debug, $store, $test, $no_load, $database, $acefile);
+
+GetOptions ( "debug:s"    => \$debug,
+	     "test"       => \$test,
+	     "store:s"    => \$store,
+             "noload"     => \$no_load,
+             "database=s" => \$database,
+             "acefile=s"  => \$acefile,
 	   );
 
 my $wormbase;
@@ -34,7 +38,7 @@ else {
 			   );
 }
 my $log = Log_files->make_build_log($wormbase);
-my $database = $wormbase->autoace;
+$database = $wormbase->autoace if not defined $database;
 
 $log->write_to("Connecting to database...$database\n");
 
@@ -42,74 +46,72 @@ my $db = Ace->connect(-path => $database) or  $log->log_and_die("Connection fail
 
 $log->write_to("done\n");
 
-my $acefile = $wormbase->acefiles."/cluster_gene.ace";
-open OUT, ">$acefile" or $log->log_and_die("cannot open $acefile : $!\n");
+$acefile = $wormbase->acefiles."/cluster_gene.ace"  if not defined $acefile;
+open(my $outfh, ">$acefile") or $log->log_and_die("cannot open $acefile : $!\n");
 
-my $query="find expression_cluster WHERE Microarray_results";
+foreach my $class ("Expression_cluster", "Expr_pattern") {
 
-my @clusters=$db->find($query);
+  my $query="find $class WHERE Microarray_results";
 
-$log->write_to("Processing " .scalar @clusters. " objects with Microarray_results...\n");
-if(scalar @clusters < 10 ) {
-  $log->log_and_die("There are less than 10 clusters in the database which is probably not right!");
-}
+  my @objs = $db->find($query);
 
-my $i=0;
-foreach my $cluster (@clusters) {
-  print OUT "\nExpression_cluster : \"$cluster\"\n";
-  print OUT "-D Gene\n";
-  print OUT "\nExpression_cluster : \"$cluster\"\n";
+  $log->write_to("Processing " . scalar(@objs) . " $class objects with Microarray_results...\n");
 
-  my @mrs=$cluster->Microarray_results(1);
-  foreach my $mr (@mrs) {
-    my @genes=$mr->Gene;
-    foreach my $gene (@genes) {
-      print OUT "Gene\t\"$gene\"\n";
+  if(scalar @objs < 10 ) {
+    $log->log_and_die("There are less than 10 $class objects with Microarray_results in the database which is probably not right!");
+  }
+
+  foreach my $obj (@objs) {
+    
+    print $outfh "\n$class : \"$obj\"\n";
+    print $outfh "-D Gene\n";
+    print $outfh "\n$class : \"$obj\"\n";
+    
+    my @mrs = $obj->Microarray_results(1);
+    foreach my $mr (@mrs) {
+      my @genes = $mr->Gene;
+      foreach my $gene (@genes) {
+        print $outfh "Gene\t\"$gene\"\n";
+      }
     }
   }
-}
-$log->write_to("Processed objects with Microarray_results\n");
+  
+  #
+  # For Expression_clusters without Microarray_results, many will have
+  # direct Gene connections from citace. We want to mostly retain these, 
+  # but remove the Dead genes
+  #
+  $query = "FIND $class WHERE Gene AND NOT Microarray_results";
+  @objs = $db->find($query);
 
-#
-# For Expression_clusters without Microarray_results, many will have
-# direct Gene connections from citace. We want to mostly retain these, 
-# but remove the Dead genes
-#
-$query = "FIND Expression_cluster WHERE Gene AND NOT Microarray_results";
-@clusters = $db->find($query);
+  $log->write_to("Processing " . scalar(@objs) . " $class objects without Microarray_results but with direct gene connections...\n");
+  foreach my $obj (@objs) {
+    my (@reject_genes);
 
-$log->write_to("Processing " .scalar @clusters. " objects without Microarray_results but with direct gene connections...\n");
-foreach my $cluster (@clusters) {
-  my (@keep_genes, @reject_genes);
+    my @genes = $obj->Gene;
+    foreach my $g (@genes) {
+      if ($g->Status ne 'Live') {
+        push @reject_genes, $g;
+      } 
+    }
 
-  my @genes = $cluster->Gene;
-  foreach my $g (@genes) {
-    if ($g->Status eq 'Live') {
-      push @keep_genes, $g;
-    } else {
-      push @reject_genes,$g;
+    foreach my $rg (@reject_genes) {
+      print $outfh "\n$class : \"$obj\"\n";
+      print $outfh "-D Gene \"$rg\"\n";
+    }
+
+    if (@reject_genes) {
+      $log->write_to("Removed from $obj because no longer live: @reject_genes\n");
     }
   }
 
-  print OUT "\nExpression_cluster : \"$cluster\"\n";
-  print OUT "-D Gene\n";
-  print OUT "\nExpression_cluster : \"$cluster\"\n";
-  foreach my $g (@keep_genes) {
-    print OUT "Gene \"$g\"\n";
-  }
-
-  if (@reject_genes) {
-    $log->write_to("Removed from $cluster because no longer live: @reject_genes\n");
-  }
+  $log->write_to("Processed objects $class objects without Microarray_results but direct Gene connections\n");
 }
-close(OUT);
+
+close($outfh) or $log->log_and_die("Could not cleanly close output file\n");
 $db->close;
 
-$log->write_to("Processed objects without Microarray_results but direct Gene connections\n");
-
 $wormbase->load_to_database($database, $acefile, 'cluster_genes', $log) unless $no_load;
-
-
 
 $log->mail;
 exit(0);
