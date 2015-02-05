dc6212a6495d721ce3c12b64f822bfce0831d217
khowe <kevin.howe@wormbase.org>
2015-02-05 14:55:44 +0000

    Updated for the new (e78) compara API calls. This script may be deprecated soon, but no harm in keeping it up-to-date for now

--- scripts/ENSEMBL/scripts/update_ensembl_orthologs.pl
+++ scripts/ENSEMBL/scripts/update_ensembl_orthologs.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl -w
+#!/usr/bin/env perl
 #===============================================================================
 #   updates ortholog connections from the main Compara database
 #
@@ -8,42 +8,27 @@
 #     REVISION:  ---
 #===============================================================================
 
-use lib '/software/worm/ensembl/bioperl-live';
 use lib $ENV{CVS_DIR};
 
 use strict;
-use IO::File;
 use Getopt::Long;
 use Storable;
 use Wormbase;
-
-
-
-#use lib '/software/worm/ensembl-64/ensembl/modules';
-#use lib '/software/worm/ensembl-64/ensembl-compara/modules';
 
 use Bio::EnsEMBL::Registry;
 use Bio::EnsEMBL::DBSQL::DBAdaptor;
 use Bio::EnsEMBL::Compara::DBSQL::DBAdaptor;
 
-
-my %speciesOfInterest = (
-  'saccharomyces_cerevisiae'  => 'yeast',
-  'drosophila_melanogaster' => 'fly',
-  'homo_sapiens'            => 'human',
-  'mus_musculus'            => 'mouse',
-);
-
-
-my ($wormbase, $debug, $test, $verbose, $store, $species, $use_current_db, %cds2wbgene, %current_genes);
+my ($wormbase, $debug, $test, $verbose, $store, $species, $use_current_db, $compara, %cds2wbgene, %current_genes);
 
 GetOptions (
-  "debug=s"    => \$debug,
-  "test"       => \$test,
-  "verbose"    => \$verbose,
-  "store:s"    => \$store,
-  "species:s"  => \$species,
+  "debug=s"      => \$debug,
+  "test"         => \$test,
+  "verbose"      => \$verbose,
+  "store:s"      => \$store,
+  "species:s"    => \$species,
   "usecurrentdb" => \$use_current_db,
+  "compara=s"    => \$compara,
             );
 
 if ( $store ) {
@@ -56,16 +41,20 @@ if ( $store ) {
 }
 
 my $species_name = $wormbase->full_name;
+$compara = "multi" if not defined $compara;
 
-Bio::EnsEMBL::Registry->load_registry_from_db(-host => 'ens-livemirror', 
-                                              -user => 'wormro',
-                                              -port => 3306,
-                                              -verbose => 0);
+Bio::EnsEMBL::Registry->load_registry_from_db(-host => 'ensembldb.ensembl.org', 
+                                              -user => 'anonymous',
+                                              -port => 5306,
+                                              -verbose => 0,
+    );
 
+my $gene_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species_name,'core','Gene');
+my $member_adaptor   = Bio::EnsEMBL::Registry->get_adaptor($compara,'compara','GeneMember');
+my $homology_adaptor = Bio::EnsEMBL::Registry->get_adaptor($compara,'compara','Homology');
 
-my $slice_adaptor = Bio::EnsEMBL::Registry->get_adaptor($species_name,'core','Slice');
-my $member_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Member');
-my $homology_adaptor = Bio::EnsEMBL::Registry->get_adaptor('Multi','compara','Homology');;
+die "Could not fetch compara homology adaptor('$compara')" if not defined $homology_adaptor;
+die "Could not fetch compara member adaptor('$compara')" if not defined $member_adaptor;
 
 
 if ($use_current_db) {
@@ -75,52 +64,43 @@ if ($use_current_db) {
 }
 map { $current_genes{$_} = 1 } values %cds2wbgene;
 
+my $genes = $gene_adaptor->fetch_all_by_biotype('protein_coding');
+foreach my $other_species ('Homo sapiens', 
+                           'Mus musculus', 
+                           'Drosophila melanogaster', 
+                           'Saccharomyces cerevisiae') {
+  my %homols_for_species;
 
-my @slices = @{$slice_adaptor->fetch_all('toplevel')};
-
-
-foreach my $slice(@slices){
-  my $genes = $slice->get_all_Genes;
-  while (my $gene = shift @{$genes}){
-    my ($db_entry) = @{$gene->get_all_DBLinks('wormbase_gene')};
-    my $wb_gene_id = $db_entry->primary_id;
+  foreach my $gene (@$genes) {
+    my $wb_gene_id = $gene->stable_id;
 
     next if not exists $current_genes{$wb_gene_id};
     
-    my $member = $member_adaptor->fetch_by_source_stable_id( 'ENSEMBLGENE',$gene->stable_id());
+    my $member = $member_adaptor->fetch_by_stable_id( $gene->stable_id());
     next unless $member;
-    my $homologies = $homology_adaptor->fetch_all_by_Member_method_link_type( $member, 'ENSEMBL_ORTHOLOGUES' );
+    my $homologies = $homology_adaptor->fetch_all_by_Member_paired_species($member, $other_species, ['ENSEMBL_ORTHOLOGUES']);
     
-    my %homol_ids;
-    my %omims;
-    my %omim_genes;
+    my %homols_for_gene;
     
     foreach my $homology ( @{$homologies} ) {
-      foreach my $ma ( @{ $homology->get_all_Member_Attribute } ) {
-        my ( $me, $at ) = @{$ma};
-        if (exists $speciesOfInterest{$me->genome_db->name}){
-          my $pepm = $me->get_canonical_Member(); 
-          $homol_ids{ $pepm->stable_id } = $pepm; #$homology->description,$homology->subtype];
-        }
+      foreach my $me (grep { $_->stable_id ne $wb_gene_id } @{$homology->get_all_GeneMembers}) {
+        $homols_for_gene{$me->stable_id} = 1;
+        $homols_for_species{$me->stable_id} = 1;
       }
     }
     
-    next unless %homol_ids;
+    next unless %homols_for_gene;
         
-    print "Gene : \"$wb_gene_id\"\n";
-    while (my ($k,$v)=each(%homol_ids)){
-      print "Ortholog_other ENSEMBL:$k  From_analysis EnsEMBL-Compara\n";
+    print "\nGene : \"$wb_gene_id\"\n";
+    while (my ($k,$v)=each(%homols_for_gene)){
+      printf "Ortholog \"%s\" \"%s\" From_analysis EnsEMBL-Compara\n", $k, $other_species;
     }
-    print "\n";
+  }
 
-    while (my ($k,$v)=each(%homol_ids)){
-      # put the OMIM orthologs into another hash based on the MIM_GENE xref
-      print "Protein : ENSEMBL:$k\n";
-      print "Species \"${\$v->taxon->name}\"\n";
-      print "DB_info Database EnsEMBL ENSEMBL_proteinID $k\n";
-      print "\n";
-    }
-
+  while (my ($k,$v)=each(%homols_for_species)){
+    print "\nGene : \"$k\"\n";
+    print "Species \"$other_species\"\n";
+    print "DB_info Database EnsEMBL ENSEMBL_geneID $k\n";
   }	
 }
 
