79dd6c05253e26172a8b644a15c005cdbded71e3
epaule <michael.paulini@wormbase.org>
2015-02-09 16:16:00 +0000

    added support for CGC like naming and extended the evidences

--- scripts/ENSEMBL/scripts/get_ensembl_xref.pl
+++ scripts/ENSEMBL/scripts/get_ensembl_xref.pl
@@ -33,54 +33,78 @@ sub dump_ace_by_production_name{
   while( my $gene = shift @genes) {
 
     my $publicName = $gene->display_xref ? $gene->display_xref->display_id : $gene->stable_id;
+    $publicName =~s/;//;
     my $geneName = $gene->stable_id;
 
-    print "Gene : \"$geneName\"\nPublic_name \"$publicName\"\nSpecies \"$species\"\n";
-    print "Database EnsEMBL ENSEMBL_geneID \"$geneName\"\n";
+    print "Gene : \"$geneName\"\nPublic_name \"$publicName\"\n";
+  
+    if ($gene->display_xref && print_evidence($gene->display_xref)){print "CGC_name \"$publicName\" ",print_evidence($gene->display_xref),"\n"}
+
+    print "Species \"$species\"\nDatabase EnsEMBL ENSEMBL_geneID \"$geneName\"\n";
 
     my @xrefs =  (@{ $gene->get_all_xrefs},$gene->display_xref); # because some of the displayxrefs are not in the objext xref table :-(
-    my %skip; # skip othernames that come from the same xref
-    $skip{$gene->display_xref->dbID}=1 if $gene->display_xref;
     my %seen; # like wsb2
-    while (my $x = shift @xrefs){
-      next if $seen{$x->dbID};
-      $seen{$x->dbID}=1;
-      given($x->dbname){
-         when(/^Uniprot\//){dbxrefP('UniProt','UniProt_AC',$x)}
-         when('ZFIN_ID'){dbxrefP('ZFIN','primary_acc',$x)}
-         when('SGD_GENE'){dbxrefS('SGD','SGD_acc',$x)}
-         when('MGI'){dbxrefS('MGI','MGI_acc',$x)         }
-         when('FlybaseCGID_gene'){dbxrefS('FLYBASE','FlyBase_gn',$x)}
-         when('flybase_gene_id'){dbxrefS('FLYBASE','FlyBase_ID',$x)}
-         when('HGNC'){dbxrefS('HGNC','symbol',$x)}
-         when('MIM_GENE'){dbxrefP('OMIM','gene',$x)}
-         when('MIM_DISEASE'){dbxrefP('OMIM','disease',$x)}
-         when('Uniprot_gn'){
-           next if $skip{$x->dbID};
-           my $oname = $x->display_id;
-           $oname=~s/;//; # strip semicolons from the IDs
-           printf "Other_name \"%s\" Inferred_automatically \"uniprot_xrefs_from_ensembl\"\n",$oname
-         }
-         default{}
-      }
+    while (my $xref = shift @xrefs){
+      next if $seen{$xref->dbID};
+      my $dbline = &print_xref($xref,$publicName);
+      print "$dbline\n" if $dbline;
+      $seen{$xref->dbID}=1;
     }
     print "\n";
   }	
 }
+sub print_evidence{
+      my($x)=@_;
+      if($x->dbname =~/^Uniprot\//){return 'Accession_evidence "UniProt" "'.$x->primary_id.'"'}
+      elsif($x->dbname eq 'ZFIN_ID'){return 'Accession_evidence "ZFIN" "'.$x->primary_id.'"'}
+      elsif($x->dbname eq 'SGD_GENE'){return 'Accession_evidence "SGD" "'.$x->display_id.'"'}
+      elsif($x->dbname eq 'MGI'){return'Accession_evidence "MGI" "'.$x->display_id.'"'}
+      elsif($x->dbname eq 'FlyBaseCGID_gene'){return 'Accession_evidence "FLYBASE" "'.$x->display_id.'"'}
+      elsif($x->dbname eq 'flybase_gene_id'){return 'Accession_evidence "FLYBASE" "'.$x->display_id.'"'}
+      elsif($x->dbname eq 'HGNC'){return 'Accession_evidence "HGNC" "'.$x->display_id.'"'}
+      elsif($x->dbname eq 'MIM_GENE'){return 'Accession_evidence "OMIM" "'.$x->primary_id.'"'}
+      elsif($x->dbname eq 'MIM_MORBID'){return 'Accession_evidence "OMIM" "'.$x->primary_id.'"'}
+      else{
+          my $tmp=$x->dbname;
+          $tmp=~s/;//;
+          return 'Inferred_automatically "'.$x->dbname.'"'
+      }
+}
+
+
+sub print_xref{
+      my($x,$p)=@_;
+      my $xs;
+      if($x->dbname =~/^Uniprot\//){$xs = dbxrefP('UniProt','UniProt_AC',$x)}
+      elsif($x->dbname eq 'ZFIN_ID'){$xs = dbxrefP('ZFIN','primary_acc',$x)}
+      elsif($x->dbname eq 'SGD_GENE'){$xs = dbxrefS('SGD','SGD_acc',$x)}
+      elsif($x->dbname eq 'MGI'){$xs = dbxrefS('MGI','MGI_acc',$x)         }
+      elsif($x->dbname eq 'FlyBaseCGID_gene'){$xs = dbxrefS('FLYBASE','FlyBase_gn',$x)}
+      elsif($x->dbname eq 'flybase_gene_id'){$xs = dbxrefS('FLYBASE','FlyBase_ID',$x)}
+      elsif($x->dbname eq 'HGNC'){$xs = dbxrefS('HGNC','symbol',$x)}
+      elsif($x->dbname eq 'MIM_GENE'){$xs = dbxrefP('OMIM','gene',$x)}
+      elsif($x->dbname eq 'MIM_MORBID'){$xs = dbxrefP('OMIM','disease',$x)}
+      elsif($x->dbname eq 'Uniprot_gn'){
+           my $oname = $x->display_id;
+           $oname=~s/;//; # strip semicolons from the IDs
+           if($p && lc($p) ne lc($oname)){
+            $xs = sprintf "Other_name \"$oname\" Accession_evidence \"UniProt\" \"$oname\"" if $p;
+           }
+      }
+      return $xs;
+}
 
 #  display_id xref printing
 sub dbxrefS{
-  my ($db,$ac,@xrefs)=@_;
-  map {  my $p = $_->info_type eq 'PROJECTION'?'projected_':'';
-         printf "Database $db $p$ac \"%s\"\n",$_->display_id 
-   } @xrefs;
+  my ($db,$ac,$xref)=@_;
+  my $p = $xref->info_type eq 'PROJECTION'?'projected_':'';
+  return sprintf "Database \"$db\" \"$p$ac\" \"%s\"",$xref->display_id 
 }
 
 # primary_id xref printing
 sub dbxrefP{
-  my ($db,$ac,@xrefs)=@_;
-  map {  my $p = $_->info_type eq 'PROJECTION'?'projected_':'';
-         printf "Database $db $p$ac \"%s\"\n",$_->primary_id
-  } @xrefs;
+  my ($db,$ac,$xref)=@_;
+  my $p = $xref->info_type eq 'PROJECTION'?'projected_':'';
+  return sprintf "Database \"$db\" \"$p$ac\" \"%s\"",$xref->primary_id;
 }
 
