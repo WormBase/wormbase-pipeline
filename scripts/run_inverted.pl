0e4f75a84127b584a915b7c0cf5d3e269938780d
khowe <kevin.howe@wormbase.org>
2015-03-17 14:43:37 +0000

    Cleanup, and made more efficient for fragmented assemblies

--- scripts/run_inverted.pl
+++ scripts/run_inverted.pl
@@ -1,10 +1,8 @@
-#!/usr/local/bin/perl5.8.0
+#!/usr/bin/env perl
 #
 # run_inverted.pl
 #
-# dl 
-#
-# Usage : run_inverted.pk [-options] <sequence_file>
+# Usage : run_inverted.pl [-options] <sequence_file>
 
 #################################################################################
 # Initialise variables                                                          #
@@ -17,38 +15,26 @@ use Getopt::Long;
 use Storable;
 use Log_files;
 
+use Bio::PrimarySeq;
+use Bio::SeqIO;
+
 ##############################
 # command-line options       #
 ##############################
                                                                                                                                      
-my $debug;              # debug mode
-my $test;
-my $store;
-my $help;               # Help/Usage page
-my $sequence;           # Sequence file handle
-my $all;                # Compute for all clones
-my $species;
-my $einverted;
+my ($debug, $test, $store, $einverted, $sequence, $all, $species, $wormbase, $acefile); 
 
 GetOptions (
-	    "all"         => \$all,
-	    "sequence:s"  => \$sequence,
-            "debug:s"     => \$debug,
-            "help"        => \$help,
-	    "test"        => \$test,
-	    "store:s"     => \$store,
-	    "species:s"   => \$species,
-            "einverted:s" => \$einverted,
+  "debug:s"     => \$debug,
+  "test"        => \$test,
+  "store:s"     => \$store,
+  "species:s"   => \$species,
+  "einverted:s" => \$einverted,
+  "all"         => \$all,
+  "sequence:s"  => \$sequence,
+  "acefile:s"   => \$acefile,
 	    );
 
-$einverted = "einverted" if not defined $einverted;
-# Help menu
-
-if ($help) {
-    exec ('perldoc',$0);
-}
-
-my $wormbase;
 if( $store ) {
   $wormbase = retrieve( $store ) or croak("cant restore wormbase from $store\n");
 }
@@ -60,195 +46,130 @@ else {
 }
 
 
-# make log files
+$einverted = "einverted" if not defined $einverted;
+$acefile = $wormbase->acefiles."/inverted_repeats.ace" if not defined $acefile;
 my $log = Log_files->make_build_log($wormbase);
 
-# Crash out if no sequence filename supplied
-unless ($all||$sequence) {
-  $log->log_and_die("You must choose an option, [-all or -sequence <seqname>]. Ciao\n\n");
+my (%clone2seq, @batches, %output);
+
+if (defined $sequence) {
+  push @batches, {
+    size => length($sequence), 
+    seqs => [Bio::PrimarySeq->new(-id => "anonymous", 
+                                  -seq => $sequence)],
+  };
+  $clone2seq{"anonymous"} = $sequence;
+} elsif ($all) {
+  %clone2seq  = $wormbase->FetchData("clone2sequence_${\$wormbase->species}");
+  foreach my $id (sort { length($clone2seq{$b}) <=> length($clone2seq{$a}) } keys %clone2seq) {
+    my $seq = $clone2seq{$id};
+    $seq =~ s/\s//g; 
+    next if not $seq;
+    
+    if (not @batches or $batches[-1]->{size} > 1000000) {
+      push @batches, {
+        size => 0,
+        seqs => [],
+      };
+    }
+    $batches[-1]->{size} += length($seq);
+    push @{$batches[-1]->{seqs}}, Bio::PrimarySeq->new(-id => $id,
+                                                       -seq => $seq);
+  }
+} else {
+  $log->log_and_die("You must supply either -sequence or -all\n");
 }
-
-my %clone2sequence  = $wormbase->FetchData("clone2sequence_${\$wormbase->species}");      # Genomic_canonical => DNA sequence
-my %clonesize       = $wormbase->FetchData('clonesize');           # Genomic_canonical => length in bp
-
-
-if (($sequence) && (!defined $clone2sequence{$sequence})) {
-  $log->log_and_die("Sequence name '$sequence' is not recognised in the COMMON_DATA hash\n\n");
-}
-
-# deal with array of clones to process
-
-my @clones2process = sort keys %clone2sequence;
-
-if ($sequence) {
-    @clones2process = "";
-    push (@clones2process, $sequence);
-}
-
-# open output acefile
-
-my $outputfilename = $wormbase->acefiles."/inverted_repeats.ace";
-
-open (ACE, ">$outputfilename") || die "Failed to open output acefile: '$outputfilename'\n";
 
 # Loop through all clone which need to be dealt with
 
-my ($score, $percent,$gaps);
+my ($clone, $score, $percent,$gaps);
 my ($loop_1_start,$loop_1_stop);
 my ($loop_2_start,$loop_2_stop);
 my ($loop_len);
 my $tag;
-my @output;
 
-foreach my $clone (@clones2process) {
+foreach my $batch (@batches) {
+  $log->write_to(sprintf("Processing batch of %d seqs (total length %d)\n", scalar(@{$batch->{seqs}}), $batch->{size}));
 
-    next if ($clone eq "");
+  my $tmp_file = "/tmp/inverted_temp.$$.fa";
+  my $seqio = Bio::SeqIO->new(-format => 'fasta',
+                              -file => ">$tmp_file");
+  foreach my $seq (@{$batch->{seqs}}) {
+    $seqio->write_seq($seq);
+  }
+  $seqio->close;
 
-    # generate sequence file for the query sequence    
-    open (INPUT, ">/tmp/inverted_temp.$$") || die "Failed to open file for dumping sequence to\n";
-    print INPUT ">$clone\n";
-    print INPUT $clone2sequence{$clone};
-    close INPUT;
+  # inverted output format: 
+  #
+  # Score 52: 20/22 ( 90%) matches, 0 gaps
+  #      3810 ataaaaactcgaattcaaaaaa 3831
+  #                                 
+  #      4164 tatttgtgagcttaattttttt 4143
 
-    ################################
-    # Run inverted on the sequence #
-    ################################
-    
-    # inverted output format: 
-    #
-    # Score 52: 20/22 ( 90%) matches, 0 gaps
-    #      3810 ataaaaactcgaattcaaaaaa 3831
-    #                                 
-    #      4164 tatttgtgagcttaattttttt 4143
-
-    # Final ace output
-    #
-    # Feature Inverted        3810    3831    90      "loop 353"
-
-    open (INV, "$einverted /tmp/inverted_temp.$$ -outfile stdout -gap 12 -threshold 50 -match 3 -mismatch -4 -outseq /dev/null -auto| ")||die(@!);
-    while (<INV>) {
-	
-	chomp;
-	next if ($_ eq "");     # Shortcut empty lines
-	
-	# Main data 
-	if (/^$clone: Score (\d+)\: \S+ \( (\d+)\%\) matches\, (\d+) gaps/) {
-	    ($score, $percent,$gaps) = ($1,$2,$3);
-	    $tag = 1;
-	    next;
-	}
-	elsif (/^$clone: Score (\d+)\: \S+ \(100\%\) matches\, (\d+) gaps/) {
-	    ($score, $percent,$gaps) = ($1,"100",$3);
-	    $tag = 1;
-	    next;
-	}
-	
-	# start loop
-	if (($tag == 1) && (/^\s+(\d+) \S+ (\d+)/)) {
-	    ($loop_1_start,$loop_1_stop) = ($1,$2);
-	    $tag++;
-	    next;
-	} 
-	
-	# end loop
-	if (($tag == 2) && (/^\s+(\d+) \S+ (\d+)/)) {
-	    ($loop_2_start,$loop_2_stop) = ($1,$2);
-	    
-	    $loop_len = $loop_2_start - $loop_1_start - 1;
-	    
-	    # output ace format for both stem structures at the same time
-	    
-	    if ($gaps > 1) {
-		push (@output, "Feature Inverted $loop_1_start $loop_1_stop $percent \"loop $loop_len, $gaps gaps\"\n");
-		push (@output, "Feature Inverted $loop_2_start $loop_2_stop $percent \"loop $loop_len, $gaps gaps\"\n");
-	    }
-	    elsif ($gaps == 1) {
-		push (@output, "Feature Inverted $loop_1_start $loop_1_stop $percent \"loop $loop_len, 1 gap\"\n");
-		push (@output, "Feature Inverted $loop_2_start $loop_2_stop $percent \"loop $loop_len, 1 gap\"\n");
-	    }
-	    elsif ($gaps == 0) {
-		push (@output, "Feature Inverted $loop_1_start $loop_1_stop $percent \"loop $loop_len\"\n");
-		push (@output, "Feature Inverted $loop_2_start $loop_2_stop $percent \"loop $loop_len\"\n");
-	    }
-	    
-	    $tag == 0;
-	}
-	
+  # Final ace output
+  #
+  # Feature Inverted        3810    3831    90      "loop 353"
+  
+  open (my $einv_fh, "$einverted $tmp_file -outfile stdout -gap 12 -threshold 50 -match 3 -mismatch -4 -outseq /dev/null -auto| ")
+      or $log->log_and_die("Could not initiate einverted command\n");
+  while (<$einv_fh>) {
+    # Main data 
+    if  (/^(\S+): Score (\d+)\: \S+ \(\s*(\d+)\%\) matches\, (\d+) gaps/) {
+      ($clone, $score, $percent,$gaps) = ($1, $2, $3, $4);
+      $tag = 1;
+      next;
     }
-    close INV;
-
-    # tidy up and remove temp file
-    system ("rm -f /tmp/inverted_temp.$$");
-
-    if (scalar @output < 1) {
-	next;
-    }
-
-    print ACE "Sequence : \"$clone\"\n";
-    print ACE "Feature_data \"${clone}:inverted\" 1 $clonesize{$clone}\n\n";
+    # start loop
+    if (($tag == 1) && (/^\s+(\d+) \S+ (\d+)/)) {
+      ($loop_1_start,$loop_1_stop) = ($1,$2);
+      $tag++;
+      next;
+    } 
     
-    print ACE "Feature_data : \"${clone}:inverted\"\n";
-    foreach (@output) {
-	next if ($_ eq "");
-	print ACE;
+    # end loop
+    if (($tag == 2) && (/^\s+(\d+) \S+ (\d+)/)) {
+      ($loop_2_start,$loop_2_stop) = ($1,$2);
+      
+      $loop_len = $loop_2_start - $loop_1_start - 1;
+      
+      # output ace format for both stem structures at the same time
+      
+      if ($gaps > 1) {
+        push @{$output{$clone}}, "Feature Inverted $loop_1_start $loop_1_stop $percent \"loop $loop_len, $gaps gaps\"\n";
+        push @{$output{$clone}}, "Feature Inverted $loop_2_start $loop_2_stop $percent \"loop $loop_len, $gaps gaps\"\n";
+      }
+      elsif ($gaps == 1) {
+        push @{$output{$clone}}, "Feature Inverted $loop_1_start $loop_1_stop $percent \"loop $loop_len, 1 gap\"\n";
+        push @{$output{$clone}}, "Feature Inverted $loop_2_start $loop_2_stop $percent \"loop $loop_len, 1 gap\"\n";
+      }
+      elsif ($gaps == 0) {
+        push @{$output{$clone}}, "Feature Inverted $loop_1_start $loop_1_stop $percent \"loop $loop_len\"\n";
+        push @{$output{$clone}}, "Feature Inverted $loop_2_start $loop_2_stop $percent \"loop $loop_len\"\n";
+      }
+      
+      $tag = 0;
     }
     
-    print ACE "\n";
-    
-    @output = "";
+  }
+  close($einv_fh);
+  unlink $tmp_file;
+}
 
-} #_ loop over each clone (@clones2process)
+open(my $outfh, ">$acefile") or $log->log_and_die("Failed to open output acefile: '$acefile'\n");
 
+foreach my $clone (sort keys %output) {  
+  print $outfh "\nSequence : \"$clone\"\n";
+  print $outfh "Feature_data \"${clone}:inverted\" 1 ", length($clone2seq{$clone}), "\n\n";
+                                                               
+  print $outfh "Feature_data \"${clone}:inverted\"\n";
+  foreach my $outline (@{$output{$clone}}) {
+    print $outfh $outline;
+  }
+}
 
-close ACE;
-close STDOUT;
-
+close($outfh);
 $log->mail();
-
-# Hasta luego
 exit (0);
 
 
 __END__
-                                                                                                                                     
-=pod
-                                                                                                                                     
-=head2   NAME - run_inverted.pl
-
-=head1 USAGE
-                                                                                                                                     
-=over 4
-                                                                                                                                     
-=item run_inverted.pl [-options]
-                                                                                                                                     
-=back
-                                                                                                                                     
-run_inverted.pl is a wrapper to run Richard Durbin's inverted script over the
-genome sequences. You can run the whole data set or specify a single sequence
-name. 
-
-All data is taken from the COMMON_DATA hashes hosted on wormsrv2.
-
-Output files are written to /wormsrv2/autoace/acefiles
-
-=back
-
-=head1 run_inverted.pl MANDATORY arguments (one of the two):
-
-=over 4 
-
-=item -all, Processes all genome sequences
-
-=back 
-
-=item -sequence <sequence_name>, Process a single genome sequence
-
-=back
-
-=head1 run_inverted.pl OPTIONAL arguements:
-
-=item -help, these help pages
-
-=back
- 
-=cut
