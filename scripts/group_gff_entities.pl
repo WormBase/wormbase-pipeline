6e2f99b5b58424cb6a99fb0b05c3ab1688a96ab1
gw3 <gary.williams@wormbase.org>
2015-03-25 14:52:47 +0000

    added support for blastx lines

--- scripts/group_gff_entities.pl
+++ scripts/group_gff_entities.pl
@@ -9,7 +9,7 @@
 # grouped with cDNA_match lines to produce the full-length alignment
 #
 # Last updated by: $Author: gw3 $     
-# Last updated on: $Date: 2015-03-25 13:50:18 $      
+# Last updated on: $Date: 2015-03-25 14:52:47 $      
 
 use strict;                                      
 use lib $ENV{'CVS_DIR'};
@@ -27,7 +27,7 @@ use Storable;
 ######################################
 
 my ($help, $debug, $test, $verbose, $store, $wormbase);
-my ($input, $output);
+my ($input, $output, $est, $blastx);
 
 GetOptions ("help"       => \$help,
             "debug=s"    => \$debug,
@@ -36,6 +36,8 @@ GetOptions ("help"       => \$help,
 	    "store:s"    => \$store,
 	    "input:s"    => \$input,
             "output:s"   => \$output,
+	    "est"        => \$est,
+	    "blastx"     => \$blastx
 	    );
 
 $debug = "gw3";
@@ -54,55 +56,137 @@ if ( $store ) {
 # in test mode?
 if ($test) {
   print "In test mode\n" if ($verbose);
+}
 
+my $parenttype;
+if ($est) {
+  $parenttype = 'cDNA_match';
+} else {
+  $parenttype = 'match';
 }
 
 # establish log file.
 my $log = Log_files->make_build_log($wormbase);
 
 my $prev_seq = '';
-my $prev_target_end = 0;
+my $prev_target_start = 0;
 my $prev_start = 0;
 my $prev_end = 0;
 my $prev_score = 0;
 my $prev_sense = '';
-my $prev_id = '';
+my $prev_target = '';
 my $parent = '';
 
+my $seq;
+my $source;
+my $type;
+my $start;
+my $end;
+my $score;
+my $sense;
+my $id;
+my $target; 
+my $target_start=0;
+my $target_end;
+
+my %seen;
+
+my @input;
+my @a;
 open (IN, "<$input") || $log->log_and_die("cant open input file: $input\n");
-open (OUT, ">$output") || $log->log_and_die("cant open output file: $output\n");
 while (my $line = <IN>) {
-  my @a = split /\t/, $line;
+  @a = split /\t/, $line;
   if (!defined $a[8]) {next}
-  my $seq   = $a[0];
-  my $start = $a[3];
-  my $end   = $a[4];
-  my $score = $a[5];
-  my $sense = $a[6];
-  my ($id) = ($a[8] =~ /^ID=(\S+?);/);
-  my ($target_start, $target_end) = ($a[8] =~ /Target=\S+\s+(\d+)\s+(\d+)\s++/);
+  $seq   = $a[0];
+  $source = $a[1];
+  $type  = $a[2];
+  $start = $a[3];
+  $end   = $a[4];
+  $score = $a[5];
+  $sense = $a[6];
+  ($id) = ($a[8] =~ /^ID=(\S+?);/);
+  ($target, $target_start, $target_end) = ($a[8] =~ /Target=(\S+)\s+(\d+)\s+(\d+)\s+/);
+  push @input, [$seq, $source, $type, $start, $end, $score, $sense, $id, $target, $target_start, $target_end];
+}
+close(IN);
+
+# sort by chromosome, sense, Target then position
+my @sorted = sort {
+  ($a->[0] cmp $b->[0]) || # seq
+    ($a->[6] cmp $b->[6]) || # sense
+      ($a->[8] cmp $b->[8]) || # Target
+	($a->[3] <=> $b->[3]) # chromosome start
+}  @input;
+
+
+
+open (OUT, ">$output") || $log->log_and_die("cant open output file: $output\n");
 
 # want to make a fresh parent each time there is a discontinuity in
 # the target_start/target_end
-  if ($id ne $prev_id) {
-    if ($prev_id ne '') {
-      print OUT "$a[0]\t$a[1]\tcDNA_match\t$prev_start\t$prev_end\t$prev_score\t$prev_sense\t.\tID=$parent\n";
+foreach my $a (@sorted) {
+  $seq   = $a->[0];
+  $source = $a->[1];
+  $type  = $a->[2];
+  $start = $a->[3];
+
+  $prev_end = $end;
+
+  $end   = $a->[4];
+  $score = $a->[5];
+  $sense = $a->[6];
+  $id = $a->[7];
+
+  $prev_target_start = $target_start;
+
+  ($target, $target_start, $target_end) = ($a->[8], $a->[9], $a->[10]);
+
+  if (($target ne $prev_target) || 
+      ($sense eq '+' && $target_start < $prev_target_start) || # discontinuity in the target alignment
+      ($sense eq '-' && $target_start > $prev_target_start) || 
+      ($start > $prev_end + 20000) # too large an intron
+     ) {
+
+    if ($prev_target ne '') {
+      print OUT "$seq\t$source\t$parenttype\t$prev_start\t$prev_end\t$prev_score\t$prev_sense\t.\tID=$parent\n";
     }
-    $prev_id = $id;
+    $prev_target = $target;
     $prev_start = $start;
     $prev_end = $end;
     $prev_score = $score;
     $prev_sense = $sense;
-    $parent = "EST:" . $id;    
+
+    # make a unique name for the parent
+    my $parent_number='';
+    if (exists $seen{$target}) {
+      $parent_number = ".$seen{$target}";
+    }
+
+    $seen{$target}++;
+
+    if ($est) {
+      $parent = "EST:" . $target . $parent_number;
+    } elsif ($blastx) {
+      $parent = "BLASTX:" . $target . $parent_number;
+    } else {
+      $log->log_and_die("-est or -blastx should be specified\n");
+    }
   }
   $prev_end = $end;
 
   $a[8] = 'Parent=' . $parent . ';' . $a[8];
-
-  my $new_line = join "\t", @a;
+  
+  my $idstr='';
+  if (defined $id) {$idstr = "ID=${id}"}
+  my $new_line = "$seq\t$source\t$type\t$start\t$end\t$score\t$sense\t.\tParent=${parent};${idstr}Target=$target $target_start $target_end +\n";
   print OUT $new_line;
 }
-close(IN);
+
+# last line
+if ($prev_target ne '') {
+  print OUT "$seq\t$source\t$parenttype\t$prev_start\t$prev_end\t$prev_score\t$prev_sense\t.\tID=$parent\n";
+}
+
 close(OUT);
 
 
