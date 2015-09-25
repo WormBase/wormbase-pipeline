67076f547b4398eb4f33128460d0afd42da55178
Kevin Howe <klh@ebi.ac.uk>
2015-09-25 12:18:09 +0100

    Polished for production

--- scripts/load_uniprot_brief_ids.pl
+++ scripts/load_uniprot_brief_ids.pl
@@ -9,7 +9,7 @@ use lib $ENV{CVS_DIR};
 use Wormbase;
 use Log_files;
 
-my ($debug, $test, $store, $species, $wb, $overwrite, $noload,
+my ($debug, $test, $store, $species, $wb, $noload, $database,
     $pid_table_file, $outace);
 
 
@@ -17,10 +17,10 @@ my ($debug, $test, $store, $species, $wb, $overwrite, $noload,
              "test"           => \$test,
              "store:s"        => \$store,
              "species:s"      => \$species,
+             "database=s"     => \$database,
              "noload"         => \$noload,
              "pidtable=s"     => \$pid_table_file,
              'acefile=s'      => \$outace,
-             'overwrite=s'    => \$overwrite,
     );
 
 
@@ -34,9 +34,10 @@ else {
     -organism => $species, 
       );
 }
-
 my $log = Log_files->make_build_log($wb);
 
+
+$database = $wb->autoace if not defined $database;
 $pid_table_file = $wb->acefiles . "/EBI_protein_ids.txt" if not defined $pid_table_file;
 $outace = $wb->acefiles . "/EBI_Uniprot_brief_ids.ace" if not defined $outace;
 
@@ -67,6 +68,8 @@ close($table_fh) or $log->log_and_die("Could not close the protein_id command/fi
 open(my $acefh, ">$outace")
     or $log->log_and_die("Could not open $outace for writing\n");
 
+my %stats;
+
 foreach my $gene (sort keys %$cur_data) {
   my %brief_ids;
   
@@ -82,13 +85,16 @@ foreach my $gene (sort keys %$cur_data) {
           if (exists $cur_data->{$gene}->{$cds}->{accession} and
               $cur_data->{$gene}->{$cds}->{accession} eq $uniprot) {
             $log->write_to("$cds : attached to UniProt $uniprot and product unchanged. Doing nothing\n") if $debug;
+            $stats{"2 - UniProt product matches WormBase brief_id and accessions match (no action)"}++;
           } else {
             $log->write_to("$cds : UniProt product matches, but accession does not. Updating.\n");
-            $update = 1;          
+            $update = 1;
+            $stats{"3 - Uniprot product matches WormBase product but accession does not (updating accession)"}++;
           }
         } else {
           $log->write_to("$cds : UniProt product is different from current. Updating.\n");
           $update = 1;
+          $stats{"4 - UniProt product different from WormBase brief_id (updating brief_id)"}++;
         }
       } else {
         $log->write_to("$cds : no brief_id. Updating.\n");
@@ -111,18 +117,23 @@ foreach my $gene (sort keys %$cur_data) {
     } else {
       if (exists $cur_data->{$gene}->{$cds}->{brief_id}) {
         $log->write_to("$cds : No UniProt product, but existing product. Doing nothing\n") if $debug; 
+        $stats{"1 - No UniProt product, but existing WormBase brief_id (no action)"}++;
       } else {
         $log->write_to("$cds : No UniProt product and no existing product. Doing nothing\n") if $debug; 
+        $stats{"0 - No UniProt product and no WormBase product (no action)"}++;
       }
     }
   }
 }
-
-
 close($acefh) or $log->log_and_die("Could not close $outace properly\n");
 
+$log->write_to("Summary:\n\n");
+foreach my $k (sort keys %stats) {
+  $log->write_to($k . " : " . $stats{$k} . "\n");
+}
+
 unless ($noload) {
-  $wb->load_to_database($wb->autoace, $outace, 'ENA_protein_xrefs', $log);
+  $wb->load_to_database($database, $outace, 'ENA_protein_xrefs', $log);
 }
 
 $log->mail();
@@ -134,7 +145,7 @@ sub fetch_current_data {
   my (%gene2brief);
 
   $log->write_to("Fetching current data...\n");
-  my $db = Ace->connect(-path => $wb->autoace );
+  my $db = Ace->connect(-path => $database );
   
   my $full_name = $wb->full_name;
   
