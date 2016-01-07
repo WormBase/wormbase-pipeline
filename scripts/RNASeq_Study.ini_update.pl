c07bdc7f00bfc600873c2463998c67edb031e79b
Gary Williams <gw3@ebi.ac.uk>
2016-01-07 17:38:53 +0000

    display list of changed files at end and added Sample data tags

--- scripts/RNASeq_Study.ini_update.pl
+++ scripts/RNASeq_Study.ini_update.pl
@@ -21,8 +21,10 @@ GetOptions ("help"       => \$help,
 
 if (!defined $dir) {$dir = glob("~/Parasite_config");}
 mkdir $dir, 0755;
+mkdir "$dir/nematoda", 0755;
+mkdir "$dir/platyhelminthes", 0755;
 
-my @updated_files;
+my %updated_files;
 
 # Retrieve all studies from ENA where the library strategy is RNASeq
 # and the taxon is Nematoda or Platyhelminth. 
@@ -43,9 +45,10 @@ my @updated_files;
 # http://www.ebi.ac.uk/ena/data/warehouse/search?query=tax_tree(6231)%20AND%20library_strategy=%22RNA-Seq%22&result=read_run&fields=study_accession,secondary_study_accession,study_title,experiment_accession,library_source,library_selection,center_name,scientific_name,tax_id&display=report
 
 
-foreach my $phylum ('6231', '6157') { # nematode and platyhelminth
+my %phyla=('6231' => 'nematoda', '6157' => 'platyhelminthes');
+foreach my $phylum (keys %phyla) { # nematode and platyhelminth
   my %data = &get_phylup_data($phylum);
-
+  my $phylum_name = $phyla{$phylum};
 
 ###  # Exclude all C. elegans studies.
 ###  delete $data{'Caenorhabditis elegans'};
@@ -55,7 +58,7 @@ foreach my $phylum ('6231', '6157') { # nematode and platyhelminth
     my $species_file = $species;
     $species_file =~ s/ /_/g;
     $species_file .= ".ini";
-    my $path = "$dir/$species_file";
+    my $path = "$dir/${phylum_name}/$species_file";
     if (!-e $path) {system("touch $path")}
     my $ini = Config::IniFiles->new( -file => $path, -allowempty => 1);
     my @existing_studies = $ini->Sections;
@@ -84,36 +87,51 @@ foreach my $phylum ('6231', '6157') { # nematode and platyhelminth
       # If the *only* library source is GENOMIC, don’t create a stanza.
       my $not_genomic = 0;
       foreach my $expt (@experiments) {
-	my ($experiment_accession, $library_source, $library_selection) = @{$expt};
+	my ($experiment_accession, $library_source, $library_selection, $secondary_sample_accession) = @{$expt};
 	if ($library_source ne 'GENOMIC') {$not_genomic = 1}
       }
 
       if ($not_genomic) {
+	# only write these if this is a new study, otherwise they would be overwritten
 	if (!grep /^$secondary_study_accession$/, @existing_studies) {
 	  print "New Study: $species\t$secondary_study_accession\n";
+	  $updated_files{"${phylum_name}/$species_file"} = 1;
 	  $ini->AddSection($secondary_study_accession);
-	  $ini->newval($secondary_study_accession, 'remark', '');
 	  $ini->newval($secondary_study_accession, 'use', '');
+	  $ini->newval($secondary_study_accession, 'remark', '');
+	  $ini->newval($secondary_study_accession, 'pubmed', '');
 	  $ini->newval($secondary_study_accession, 'status', '');
+	  $ini->newval($secondary_study_accession, 'longLabel', '');
+	  $ini->newval($secondary_study_accession, 'shortLabel', '');
+	  $ini->newval($secondary_study_accession, 'ArrayExpress_ID', '');
+
+	  $ini->newval($secondary_study_accession, 'scientific_name', $species);
+	  $ini->newval($secondary_study_accession, 'secondary_study_accession', $secondary_study_accession);
+	  foreach my $param (qw(study_accession study_title center_name tax_id)) {
+	    my $value = $data{$species}{$secondary_study_accession}{$param};
+	    $ini->newval($secondary_study_accession, $param, $value);
+	  }
+	  
+	  # we can't get this field from the 'read_run' results section of the ENA warehouse, it has to be a separate query
+	  my $study_description = &get_study_description($secondary_study_accession);
+	  $ini->newval($secondary_study_accession, 'study_description', $study_description);
+	  
+	  my %samples = ();
+	  foreach my $expt (@experiments) {
+	    my ($experiment_accession, $library_source, $library_selection, $secondary_sample_accession) = @{$expt};
+	    #	$ini->newval($secondary_study_accession, 'experiment_accession', "$experiment_accession");
+# always TRANSCRIPTOMIC	    $ini->newval($secondary_study_accession, "library_source_$experiment_accession", "$library_source");
+	    $ini->newval($secondary_study_accession, "library_selection_$experiment_accession", "$library_selection");      
+	    $ini->newval($secondary_study_accession, "library_sample_$experiment_accession", "$secondary_sample_accession");      
+	    $samples{$secondary_sample_accession} = 1;
+	  }
+	  
+	  foreach my $secondary_sample_accession (keys %samples) {
+	    $ini->newval($secondary_study_accession, "sample_Colour_$secondary_sample_accession", '');
+	    $ini->newval($secondary_study_accession, "sample_ChEBI_ID_$secondary_sample_accession", '');
+	    $ini->newval($secondary_study_accession, "sample_WormBaseLifeStage_$secondary_sample_accession", '');
+	  }
 	}
-
-	foreach my $param (qw(study_accession study_title center_name tax_id)) {
-	  my $value = $data{$species}{$secondary_study_accession}{$param};
-	  $ini->newval($secondary_study_accession, $param, $value);
-	}
-
-	# we can't get this field from the 'read_run' results section of the ENA warehouse, it has to be a separate query
-	my $study_description = &get_study_description($secondary_study_accession);
-	$ini->newval($secondary_study_accession, 'study_description', $study_description);
-
-	foreach my $expt (@experiments) {
-	  my ($experiment_accession, $library_source, $library_selection) = @{$expt};
-	  #	$ini->newval($secondary_study_accession, 'experiment_accession', "$experiment_accession");
-	  $ini->newval($secondary_study_accession, "library_source_$experiment_accession", "$library_source");
-	  $ini->newval($secondary_study_accession, "library_selection_$experiment_accession", "$library_selection");      
-	}
-
-	push @updated_files, $species_file;
       }
     }
     $ini->RewriteConfig;
@@ -121,8 +139,8 @@ foreach my $phylum ('6231', '6157') { # nematode and platyhelminth
 }
 
 print "Finished updating\n";
-print "You may now do a git commit and git push on the following changed file:\n\n";
-foreach my $file (@updated_files) {
+print "You may now do a git commit and git push on the following changed files:\n\n";
+foreach my $file (sort {$a cmp $b} keys %updated_files) {
   print "\t$file\n";
 }
 print "\n";
@@ -132,7 +150,7 @@ print "\n";
 sub get_phylup_data {
   my ($phylum) = @_;
   
-  my $query = 'http://www.ebi.ac.uk/ena/data/warehouse/search?query=tax_tree(PHYLUM)%20AND%20library_strategy=%22RNA-Seq%22&result=read_run&fields=study_accession,secondary_study_accession,study_title,experiment_accession,library_source,library_selection,center_name,scientific_name,tax_id&display=report';
+  my $query = 'http://www.ebi.ac.uk/ena/data/warehouse/search?query=tax_tree(PHYLUM)%20AND%20(library_strategy=%22RNA-Seq%22%20OR%20library_strategy=%22FL-cDNA%22%20OR%20library_strategy=%22EST%22)&result=read_run&fields=study_accession,secondary_study_accession,study_title,secondary_sample_accession,experiment_accession,library_source,library_selection,library_strategy,center_name,scientific_name,tax_id&display=report';
   $query =~ s/PHYLUM/$phylum/;
   
   my %data;
@@ -147,18 +165,21 @@ sub get_phylup_data {
 	$study_accession,
 	$secondary_study_accession,
 	$study_title,
+	$secondary_sample_accession,
 	$experiment_accession,
 	$library_source,
 	$library_selection,
+	$library_stategy,
 	$center_name,
 	$scientific_name,
 	$tax_id,
        ) = split /\t/, $line;
+    if ($library_stategy ne 'RNA-Seq' && $scientific_name !~ /^Caenorhabditis/) {next}; # The Parasite project is only interested in $library_stategy = 'RNA-Seq', but WormBase likes the others as well
     $data{$scientific_name}{$secondary_study_accession}{study_accession} = $study_accession;
     $data{$scientific_name}{$secondary_study_accession}{study_title} = $study_title;
     $data{$scientific_name}{$secondary_study_accession}{center_name} = $center_name;
     $data{$scientific_name}{$secondary_study_accession}{tax_id} = $tax_id;
-    push @{$data{$scientific_name}{$secondary_study_accession}{experiment}}, [$experiment_accession, $library_source, $library_selection];
+    push @{$data{$scientific_name}{$secondary_study_accession}{experiment}}, [$experiment_accession, $library_source, $library_selection, $secondary_sample_accession];
   }
   close(DATA);
 
