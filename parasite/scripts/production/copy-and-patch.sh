a5984314dd4b90e12bea7ebe25fe3597c605601b
Bruce Bolt <bbolt@ebi.ac.uk>
2016-02-25 14:21:05 +0000

    ParaSite: add script to copy and patch databases between releases

---
+++ parasite/scripts/production/copy-and-patch.sh
@@ -0,0 +1,24 @@
+
+## Script to copy core databases from previous staging site to current
+## After copy, relevant patches are applied to bring databases up to the relevant Ensembl schema
+## Before running, load the correct module as this creates the environment variables, e.g. "module load parasite_prod_rel6"
+
+echo "Begin database copying"
+
+databaselist=$($PREVIOUS_PARASITE_STAGING_MYSQL -NB -e 'SHOW DATABASES LIKE "%\_core\_%"')
+databases=( `echo ${databaselist}` )
+
+for DB in "${databases[@]}"
+do
+  echo "Copying $DB"
+  NEWDB=$(echo $DB | sed "s/core_$PREVIOUS_PARASITE_VERSION\_$PREVIOUS_ENSEMBL_VERSION/core_$PARASITE_VERSION\_$ENSEMBL_VERSION/" | sed "s/core_$PREVIOUS_ENSEMBL_VERSION/core_$ENSEMBL_VERSION/" | sed "s/_$PREVIOUS_WORMBASE_VERSION/_$WORMBASE_VERSION/")
+  echo  "Creating $NEWDB"
+  $PARASITE_STAGING_MYSQL -e "CREATE DATABASE $NEWDB"
+  echo "Dumping $DB to $NEWDB"
+  $PREVIOUS_PARASITE_STAGING_MYSQL mysqldump $DB | $PARASITE_STAGING_MYSQL $NEWDB
+done
+
+echo "Using schema_patcher.pl to patch databases"
+
+perl $ENSEMBL_CVS_ROOT_DIR/ensembl/misc-scripts/schema_patcher.pl $($PARASITE_STAGING_MYSQL details script) --release $ENSEMBL_VERSION --type core --verbose --nointeractive
+
