18d27795104d7ab6df18036d79b06c283cf0bc74
Bruce Bolt <bbolt@ebi.ac.uk>
2016-03-18 12:12:15 +0000

    Change data loading from CSV to ini files

--- createHub.pl
+++ createHub.pl
@@ -8,10 +8,10 @@ use HTML::Entities;
 use XML::Simple;
 use JSON;
 
-my @species_list = `ls ./in/*.txt | xargs -n1 basename`;
+my @species_list = `ls ./in/*.ini | xargs -n1 basename`;
 foreach(@species_list) {
   chomp;
-  $_ =~ s/\.txt//;
+  $_ =~ s/\.ini//;
 }
 
 my $counter = 0;
@@ -43,7 +43,7 @@ foreach my $species (@species_list) {
   }
   close(OUTFILE);
  
-  my $file = "./in/$species.txt";
+  my $file = "./in/$species.ini";
   open(INFILE, $file);
   mkdir "myHub/$species" unless -d "myHub/$species";
   open(OUTFILE, ">myHub/$species/trackDb.txt");
@@ -51,93 +51,109 @@ foreach my $species (@species_list) {
   my $groups;
   my $files;
   my %names;
-  
+
+  # Get each study
+  my %studies;
+  my $current;
   foreach(<INFILE>) {
     chomp;
-    my @parts = split("\t", $_);
-    next unless $parts[0];
+    if($_ =~ /^\[(.*)\]$/) {
+      $current = $1;
+    } else {
+      $studies{$current} .= $_ . "\n";
+    }
+  }
   
-    # Remove all the Excel crap
-    foreach(@parts) {
+  # Get the details for each study
+  foreach my $study (keys %studies) {
+    warn "-- Study: $study";
+    my %ini;
+    # Put the key value pairs into a hash
+    foreach(split("\n", $studies{$study})) {
       chomp;
-      $_ =~ s/^"//g;
-      $_ =~ s/"$//g;
+      $_ =~ /^(.*?)=(.*)$/;
+      my $key = $1;
+      my $value = $2;
+      $ini{$key} = $value;
     }
-
-    # Deal with study/project being wrong way round
-    if($parts[6] =~ /^(ERS|SRS)/ && $parts[5] =~ /^(ERP|SRP)/) {
-      my $temp = $parts[5];
-      $parts[5] = $parts[6];
-      $parts[6] = $temp;
-    }
- 
-    # Create the unique track ID
-    my $track_id = sprintf("%03d", $counter) . "_" . $parts[5];
- 
-    # Process colour
-    $parts[4] =~ s/ //g;
-    my @rgb = split(",", $parts[4]);
-    my $hex = '#';
-    foreach my $col (@rgb) {
-      $hex .= sprintf("%02X", $col);
-    }
-  
+    # Should we include this study?
+    next unless $ini{'use'} eq 'YES';
+    # Create the trackDb text
+    $groups .= sprintf("track %s\nsuperTrack on\ngroup %s\nshortLabel %s\nlongLabel %s\nhtml doc/%s\n\n", $study, $study, $study, $ini{'study_title'} || $study, $study);
     # Create the project description
-    my $proj_desc = sprintf('
-                 ENA Project ID: <a href="http://www.ebi.ac.uk/ena/data/view/%s">%s</a><br />
-                 ArrayExpress ID: <a href="http://www.ebi.ac.uk/arrayexpress/experiments/%s">%s</a>',
-              $parts[6], $parts[6], $parts[7], $parts[7]);
-    if($parts[6]) {
-      my $ref = get_ENA_project($parts[6]);
-      $proj_desc .= "<br /><br />$ref<br />" if $ref;
+    my $proj_desc = sprintf('ENA Project ID: <a href="http://www.ebi.ac.uk/ena/data/view/%s">%s</a><br />', $study, $study);
+    $proj_desc .= sprintf('ArrayExpress ID: <a href="http://www.ebi.ac.uk/arrayexpress/experiments/%s">%s</a><br />', $ini{'ArrayExpress_ID'}, $ini{'ArrayExpress_ID'}) if $ini{'ArrayExpress_ID'};
+    if($ini{'study_accession'}) {
+      my $ref = get_ENA_project($ini{'study_accession'});
+      $proj_desc .= "<br />$ref<br />" if $ref;
     }
-    if($parts[8]) {
-      my $ref = get_reference($parts[8]);
+    if($study) {
+      my $ref = get_ENA_project($study);
+      $proj_desc .= "<br />$ref<br />" if $ref;
+    }
+    if($ini{'pubmed'}) {
+      my $ref = get_reference($ini{'pubmed'});
       if($ref) {
-        $proj_desc .= "<br />Reference<br />$ref"; 
+        $proj_desc .= "<br />Reference<br />$ref";
       } else {
-        $proj_desc .= sprintf('<br />PubMed ID: <a href="http://europepmc.org/abstract/MED/%s">%s</a>', $parts[8], $parts[8]);
+        $proj_desc .= sprintf('<br />PubMed ID: <a href="http://europepmc.org/abstract/MED/%s">%s</a>', $ini{'pubmed'}, $ini{'pubmed'});
       }
     }
     $proj_desc =~ s/\n//g;
     mkdir "myHub/$species/doc" unless -d "myHub/$species/doc";
-    open(HTMLOUT, ">myHub/$species/doc/$parts[6].html");
+    open(HTMLOUT, ">myHub/$species/doc/$study.html");
     print HTMLOUT $proj_desc;
     close(HTMLOUT);
-
-    # Generate the URL
-    my $url = "http://ngs.sanger.ac.uk/production/parasites/wormbase/RNASeq_alignments/$species/$parts[5].bw";
-
-    # Re-format the WB life stage
-    $parts[10] =~ s/^WBls_/WBls:/ if $parts[10];
-
-    # Create the sample description
-    my $desc = sprintf(
-                'ENA Sample ID: <a href="http://www.ebi.ac.uk/ena/data/view/%s">%s</a><br />
-                 %s%s%s',
-              $parts[5], $parts[5], $parts[9] ? "ChEBI: <a href=\"https://www.ebi.ac.uk/chebi/searchId.do?chebiId=$parts[9]\">$parts[9]</a><br />" : '',
-              $parts[10] ? "WormBase Life Stage: <a href=\"http://www.wormbase.org/species/all/life_stage/$parts[10]\">$parts[10]</a><br />" : '',  $proj_desc);
-    $desc =~ s/\n//g;
-    $desc .= "<br /><br />This data comes from URL: $url";
-    mkdir "myHub/$species/doc" unless -d "myHub/$species/doc";
-    open(HTMLOUT, ">myHub/$species/doc/$track_id.html");
-    print HTMLOUT $desc;
-    close(HTMLOUT);
- 
-    # Create the trackDB text
-    $groups .= sprintf("track %s\nsuperTrack on\ngroup %s\nshortLabel %s\nlongLabel %s\nhtml doc/%s\n\n", $parts[6], $parts[6], $parts[6], $parts[6], $parts[6]) unless $names{$parts[6]};
-    $files .= sprintf("track %s\nparent %s\ntype bigWig\nbigDataUrl %s\nshortLabel %s\nlongLabel %s\ncolor %s\nhtml doc/%s\n\n", $track_id, $parts[6], $url, $parts[0], $parts[1], $parts[4], $track_id);
-    $counter++;
-    $names{$parts[6]} = 1;
-
+    # Get the unique sample IDs
+    my @samples;
+    foreach my $key (keys %ini) {
+      if($key =~ /^library_sample/) {
+        push(@samples, $ini{$key}) unless grep{$_ eq $ini{$key}} @samples;
+      }
+    }
+    # Loop through each sample
+    foreach my $sample (@samples) {
+      warn "  -- Sample: $sample";
+      $counter++;
+      # Create the unique track ID
+      my $track_id = sprintf("%03d", $counter) . "_" . $sample;
+      my $url = "http://ngs.sanger.ac.uk/production/parasites/wormbase/RNASeq_alignments/$species/$sample.bw";
+      # Create the sample description
+      my $desc = sprintf(
+                  'ENA Sample ID: <a href="http://www.ebi.ac.uk/ena/data/view/%s">%s</a><br />
+                   %s%s%s<br />',
+                $sample, $sample,
+                $ini{"sample_ChEBI_ID_$sample"} ? sprintf(
+                    'ChEBI: <a href="https://www.ebi.ac.uk/chebi/searchId.do?chebiId=%s">%s</a><br />',
+                    $ini{"sample_ChEBI_ID_$sample"}, $ini{"sample_ChEBI_ID_$sample"}) : '',
+                $ini{"sample_WormBaseLifeStage_$sample"} ? sprintf(
+                    'WormBase Life Stage: <a href="http://www.wormbase.org/species/all/life_stage/%s">%s</a><br />',
+                    $ini{"sample_WormBaseLifeStage_$sample"}, $ini{"sample_WormBaseLifeStage_$sample"}) : '',
+                $proj_desc);
+      $desc =~ s/\n//g;
+      $desc .= "<br /><br />This data comes from URL: $url";
+      mkdir "myHub/$species/doc" unless -d "myHub/$species/doc";
+      open(HTMLOUT, ">myHub/$species/doc/$track_id.html");
+      print HTMLOUT $desc;
+      close(HTMLOUT);
+      # Process the colour
+      $ini{'Colour'} =~ s/ //g;
+      my @rgb = split(",", $ini{'Colour'});
+      my $hex = '#';
+      foreach my $col (@rgb) {
+        $hex .= sprintf("%02X", $col);
+      }
+      # Create the trackDb text
+      $files .= sprintf("track %s\nparent %s\ntype bigWig\nbigDataUrl %s\nshortLabel %s\nlongLabel %s\ncolor %s\nhtml doc/%s\n\n", $track_id, $study, $url, $ini{"sample_shortLabel_$sample"}, $ini{"sample_longLabel_$sample"}, $hex, $track_id);
+    }
   }
-  
+
   print OUTFILE $groups, "\n";
   print OUTFILE $files;
-  
+
   close(INFILE);
   close(OUTFILE);
- 
+
 }
 
 sub get_reference {
@@ -161,7 +177,14 @@ sub get_ENA_project {
   my $response = $ua->get($url);
   if ($response->is_success) {
     my $result = XMLin($response->decoded_content);
-    my $text = $result->{STUDY}->{DESCRIPTOR}->{STUDY_DESCRIPTION} ? "Project Description<br />$result->{STUDY}->{DESCRIPTOR}->{STUDY_DESCRIPTION}" : '';
+    my $text;
+    if($result->{PROJECT}->{DESCRIPTION}) {
+      my $formatted = encode_entities($result->{PROJECT}->{DESCRIPTION});
+      $text = "Project Description<br />$formatted";
+    } elsif($result->{STUDY}->{DESCRIPTOR}->{STUDY_DESCRIPTION}) {
+      my $formatted = encode_entities($result->{STUDY}->{DESCRIPTOR}->{STUDY_DESCRIPTION});
+      $text = "Study Description<br />$formatted";
+    }
     return $text;
   }
 }
