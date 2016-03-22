0cc951d322d3ce380e06b7873f85ed064297fb6d
Gary Williams <gw3@ebi.ac.uk>
2016-03-22 17:06:19 +0000

    added some context and evidence for the changes for Karen

--- scripts/get_changed_cgc_names.pl
+++ scripts/get_changed_cgc_names.pl
@@ -24,8 +24,8 @@ use Storable;
 # variables and command-line options # 
 ######################################
 
-my ($help, $debug, $test, $verbose, $store, $wormbase, $outfile);
-
+my ($help, $debug, $test, $verbose, $store, $wormbase, $outfile, $database);
+my ($RELEASE, $PREV_RELEASE);
 
 GetOptions ("help"       => \$help,
             "debug=s"    => \$debug,
@@ -33,6 +33,8 @@ GetOptions ("help"       => \$help,
 	    "verbose"    => \$verbose,
 	    "store:s"    => \$store,
 	    "outfile:s"  => \$outfile,
+	    "RELEASE:s"  => \$RELEASE, # optional - for use when looking at historical releases
+	    "database:s" => \$database, # ditto
 	    );
 
 if ( $store ) {
@@ -58,15 +60,19 @@ my $log = Log_files->make_build_log($wormbase);
 
 ##########################
 
+my $tace = $wormbase->tace;
+$database ||= $wormbase->autoace;
+
 $outfile ||= $wormbase->misc_output . "/changed_CGC_names.dat";
 
-my $RELEASE = $wormbase->get_wormbase_version;;
-my $PREV_RELEASE = $RELEASE - 1;
+$RELEASE ||= $wormbase->get_wormbase_version;
+$PREV_RELEASE = $RELEASE - 1;
+my $releasedir = ($RELEASE == $wormbase->get_wormbase_version) ? $wormbase->common_data :  $wormbase->wormpub . "/DATABASES/WS${RELEASE}/COMMON_DATA/";
 my $prevdir = $wormbase->wormpub . "/DATABASES/WS${PREV_RELEASE}/COMMON_DATA/";
 my %gene2cgc;
 my %prev_gene2cgc;
 
-$wormbase->FetchData('worm_gene2cgc_name', \%gene2cgc);
+$wormbase->FetchData('worm_gene2cgc_name', \%gene2cgc, $releasedir);
 $wormbase->FetchData('worm_gene2cgc_name', \%prev_gene2cgc, $prevdir);
 
 &remove_other_species(\%gene2cgc);
@@ -75,10 +81,16 @@ $wormbase->FetchData('worm_gene2cgc_name', \%prev_gene2cgc, $prevdir);
 my ($cgc2gene, $cgc2biotype, $sequence_name, $geneid2cgc) = &key_by_cgc(\%gene2cgc);
 my ($prev_cgc2gene, $prev_cgc2biotype, $prev_sequence_name, $prev_geneid2cgc) = &key_by_cgc(\%prev_gene2cgc);
 
+$log->write_to("Connecting to database...\n");
+my $db = Ace->connect (-path => "$database",
+		       -program => $tace) || $log->log_and_die("Cannot connect to database at $database\n");
+
 
 open (OUT, "> $outfile") || $log->log_and_die("Can't open $outfile\n");
 &output_changed_cgc();
 close OUT;
+
+$db->close();
 
 
 $log->mail();
@@ -108,7 +120,7 @@ sub remove_other_species {
 #          'C05E4.1' => 'CDS srp-2 WBGene00005643',
  
   foreach my $key (keys %{$hash_ref}) {
-    if ($hash_ref->{$key} =~ /^\S+\s+\w+\-\w+\-[\d\.]+\s+\S+$/) {
+    if ($hash_ref->{$key} =~ /^\S+\s+\S+\-\S+\-\S+\s+\S+$/) {
 #      print "delete $hash_ref->{$key}\n";
       delete $hash_ref->{$key};
     }
@@ -181,7 +193,8 @@ sub output_changed_cgc {
 	  $all_cgcs{$cgc} = 0; # don't process this one again
 	  $all_cgcs{$new_cgc} = 0; # don't process this one again
 	  $new = "$new_cgc\t$cgc2gene->{$new_cgc}\t$sequence_name->{$new_cgc}\t$cgc2biotype->{$new_cgc}";
-	  push @changed_cgc_name_on_a_gene, "$prev\t$new\n";
+	  my $details = get_cgc_name_changed_details($cgc); # see if the CGC_name is now an Other_name
+	  push @changed_cgc_name_on_a_gene, "$prev\t$new\tCGC name changed on this gene$details\n";
 	} 
       }
     }
@@ -192,46 +205,136 @@ sub output_changed_cgc {
     my $prev = (exists $prev_cgc2gene->{$cgc}) ? "$cgc\t$prev_cgc2gene->{$cgc}\t$prev_sequence_name->{$cgc}\t$prev_cgc2biotype->{$cgc}" : ".\t.\t.\t.";
     my $new = (exists $cgc2gene->{$cgc}) ? "$cgc\t$cgc2gene->{$cgc}\t$sequence_name->{$cgc}\t$cgc2biotype->{$cgc}" : ".\t.\t.\t.";
     if ($prev ne $new) {
-      if (!exists $prev_cgc2gene->{$cgc}) {push @no_previous_cgc_name, "$prev\t$new\n"}
-      elsif (!exists $cgc2gene->{$cgc}) {push @no_new_cgc_name, "$prev\t$new\n"}
-      elsif ($prev_cgc2gene->{$cgc} ne $cgc2gene->{$cgc}) {push @geneid_different_to_prev, "$prev\t$new\n"}
-      elsif ($prev_cgc2biotype->{$cgc} ne $cgc2biotype->{$cgc}) {push @biotype_changed, "$prev\t$new\n"}
-      else {push @other, "$prev\t$new\n"} # can have a change in the Sequence name of a Gene
+      if (!exists $prev_cgc2gene->{$cgc}) { # new CGC-name - get details
+	my $details = get_new_cgc_details($cgc);
+	push @no_previous_cgc_name, "$new\t$details\n"
+      }
+      elsif (!exists $cgc2gene->{$cgc}) {
+	my $details = get_retired_cgc_details($cgc); # try to discover why it has retired
+	push @no_new_cgc_name, "$prev\t$new\tCGC_name retired$details\n"
+      }
+      elsif ($prev_cgc2gene->{$cgc} ne $cgc2gene->{$cgc}) {push @geneid_different_to_prev, "$prev\t$new\tCGC_name assigned to a new gene\n"}
+      elsif ($prev_cgc2biotype->{$cgc} ne $cgc2biotype->{$cgc}) {push @biotype_changed, "$prev\t$new\tBiotype changed\n"}
+      else {push @other, "$prev\t$new\tSequence ID changed\n"} # can have a change in the Sequence name of a Gene
     }
   }
 
-  print OUT "# Genes with changed CGC names\n";
-  print OUT "# old CGC\told GeneID\told SequenceID\told Biotype\tnew CGC\tnew GeneID\tnew SequenceID\tnew Biotype\n";
-  print OUT @changed_cgc_name_on_a_gene;
-  print OUT "\n";
-
   print OUT "# new CGC names\n";
-  print OUT "# old CGC\told GeneID\told SequenceID\told Biotype\tnew CGC\tnew GeneID\tnew SequenceID\tnew Biotype\n";
+  print OUT "# CGC_name\tGeneID\tSequenceID\tBiotype\tEvidence\n";
   print OUT @no_previous_cgc_name;
   print OUT "\n";
   
-  print OUT "# removed CGC names\n";
+  print OUT "# Other changes\n";
+#  print OUT "# Genes with changed CGC names\n";
   print OUT "# old CGC\told GeneID\told SequenceID\told Biotype\tnew CGC\tnew GeneID\tnew SequenceID\tnew Biotype\n";
+  print OUT @changed_cgc_name_on_a_gene;
+#  print OUT "# removed CGC names\n";
   print OUT @no_new_cgc_name;
-  print OUT "\n";
-  
-  print OUT "# assigned Gene changed\n";
-  print OUT "# old CGC\told GeneID\told SequenceID\told Biotype\tnew CGC\tnew GeneID\tnew SequenceID\tnew Biotype\n";
+#  print OUT "# assigned Gene changed\n";
   print OUT @geneid_different_to_prev;
-  print OUT "\n";
-  
-  print OUT "# Biotype changed\n";
-  print OUT "# old CGC\told GeneID\told SequenceID\told Biotype\tnew CGC\tnew GeneID\tnew SequenceID\tnew Biotype\n";
+#  print OUT "# Biotype changed\n";
   print OUT @biotype_changed;
-  print OUT "\n";
-  
-  print OUT "# other changes\n";
-  print OUT "# old CGC\told GeneID\told SequenceID\told Biotype\tnew CGC\tnew GeneID\tnew SequenceID\tnew Biotype\n";
+#  print OUT "# other changes\n";
   print OUT @other;
   print OUT "\n";
-  
 
     
+}
+
+##########################################
+# see if the old CGC_name is now an Other_name
+sub get_cgc_name_changed_details {
+  my ($cgc) = @_; 
+
+  my $details;
+  my $cgc_gene = $prev_cgc2gene->{$cgc};
+  my $gene_obj = $db->fetch(Gene => $cgc_gene);
+
+  print "CGC_name changed in gene\t$cgc\t$cgc_gene\n";
+  my @other_names = $gene_obj->Other_name;
+  if (grep /^$cgc$/, @other_names) {
+    $details .= ", $cgc has been assigned as an Other_name of $cgc_gene";
+  } else {
+    $details .= ", $cgc is NOT an Other_name of $cgc_gene"; # something odd happening here
+  }
+
+  return $details;
+}
+##########################################
+# get the evidence for the association of a CGC_name with the gene object
+sub get_new_cgc_details {
+  my ($cgc) = @_;
+
+  my $details;
+  my $new_cgc_gene = $cgc2gene->{$cgc};
+  my $gene_obj = $db->fetch(Gene => $new_cgc_gene);
+
+  my $cgc_escaped = $cgc;
+  $cgc_escaped =~ s/\./\\./g;
+  print "New CGC_name\t$cgc\t$new_cgc_gene\n";
+  my @evidence = $gene_obj->at("Identity.Name.CGC_name.$cgc_escaped")->col(2);
+  if (!scalar @evidence) {
+    $details = "No evidence given for this!";
+  } else {
+    foreach my $evidence (@evidence) {
+      if ($evidence =~ /^WBPaper/) {
+	my $paper_obj = $db->fetch(Paper => $evidence);
+	if (defined $details) {$details .= ', '}
+	my $citation .= $paper_obj->Brief_citation;
+	$citation =~ s/\s*".+//; # remove the quoted title
+	$details .= $citation;
+	$details .= ", ($evidence)";
+      } elsif ($evidence =~ /^WBPerson/) {
+	my $person_obj = $db->fetch(Person => $evidence);
+	if (defined $details) {$details .= ', '}
+	$details .= $person_obj->Standard_name;
+
+      } else {
+	if (defined $details) {$details .= ', '}
+	$details .= "$evidence"; # not a paper or a person, just display the ID
+      }
+    }
+  }
+  return $details;
+}
+
+##########################################
+# try to find out why it was retired
+sub get_retired_cgc_details {
+  my ($cgc) = @_;
+  
+  my $details = "";
+  my $old_cgc_gene = $prev_cgc2gene->{$cgc};
+  my $gene_obj = $db->fetch(Gene => $old_cgc_gene);
+
+  # has the gene been merged and the CGC is now an Other_name?
+  if ($gene_obj->Status->name eq 'Dead') {
+    print "Retired CGC_name in Dead gene\t$cgc\t$old_cgc_gene\n";
+    my @merged_genes = $gene_obj->at("Identity.History.Version_change")->col(6);
+    
+    foreach my $merged (@merged_genes) {
+      if ($merged =~ /^WBGene/) {
+	my $merged_obj = $db->fetch(Gene => $merged);
+	my @other_names = $merged_obj->Other_name;
+	foreach my $other_name (@other_names) {
+	  if ($other_name eq $cgc) {
+	    $details .= ", $cgc has been assigned as an Other_name of $merged_obj";
+	  }
+	}
+      }
+    }
+  } else {
+    $details .= ", $old_cgc_gene is still a Live gene";
+    print "Retired CGC_name in Live gene\t$cgc\t$old_cgc_gene\n";
+    my @other_names = $gene_obj->Other_name;
+    if (grep /^$cgc$/, @other_names) {
+      $details .= ", $cgc has been assigned as an Other_name of $old_cgc_gene";
+    } else {
+      $details .= ", $cgc is NOT an Other_name of $old_cgc_gene"; # something odd happening here
+    }
+  }
+
+  return $details;
 }
 
 ##########################################
