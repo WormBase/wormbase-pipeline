6074f05085fb0d2243e4895b308920e75fd5696a
Paul-Davis <pad@ebi.ac.uk>
2016-05-03 12:46:51 +0100

    Updated to take a type CGC/Sequence

--- scripts/GENEACE/batch_delName.pl
+++ scripts/GENEACE/batch_delName.pl
@@ -18,19 +18,20 @@ use strict;
 
   -user      username
   -password  password
-  -file      file containing list of GeneIDs and CGC name eg WBGene00008040 ttr-5
+  -file      file containing list of GeneIDs and CGC/Sequence names eg WBGene00008040 ttr-5
   -test      use the test nameserver
 
 e.g. perl batch_delName.pl -u fred -p secret -file genenames.txt
 
 =cut
 
-my ($USER,$PASS, $test, $file);
+my ($USER,$PASS, $test, $file, $type,);
 GetOptions(
 	   'user:s'     => \$USER,
 	   'password:s' => \$PASS,
 	   'test'       => \$test,
 	   'file:s'     => \$file,
+	   'type:s'     => \$type,
 	  ) or die;
 
 my $log = Log_files->make_log("NAMEDB:$0:$file", $USER);
@@ -43,6 +44,10 @@ if ($test) {
 
 $log->write_to("loading $file to $DB\n\n");
 $log->write_to("TEST mode is ON!\n\n") if $test;
+
+unless (($type eq  'CGC') || ($type eq 'Sequence')) {
+  $log->log_and_die ("$type is not CGC or Sequence\n");
+}
 
 my $db = NameDB_handler->new($DB,$USER,$PASS);
 
@@ -59,9 +64,9 @@ my $count=0;
 while(<FILE>) {
     my($id,$name,$cds) = split;
     eval{
-	my $success = $db->delName($id,CGC => $name);
+	my $success = $db->delName($id,$type => $name);
 	my $msg = defined $success ? 'deleted' : 'FAILED to delete';
-	$log->write_to("$msg CGC name $name for $id\n");
+	$log->write_to("$msg $type name $name for $id\n");
         $success = $db->_update_public_name($id);
         $msg = defined $success ? 'updated' : 'FAILED to update';
 	$log->write_to("$msg public name to $success for $id\n");
