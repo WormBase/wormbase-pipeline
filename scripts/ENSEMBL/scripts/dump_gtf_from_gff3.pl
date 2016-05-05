7aab5820462b2a0df6833db43c561ec3396127f8
Kevin Howe <klh@ebi.ac.uk>
2016-05-05 11:21:44 +0100

    Can now be run as part of build at Sanger or EBI

--- scripts/ENSEMBL/scripts/dump_gtf_from_gff3.pl
+++ scripts/ENSEMBL/scripts/dump_gtf_from_gff3.pl
@@ -8,24 +8,52 @@ use lib "$Bin/../lib";
 
 use Getopt::Long;
 
+use Wormbase;
+use Log_files;
 use WormBase2Ensembl;
 use Bio::SeqIO;
 use Bio::EnsEMBL::Analysis;
 use Bio::EnsEMBL::Utils::IO::GTFSerializer;
 
-my ($debug, $wb_version, $genome_file,  
-    %genes_by_slice, $out_file, $out_fh, $gff3);
+my ($debug, $test, $store, $species,  $verbose, $wb,
+    $out_file, $out_fh, $gff3, $genome, %genes_by_slice);
 
 
-GetOptions(
-  'genome=s'     => \$genome_file,
-  'gff3=s'       => \$gff3,
-  'outfile:s'    => \$out_file,
-  'wbversion:s'  => \$wb_version,
-  'debug'        => \$debug,
-          )or die ("Couldn't get options");
+&GetOptions(
+  "debug=s"     => \$debug,
+  "test"        => \$test,
+  "verbose"     => \$verbose,
+  "store:s"     => \$store,
+  "species:s"   => \$species,
+  "genome=s"    => \$genome,
+  "gff3=s"      => \$gff3,
+  "outgtf=s"    => \$out_file,
+    )or die ("Couldn't get options");
 
-my $slices = &make_slices($genome_file);
+
+if ( $store ) {
+  $wb = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
+} else {
+  $wb = Wormbase->new( -debug    => $debug,
+                       -test     => $test,
+                       -organism => $species,
+      );
+}
+
+my $log = Log_files->make_build_log($wb);
+
+
+$genome = $wb->genome_seq if not defined $genome;
+$gff3 = $wb->processed_GFF_file( 1 ) if not defined $gff3;
+if (not -e $gff3) {
+  $gff3 .= ".gz";
+  if (not -e $gff3) {
+    $log->log_and_die("Could not find GFF3 file $gff3\n");
+  }
+}
+$out_file =  sprintf("%s/%s.gtf", $wb->sequences, $wb->species) if not defined $out_file;
+
+my $slices = &make_slices($genome);
 
 if ($gff3) {
   my $wb2ens = WormBase2Ensembl->new(    
@@ -48,12 +76,12 @@ if ($gff3) {
 
 
 if ($out_file) {
-  open( $out_fh, ">$out_file") or die "Could not open $out_file for writing\n";
+  open( $out_fh, ">$out_file") or $log->log_and_die("Could not open $out_file for writing\n");
 } else {
   $out_fh = \*STDOUT;
 }
 
-print $out_fh "#!genebuild-version $wb_version\n";
+print $out_fh "#!genebuild-version ", $wb->get_wormbase_version_name, "\n";
 
 my $serializer = Bio::EnsEMBL::Utils::IO::GTFSerializer->new($out_fh);
 foreach my $slice (values %$slices) {
@@ -64,6 +92,7 @@ foreach my $slice (values %$slices) {
   }
 }
 
+$log->mail();
 exit(0);
 
 
@@ -73,9 +102,9 @@ sub make_slices {
 
   my $gfh;
   if ($genome =~ /\.gz$/) {
-    open($gfh, "gunzip -c $genome |") or die "Could not open gunzip stream to $genome\n";
+    open($gfh, "gunzip -c $genome |") or $log->log_and_die("Could not open gunzip stream to $genome\n");
   } else {
-    open($gfh, $genome) or die "Could not open $genome for reading\n";
+    open($gfh, $genome) or $log->log_and_die("Could not open $genome for reading\n");
   }
     
   my $seqio = Bio::SeqIO->new(-fh => $gfh,
@@ -84,14 +113,20 @@ sub make_slices {
   my %slices;
 
   while(my $seq = $seqio->next_seq) {
+    my $name = $seq->id;
+    if ($wb->species eq 'elegans' and $name =~ /^CHROMOSOME_/) {
+      $name =~ s/^CHROMOSOME_//; 
+    } elsif ($wb->species eq 'briggsae' and $name =~ /^chr/) {
+      $name =~ s/^chr//; 
+    }
     my $slice = Bio::EnsEMBL::Slice->new(
-      -seq               => $seq->seq,
-      -seq_region_name   => $seq->id, 
+      -seq               => uc($seq->seq),
+      -seq_region_name   => $name,
       -start             => 1,
       -end               => $seq->length,
       -seq_region_length => $seq->length,
       -strand            => 1);
-    $slices{$seq->id} = $slice;
+    $slices{$name} = $slice;
   }
 
 
