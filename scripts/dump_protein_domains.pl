a05cc6c867e9eac0d126f3d85f78dcfd18277743
elpaule <michael.paulini@wormbase.org>
2016-07-15 16:44:57 +0100

    converted to post-merge

--- scripts/dump_protein_domains.pl
+++ scripts/dump_protein_domains.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/env perl
-# dump the protein domains into a flatfile
+# script to dump the interpro domains for each species into a flatfile in MISC_OUTPUT
+# should be run post-merge
 
 use feature qw(say);
 
@@ -9,43 +10,37 @@ use Getopt::Long;
 use IO::File;
 use strict;
 
-
-my($species,$store,$test,$debug);
+my($test,$debug);
 GetOptions(
-  'species=s' => \$species,
-  'store=s'   => \$store,
   'test'      => \$test,
   'debug=s'   => \$debug
 )||die();
 
-my $wb;
-
-if ($store) {
-  $wb = Storable::retrieve($store)
-      or croak("cannot restore wormbase from $store");
-}
-else { 
-  $wb = Wormbase->new( -debug => $debug, 
+my $wb = Wormbase->new(-debug => $debug, 
                        -test => $test, 
-                       -organism => $species,
-		     )
-}
+		      );
 
 my $log = Log_files->make_build_log($wb);
-
 my $db = Ace->connect(-path => $wb->autoace)||$log->log_and_die(Ace->error);
-my $outfile =$wb->autoace.'/MISC_OUTPUT/protein_domains.tsv';
-my $out = IO::File->new($outfile,'w');
-$log->log_and_die("cannot open $outfile\n") unless defined $out;
 
-my $cdses = $db->fetch_many(-query => "find CDS Corresponding_protein; Species=\"${\$wb->long_name}\"") 
+my %species_accessors=$wb->species_accessors;
+
+while (my($k,$v)=each %species_accessors){
+
+  my $outfile =$v->autoace.'/MISC_OUTPUT/protein_domains.tsv';
+  my $out = IO::File->new($outfile,'w');
+  $log->log_and_die("cannot open $outfile\n") unless defined $out;
+  $log->write_to("dumping domains for $k to $outfile\n");
+
+  my $cdses = $db->fetch_many(-query => "find CDS Corresponding_protein; Species=\"${\$v->long_name}\"") 
    ||$log->log_and_die(Ace->error);
 
-while (my $cds = $cdses->next){
-  my $protein = $cds->Corresponding_protein;
-  my @motifs = grep {/INTERPRO:/} $protein->Motif_homol;
-  say $out join("\t",($cds->Gene,$cds->Gene->Public_name,$protein,
+  while (my $cds = $cdses->next){
+    my $protein = $cds->Corresponding_protein;
+    my @motifs = grep {/INTERPRO:/} $protein->Motif_homol;
+    say $out join("\t",($cds->Gene,$cds->Gene->Public_name,$protein,
                 map {"$_". ($_->fetch->Title?' "'.$_->fetch->Title.'"':'')} @motifs));
+  }
 }
 
 $log->mail();
