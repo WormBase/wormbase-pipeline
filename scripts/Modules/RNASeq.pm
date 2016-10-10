e6db6aedd6fc6a5dd3d4e4262a409070d4eb5e7e
Gary Williams <gw3@ebi.ac.uk>
2016-10-10 16:09:43 +0100

    samtools has changed the command-line syntax for doing a sort

--- scripts/Modules/RNASeq.pm
+++ scripts/Modules/RNASeq.pm
@@ -1570,7 +1570,7 @@ sub sort_bam_file {
   my $log = $self->{log};
   
   my $failed = 0;
-  my $status = $self->{wormbase}->run_command($self->{Software}."/samtools/samtools sort -m 3500000000 $bamfile $bamfile.sorted", $log); # 3.5Gb
+  my $status = $self->{wormbase}->run_command($self->{Software}."/samtools/samtools sort -m 3500000000 $bamfile -o $bamfile.sorted", $log); # 3.5Gb
   $failed = $status;
   $status = $self->{wormbase}->run_command("mv $bamfile.sorted.bam $bamfile", $log);
   return ($failed & $status);
