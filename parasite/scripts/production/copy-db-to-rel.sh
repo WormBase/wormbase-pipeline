471bc68b05f326071a28fc3c6639970ff3655eb6
Bruce Bolt <bbolt@ebi.ac.uk>
2016-12-08 15:08:11 +0000

    Correct indentation

--- parasite/scripts/production/copy-db-to-rel.sh
+++ parasite/scripts/production/copy-db-to-rel.sh
@@ -7,14 +7,14 @@ databaselist=$($PARASITE_STAGING_MYSQL -e 'SHOW DATABASES LIKE "%_core%_'${ENSEM
 databases=( `echo ${databaselist}` ensembl_compara_parasite_${PARASITE_VERSION}_${ENSEMBL_VERSION} ensembl_ontology_${ENSEMBL_VERSION} ensembl_website_${ENSEMBL_VERSION} ensemblgenomes_info_${EG_VERSION} ensemblgenomes_stable_ids_${PARASITE_VERSION}_${ENSEMBL_VERSION} )
 for DB in "${databases[@]}"
 do
-	echo $DB
-	$PARASITE_STAGING_MYSQL mysqldump $DB > $DB.sql
-        mysql-ps-rel-ensrw -e "CREATE DATABASE $DB"
-        mysql-ps-rel-ensrw $DB < $DB.sql
-        mysql-ps-rest-rel-ensrw -e "CREATE DATABASE $DB"
-        mysql-ps-rest-rel-ensrw $DB < $DB.sql
-        mysql-ps-intrel-ensrw -e "CREATE DATABASE $DB"
-        mysql-ps-intrel-ensrw $DB < $DB.sql
+  echo $DB
+  $PARASITE_STAGING_MYSQL mysqldump $DB > $DB.sql
+  mysql-ps-rel-ensrw -e "CREATE DATABASE $DB"
+  mysql-ps-rel-ensrw $DB < $DB.sql
+  mysql-ps-rest-rel-ensrw -e "CREATE DATABASE $DB"
+  mysql-ps-rest-rel-ensrw $DB < $DB.sql
+  mysql-ps-intrel-ensrw -e "CREATE DATABASE $DB"
+  mysql-ps-intrel-ensrw $DB < $DB.sql
 done
 
 # Compress the archive then push to the EBI Archive Freezer
