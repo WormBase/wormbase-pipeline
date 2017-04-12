0f1c1719c13a5840ce051c30186ad323354743df
Bruce Bolt <bbolt@ebi.ac.uk>
2017-04-12 09:47:26 +0100

    Add an integrity check to compare content of database on staging to rel

--- parasite/scripts/production/copy-db-to-rel.sh
+++ parasite/scripts/production/copy-db-to-rel.sh
@@ -5,19 +5,26 @@ cd /nfs/nobackup/ensemblgenomes/wormbase/parasite/parasite-release
 echo "Dumping and loading databases"
 databaselist=$($PARASITE_STAGING_MYSQL -e 'SHOW DATABASES LIKE "%_core%_'${ENSEMBL_VERSION}'_%"')
 databases=( `echo ${databaselist}` ensembl_compara_parasite_${PARASITE_VERSION}_${ENSEMBL_VERSION} ensembl_ontology_${ENSEMBL_VERSION} ensembl_website_${ENSEMBL_VERSION} ensemblgenomes_info_${EG_VERSION} ensemblgenomes_stable_ids_${PARASITE_VERSION}_${ENSEMBL_VERSION} )
+stagingurl=$(echo $($PARASITE_STAGING_MYSQL details url) | awk '{ sub(/^mysql:\/\//,""); print }' | awk '{ sub(/\/$/,""); print }')
 for DB in "${databases[@]}"
 do
   echo $DB
   $PARASITE_STAGING_MYSQL mysqldump $DB > $DB.sql
+  echo "Loading to mysql-ps-rel"
   mysql-ps-rel-ensrw -e "CREATE DATABASE $DB"
   mysql-ps-rel-ensrw $DB < $DB.sql
   mysql-ps-rel-ensrw mysqlcheck -o $DB
+  mysqldbcompare --server1=$stagingurl --server2=$(echo $(mysql-ps-rel details url) | awk '{ sub(/^mysql:\/\//,""); print }' | awk '{ sub(/\/$/,""); print }') $DB:$DB
+  echo "Loading to mysql-ps-rest-rel"
   mysql-ps-rest-rel-ensrw -e "CREATE DATABASE $DB"
   mysql-ps-rest-rel-ensrw $DB < $DB.sql
   mysql-ps-rest-rel-ensrw mysqlcheck -o $DB
+  mysqldbcompare --server1=$stagingurl --server2=$(echo $(mysql-ps-rest-rel details url) | awk '{ sub(/^mysql:\/\//,""); print }' | awk '{ sub(/\/$/,""); print }') $DB:$DB
+  echo "Loading to mysql-ps-intrel"
   mysql-ps-intrel-ensrw -e "CREATE DATABASE $DB"
   mysql-ps-intrel-ensrw $DB < $DB.sql
   mysql-ps-intrel-ensrw mysqlcheck -o $DB
+  mysqldbcompare --server1=$stagingurl --server2=$(echo $(mysql-ps-intrel details url) | awk '{ sub(/^mysql:\/\//,""); print }' | awk '{ sub(/\/$/,""); print }') $DB:$DB
 done
 
 # Compress the archive then push to the EBI Archive Freezer
