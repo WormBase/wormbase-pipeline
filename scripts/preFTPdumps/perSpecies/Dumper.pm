70f70e99a30b906b3be97b559e99e51205fe3b48
elpaule <michael.paulini@wormbase.org>
2017-05-23 16:32:36 +0100

    modulification

---
+++ scripts/preFTPdumps/perSpecies/Dumper.pm
@@ -0,0 +1,39 @@
+#!/usr/bin/env perl
+#
+# Utility functions:
+#
+
+package preFTPdumps::perSpecies::Dumper;
+
+use Exporter qw(import);
+our @ISA =   qw(Exporter);
+our @EXPORT = qw(get_data rewrap);
+
+use Time::localtime;
+
+# yyyy-mm-dd
+sub get_date{
+ my $tm = localtime;
+ my ($day,$month,$year)=($tm->mday,$tm->mon,$tm->year+1900);
+ return "$year-$month-$day";
+}
+
+
+# wrap a paragraph to 80 characters
+sub rewrap {
+    my $text = shift;
+    $text =~ s/^\n+//gs;
+    $text =~ s/\n+$//gs;
+    my @words = split(/\s/,$text);
+    my ($para,$line);
+    foreach (@words) {
+            $line .= "$_ ";
+            next if length ($line) < 80;
+            $para .= "$line\n";
+            $line = undef;
+    }
+    $para .= "$line\n" if ($line);
+    return $para;
+}
+
+1;
