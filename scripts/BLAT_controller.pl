9b9c419fda49cc731e45fa015e97c22a6223a3bc
Paul-Davis <pad@ebi.ac.uk>
2018-02-01 10:53:53 +0000

    added tmuris to the list of species/types.

--- scripts/BLAT_controller.pl
+++ scripts/BLAT_controller.pl
@@ -76,6 +76,7 @@ my %mol_types = ( 'elegans'          => [qw( EST mRNA ncRNA OST tc1 RST Trinity)
 		  'pristionchus'     => [qw( mRNA EST Trinity)],
                   'ovolvulus'        => [qw( mRNA EST Trinity)],
                   'sratti'           => [qw( mRNA EST Trinity)],
+		  'tmuris'           => [qw( mRNA EST Trinity)],
 		  'nematode'         => [qw( EST )],
 		  'nembase'          => [qw( EST )],
 		  'washu'            => [qw( EST )],
