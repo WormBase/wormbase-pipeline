4dec6ef8523ec35d3b19b244cc5c3f95c31c68d3
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2018-03-05 15:55:49 +0000

    Tidy up wrapper script

--- parasite/scripts/production/jbrowse/lsf_wrapper.sh
+++ parasite/scripts/production/jbrowse/lsf_wrapper.sh
@@ -1,9 +1,21 @@
 
-species=($(ls -d $2/species/*))
+if [ $# -ne 3 ] ; then
+  >&2 echo "Usage: $0 <ftp dir> <jbrowse_path> <out_dir> "
+  exit 1
+fi
+log=$PARASITE_SCRATCH/log/jbrowse/$(date -u +"%Y-%m-%dT%H:%M" )
+mkdir -p $log
 
-for s in "${species[@]}"
-do
+#Conveniently Ensembl has all the right modules installed for jbrowse
+# You could install them yourself - see Makefile.pl in the repo
+PERL5LIB=/nfs/public/rw/ensembl/libs/perl/share/perl5/:$PERL5LIB
+export PERL5LIB
+
+#species=($(ls -d $1/species/*))
+
+for s in /nfs/nobackup/ensemblgenomes/wormbase/parasite/production/log/jbrowse/2018-03-05T16:52/*out.failed ; do
   s="$(basename $s)"
-  bsub -q production-rh7 "module load parasite_prod_rel9; export PERL5LIB=/nfs/panda/ensemblgenomes/wormbase/software/perlmodules/:/nfs/panda/ensemblgenomes/wormbase/software/perlmodules/x86_64-linux-thread-multi-ld/:/nfs/public/rw/ensembl/libs/perl/share/perl5/:$PERL5LIB; perl $1/create_species_config.pl --ftp_dir $2 --jbrowse_path $3 --out_dir $4 --species $s"
+  s="$(cut -f 1 -d . <<< $s)"
+  bsub -M4000 -o $log/${s}.out -e $log/${s}.err  "perl $(dirname $0)/create_species_config.pl --ftp_dir $1 --jbrowse_path $2 --out_dir $3 --species $s"
 done
 
