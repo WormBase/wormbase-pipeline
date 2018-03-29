cbf1785049dfcb9bef0ff58367f008ad50dba54e
Kevin Howe <klh@ebi.ac.uk>
2018-03-29 11:17:53 +0100

    Look up GFF3 files from staging area

--- scripts/make_FTP_sites.pl
+++ scripts/make_FTP_sites.pl
@@ -491,7 +491,7 @@ sub copy_gff_files{
     my $cur_ver_label = sprintf("%s.%s.WS%d", $g_species, $bioproj, $WS_version);
 
     my $prev_ver_gff3_file = sprintf("%s/releases/WS%d/species/%s/%s/%s.%s.WS%s.annotations.gff3.gz", 
-                                     $wormbase->ftp_site,
+                                     $wormbase->ftp_staging,
                                      $WS_version - 1,
                                      $g_species,
                                      $bioproj,
