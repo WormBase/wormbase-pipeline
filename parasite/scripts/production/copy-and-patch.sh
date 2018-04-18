78dd7973e3ffcb49f49808bb97948aba0699cf4c
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2018-04-18 14:50:59 +0100

    Copy and patch also tells us a thing, and clears out old database versions

--- parasite/scripts/production/copy-and-patch.sh
+++ parasite/scripts/production/copy-and-patch.sh
@@ -3,6 +3,8 @@
 ## After copy, relevant patches are applied to bring databases up to the relevant Ensembl schema
 ## Before running, load the correct module as this creates the environment variables, e.g. "module load parasite_prod_rel6"
 
+echo "These were the comparator databases: No refresh needed? "
+$PREVIOUS_PARASITE_STAGING_MYSQL -e "show databases like \"%_core_%\"" | grep -v core_${PREVIOUS_PARASITE_VERSION}
 echo "Begin database copying"
 
 databaselist=$($PREVIOUS_PARASITE_STAGING_MYSQL -NB -e 'SHOW DATABASES LIKE "%\_core\_%"')
@@ -11,9 +13,18 @@ databases=( `echo ${databaselist}` )
 for DB in "${databases[@]}"
 do
   echo "Copying $DB"
-  NEWDB=$(echo $DB | sed "s/core_$PREVIOUS_PARASITE_VERSION\_$PREVIOUS_ENSEMBL_VERSION/core_$PARASITE_VERSION\_$ENSEMBL_VERSION/" | sed "s/core_$PREVIOUS_ENSEMBL_VERSION/core_$ENSEMBL_VERSION/" | sed "s/_$PREVIOUS_WORMBASE_VERSION/_$WORMBASE_VERSION/")
+  NEWDB=$(echo $DB | sed "s/core_$PREVIOUS_PARASITE_VERSION\_$PREVIOUS_ENSEMBL_VERSION/core_$PARASITE_VERSION\_$ENSEMBL_VERSION/" | sed "s/_$PREVIOUS_ENSEMBL_VERSION/_$ENSEMBL_VERSION/" | sed "s/_$PREVIOUS_WORMBASE_VERSION$/_$WORMBASE_VERSION/")
+  echo "Looking for previous versions of $NEWDB"
+  DB_PAT=$(echo $NEWDB | sed "s/core_.*/core%/")
+  ${PARASITE_STAGING_MYSQL} -e "show databases like \"$DB_PAT\"" | grep -v Database | grep -v "$NEWDB" | while read -r OLD_DB ; do
+    echo "Dropping database $OLD_DB"
+    ${PARASITE_STAGING_MYSQL}-ensrw -e "DROP DATABASE $OLD_DB"
+  done
   echo  "Creating $NEWDB"
-  ${PARASITE_STAGING_MYSQL}-ensrw -e "CREATE DATABASE $NEWDB"
+  if ! ${PARASITE_STAGING_MYSQL}-ensrw -e "CREATE DATABASE $NEWDB"; then
+    echo "Could not create $NEWDB - should this script be running? Bailing out."
+    exit 1
+  fi
   echo "Dumping $DB to $NEWDB"
   ${PREVIOUS_PARASITE_STAGING_MYSQL}-ensrw mysqldump $DB | ${PARASITE_STAGING_MYSQL}-ensrw $NEWDB
 done
