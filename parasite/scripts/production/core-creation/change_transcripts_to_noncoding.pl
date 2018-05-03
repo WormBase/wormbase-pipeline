75c1d034a5c93def68c2e36609f8994662b5b9dd
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2018-05-03 15:00:37 +0100

    Slightly more cautious and responsible way of treating bad transcripts

--- parasite/scripts/production/core-creation/change_transcripts_to_noncoding.pl
+++ parasite/scripts/production/core-creation/change_transcripts_to_noncoding.pl
@@ -1,6 +1,10 @@
 #! /usr/bin/perl 
-# change mRNAs to noncoding
+# change mRNAs to pseudogenic_transcript
 # remove everything these had as a parent
+# what we're claiming biologically when we do this:
+# - these transcripts belong to genes that have no function
+# - they look like protein-coding genes but are not (because they have stops)
+# - they are probably pseudogenes arising from retrotansposition, i.e. copying an re-integration back into somewhere else in the genome, followed by degradation
 @ARGV or die "Usage: $0 in.gff transcripts.txt";
 
 my $f = shift;
@@ -22,20 +26,24 @@ while (<$gh>) {
   chomp;
   $ts{$_}++;
 }
-
+my %gs;
+sub extract_attribute {
+   my ($attr,$str) = @_;
+   $str =~ /$attr=(.*?);/;
+   $str =~ /$attr=(.*)$/ unless $1;
+   chomp $1;
+   return $1;
+}
 while (<$fh>) {
     my @c = split "\t";
     if ( @c[2] eq "mRNA" ) {
-        @c[8] =~ /ID=(.*?);/;
-        @c[8] =~ /ID=(.*)$/ unless $1;
-        warn "@c[8] without an ID? " unless $1;
-        chomp $1;
-        @c[2] = "ncRNA" if $ts{$1};
+        my $id = &extract_attribute("ID", @c[8]);
+        my $parent =  &extract_attribute("Parent", @c[8]);
+        warn "@c[8] without an ID? " unless $id;
+        @c[2] = "pseudogenic_transcript" if $ts{$id};
+        warn "@c[8] pseudogene with multiple transcripts? " if $ts{$id} and ++$gs{$parent} > 1 ;
     } else {
-        @c[8] =~ /Parent=(.*?);/;
-        @c[8] =~ /Parent=(.*)$/ unless $1;
-        chomp $1;
-        @c = qw// if $ts{$1};
+        @c = qw// if $ts{&extract_attribute("Parent", @c[8])};
     }
     print join "\t", @c;
 }
