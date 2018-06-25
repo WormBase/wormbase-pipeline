050ee672e3537b8db209666cfd508483771c9ed5
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2018-06-25 16:03:39 +0100

    Tracks.pm can become trackList.json

--- parasite/modules/GenomeBrowser/Tracks.pm
+++ parasite/modules/GenomeBrowser/Tracks.pm
@@ -1,6 +1,7 @@
 
 package GenomeBrowser::Tracks;
 
+use ProductionMysql;
 use GenomeBrowser::ArrayExpressMetadata;
 use GenomeBrowser::RnaseqerMetadata;
 use GenomeBrowser::Studies;
@@ -13,31 +14,29 @@ use GenomeBrowser::Factors;
 # Convert me to JBrowse format
 # Dump me to yaml to see what I'm like
 # Use me to move files EBI -> Sanger
-
-# TODO 
-# Figure out what's up with hubs 
-# Do we need more data fetched from somewhere?
-# make yourself a FactorsCurationWizard
+# Create hub.txt
 sub new {
-  my ($class, $root_dir, $species, $assembly) = @_;
-  my $rnaseqer_metadata = GenomeBrowser::RnaseqerMetadata->new($root_dir, $species);
-  my $array_express_metadata = GenomeBrowser::ArrayExpressMetadata->new($root_dir, $species);
-  my $studies = GenomeBrowser::Studies->new($root_dir, $species, $assembly, $rnaseqer_metadata); 
-  my $factors = GenomeBrowser::Factors->new($root_dir, $species, $assembly, $rnaseqer_metadata, $array_express_metadata);
+  my ($class, $root_dir, $core_db) = @_;
+  my $assembly = ProductionMysql->staging->meta_value($core_db, "assembly.name");
+  my ($spe, $cies, $bioproject) = split "_", $core_db;
+  my $rnaseqer_metadata = GenomeBrowser::RnaseqerMetadata->new($root_dir, "${spe}_${cies}");
+  my $array_express_metadata = GenomeBrowser::ArrayExpressMetadata->new($root_dir, "${spe}_${cies}");
+  my $studies = GenomeBrowser::Studies->new($root_dir, "${spe}_${cies}", $assembly, $rnaseqer_metadata); 
+  my $factors = GenomeBrowser::Factors->new($root_dir, "${spe}_${cies}", $assembly, $rnaseqer_metadata, $array_express_metadata);
   my %data;
   for my $study_id (@{$rnaseqer_metadata->access($assembly)}){
     my $study_attributes = $studies->{$study_id};
     for my $run_id (@{$rnaseqer_metadata->access($assembly, $study_id)}){
        my $run_attributes = $rnaseqer_metadata->{$assembly}{$study_id}{$run_id};
        my %attributes = %{{ %$study_attributes, %$run_attributes }};
-       $attributes{Path} = &_public_url($run_id);
-       $attributes{Label} = &_label($run_id,
+       $attributes{label} = &_label($run_id,
           map {exists $attributes{$_} ? $attributes{$_}: ()} @$factors
        );
+       $attributes{urlTemplate} = "http://ngs.sanger.ac.uk/production/parasites/wormbase/RNASeq_alignments/${spe}_${cies}_${bioproject}/$run_id.bw";
        $data{$run_id}=\%attributes;
     }
   }
-  return bless \%data, $class;
+  return bless {tracks => \%data }, $class;
 }
 
 sub _label {
@@ -48,6 +47,24 @@ sub _label {
   ); 
 }
 
+our $TRACK_STANZA = {
+  storeClass => "JBrowse/Store/SeqFeature/BigWig",
+  type => "JBrowse/View/Track/Wiggle/XYPlot",
+  category => "RNASeq",
+  autoscale => "local",
+  ScalePosition => "right",
+};
+
+sub as_jbrowse_track_list {
+  my $self = shift;
+
+  my @result;
+  for my $track_id (keys $self->{tracks}){
+     push @result, {%{$self->{tracks}{$track_id}}, %$TRACK_STANZA};
+  }
+  sort { scalar (%$b) <=> scalar (%$a) || $a->{label} cmp $b->{label} } @result;
+  return { tracks => \@result };
+}
 sub list_names {
    return keys shift;
 }
@@ -62,14 +79,8 @@ sub storage_path {
   return "sanger:/ngs_folder/$track_id.bw";
 }
 
-
-sub as_jbrowse_config {
-  my $self = shift;
-  return "Perl object that can be converted to tracks.conf, probably with IniFiles";
-}
-
 sub _public_url {
-  my $track_id = shift;
-  return "https://ngs.sanger.ac.uk/$track_id.bw";
+  my ($self, $track_id) = @_;
+  return $self->{$track_id}{urlTemplate};
 }
 1;
