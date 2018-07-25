01df20e45b4b3b44205b58b83e8a2c696e43a977
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2018-07-25 11:56:20 +0100

    do_sync=0 : never sync. do_sync=1: force sync. Otherwise: check if file is online, then sync if not.

--- parasite/modules/GenomeBrowser/Deployment.pm
+++ parasite/modules/GenomeBrowser/Deployment.pm
@@ -28,7 +28,7 @@ sub sync_ebi_to_sanger {
   my ($run_id, $source_url, %opts) = @_;
   my $target_path = location ($SANGER_PATH, $run_id);
   my $target_dir = dirname $target_path;
-  if ($opts{force_sync} or not file_is_online($run_id)){
+  if ($opts{do_sync} // file_is_online($run_id)){
     run_in_sanger("mkdir -p $target_dir");
     run_in_sanger("wget --continue --no-verbose -O $target_path $source_url");
   }
