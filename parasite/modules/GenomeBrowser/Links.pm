2117c35e3ebfc067c0545bc76b567430e9c5e1c6
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2018-08-02 12:02:17 +0100

    Nicer links

--- parasite/modules/GenomeBrowser/Links.pm
+++ parasite/modules/GenomeBrowser/Links.pm
@@ -3,15 +3,19 @@ use List::Util qw(sum);
 use List::MoreUtils qw(uniq);
 use parent GenomeBrowser::LocallyCachedResource;
 
+sub _link {
+   my ($url, $name) = @_;
+   return "<a href=\"$url\">$name</a>";
+}
 sub _fetch {
     my ( $class, $species, $assembly, $rnaseqer_metadata ) = @_;
     my %data;
     for my $study_id ( @{ $rnaseqer_metadata->access($assembly) } ) {
         for my $run_id (@{ $rnaseqer_metadata->access($assembly, $study_id)}) {
-            $data{$run_id}{"Link: RNASeq-er analysis results"} =
-               $rnaseqer_metadata->data_location($run_id);
-            $data{$run_id}{"Link: ENA study page"} =
-               "http://www.ebi.ac.uk/ena/data/view/$study_id";
+            $data{$run_id}{"Mapping results"} =
+               _link($rnaseqer_metadata->data_location($run_id), "RNASeq-er processing directory: $run_id");
+            $data{$run_id}{"ENA study"} =
+               _link("http://www.ebi.ac.uk/ena/data/view/$study_id", "Study page: $study_id");
         }
     }
     return \%data;
