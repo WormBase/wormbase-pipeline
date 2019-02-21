3b76030cdcc58c9509d96de4a4d12e6609b3291a
Michael Paulini <michael.paulini@wormbase.org>
2019-02-21 13:45:47 +0000

    cleanup

--- scripts/GENEACE/get_gene_ids.pl
+++ scripts/GENEACE/get_gene_ids.pl
@@ -1,45 +1,40 @@
 #!/software/bin/perl -w
-
 use lib $ENV{'CVS_DIR'};
-use lib $ENV{'CVS_DIR'};
-
 use lib '/software/worm/lib/perl';
+
 use NameDB_handler;
 use Wormbase;
 use Storable;
 use Getopt::Long;
 
-#connect to name server and set domain to 'Gene'
+# connect to name server and set domain to 'Gene'
 
-my $PASS;
-my $USER;
 my $DOMAIN  = 'Gene';
-my ($debug, $test, $store, $species, $database);
+my ($PASS,$USER,$debug, $test, $store, $species, $database);
 
 GetOptions (
-	    "debug=s"    => \$debug,
-	    "test"       => \$test,
-	    "store:s"    => \$store,
-	    "species:s"  => \$species,
-	    "database:s" => \$database,
-	    "user:s"	 => \$USER,
-	    "pass:s"	 => \$PASS,
-	   );
+	    'debug=s'    => \$debug,
+	    'test'       => \$test,
+	    'store:s'    => \$store,
+	    'species:s'  => \$species,
+	    'database:s' => \$database,
+	    'user:s'	 => \$USER,
+	    'pass:s'	 => \$PASS,
+	   )||die();
 
 
 my $wormbase;
-
 die "Species option is mandatory\n" unless $species;
 
-
-if ( $store ) {
+if ($store) {
   $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
 } else {
-  $wormbase = Wormbase->new( -debug   => $debug,
-                             -test    => $test,
+  $wormbase = Wormbase->new( -debug    => $debug,
+                             -test     => $test,
                              -organism => $species
 			     );
 }
+
 # establish log file.
 my $log = Log_files->make_build_log($wormbase);
 my $DB = $wormbase->test ? 'test_wbgene_id;utlt-db:3307' : 'nameserver_live;web-wwwdb-core-02:3449';
@@ -54,4 +49,3 @@ while (<>) {
 }
 
 $log->mail;
-exit;
