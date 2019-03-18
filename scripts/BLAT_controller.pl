acc009109c1b7a7d982303983b8882333d55f296
Paul-Davis <pad@ebi.ac.uk>
2019-03-18 20:16:46 +0000

    Removed Trinity from pristionchus

--- scripts/BLAT_controller.pl
+++ scripts/BLAT_controller.pl
@@ -73,7 +73,7 @@ my %mol_types = ( 'elegans'          => [qw( EST mRNA ncRNA OST tc1 RST Trinity)
 		  'brenneri'         => [qw( mRNA EST)],
 		  'japonica'         => [qw( mRNA EST Trinity)],
 		  'brugia'           => [qw( mRNA EST Trinity)],
-		  'pristionchus'     => [qw( mRNA EST Trinity)],
+		  'pristionchus'     => [qw( mRNA EST)],
                   'ovolvulus'        => [qw( mRNA EST Trinity)],
                   'sratti'           => [qw( mRNA EST Trinity)],
 		  'tmuris'           => [qw( mRNA EST Trinity IsoSeq)],
