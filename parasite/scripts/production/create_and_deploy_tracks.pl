38551e693098c549ef3a0e9a89081f7346f4839c
Wojtek Bazant <wojtek.bazant@sanger.ac.uk>
2019-04-10 16:44:29 +0100

    Hub also in run script

--- parasite/scripts/production/create_and_deploy_tracks.pl
+++ parasite/scripts/production/create_and_deploy_tracks.pl
@@ -14,30 +14,42 @@ use lib "/nfs/panda/ensemblgenomes/wormbase/parasite/wbps-expression/local/lib/p
 use WbpsExpression;
 
 use GenomeBrowser::JBrowseDisplay;
+use GenomeBrowser::RnaseqHub;
+
 my @species;
-my $do_sync = 0;
+my $jbrowse_skip = 0;
+my $hub_skip = 0;
+my $sanger_deployment_skip = 0;
 my $jbrowse_tools_skip = 0;
 my $jbrowse_tools_nofatal = 0;
 
 GetOptions(
   'species=s@'    => \@species,
-  'do_sync' => \$do_sync,
+  'jbrowse_skip' => \$jbrowse_skip,
+  'hub_skip' => \$hub_skip,
+  'sanger_deployment_skip' => \$sanger_deployment_skip,
   'jbrowse_tools_skip' => \$jbrowse_tools_skip,
   'jbrowse_tools_nofatal' => \$jbrowse_tools_nofatal,
 ) || exit_with_usage();
 
-
-
-
 for my $species (@species){
-  GenomeBrowser::JBrowseDisplay->new->make_tracks(
+  next if $jbrowse_skip;
+  GenomeBrowser::JBrowseDisplay->new->make_displays(
     $species,
-    do_sync=>$do_sync,
+    sanger_deployment_skip=>$sanger_deployment_skip,
     jbrowse_tools_skip => $jbrowse_tools_skip,
     jbrowse_tools_nofatal=> $jbrowse_tools_nofatal,
   );
 }
+for my $species (@species){
+  next if $hub_skip;
+  GenomeBrowser::RnaseqHub->new->make_hubs(
+    $species,
+    sanger_deployment_skip=>$sanger_deployment_skip,
+  );
+}
 
+# TODO copy to FTP, too
 sub exit_with_usage {
   print STDERR "Usage: $0 <params>";
   exit 1;
