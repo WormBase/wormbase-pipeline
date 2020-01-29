be56c1356053bed6bea1284f310119d3e4426ec0
Michael Paulini <michael.paulini@wormbase.org>
2020-01-29 14:22:38 +0000

    updated for 3.0.0

--- scripts/AGR/make_agr_gff.pl
+++ scripts/AGR/make_agr_gff.pl
@@ -1,50 +1,46 @@
 #!/usr/bin/env perl
 
 use strict;
-use Storable;	
 use Getopt::Long;
 
 use lib $ENV{CVS_DIR};
-use Wormbase;
 use Modules::AGR;
 
-my ($help, $debug, $test, $verbose, $store, $wormbase, $bgi_json);
-my ($gff_in, $gff_out, $ws_version);
+my ($bgi_json, $gff_in, $gff_out, $ws_version);
 
-GetOptions ("help"        => \$help,
-            "debug=s"     => \$debug,
-	    "test"        => \$test,
-	    "verbose"     => \$verbose,
-	    "store:s"     => \$store,
+GetOptions (
 	    "gffout:s"    => \$gff_out,
             "gffin=s"     => \$gff_in,
             "bgijson=s"   => \$bgi_json,
             "wsversion=s" => \$ws_version,
-	    );
+	   )||die();
 
-if ( $store ) {
-  $wormbase = retrieve( $store ) or croak("Can't restore wormbase from $store\n");
-} else {
-  $wormbase = Wormbase->new( -debug   => $debug,
-                             -test    => $test,
-      );
-}
-
-die "You must supply an input GFF file\n" if not defined $gff_in or not -e $gff_in;
-die "You must supply an output file name\n" if not defined $gff_out;
-die "You must supply an BGI JSON file\n" if not defined $bgi_json;
+$gff_in   || die "You must supply an input GFF file\n";
+$gff_out  || die "You must supply an output file name\n";
+$bgi_json || die "You must supply an BGI JSON file\n";
 
 my %bgi_genes = %{AGR::get_bgi_genes($bgi_json)};
+my $date      = AGR::get_rfc_date();
 
 my $in_fh = &open_gff_file($gff_in);
 open(my $out_fh, ">$gff_out") or die "Could not open $gff_out for writing\n";
 print $out_fh "# WormBase release $ws_version\n" if defined $ws_version;
 
+my %headers; # do not print redundant headers
+
 while(<$in_fh>) {
-  /^\#/ and do {
-    print $out_fh $_;
+  if (/^\#/){
+    print $out_fh $_ unless $headers{$_};
+    $headers{$_}=1;
+    if (/gff-version 3/){
+	    print $outfh<<HERE;
+#!date-prodiced $date
+#!data-source WormBase
+#!assembly WBcel235
+HERE
+    }
     next;
-  };
+  }
 
   if (/^\S+\s+WormBase\s+gene\s+/) {
     chomp;
@@ -64,8 +60,20 @@ while(<$in_fh>) {
     if (exists $bgi_genes{$gid}->{geneSynopsis}) {
       $attr{description} = $bgi_genes{$gid}->{geneSynopsis};
     }
+    $attr{Ontology_term}='SO:0000704';
+
     $l[8] = join(";", map { $_ . "=" . $attr{$_} } keys %attr);
     print $out_fh join("\t", @l), "\n";
+
+  } elsif(/^\S+\sWormBase\smRNA/){
+          my @l = split(/\t/, $_);
+	  my $ontologyID = 'SO:0000234';
+	  /Name=([^;]+)/;
+	  my $transcriptID="$1";
+	  /(WBGene\d+)/;
+          my $geneID="$1";
+	  s/;Parent/curie=WB:$transcriptID;curie=WB:$geneID;Ontology_term=$ontologyID;Parent/;
+	  print $out_fh $_;
 
   } elsif (/^\S+\s+WormBase\s+/) {
     print $out_fh $_;
@@ -74,7 +82,6 @@ while(<$in_fh>) {
 close($out_fh) or die "Could not close $gff_out after writing (probably file system full)\n";
 
 exit(0);
-
 
 sub open_gff_file {
   my ($file) = @_;
