fd948b866b95a64e573df04ca770762acb8cac03
Michael Paulini <michael.paulini@wormbase.org>
2020-03-16 10:25:56 +0000

    Update make_agr_disease_json.pl
    fixed the capitalisation

--- scripts/AGR/make_agr_disease_json.pl
+++ scripts/AGR/make_agr_disease_json.pl
@@ -170,7 +170,7 @@ while( my $obj = $it->next) {
     push @with_list, "WBTransgene:" . $transgene->name if (defined $transgene && $build);
 
   } elsif (defined $transgene) {
-    $obj_type = $build ? 'Transgene' : 'Allele';# as quick fix for 3.0
+    $obj_type = $build ? 'transgene' : 'allele';# as quick fix for 3.0
     $obj_name = $transgene->Public_name->name;
     $assoc_type = 'is_implicated_in';
     $obj_id = 'WB:' . $transgene->name;
