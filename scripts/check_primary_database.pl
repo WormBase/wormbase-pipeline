f8e32bd13cb644f7411f47dda612e41edd6745b9
Paul-Davis <pad@ebi.ac.uk>
2020-04-02 14:35:04 +0100

    Removed the old NS checking call.

--- scripts/check_primary_database.pl
+++ scripts/check_primary_database.pl
@@ -108,9 +108,9 @@ if ((-e $tmuris) && ($species eq 'tmuris')) {push(@xaceinstances,"$tmuris");}
 
 # example of running another script
 
-foreach $xaceinstances (@xaceinstances) {
-  print "running camace_nameDB_comm.pl -database $xaceinstances\n";
-  $wormbase->run_script("NAMEDB/camace_nameDB_comm.pl -database $xaceinstances", $log);
+#foreach $xaceinstances (@xaceinstances) {
+#  print "running camace_nameDB_comm.pl -database $xaceinstances\n";
+#  $wormbase->run_script("NAMEDB/camace_nameDB_comm.pl -database $xaceinstances", $log);
   
   # Run additional checks for camace
   if ($xaceinstances eq "/nfs/wormpub/BUILD/PRIMARIES/camace"){
