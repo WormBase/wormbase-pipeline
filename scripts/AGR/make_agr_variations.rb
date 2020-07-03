26a5d4971473785bbb7d5cf83ae5f5242239c3e1
Michael Paulini <michael.paulini@wormbase.org>
2020-07-03 10:06:34 +0100

    * added support for TableMaker queries and keysets * added documentation (-h) and nicer commandline options

--- scripts/AGR/make_agr_variations.rb
+++ scripts/AGR/make_agr_variations.rb
@@ -1,24 +1,93 @@
 #!/usr/bin/env ruby
-#
-# THIS IS JUST A DRAFT, DON'T USE IN ANGER
-#
-
-# YAML file can be created from a TSV, as example from Table Maker
-# perl -anE 'BEGIN{use YAML}$h{$F[0]}{name}=$F[0];$h{$F[0]}{paper}{$F[1]} = $F[2]||"n/a" if $F[1]; END{say Dump(\%h)}'
-# looks like:
-# ---
-# WBVar00000001:
-#   name: WBVar00000001
-#   paper:
-#     WBPaper00000932: 2428682
-#     WBPaper00014052: n/a
-#
-# usage: ruby make_agr_variations.rb <filter.yaml> <genomic fasta> <wormbase gff3>
 
 require 'bio'
 require 'date'
 require 'json'
-require 'yaml'
+require 'optparse'
+
+Options = Struct.new(:fasta,:gff,:db,:ws_version,:wquery)
+
+class Parser
+	def self.parse(options)
+		args = Options.new("world")
+
+		args.wquery = "agr_variations.def" # default
+
+		opt_parser = OptionParser.new do |opts|
+
+			opts.banner = "Usage: make_agr_variations.rb [options]"
+
+			opts.on("-f","--file NAME","FASTA file") do |f|
+				args.fasta = f
+			end
+
+			opts.on("-g","--gff NAME","GFF file") do |g|
+				args.gff = g
+			end
+
+			opts.on("-d","--database NAME","ACeDB database directory") do |d|
+				args.db = d
+			end
+
+			opts.on("-w","--ws_version NAME","WormBase release (a.e. WS277)") do |v|
+				args.ws_version = v
+			end
+
+			opts.on("-q","--wquery [DEFFILE]","Table-Maker DEF file") do |t|
+				args.wquery = t
+			end
+
+			opts.on("-h","--help","print help") do
+				puts opts
+				exit
+			end
+		end
+
+		opt_parser.parse!(options)
+		return args
+	end
+end
+
+class TableMaker
+	def initialize(giface,database)
+		@giface = giface
+		@database = database
+	end
+
+	# as example query="query find Variation *;Reference"
+	def execute_wquery(query,wquery,outfile='/tmp/tablemaker.outfile')
+		IO.popen("#{@giface} #{@database}","r+"){|pipe|
+			unless query.nil? # will quey all variation
+				pipe.puts query
+				pipe.puts "Table-maker -active -o #{outfile} #{wquery}"
+			else # query only the current keyset from the query
+				pipe.puts "Table-maker -o #{outfile} #{wquery}"
+			end
+			pipe.puts "quit"
+			Process.wait(pipe.pid)
+		}
+
+                return self.parse_output(outfile)
+	end
+
+	# that one is specific to that particular query, so probably not too generally useful
+	def parse_output(outfile)
+		results=Hash.new
+		File.open(outfile).each{|line|
+			line.chomp!
+			line.gsub!('"','')
+			c = line.split
+			results[c[0]]=Hash.new # WBVarXXX
+			results[c[0]]["name"] = c[0] # WBVarXXX
+			if columns[1]
+				results[c[0]]["paper"]=Hash.new # WBPaperXXX
+				results[c[0]]["paper"][c[1]] = c[2] || 'n/a' # PubmedID
+			end
+		}
+		File.unlink(outfile)
+		return results
+	end
+end
 
 term2so = {
 	'insertion_site' => 'SO:0000667', # changed insertion_site to insertion
@@ -37,16 +106,21 @@ chrom2ncbi = {
 	'MtDNA' => 'RefSeq:NC_001328.1',
 }
 
+options = Parser.parse(ARGV)
+
 variations = Array.new
 chromosomes= Hash.new
 
-filter = YAML.load_file(ARGV.shift)
+# hardcoded giface ... which is probably not needed
+tablemaker = TableMaker.new('/nfs/panda/ensemblgenomes/wormbase/software/packages/acedb/RHEL7/4.9.62/giface',options.db)
+filter = tablemaker.execute_wquery("query find Variation *;Reference",options.wquery) 
 
-Bio::FlatFile.open(Bio::FastaFormat, ARGV.shift).each_entry{|e|
+Bio::FlatFile.open(Bio::FastaFormat, options.fasta).each_entry{|e|
    chromosomes[e.entry_id.to_s] = e.seq
 }
 
-ARGF.each_line{|line|
+# parse from the GFF the respective variation lines
+File.open(options.gff).each{|line|
   variation = Hash.new
   cols = line.split("\t")
   next unless term2so[cols[2]]
@@ -61,6 +135,7 @@ ARGF.each_line{|line|
   variation[:chromosome] = cols[0]
   variation[:assembly] = 'WBcel235'
   variation[:type]=term2so[cols[2]]
+
 
   if cols[2].eql?('insertion_site')
 	  next unless cols[8]=~/insertion=([^;]+)/
@@ -78,12 +153,18 @@ ARGF.each_line{|line|
 
   elsif ['point_mutation','substitution'].include?(cols[2]) # multi-bp substitutions need to become DELINS in the future
 	  next unless cols[8]=~/substitution=/ # to skip the crispr/cas9 alleles
-	  next unless variation[:start] == variation[:end] # only allow single bp "substitutions"
+
+	  next unless variation[:start] == variation[:end] # only allow single bp delin "substitutions" due to AGR limitations
+
 	  to = cols[8][/substitution=([^;]+)/,1].split('/')[1]
 	  variation[:genomicReferenceSequence] = chromosomes[cols[0]].subseq(variation[:start],variation[:end])
 	  s = Bio::Sequence::NA.new(to)
 	  s.complement! if cols[6].eql?('-') # as all variations are supposed to be on the forward strand
 	  variation[:genomicVariantSequence] = s.to_s.upcase
+
+	  if to.length == 1 # WS276 hack for the missing term, so if from is 1bp and to is 1bp it is a point mutation
+	          variation[:type]='SO:1000008' 	  
+	  end
   end
   
   variation[:references] = filter[variation[:alleleId]]["paper"].map{|k,v| # creates the publication part 
@@ -105,7 +186,7 @@ meta = {
                 :type => 'curated'
 	},
 	:dateProduced => DateTime.now.to_s,
-	:release => ENV['WS_RELEASE']
+	:release => options.ws_version
 }
 
 bleh =  {:data => variations, :metaData => meta}
