946fbf61c4c1df4f5051703a7e87527de1bd7160
Michael Paulini <michael.paulini@wormbase.org>
2020-07-08 16:54:16 +0100

    * lower-case the sex names * add the life-stages to the microarrays

--- scripts/AGR/make_agr_HTS.pl
+++ scripts/AGR/make_agr_HTS.pl
@@ -73,10 +73,8 @@ while (my $analysis = $it->next){
 		}; # required
 		$json_obj{sampleTitle} = $subproject->Title->name;
 		$json_obj{taxonId} = $wormbase->ncbi_tax_id;
-
 		$json_obj{datasetId} = [$datasetId];
-
-		$json_obj{sex}=$sample->Sex->name if $sample->Sex;
+		$json_obj{sex}=lc($sample->Sex->name) if $sample->Sex;
 
 		if ($sample->Life_stage){
 			my $ls=$sample->Life_stage;
@@ -161,8 +159,24 @@ while (my $array = $it->next){
 
 	$json_obj{taxonId} = $wormbase->ncbi_tax_id;
 	#	$json_obj{datasetId} = [$datasetId];
-	$json_obj{sex}=$sample->Sex->name if $sample->Sex;
+	$json_obj{sex}=lc($sample->Sex->name) if $sample->Sex;
 	$json_obj{sampleAge}="WB:${\$sample->Life_stage->name}" if $sample->Life_stage;
+	if ($sample->Life_stage){
+		my $ls=$sample->Life_stage;
+		my $uterm = 'post embryonic, pre-adult';
+		if ($uberon{$ls->name}){
+			 ($uterm) = keys %{$uberon{$ls->name}};
+		}
+		next unless $ls && $ls->Public_name;
+		$json_obj{sampleAge}={stage => { stageTermId => 'WB:'.$ls->name,
+			                         stageName => $ls->Public_name->name ,
+						 stageUberonSlimTerm => {uberonTerm => $uterm},
+					       }
+			 	     };
+	}
+
+
+
 	if ($sample->Tissue){
 		$json_obj{sampleLocation}=[];
 		map {push @{$json_obj{sampleLocation}},"WB:$_"} $sample->Tissue;
