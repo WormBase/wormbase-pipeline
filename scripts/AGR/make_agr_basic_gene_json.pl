4b95f7bf24dfd9e03dc69df37064a65eda6fb14a
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2020-08-27 18:45:16 +0100

    Remove incorrect logic for phenotype check in AGR BGI JSON script

--- scripts/AGR/make_agr_basic_gene_json.pl
+++ scripts/AGR/make_agr_basic_gene_json.pl
@@ -188,8 +188,7 @@ foreach my $sub_query (
     }
 
     # Back-to-mod xrefs
-    my @baseXrefs = qw(gene gene/expression gene/references);
-    push @baseXrefs, 'gene/phenotypes' if $obj->Phenotype;
+    my @baseXrefs = qw(gene gene/expression gene/references gene/phenotypes);
     push @baseXrefs, 'gene/expression_images' if ($obj->Expr_pattern && grep {$_->Picture} $obj->Expr_pattern);
 
     push @xrefs, {
