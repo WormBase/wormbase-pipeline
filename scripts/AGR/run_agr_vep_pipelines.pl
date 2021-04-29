0df218615fa29b42f2f300103f7906514da3e12b
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2021-04-29 10:54:59 +0100

    Fix human RefSeq chromosome ID map

--- scripts/AGR/run_agr_vep_pipelines.pl
+++ scripts/AGR/run_agr_vep_pipelines.pl
@@ -164,31 +164,32 @@ const my %REFSEQ_CHROMOSOMES => (
 	'MT' => 'NC_002333.2',
     },
     'HUMAN' => {
-	'1'  => 'NC000001.11',
-	'2'  => 'NC000002.12',
-	'3'  => 'NC000003.12',
-	'4'  => 'NC000004.12',
-	'5'  => 'NC000005.10',
-	'6'  => 'NC000006.12',
-	'7'  => 'NC000007.14',
-	'8'  => 'NC000008.11',
-	'9'  => 'NC000009.12',
-	'10' => 'NC000010.11',
-	'11' => 'NC000011.10',
-	'12' => 'NC000012.12',
-	'13' => 'NC000013.11',
-	'14' => 'NC000014.9',
-	'15' => 'NC000015.10',
-	'16' => 'NC000016.10',
-	'17' => 'NC000017.11',
-	'18' => 'NC000018.10',
-	'19' => 'NC000019.10',
-	'20' => 'NC000020.11',
-	'21' => 'NC000021.9',
-	'22' => 'NC000022.11',
-	'X'  => 'NC0000023.11',
-	'Y'  => 'NC0000024.10',
-	'MT' => 'NC012920.1'
+
+	'1'  => 'NC_000001.11',
+	'2'  => 'NC_000002.12',
+	'3'  => 'NC_000003.12',
+	'4'  => 'NC_000004.12',
+	'5'  => 'NC_000005.10',
+	'6'  => 'NC_000006.12',
+	'7'  => 'NC_000007.14',
+	'8'  => 'NC_000008.11',
+	'9'  => 'NC_000009.12',
+	'10' => 'NC_000010.11',
+	'11' => 'NC_000011.10',
+	'12' => 'NC_000012.12',
+	'13' => 'NC_000013.11',
+	'14' => 'NC_000014.9',
+	'15' => 'NC_000015.10',
+	'16' => 'NC_000016.10',
+	'17' => 'NC_000017.11',
+	'18' => 'NC_000018.10',
+	'19' => 'NC_000019.10',
+	'20' => 'NC_000020.11',
+	'21' => 'NC_000021.9',
+	'22' => 'NC_000022.11',
+	'X'  => 'NC_000023.11',
+	'Y'  => 'NC_000024.10',
+	'MT' => 'NC_012920.1'
     },
     );
 
@@ -318,7 +319,7 @@ sub check_if_actually_compressed {
 sub process_input_files {
     my $mod = shift;
     
-    cleanup_intermediate_files($mod);
+    #cleanup_intermediate_files($mod);
 
     my $chr_map;
     check_chromosome_map($mod);
@@ -510,7 +511,7 @@ sub sort_vcf_files {
 sub munge_gff {
     my ($mod) = @_;
     
-    system_cmd("rm ${mod}_GFF.refseq.gff", "Deleting old munged GFF file") if -e "${mod}_GFF.refseq.gff";
+    run_system_cmd("rm ${mod}_GFF.refseq.gff", "Deleting old munged GFF file") if -e "${mod}_GFF.refseq.gff";
 
     my $reverse_map = get_reverse_chromosome_map($mod);
     
@@ -522,15 +523,13 @@ sub munge_gff {
 	next if $line =~ /^\n$/;
 	
 	my @columns = split("\t", $_);
-	if ($mod ne 'HUMAN') {
-	    if (exists $REFSEQ_CHROMOSOMES{$mod}{$columns[0]}) {
-		$columns[0] = $REFSEQ_CHROMOSOMES{$mod}{$columns[0]};
-		$line = join("\t", @columns);
-	    }
-	    else {
-		die "Could not map $mod chromosome in GFF " . $columns[0] . " to RefSeq ID\n"
-		    unless exists $reverse_map->{$columns[0]};
-	    }
+	if (exists $REFSEQ_CHROMOSOMES{$mod}{$columns[0]}) {
+	    $columns[0] = $REFSEQ_CHROMOSOMES{$mod}{$columns[0]};
+	    $line = join("\t", @columns);
+	}
+	else {
+	    die "Could not map $mod chromosome in GFF " . $columns[0] . " to RefSeq ID\n"
+		unless exists $reverse_map->{$columns[0]};
 	}
 	
 	if ($mod eq 'FB') {
