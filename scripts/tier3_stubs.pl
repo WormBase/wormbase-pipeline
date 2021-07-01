945ff527773831fd3a5155c9cf26cb0983caf2bb
Kevin Howe <klh@ebi.ac.uk>
2021-07-01 17:38:02 +0100

    Do not add BioProject prefix to tier3 genes - these are now made unique upstream

--- scripts/tier3_stubs.pl
+++ scripts/tier3_stubs.pl
@@ -7,14 +7,16 @@ use Storable;
 use Wormbase;
 use Bio::SeqIO;
 
-my ($wormbase,$test,$debug, $store, $acefile, $noload, $database);
+my ($wormbase,$test,$debug, $store, $acefile, $noload, $database, $single);
 
 GetOptions( "debug=s"     => \$debug,
             "test"        => \$test,
             "acefile=s"   => \$acefile,
             "database=s"  => \$database,
             "noload"      => \$noload,
-            "store:s"     => \$store);
+            "store:s"     => \$store, 
+            "species=s"   => \$single,
+    );
 
 
 if ( $store ) {
@@ -31,13 +33,14 @@ my $log = Log_files->make_build_log($wormbase);
 $acefile = $wormbase->acefiles . "/tier3_stubs.ace" if not defined $acefile;
 $database = $wormbase->autoace if not defined $database;
 
-
-
 open(my $acefh, ">$acefile") or $log->log_and_die("Could not open $acefile for writing\n");
 
 my %accessors = $wormbase->tier3_species_accessors;
 
-while (my($species,$wb)=each %accessors){
+my @species = (defined $single) ? ($single) : (sort keys %accessors);
+
+foreach my $species (@species) {
+  my $wb = $accessors{$species};
   my $seqdir = $wb->sequences;
   my $gff3 = sprintf("%s/%s.gff3", $wb->sequences, $species);
   my $prot = sprintf("%s/%s.prot.fa", $wb->sequences, $species);
@@ -54,17 +57,6 @@ while (my($species,$wb)=each %accessors){
     next;
   }
 
-  while (my $seq = $prot_seqio->next_seq) {
-    my $id = sprintf("%s_%s", $wb->ncbi_bioproject, $seq->id);
-    printf($acefh "\nProtein : \"%s\"\n", $id);
-    printf($acefh "Species \"%s\"\n", $wb->full_name);
-    printf($acefh "Corresponding_CDS \"%s\"\n", $id);
-    print $acefh "Live\n";
-
-    printf($acefh "\nPeptide : \"%s\"\n", $id);
-    printf($acefh "%s\n", $seq->seq);
-  }
-
   if (-e $gff3) {
     open($gff3_fh, $gff3);
   } elsif (-e "$gff3.gz") {
@@ -73,7 +65,7 @@ while (my($species,$wb)=each %accessors){
     $log->log_and_die("Could not find $gff3 or $gff3.gz");
   }
 
-  my (%geneid2name, %cdsname2geneid);
+  my (%genes, %cds);
 
   while(<$gff3_fh>) {
     /^\#/ and next;
@@ -91,48 +83,60 @@ while (my($species,$wb)=each %accessors){
 
       next unless($name && $id); # both need to be defined to produce a valid ace-file
 
-      my $ace_name = sprintf("%s_%s", $wb->ncbi_bioproject, $name);
-      my $biop =  $wb->ncbi_bioproject;
-
       if ($l[2] eq 'gene') {
-        $geneid2name{$id} = $ace_name;
-
-        printf($acefh "\nGene : \"%s\"\n", $ace_name);
-	if ($name =~ /^WBGene\d{8}/) {
-	    printf($acefh "Public_name \"${biop}_%s\"\n", $name);
-	}
-	else {
-	    printf($acefh "Public_name \"%s\"\n", $name);
-	}
-        printf($acefh "Species \"%s\"\n", $wb->full_name);
-        print $acefh "Method \"Gene\"\n";
-        print $acefh "Remark \"Tier3 species gene\" Inferred_automatically \"tier3_stubs.pl\"\n";
-        print $acefh "Live\n";
+        $genes{$id}->{name} = $name;
+        $genes{$id}->{id} = $id;
       } else {
         my ($parent_attr) = grep { /^Parent=/ } @attr;
         my ($parent) = $parent_attr =~ /Parent=(\S+)/;
-        $cdsname2geneid{$ace_name} = $parent; 
- 
-        printf($acefh "\nCDS : \"%s\"\n", $ace_name);
-        printf($acefh "Species \"%s\"\n", $wb->full_name);
-        print $acefh "Method \"curated\"\n";
-        printf($acefh "Database WormBase TierIII \"%s\"\n", $name);
-        print $acefh "Remark \"Tier3 species gene\" Inferred_automatically \"tier3_stubs.pl\"\n";
-        print $acefh "Coding CDS\n";
-        print $acefh "From_laboratory \"HX\"\n";
+        $cds{$name}->{name} = $name;
+        $cds{$name}->{id} = $id;
+        $cds{$name}->{gene} = $genes{$parent}->{name};
       }
     }
   }
 
-  foreach my $cds_name (sort keys %cdsname2geneid) {
-    my $gene_id = $cdsname2geneid{$cds_name};
-    $log->log_and_die("Could not find gene name for $cds_name\n") 
-        if not exists $geneid2name{$gene_id};
-    my $gene_name = $geneid2name{$gene_id};
+  while (my $seq = $prot_seqio->next_seq) {
+    my $prot_name = $seq->id;
 
-    printf($acefh "\nCDS \"%s\"\n", $cds_name);
-    printf($acefh "Gene \"%s\"\n", $gene_name);
+    $cds{$seq->id}->{sequence} = $seq->seq;
   }
+
+  # Finally, write the Ace
+  foreach my $cds_name (sort keys %cds) {
+    next if not exists $cds{$cds_name}->{sequence};
+
+    my $cds_seq = $cds{$cds_name}->{sequence};
+    my $gene_name = $cds{$cds_name}->{gene};
+    
+    printf($acefh "\nProtein : \"%s\"\n", $cds_name);
+    printf($acefh "Species \"%s\"\n", $wb->full_name);
+    printf($acefh "Corresponding_CDS \"%s\"\n", $cds_name);
+    print $acefh "Live\n";
+    
+    printf($acefh "\nPeptide : \"%s\"\n", $cds_name);
+    printf($acefh "%s\n", $cds_seq);
+    
+    printf($acefh "\nCDS : \"%s\"\n", $cds_name);
+    printf($acefh "Species \"%s\"\n", $wb->full_name);
+    print $acefh "Method \"curated\"\n";
+    printf($acefh "Database WormBase TierIII \"%s\"\n", $cds_name);
+    print $acefh "Remark \"Tier3 species gene\" Inferred_automatically \"tier3_stubs.pl\"\n";
+    print $acefh "Coding CDS\n";
+    print $acefh "From_laboratory \"HX\"\n";
+    print $acefh "Gene \"$gene_name\"\n";
+  }
+  
+  
+  foreach my $gene_id (sort keys %genes) {
+    my $gene_name = $genes{$gene_id}->{name};
+    printf($acefh "\nGene : \"%s\"\n",  $gene_name);
+    printf($acefh "Public_name \"%s\"\n", $gene_name);
+    printf($acefh "Species \"%s\"\n", $wb->full_name);
+    print $acefh "Method \"Gene\"\n";
+    print $acefh "Remark \"Tier3 species gene\" Inferred_automatically \"tier3_stubs.pl\"\n";
+    print $acefh "Live\n";
+  } 
 }
 
 close($acefh) or $log->log_and_die("Could not successfully close $acefile after writing\n");
