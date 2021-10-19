7b6baa220a320e4b76027f6adb47f3390a1274e9
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2021-10-19 11:57:02 +0100

    Replace csh setup script with bash version

--- scripts/ENSEMBL/etc/interproscan_setup.sh
+++ scripts/ENSEMBL/etc/interproscan_setup.sh
@@ -1,22 +1,22 @@
 
 
-setenv IPR_PIPELINE_NAME $1
+export IPR_PIPELINE_NAME="$1"
 shift
 
-setenv EHIVE_PROD2_DBPASS $1
+export EHIVE_PROD2_DBPASS="$1"
 shift
 
-setenv HIVEDB "wormbase_${IPR_PIPELINE_NAME}"
-setenv PATH ${PATH}:$WORM_PACKAGES/ensembl/ensembl-hive/scripts
-setenv EG_ENSEMBL_ROOT $WORM_PACKAGES/ensembl_genomes
+export HIVEDB="wormbase_${IPR_PIPELINE_NAME}"
+export PATH=${PATH}:$WORM_PACKAGES/ensembl/ensembl-hive/scripts
+export EG_ENSEMBL_ROOT=$WORM_PACKAGES/ensembl_genomes
 
-setenv PIPELINE_DIR $PIPELINE/interpro_scratch
+export PIPELINE_DIR=$PIPELINE/interpro_scratch
 
-setenv PERL5LIB ${WORM_SW_ROOT}/packages/ensembl/branches/master/ensembl-production/modules:${PERL5LIB}
+export PERL5LIB=${WORM_SW_ROOT}/packages/ensembl/branches/master/ensembl-production/modules:${PERL5LIB}
 
-setenv HIVE_URL  mysql://${EHIVE_PROD2_DBUSER}:${EHIVE_PROD2_DBPASS}@${EHIVE_PROD2_DBHOST}:${EHIVE_PROD2_DBPORT}/${HIVEDB}
+export HIVE_URL=mysql://${EHIVE_PROD2_DBUSER}:${EHIVE_PROD2_DBPASS}@${EHIVE_PROD2_DBHOST}:${EHIVE_PROD2_DBPORT}/${HIVEDB}
 
-setenv INTERPROSCAN_VERSION `interproscan.sh --version|perl -ne 'print $1 if /InterProScan version (\S+)/'`
+export INTERPROSCAN_VERSION=$(interproscan.sh --version|perl -ne 'print $1 if /InterProScan version (\S+)/')
 
 #Delete the old hive database
 
