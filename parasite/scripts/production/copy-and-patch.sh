890972e58d56151dbc069b294d91f558c9aa4175
Dionysios Grigoriadis <digri@hl-codon-49-03.ebi.ac.uk>
2021-11-18 15:57:09 +0000

    worm_lite update changes

--- parasite/scripts/production/copy-and-patch.sh
+++ parasite/scripts/production/copy-and-patch.sh
@@ -15,17 +15,17 @@ perl -MProductionMysql -E '
   fi
   ${PARASITE_STAGING_MYSQL} -Ne "show databases like \"$DB_PAT\"" | grep -v "$NEWDB" | while read -r OLD_DB ; do
     echo "Dropping database $OLD_DB"
-    ${PARASITE_STAGING_MYSQL}-ensrw -e "DROP DATABASE $OLD_DB"
+    ${PARASITE_STAGING_MYSQL}-w -e "DROP DATABASE $OLD_DB"
   done
   echo  "Creating $NEWDB"
-  if ! ${PARASITE_STAGING_MYSQL}-ensrw -e "CREATE DATABASE $NEWDB"; then
+  if ! ${PARASITE_STAGING_MYSQL}-w -e "CREATE DATABASE $NEWDB"; then
     echo "Could not create $NEWDB - should this script be running? Bailing out."
     exit 1
   fi
   echo "Dumping $DB to $NEWDB"
-  ${PREVIOUS_PARASITE_STAGING_MYSQL} mysqldump $DB | ${PARASITE_STAGING_MYSQL}-ensrw $NEWDB
+  ${PREVIOUS_PARASITE_STAGING_MYSQL} mysqldump $DB | ${PARASITE_STAGING_MYSQL}-w $NEWDB
   echo "Using schema_patcher.pl to patch $NEWDB"
-  perl $ENSEMBL_CVS_ROOT_DIR/ensembl/misc-scripts/schema_patcher.pl $(${PARASITE_STAGING_MYSQL}-ensrw details script) \
+  perl $ENSEMBL_CVS_ROOT_DIR/ensembl/misc-scripts/schema_patcher.pl $(${PARASITE_STAGING_MYSQL}-w details script) \
      --release $ENSEMBL_VERSION --nointeractive \
      --database "$NEWDB"
 done
---
+++ parasite/scripts/production/new_release_setup/checkout_git_repos_genebuild.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+if [ -z $GENEBUILD_ENSEMBL_VERSION ]; then
+  echo "Need to define GENEBUILD_ENSEMBL_VERSION in the environment - have you loaded the parasite production module file?"
+  exit
+fi
+
+if [ -z $ ]; then
+  echo "Need to define GENEBUILD_CVS_ROOT_DIR in the environment - have you loaded the parasite production module file?"
+  exit
+fi
+
+echo "Making ${GENEBUILD_CVS_ROOT_DIR}"
+mkdir -pv ${GENEBUILD_CVS_ROOT_DIR}
+
+checkout_ensembl_branch(){
+ repo_name=$1
+ branch_name=$2
+ repo_owner=${3:-ensembl}
+ where_to_clone=${GENEBUILD_CVS_ROOT_DIR}/${repo_name}
+ if [ -d $where_to_clone ] ; then
+    echo "$where_to_clone already exists - removing"
+    rm -rf $where_to_clone
+ fi
+ git clone --single-branch -b $branch_name --depth 1 https://github.com/${repo_owner}/${repo_name}.git ${where_to_clone}
+}
+
+checkout_ensembl_branch ensembl release/${GENEBUILD_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-analysis ${ENSEMBL_ANALYSIS_BRANCH}
+checkout_ensembl_branch ensembl-hive version/2.5
+checkout_ensembl_branch ensembl-production release/${GENEBUILD_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-killlist master
+checkout_ensembl_branch ensembl-io release/${GENEBUILD_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-variation release/${GENEBUILD_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-funcgen master
+checkout_ensembl_branch ensembl-taxonomy master
+checkout_ensembl_branch ensembl-metadata release/${GENEBUILD_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-datacheck master
+checkout_ensembl_branch ensembl-orm release/${GENEBUILD_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-compara release/${COMPARA_ENSEMBL_VERSION}
+checkout_ensembl_branch ensembl-genes master
+
+
+
