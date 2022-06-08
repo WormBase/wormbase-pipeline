0657b5f3f11a4176911938c0415f20302b5cb05e
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2022-06-08 13:33:20 +0100

    Use species/all as input instead of infile/outfile

--- scripts/MISC_SCRIPTS/reformat_tier3_gff.pl
+++ scripts/MISC_SCRIPTS/reformat_tier3_gff.pl
@@ -11,63 +11,74 @@ use strict;
 use Wormbase;
 use Getopt::Long;
 use Log_files;
+use File::Copy qw(move);
 use Path::Class;
 
-my ($in_gff_file, $out_gff_file, $log_file, $debug);
+my ($species, $log_file, $debug);
 
 GetOptions(
-    "in|i=s" => \$in_gff_file,
-    "out|o=s" => \$out_gff_file,
+    "species:s" => \$species,
     "log|l=s" => \$log_file,
     "debug|d:s" => \$debug
     );
 
+$species = 'all' unless $species;
 my $log = Log_files->make_log($log_file, $debug);
 my $wormbase = Wormbase->new(-debug => $debug);
 
-my $target_mappings = get_target_mappings();
-my $in_fh = file($in_gff_file)->openr;
-my $out_fh = file($out_gff_file)->openw;
 my %unknown_species;
-while (my $line = $in_fh->getline()) {
-    chomp $line;
-    if ($line =~ /\s(\S+)\-BLASTX/) {
-	my $species = $1;
-	$species =~ s/_proteins//;
-	my @cols = split("\t", $line);
-	my %attr = split(/[;=]/, $cols[8]);
-	$log->log_and_die("No name for $species BLASTX line $line\n") unless exists $attr{'Name'};
+my $target_mappings = get_target_mappings();
 
-	if (!exists $target_mappings->{$species}) {
-	    $log->log_and_die("No target for $species BLASTX line $line\n") unless exists $attr{'Target'};
-	    if ($species eq 'scerevisiae') {
-		$cols[8] = 'Target=SGD:' . $attr{'Target'};
-	    } elsif ($species eq 'hsapiens' && ($attr{'Target'} =~ /^ENS/ || $attr{'Target'} =~ /^HIT/ || $attr{'Target'} =~ /^OTTHUMP/)){
-		$cols[8] = 'Target=ENSEMBL:' . $attr{'Target'};
-	    } elsif ($species eq 'dmelanogaster') {
-		$cols[8] = 'Target=FLYBASE:' . $attr{'Target'};
-	    } elsif ($species eq 'UniProt') {
-		$cols[8] = 'Target=SW:' . $attr{'Target'};
-	    } else {
-		if (!exists $unknown_species{$species}) {
-		    $log->write_to("WARNING - no accessions for $species\n");
-		    $unknown_species{$species}++;
+my %tier3_accessors = $wormbase->tier3_species_accessors;
+for my $t3 (values %tier3_accessors) {
+    next unless $species eq 'all' || lc $species eq $t3->species;
+    $log->write_to('Processing ' . $t3->species . " GFF3 file\n");
+    my $gff3_file = $t3->sequences . '/' . $t3->species . '.gff3';
+    my $backup_file = $gff3_file . '.reformat_backup';
+    move $gff3_file, $backup_file;
+    
+    my $in_fh = file($backup_file)->openr;
+    my $out_fh = file($gff3_file)->openw;
+    while (my $line = $in_fh->getline()) {
+	chomp $line;
+	if ($line =~ /\s(\S+)\-BLASTX/) {
+	    my $target_species = $1;
+	    $target_species =~ s/_proteins//;
+	    my @cols = split("\t", $line);
+	    my %attr = split(/[;=]/, $cols[8]);
+	    $log->log_and_die("No name for $species BLASTX line $line\n") unless exists $attr{'Name'};
+
+	    if (!exists $target_mappings->{$target_species}) {
+		$log->log_and_die("No target for $target_species BLASTX line $line\n") unless exists $attr{'Target'};
+		if ($target_species eq 'scerevisiae') {
+		    $cols[8] = 'Target=SGD:' . $attr{'Target'};
+		} elsif ($target_species eq 'hsapiens' && ($attr{'Target'} =~ /^ENS/ || $attr{'Target'} =~ /^HIT/ || $attr{'Target'} =~ /^OTTHUMP/)){
+		    $cols[8] = 'Target=ENSEMBL:' . $attr{'Target'};
+		} elsif ($target_species eq 'dmelanogaster') {
+		    $cols[8] = 'Target=FLYBASE:' . $attr{'Target'};
+		} elsif ($target_species eq 'UniProt') {
+		    $cols[8] = 'Target=SW:' . $attr{'Target'};
+		} else {
+		    if (!exists $unknown_species{$target_species}) {
+			$log->write_to("WARNING - no accessions for $target_species\n");
+			$unknown_species{$target_species}++;
+		    }
 		}
 	    }
-	}
-	else {
-	    my ($name, $position) = $attr{'Target'} =~ /^(\S+)(\s.+)$/;
-	    if (exists $target_mappings->{$species}{$name}) {
-		$cols[8] = 'Target=' . $target_mappings->{$species}{$name} . $position;
-	    }
 	    else {
-		$log->write_to("WARNING - no mapping found for $name in $species\n");
-		$cols[8] = 'Target=' . $attr{'Target'};
+		my ($name, $position) = $attr{'Target'} =~ /^(\S+)(\s.+)$/;
+		if (exists $target_mappings->{$target_species}{$name}) {
+		    $cols[8] = 'Target=' . $target_mappings->{$target_species}{$name} . $position;
+		}
+		else {
+		    $log->write_to("WARNING - no mapping found for $name in $target_species\n");
+		    $cols[8] = 'Target=' . $attr{'Target'};
+		}
 	    }
+	    $line = join("\t", @cols);
 	}
-	$line = join("\t", @cols);
+	$out_fh->print($line . "\n");
     }
-    $out_fh->print($line . "\n");
 }
 
 $log->mail;
