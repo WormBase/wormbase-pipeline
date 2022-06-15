75ace3d91b810e9b8d6c21740c286d1ff384d975
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2022-06-15 14:38:51 +0100

    Revert to uppercase N for filename but not GFF column

--- scripts/WBGene_span.pl
+++ scripts/WBGene_span.pl
@@ -52,7 +52,7 @@ my %worm_gene2geneID_name = $wormbase->FetchData('worm_gene2geneID_name');
 
 my (%gene_coords, %gene_span);
 
-my  @methods = qw(Coding_transcript non_coding_transcript Pseudogene ncRNA 7kncRNA tRNA miRNA pre_miRNA miRNA_primary_transcript snRNA snlRNA snoRNA rRNA scRNA stRNA lincRNA asRNA piRNA Transposon_CDS Transposon_Pseudogene rRNA_Pseudogene tRNA_Pseudogene Transposon_ncRNA circRNA);
+my  @methods = qw(Coding_transcript Non_coding_transcript Pseudogene ncRNA 7kncRNA tRNA miRNA pre_miRNA miRNA_primary_transcript snRNA snlRNA snoRNA rRNA scRNA stRNA lincRNA asRNA piRNA Transposon_CDS Transposon_Pseudogene rRNA_Pseudogene tRNA_Pseudogene Transposon_ncRNA circRNA);
   
 foreach my $method (@methods) {
   print "checking $method \n" if $debug;
