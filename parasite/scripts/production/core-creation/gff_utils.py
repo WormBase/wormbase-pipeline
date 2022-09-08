b670ad0226505173f920b431d04336a3dee9bf13
Matej Vucak <mvucak@codon-login-01.ebi.ac.uk>
2022-09-08 11:54:32 +0100

    Added sort and .gff writing functionality to gff_utils.py. Created reformat_gff.py as CLI script for processing .gff files

--- parasite/scripts/production/core-creation/gff_utils.py
+++ parasite/scripts/production/core-creation/gff_utils.py
@@ -3,14 +3,16 @@ import re
 from Bio import SeqIO
 from argparse import ArgumentParser
 
-def get_args():
-    parser = ArgumentParser()
-    parser.add_argument("input_gff",
-        help = "Unprocessed .gff file which requires parsing.")
-    parser.add_argument("output_file",
-    	help = "Output .gff file to write processed GFF to.")
-    args = parser.parse_args()
-    return args 
+# def get_args():
+#     parser = ArgumentParser()
+#     parser.add_argument("input_gff",
+#         help = "Unprocessed .gff file which requires parsing.")
+#     parser.add_argument("output_file",
+#     	help = "Output .gff file to write processed GFF to.")
+#     parser.add_argument("synonyms_file",
+#     	help = "Synonyms.tsv file for mapping scaffold names.")
+#     args = parser.parse_args()
+#     return args 
 
 
 # Takes a GFF dataframe and adds a column for each attribute specified
@@ -52,6 +54,22 @@ def parse_gff(input_gff):
 
 	return gff_df
 
+
+# Replaces INSDCID scaffold names with community scaffold names using 
+# synonyms mapping provided in a synonyms.tsv file.
+# TODO: Spits out NaNs as scaffold names if the .gff is already using
+# community scaffold names. Implement a check for this? Or rely on 
+# correct calls on the user's part? 
+def rename_scaffolds(gff_df, synonyms_file):
+	# TODO: Infer synonyms headers from file rather than hardcoding as below.
+	colnames = ["toplevel", "CommunityID", "INSDCID", "INSDC"]
+	scaffold_df = pd.read_csv (synonyms_file, sep = '\t', names = colnames, usecols = ["CommunityID", "INSDCID"])
+	scaffold_df.set_index("INSDCID", inplace = True)
+	gff_df.loc[:, "scaffold"] = gff_df["scaffold"].map(scaffold_df["CommunityID"])
+	return gff_df
+
+
+
 # Copies features ID attributes into Name attributes
 def infer_name_attribute_from_id(gff_df):
 	gff_df["Name"] = gff_df["ID"]
@@ -79,6 +97,10 @@ def add_prefix_to_name(gff_df, prefix):
 	return gff_df
 
 
+def rename_sources(gff_df, source):
+	gff_df["source"] = source
+	return gff_df
+
 def get_all_gene_features(gff_df):
 	gene_mask = gff_df.loc[:, "type"] == "gene"
 	gene_df = gff_df.loc[gene_mask]
@@ -104,17 +126,55 @@ def get_all_cds_features(gff_df):
 	return cds_df
 
 
+# Creates Parent Gene column for all gene features. This contains the ID
+# the gene's own gene ID (quick copy). Used for sorting the final .gff.
+def _get_parent_gene_for_genes(gene_df):
+	# TODO: Still raises a chained assignment warning:
+	# A value is trying to be set on a copy of a slice from a DataFrame.
+	gene_df.loc[:,"Parent_Gene"] = gene_df["ID"]
+	return gene_df 
+
+
+# Creates a Parent Gene column for all transcript features. This contains
+# the ID of the transcripts' parent gene (quick copy from Parent col).
+# Used for sorting the final .gff.
+def _get_parent_gene_for_transcripts(transcript_df):
+	# TODO: Still raises a chained assignment warning:
+	# A value is trying to be set on a copy of a slice from a DataFrame.
+	transcript_df.loc[:,["Parent_Gene"]] = transcript_df["Parent"]
+	return transcript_df
+
+
+# Creates Parent Gene column for all exon or CDS features. This contains
+# the ID of the exon's/CDS's parent gene. It has to be inferred from its 
+# parent transcript, requiring iterrows instead of vectorised copying. 
+# Used for sorting the final .gff.
+def _get_parent_gene_for_exons_or_cds(exon_df, transcript_df):
+	for index, exon in exon_df.iterrows():
+		# TODO: Still raises a chained assignment warning:
+		# A value is trying to be set on a copy of a slice from a DataFrame.
+		parent_transcript_id = exon["Parent"]
+		parent_gene_id = transcript_df.at[parent_transcript_id, "Parent_Gene"]
+		exon_df.at[index, "Parent_Gene"] = parent_gene_id
+	return exon_df
+
+
 # Returns numpy array containing all unique type values in .gff
 def get_all_unique_types(gff_df):
 	return pd.unique(gff_df["type"])
+
 
 # Returns numpy array containing all unique source values in .gff
 def get_all_unique_sources(gff_df):
 	return pd.unique(gff_df["source"])
 
+
+# Prints all unique type values across all features to stdout.
+# Points out any non-standard types.
 def print_all_unique_types(gff_df):
 	print(".gff file contains the following type values: ")
 	for unique_type in get_all_unique_types(gff_df):
+		# Could be refactored so that standard types are not defined in function.
 		if unique_type not in ["gene", "mRNA", "tRNA", "rRNA", "exon", "CDS"]:
 			suffix = "\t<---- Non-standard type"
 		else:
@@ -129,46 +189,81 @@ def print_all_unique_sources(gff_df):
 
 
 # Recreates attribute field according to ParaSite specification.
-# TODO: Pretty crude implementation. There might be a better way to 
-# concatenate string columns with pandas
 def finalise_attributes_column(gff_df):
-	gff_df["attributes"] = "ID=" + gff_df["ID"] + ';' + "Name=" + gff_df["Name"] + ';' + "Parent=" + gff_df["Parent"]
+	gff_df["attributes"] = "ID=" + gff_df["ID"].fillna('') + ';' + "Name=" + gff_df["Name"].fillna('') + ';' + "Parent=" + gff_df["Parent"].fillna('')
+	# Omit Parent attribute in features that lack it (e.g. genes)
+	gff_df["attributes"] = gff_df["attributes"].mask(gff_df["Parent"].isna(), 
+		"ID=" + gff_df["ID"].fillna('') + ';' + "Name=" + gff_df["Name"].fillna(''))
 	return gff_df
+
 
 # Drops any columns created during .gff processing but not required
 # in the final .gff file. Only call right before writing .gff to file.
-# TODO: Could refer to a global list of final columns for maintainability
 def drop_superfluous_columns(gff_df):
+	# TODO: Could refer to a global list of final columns for maintainability
 	final_columns = ["scaffold", "source", "type", "start", "end", "score", "strand", "phase", "attributes"]
 	gff_df = gff_df.drop(columns=[column for column in gff_df if column not in final_columns])
 	return gff_df
 
+
+# Outputs a finalised .gff dataframe that contains only gene, transcript,
+# exon and CDS features. Orders the dataframe so that features exist in a
+# hierarchy: gene, gene's transcripts, transcript's exons/CDSs
+# TODO: May need dramatic speeding up due to reliance on iterrows.
+# TODO: Sort is purely alphabetic (e.g. g10 comes after g1, should be g2)
+def reorder_gff_features(gff_df):
+	gff_df.set_index("ID", drop = False, inplace = True)
+
+	gene_df = get_all_gene_features(gff_df)
+	transcript_df = get_all_transcript_features(gff_df)
+	exon_df = get_all_exon_features(gff_df)
+	cds_df = get_all_cds_features(gff_df)
+
+	# Create a "Parent Gene" column for each feature to allow sorting.
+	gene_df = _get_parent_gene_for_genes(gene_df)
+	transcript_df  = _get_parent_gene_for_transcripts(transcript_df)
+	exon_df = _get_parent_gene_for_exons_or_cds(exon_df, transcript_df)
+	cds_df = _get_parent_gene_for_exons_or_cds(cds_df, transcript_df)
+
+	# Stick all features together and sort them by parent gene.
+	reordered_gff = pd.concat([gene_df, transcript_df, exon_df, cds_df])
+	reordered_gff = reordered_gff.sort_values(by=["Parent_Gene", "Name"])
+	return reordered_gff
+
+
 # Updates attributes field and drops unecessary columns, then writes
 # processed .gff to a specified output file.
-def output_gff(gff_df, output_file):
+def write_output_gff(gff_df, output_file):
+	print(gff_df)
 	gff_df = finalise_attributes_column(gff_df)
+	gff_df = reorder_gff_features(gff_df)
+	print(gff_df)
 	gff_df = drop_superfluous_columns(gff_df)
 
-	gff_df.to_csv(output_file, sep = '\t')
+	with open(output_file, 'w') as output_gff:
+		output_gff.write("##gff-version 3\n")
 
-args = get_args()
-
-gff_df = parse_gff(args.input_gff)
-
-# Infer Name attribute from ID if absent
-if not has_name_attribute(gff_df):
-	gff_df = infer_name_attribute_from_id(gff_df)
+	gff_df.to_csv(output_file, mode = 'a', header = False, sep = '\t', index = False)
 
 
-print(gff_df.head(n=20))
 
-print_all_unique_types(gff_df)
-print_all_unique_sources(gff_df)
+# args = get_args()
 
-# gene_df = get_all_gene_features(gff_df)
-# transcript_df = get_all_transcript_features(gff_df)
-# exon_df = get_all_exon_features(gff_df)
-# cds_df = get_all_cds_features(gff_df)
+# gff_df = parse_gff(args.input_gff)
+
+# # Infer Name attribute from ID if absent
+# if not has_name_attribute(gff_df):
+# 	gff_df = infer_name_attribute_from_id(gff_df)
+
+# gff_df = rename_sources(gff_df, "WormBase_imported")
+
+# gff_df = rename_scaffolds(gff_df, args.synonyms_file)
+
+
+# # gene_df = get_all_gene_features(gff_df)
+# # transcript_df = get_all_transcript_features(gff_df)
+# # exon_df = get_all_exon_features(gff_df)
+# # cds_df = get_all_cds_features(gff_df)
 
 # output_gff(gff_df, args.output_file)
 
---
+++ parasite/scripts/production/core-creation/reformat_gff.py
@@ -0,0 +1,98 @@
+#!/hps/software/users/wormbase/parasite/shared/.pyenv/versions/p395/bin/python
+from gff_utils import *
+from Bio import SeqIO
+import re
+import sys
+import glob
+from argparse import ArgumentParser
+
+
+# Grabs and return command line arguments. Only input.gff and output
+# destination are required arguments. -f and -s options allow for 
+# specifying a  scaffolds .fasta file and a .tsv mapping NCBI accessions 
+# to community scaffold names, but are not required. Other options 
+# specify what processing steps should be taken (will vary) 
+def get_args():
+	parser = ArgumentParser()
+	parser.add_argument("input_gff",
+		help = "Unprocessed .gff file which requires parsing.")
+	parser.add_argument("output_gff",
+		help = "Output path to write modified .gff file to.")
+	# source_to_WB is set to True by default
+	parser.add_argument("--keep_source", action = "store_false", dest = "source_to_WB",
+		help = "Leave the source column unchanged. By default, it is changed to WormBase_imported")
+	# scaffold_rename is set to True by default.
+	parser.add_argument("--keep_scaffold_names", action = "store_false", dest = "scaffold_rename",
+		help = "Leave scaffold names unchanged. By default, they are mapped to an alternative name in the synonyms file and renamed.")
+	# has_parentage is set to True by default.
+	parser.add_argument("--no_parentage", action = "store_false", dest = "has_parentage",
+		help = "Indicate input .gff file has no parentage information to link features. By default, assumes parentage information is included.")
+	parser.add_argument("-g", "--gene_prefix", default = False,
+		help = "A string to append to each gene and ID name, in case original names are lacking. No changes made by default.")
+	parser.add_argument("-f", "--fasta", required = False, default = None, type = str,
+		help = ".fasta file containing assembly scaffolds. If not specified, .fasta file will be inferred from directory contents.")
+	parser.add_argument("-s", "--synonyms_file", required = False, default = None, type = str,
+		help = ".tsv file containing mappings between \"community\" scaffold names and NCBI scaffold names. If not specified, .tsv file will be inferred from directory contents.")
+
+	args = parser.parse_args()
+	return args 
+
+
+# Infers an assembly .fasta file from working directory
+# Exits if none is found.
+def infer_fasta_file():
+	fastas = glob.glob("./*.fa")
+	if len(fastas) < 1:
+		sys.exit("No sequence fasta was found")
+	else:
+		fasta = fastas[0]
+	return fasta
+
+# Infers a scaffold synonym file from working directory
+# Exits if none or more than 1 found.
+def infer_synonyms_file():
+    synonyms_files = glob.glob("./*seq_region_synonyms.tsv")
+    if len(synonyms_files) != 1:
+       	sys.exit("Expected 1 but got " + str(len(synonyms_files)) + " files ending with seq_regions_synonyms.tsv in the directory")
+    else:
+       	synonyms_file = synonyms_files[0]
+    return synonyms_file
+
+
+def main():
+	# Grab command-line arguments.
+	args = get_args()
+	
+	input_gff = args.input_gff
+	output_gff = args.output_gff
+	
+	# If no .fasta file has been provided, infer one from directory.
+	if args.fasta is None:
+		fasta = infer_fasta_file()
+	else:
+		fasta = args.fasta
+
+	# If no synonyms.tsv file has been provided, infer one from directory.
+	if args.synonyms_file is None:
+		synonyms_file = infer_synonyms_file()
+	else:
+		synonyms_file = input.synonyms_file
+
+	gff_df = parse_gff(input_gff)
+
+	# TODO: Find a way to not hardcode WormBase_imported
+	if args.source_to_WB is True:
+		gff_df = rename_sources(gff_df, "WormBase_imported")
+
+	if args.gene_prefix:
+		gff_df = add_prefix_to_id(gff_df, args.gene_prefix)
+		gff_df = add_prefix_to_name(gff_df, args.gene_prefix)
+
+	if args.scaffold_rename:
+		gff_df = rename_scaffolds(gff_df, synonyms_file)
+
+	write_output_gff(gff_df, output_gff)
+	
+
+if __name__ == '__main__':
+	main()
