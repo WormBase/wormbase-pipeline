47bb2763499046ae0a80c5d34c256fbbf0c8a744
Mark Quinton-Tulloch <markquintontulloch@gmail.com>
2022-11-03 12:11:47 +0000

    Fix symbol/name logic

--- scripts/AGR/curation_db_submissions/make_agr_allele_json.pl
+++ scripts/AGR/curation_db_submissions/make_agr_allele_json.pl
@@ -78,14 +78,13 @@ sub process_variations {
 	
 	my $json_obj = {
 	    curie    => "WB:" . $obj->name,
-	    name     => $obj->Public_name ? $obj->Public_name->name : $obj->name,
+	    symbol     => $obj->Public_name ? $obj->Public_name->name : $obj->name,
 	    taxon       => "NCBITaxon:" . $obj->Species->NCBITaxonomyID->name,
 	    internal      => JSON::false,
 	    obsolete   => $obj->Live ? JSON::true : JSON::false,
 	    created_by => 'WB:curator',
 	    updated_by => 'WB:curator'
 	};
-	$json_obj->{symbol} = $obj->Public_name->name if $obj->Public_name;
 	if ($obj->Method) {
 	    my $collection = $obj->Method->name;
 	    unless ($collection eq 'Allele'
