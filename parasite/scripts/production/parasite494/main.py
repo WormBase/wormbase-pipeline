fc58d4e98d5ae98317d96bc47a4f7b361f38a0eb
steph-brown28 <stephanie.brown.3@glasgow.ac.uk>
2023-07-24 18:12:14 +0100

    working on making a main.py script and db insert

--- parasite/scripts/production/parasite494/main.py
+++ parasite/scripts/production/parasite494/main.py
@@ -4,6 +4,8 @@ from ProductionUtils import *
 import os
 import gzip
 import csv
+from sqlalchemy import insert, Table, MetaData
+from sqlalchemy.orm import sessionmaker
 
 # setting up environmental variables
 PARASITE_STAGING = os.environ['PARASITE_STAGING_MYSQL']
@@ -20,7 +22,6 @@ databases = (staging.core_dbs(databases_search_key))
 # creating a file path to each of the shared databases
 # main function get_file_paths() 
 # nested function get_file_path() will take db string and return organism genus _ organism species _ bioproject id for each database
-# move this to ProductionMysql.py
 def get_file_paths(WORMBASE_FTP, WORMBASE_VERSION, databases):
     def get_file_path(s):
         organism_genus = s.split("_")[0][0]
@@ -37,38 +38,7 @@ def get_file_paths(WORMBASE_FTP, WORMBASE_VERSION, databases):
 file_paths = get_file_paths(WORMBASE_FTP, WORMBASE_VERSION, databases)
 
 
-# creating a file path to each of the shared databases
-# function that will take db string and return organism genus _ organism species _ bioproject id 
-# move this to ProductionMysql.py
-#def get_file_path(s):
-#    organism_genus = s.split("_")[0][0]
-#    organism_species = s.split("_")[1]
-#    bioproject_id = s.split("_")[2].upper()
-#    return f"{organism_genus}_{organism_species}/{bioproject_id}/annotation/{organism_genus}_{organism_species}.{bioproject_id}.WS{WORMBASE_VERSION}.functional_descriptions.txt.gz"
-
-# list comprehension to feed each element in the databases list into the get_file_path() function
-# outputs a list containing the file path for each element in the initial databases list 
-#annotation_output = [get_file_path(x) for x in databases] 
-
-# store the output of the get_file_path() function for each database in a separate variable.
-#b_malayi, c_brenneri, c_briggsae, c_elegans, c_japonica, c_remanei, o_volvulus, p_pacificus, s_ratti, t_muris = annotation_output
-
-# construct file paths
-#malayi_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', b_malayi)
-#brenneri_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', c_brenneri)
-#briggsae_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', c_briggsae)
-#elegans_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', c_elegans)
-#japonica_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', c_japonica)
-#remanei_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', c_remanei)
-#volvulus_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', o_volvulus)
-#pacificus_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', p_pacificus)
-#ratti_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', s_ratti)
-#muris_path = os.path.join(WORMBASE_FTP, 'releases', 'WS' + WORMBASE_VERSION, 'species', t_muris)
-
-# list of file paths
-# to feed into for loop below
-#file_paths = [malayi_path, brenneri_path, briggsae_path, elegans_path, japonica_path, remanei_path, volvulus_path, pacificus_path, ratti_path, muris_path]
-
+# process each functional annotation file and store each stable_id and corresponding description as an output .tsv in a scratch directory
 def process_files(file_paths):
     # variables to store hard coded text
     WB_gene_prefix = 'WBGene'
@@ -126,61 +96,61 @@ def process_files(file_paths):
         
         # Write .tsv file with extracted data inside the "gene_descs" folder
         output_file_path = os.path.join(int_folder_path, output_file_name)
+
         with open(output_file_path, 'w', newline='') as file:
             writer = csv.writer(file, delimiter='\t')
             writer.writerow(['stable_id', 'description'])
             for row in data:
                 writer.writerow(row)
 
-# call function
+# call function 
 process_files(file_paths)
 
+int_folder = 'gene_descs'
+int_scratch_directory = os.path.join(PARASITE_SCRATCH, int_folder)
+gene_attrib_type = 'description'
 
+for file in os.listdir(int_scratch_directory):
+    file_path = os.path.join(int_scratch_directory, file)
+    df = pd.read_csv(file, sep='\t', names=["stable_id", "description"])
+    #print(df)
+    stable_ids = df['stable_id'].tolist()
+    # connect to db associated with each .tsv file
+    for database in databases:
+        core_db = Core(STAGING_HOST, database)
 
-#for file_path in file_paths:
-    # create a unique output file name based on the input file name
-    #output_file_name = os.path.basename(file_path) + '.tsv'
-    
-    # reset the data list to an empty list
-    #data = []
-    # variables to store hard coded text
-    #WB_gene_prefix = 'WBGene'
-    #AD_prefix = 'Automated description:'
-    #GD_prefix = 'Gene class description:'
+        # write queries
+        # query gene table and return gene_id associated with each stable_id as a tuple
+        GENES_QUERY = "SELECT stable_id, gene_id FROM gene"
+        # query attrib_type table and return description attrib_type_id
+        ATTRIB_QUERY = "SELECT attrib_type_id FROM attrib_type WHERE name = '{}'".format(gene_attrib_type)
+        
+        execution = core_db.connect().execute(GENES_QUERY)
+        gene_id_rows = execution.fetchall()
+        # create a df from the tuples
+        gene_id_df = pd.DataFrame(gene_id_rows, columns=['stable_id', 'gene_id'])
+        attrib_execution = core_db.connect().execute(ATTRIB_QUERY)
+        attrib_type_id = attrib_execution.fetchone()[0]
+        # add attrib type id as a column to df
+        gene_id_df['attrib_type_id'] = attrib_type_id
+        # merge .tsv df and df created from querying db on the stable_id column 
+        merged_df = pd.merge(gene_id_df, df, on='stable_id', how='inner')
+        merged_df = merged_df[~merged_df['description'].str.contains('none available')]
+        merged_df = merged_df.drop('stable_id', axis=1)
+        merged_df = merged_df.rename(columns={'description': 'value'})
+        # convert df to dictionary to make easier to insert into db?
+        data = merged_df.to_dict(orient='records')
+        print(data)
 
-    #with gzip.open(file_path, 'rt') as file:
-        #stable_id = None
-        #description = None
+        conn = core_db.engine.connect()
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-        #for line in file:
-            #line = line.strip()
-            #if line.startswith(WB_gene_prefix):
-                #stable_id = line.split()[0]
-            #elif line.startswith(AD_prefix):
-                #description = line.replace(AD_prefix, '').strip()
-                #for subline in file:
-                    #subline = subline.strip()
-                    #if subline.startswith(GD_prefix):
-                        #data.append([stable_id, description])
-                        #break
-                    #else:
-                        #description += ' ' + subline
-                #stable_id = None
-                #description = None
+        metadata = MetaData(bind=engine)
+        table_name = Table('gene_attrib', metadata, autoload=True)
 
-    # print the output file name and the data for debugging purposes
-    #print(output_file_name)
-    #print(data)
+        for row in data:
+            insert_stmt = table_name.insert().values(**row)
+            session.execute(insert_stmt)
 
-# create new directory within scratch to store the intermediate .tsv files
-
-
-# create a new directory within PARASITE_SCRATCH = os.environ["PARASITE_SCRATCH"] (/hps/nobackup/flicek/wormbase/parasite)
-# this will store the intermediate .tsv files generated by this script
-
-    # write .tsv file with extracted data
-    #with open(output_file_name, 'w', newline='') as file:
-        #writer = csv.writer(file, delimiter='\t')
-        #writer.writerow(['stable_id', 'description'])
-        #for row in data:
-            #writer.writerow(row)
+        session.commit()
