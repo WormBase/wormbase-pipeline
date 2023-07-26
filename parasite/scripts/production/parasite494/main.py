8cc773454af3f0da8f7d4288c5ea767e9aed6761
steph-brown28 <stephanie.brown.3@glasgow.ac.uk>
2023-07-26 16:35:56 +0100

    fixed nested for loop problem

--- parasite/scripts/production/parasite494/main.py
+++ parasite/scripts/production/parasite494/main.py
@@ -33,65 +33,138 @@ int_scratch_directory = os.path.join(PARASITE_SCRATCH, int_folder)
 # variable to hold term description so it can be configured easily
 gene_attrib_type = 'description'
 
-# for each file in scratch directory, reconstruct file path, open as a df
-for file in os.listdir(int_scratch_directory):
+
+# Create an iterator that pairs each file in int_scratch_directory with a database from the databases list
+file_database_pairs = zip(os.listdir(int_scratch_directory), databases)
+
+for file, database in file_database_pairs:
     file_path = os.path.join(int_scratch_directory, file)
     df = pd.read_csv(file_path, sep='\t', names=["stable_id", "description"])
     print(len(df))
 
-    # connect to db associated with each .tsv file
-    for database in databases:
-        core_db = Core(STAGING_HOST, database)
+    core_db = Core(STAGING_HOST, database)
+    print(file_path)
+    print(core_db)
 
-        # write query statements
-        # query gene table and return gene_id associated with each stable_id as a tuple
-        GENES_QUERY = "SELECT stable_id, gene_id FROM gene"
-        # query attrib_type table and return description attrib_type_id
-        ATTRIB_QUERY = "SELECT attrib_type_id FROM attrib_type WHERE name = '{}'".format(gene_attrib_type)
-        # query to check if any of the genes already have a description associated with them 
-        DESCRIP_QUERY = "SELECT DISTINCT(gene_id) FROM gene JOIN gene_attrib USING (gene_id) JOIN attrib_type USING (attrib_type_id) WHERE attrib_type.name='description'"
-        # query to delete values from gene_attrib table where there are already descriptions present. They will be replaced with the new descriptions
-        DELETE_QUERY = "DELETE FROM gene_attrib WHERE attrib_type_id = '49'"
+    # write query statements
+    # query gene table and return gene_id associated with each stable_id as a tuple
+    GENES_QUERY = "SELECT stable_id, gene_id FROM gene"
+    # query attrib_type table and return description attrib_type_id
+    ATTRIB_QUERY = "SELECT attrib_type_id FROM attrib_type WHERE name = '{}'".format(gene_attrib_type)
+    # query to check if any of the genes already have a description associated with them 
+    DESCRIP_QUERY = "SELECT DISTINCT(gene_id) FROM gene JOIN gene_attrib USING (gene_id) JOIN attrib_type USING (attrib_type_id) WHERE attrib_type.name='description'"
+    # query to delete values from gene_attrib table where there are already descriptions present. They will be replaced with the new descriptions
+    DELETE_QUERY = "DELETE FROM gene_attrib WHERE attrib_type_id = '49'"
 
-        # execute queries
-        genes_query_execution = core_db.connect().execute(GENES_QUERY)
-        gene_id_rows = genes_query_execution.fetchall()
-        attrib_query_execution = core_db.connect().execute(ATTRIB_QUERY)
-        attrib_type_id = attrib_query_execution.fetchone()[0]
-        descrip_query_execute = core_db.connect().execute(DESCRIP_QUERY)
-        descrip_q = descrip_query_execute.fetchall()
+    # execute queries
+    genes_query_execution = core_db.connect().execute(GENES_QUERY)
+    gene_id_rows = genes_query_execution.fetchall()
+    attrib_query_execution = core_db.connect().execute(ATTRIB_QUERY)
+    attrib_type_id = attrib_query_execution.fetchone()[0]
+    descrip_query_execute = core_db.connect().execute(DESCRIP_QUERY)
+    descrip_q = descrip_query_execute.fetchall()
         
-        # create a df from the tuples that are returned as output to the gene query
-        # add attrib_type_id column
-        gene_id_df = pd.DataFrame(gene_id_rows, columns=['stable_id', 'gene_id'])
-        gene_id_df['attrib_type_id'] = attrib_type_id
+    # create a df from the tuples that are returned as output to the gene query
+    # add attrib_type_id column
+    gene_id_df = pd.DataFrame(gene_id_rows, columns=['stable_id', 'gene_id'])
+    gene_id_df['attrib_type_id'] = attrib_type_id
     
-        # merge .tsv df and df created from querying db on the stable_id column, drop rows with 'none available' description
-        merged_df = pd.merge(gene_id_df, df[df['description'] != 'none available'], on='stable_id').drop('stable_id', axis=1).rename(columns={'description': 'value'}) 
+    # merge .tsv df and df created from querying db on the stable_id column, drop rows with 'none available' description
+    merged_df = pd.merge(gene_id_df, df[df['description'] != 'none available'], on='stable_id').drop('stable_id', axis=1).rename(columns={'description': 'value'}) 
 
-        # convert df to dictionary to make easier to insert into db?
-        data = merged_df.to_dict(orient='records')
+    # convert df to dictionary to make easier to insert into db?
+    data = merged_df.to_dict(orient='records')
+    print(len(data))
 
-        # database insertion
-        core_db_w = Core(STAGING_HOST, database, writable=True)
-        attrib_table = 'gene_attrib'
-        #delete_query_execution = core_db_w.connect().execute(DELETE_QUERY)
+    # database insertion
+    core_db_w = Core(STAGING_HOST, database, writable=True)
+    attrib_table = 'gene_attrib'
+    # execute query to delete existing descriptions from the gene_attrib table before inserting the new descriptions
+    delete_query_execution = core_db_w.connect().execute(DELETE_QUERY)
 
-        # Create the table object using the MyTableName class
-        # metadata object is a container for the db schema info
-        # gene_attrib_table object represents the gene_attrib db table 
-        # The autoload_with parameter specifies that the table structure will be loaded from the database using the connection defined by core_db_w.engine.
-        metadata = sa.MetaData()
-        gene_attrib_table = sa.Table(attrib_table, metadata, autoload_with=core_db_w.engine)
+    # Create the table object using the MyTableName class
+    # metadata object is a container for the db schema info
+    # gene_attrib_table object represents the gene_attrib db table 
+    # The autoload_with parameter specifies that the table structure will be loaded from the database using the connection defined by core_db_w.engine.
+    metadata = sa.MetaData()
+    gene_attrib_table = sa.Table(attrib_table, metadata, autoload_with=core_db_w.engine)
 
-        # create a session, an intermediate object for interacting with the database
-        Session = sessionmaker(bind=core_db_w.engine)
-        session = Session()
+    # create a session, an intermediate object for interacting with the database
+    Session = sessionmaker(bind=core_db_w.engine)
+    session = Session()
 
-        for item in data:
-        # insert method used to insert data into the table row by row from the list of dictionaries
-            ins = gene_attrib_table.insert().values(gene_id=item['gene_id'], attrib_type_id=item['attrib_type_id'], value=item['value'])
-            session.execute(ins)
+    for item in data:
+    # insert method used to insert data into the table row by row from the list of dictionaries
+        ins = gene_attrib_table.insert().values(gene_id=item['gene_id'], attrib_type_id=item['attrib_type_id'], value=item['value'])
+        session.execute(ins)
 
-        session.commit()
-        session.close()
+    session.commit()
+    session.close()
+
+
+
+
+# # for each file in scratch directory, reconstruct file path, open as a df
+# for file in os.listdir(int_scratch_directory):
+#     file_path = os.path.join(int_scratch_directory, file)
+#     df = pd.read_csv(file_path, sep='\t', names=["stable_id", "description"])
+#     print(len(df))
+
+#     # connect to db associated with each .tsv file
+#     for database in databases:
+#         core_db = Core(STAGING_HOST, database)
+
+#         # write query statements
+#         # query gene table and return gene_id associated with each stable_id as a tuple
+#         GENES_QUERY = "SELECT stable_id, gene_id FROM gene"
+#         # query attrib_type table and return description attrib_type_id
+#         ATTRIB_QUERY = "SELECT attrib_type_id FROM attrib_type WHERE name = '{}'".format(gene_attrib_type)
+#         # query to check if any of the genes already have a description associated with them 
+#         DESCRIP_QUERY = "SELECT DISTINCT(gene_id) FROM gene JOIN gene_attrib USING (gene_id) JOIN attrib_type USING (attrib_type_id) WHERE attrib_type.name='description'"
+#         # query to delete values from gene_attrib table where there are already descriptions present. They will be replaced with the new descriptions
+#         DELETE_QUERY = "DELETE FROM gene_attrib WHERE attrib_type_id = '49'"
+
+#         # execute queries
+#         genes_query_execution = core_db.connect().execute(GENES_QUERY)
+#         gene_id_rows = genes_query_execution.fetchall()
+#         attrib_query_execution = core_db.connect().execute(ATTRIB_QUERY)
+#         attrib_type_id = attrib_query_execution.fetchone()[0]
+#         descrip_query_execute = core_db.connect().execute(DESCRIP_QUERY)
+#         descrip_q = descrip_query_execute.fetchall()
+        
+#         # create a df from the tuples that are returned as output to the gene query
+#         # add attrib_type_id column
+#         gene_id_df = pd.DataFrame(gene_id_rows, columns=['stable_id', 'gene_id'])
+#         gene_id_df['attrib_type_id'] = attrib_type_id
+    
+#         # merge .tsv df and df created from querying db on the stable_id column, drop rows with 'none available' description
+#         merged_df = pd.merge(gene_id_df, df[df['description'] != 'none available'], on='stable_id').drop('stable_id', axis=1).rename(columns={'description': 'value'}) 
+
+#         # convert df to dictionary to make easier to insert into db?
+#         data = merged_df.to_dict(orient='records')
+#         print(len(data))
+
+#         # database insertion
+#         core_db_w = Core(STAGING_HOST, database, writable=True)
+#         attrib_table = 'gene_attrib'
+#         # execute query to delete existing descriptions from the gene_attrib table before inserting the new descriptions
+#         # delete_query_execution = core_db_w.connect().execute(DELETE_QUERY)
+
+#         # Create the table object using the MyTableName class
+#         # metadata object is a container for the db schema info
+#         # gene_attrib_table object represents the gene_attrib db table 
+#         # The autoload_with parameter specifies that the table structure will be loaded from the database using the connection defined by core_db_w.engine.
+#         metadata = sa.MetaData()
+#         gene_attrib_table = sa.Table(attrib_table, metadata, autoload_with=core_db_w.engine)
+
+#         # create a session, an intermediate object for interacting with the database
+#         Session = sessionmaker(bind=core_db_w.engine)
+#         session = Session()
+
+#         for item in data:
+#         # insert method used to insert data into the table row by row from the list of dictionaries
+#             ins = gene_attrib_table.insert().values(gene_id=item['gene_id'], attrib_type_id=item['attrib_type_id'], value=item['value'])
+#             session.execute(ins)
+
+#         session.commit()
+#         session.close()
